d4c2a5ff5e6dc2c2cb5bd8e13b68785c7d4fe399
ozymandias <github@jochenduckeck.de>
2018-06-20 06:00:59 +0200

    various fixes

---
+++ admin/insert.sql
@@ -0,0 +1,5 @@
+
+
+
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Bundesbank-Bunker', '/english/de/misc/Bundesbank.html', 'Bundesbank', 'Europe', 'de', 'Germany', 'subterranea', 'Eifel');
