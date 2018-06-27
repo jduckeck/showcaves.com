5be14e1bf35428d93215e1fbac0493adfd5d0575
ozymandias <github@jochenduckeck.de>
2018-06-27 22:45:57 +0200

    be completed

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,3 +3,6 @@
 
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Bundesbank-Bunker', '/english/de/misc/Bundesbank.html', 'Bundesbank', 'Europe', 'de', 'Germany', 'subterranea', 'Eifel');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Délirium Café', '/english/be/subterranea/Delirium.html', 'Delirium', 'Europe', 'be', 'Belgium', 'subterranea', 'Brussels');
+
