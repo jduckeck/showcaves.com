cec1a40b4f377bfb40a47a3909875f07b14d1949
ozymandias <github@jochenduckeck.de>
2018-07-04 08:56:21 +0200

    various fixes

---
+++ english/explain/Index/classes/Statistics.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: ozymandias
+ * Date: 6/21/18
+ * Time: 10:23 PM
+ */
+
+namespace classes;
+
+
+class Statistics
+{
+
+    /**
+     * Statistics constructor.
+     */
+    public function __construct()
+    {
+    }
+}
