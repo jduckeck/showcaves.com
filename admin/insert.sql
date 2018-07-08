4ca45bd10d5a52f3ff37c3e59230c4f3b595df5e
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2018-07-08 18:31:10 +0200

    misc

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,10 +5,12 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Bundesbank-Bunker', '/english/de/misc/Bundesbank.html', 'Bundesbank', 'Europe', 'de', 'Germany', 'subterranea', 'Eifel');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Délirium Café', '/english/be/subterranea/Delirium.html', 'Delirium', 'Europe', 'be', 'Belgium', 'subterranea', 'Brussels');
-        INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-        VALUES('yes','Command Bunker Kemmel', '/english/be/subterranea/Kemmel.html', 'Kemmel', 'Europe', 'be', 'Belgium', 'subterranea', 'WestVlaanderen');
-        INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-        VALUES('yes','Nemo 33', '/english/be/subterranea/Nemo33.html', 'Nemo33', 'Europe', 'be', 'Belgium', 'subterranea', 'Brussels');
-        INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-        VALUES('yes','Coudenberg Palace', '/english/be/subterranea/Coudenberg.html', 'Coudenberg', 'Europe', 'be', 'Belgium', 'subterranea', 'Brussels');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Command Bunker Kemmel', '/english/be/subterranea/Kemmel.html', 'Kemmel', 'Europe', 'be', 'Belgium', 'subterranea', 'WestVlaanderen');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Nemo 33', '/english/be/subterranea/Nemo33.html', 'Nemo33', 'Europe', 'be', 'Belgium', 'subterranea', 'Brussels');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Coudenberg Palace', '/english/be/subterranea/Coudenberg.html', 'Coudenberg', 'Europe', 'be', 'Belgium', 'subterranea', 'Brussels');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Besucherstollen Morgenstern', '/english/de/mines/Morgenstern.html', 'Morgenstern', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge');
 
