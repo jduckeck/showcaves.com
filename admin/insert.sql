9fd2dda6d356e3a573d63858b689c63aac4d1b55
ozymandias <github@jochenduckeck.de>
2018-07-10 22:03:01 +0200

    some fixes

--- admin/insert.sql
+++ admin/insert.sql
@@ -13,4 +13,16 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Coudenberg Palace', '/english/be/subterranea/Coudenberg.html', 'Coudenberg', 'Europe', 'be', 'Belgium', 'subterranea', 'Brussels');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Besucherstollen Morgenstern', '/english/de/mines/Morgenstern.html', 'Morgenstern', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Hitonhauta', '/english/fi/gorges/Hitto.html', 'Hitto', 'Europe', 'fi', 'Finland', 'gorges', 'CentralFinland');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Valkulammi', '/english/fi/caves/Valkulammi.html', 'Valkulammi', 'Europe', 'fi', 'Finland', 'caves', 'CentralFinland');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Pirunkirkko', '/english/fi/caves/Pirunkirkko.html', 'Pirunkirkko', 'Europe', 'fi', 'Finland', 'caves', 'NorthKarelia');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Högberget Cave', '/english/fi/gorges/Hogberget.html', 'Hogberget', 'Europe', 'fi', 'Finland', 'caves', 'SouthernFinland');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Askola Potholes', '/english/fi/gorges/Askola.html', 'Askola', 'Europe', 'fi', 'Finland', 'gorges', 'Uusimaa');
 
+        INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+        VALUES('yes','Pihlajamäki Potholes', '/english/fi/gorges/Pihlajamaki.html', 'Pihlajamaki', 'Europe', 'fi', 'Finland', 'gorges', 'SouthernFinland');
