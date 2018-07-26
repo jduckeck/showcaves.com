92316150977fd6af3745ee49673fae77342b2d98
ozymandias <github@jochenduckeck.de>
2018-07-26 18:57:25 +0200

    add lightbox

--- js/global.js
+++ js/global.js
@@ -7,15 +7,4 @@ $(document).ready(function() {
 
 function init() {
 
-// <!--http://labs.juliendecaudin.com/barousel/#ba_download-->
-// <!--https://opensource.org/licenses/mit-license.php-->
-    $('#barousel_thslide').barousel({
-        navWrapper: '#thslide_barousel_nav .thslide_list',
-        manualCarousel: 1,
-        navType: 3
-    });
-
-    $('#thslide_barousel_nav').thslide({
-        itemOffset: 93
-    });
 }
