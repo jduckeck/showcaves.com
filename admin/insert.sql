65380e8b7feb572bd4af8886552a283ec2634e4d
ozymandias <github@jochenduckeck.de>
2018-08-15 01:11:17 +0200

    gb remove streetmap location

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Brouwerij De Kazematten', '/english/be/subterranea/KazemattenBrewery.html', 'KazemattenBrewery', 'Europe', 'be', 'Belgium', 'subterranea', 'WestVlaanderen');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Brasserie Kazematten', '/english/be/subterranea/KasemattenBrasserie.html', 'KasemattenBrasserie', 'Europe', 'be', 'Belgium', 'subterranea', 'WestVlaanderen');
