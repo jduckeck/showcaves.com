41184760b5874f86f4d9343f29f8a8c427e44fd0
ozymandias <github@jochenduckeck.de>
2018-08-15 18:05:10 +0200

    all countries checked

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Brouwerij De Kazematten', '/english/be/subterranea/KazemattenBrewery.html', 'KazemattenBrewery', 'Europe', 'be', 'Belgium', 'subterranea', 'WestVlaanderen');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Brasserie Kazematten', '/english/be/subterranea/KasemattenBrasserie.html', 'KasemattenBrasserie', 'Europe', 'be', 'Belgium', 'subterranea', 'WestVlaanderen');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Museo ng Libingan sa Ilalim ng Lupa', '/english/phhref="../subterranea/Lupa.html', 'Lupa', 'Asia', 'ph', 'Philippines', 'subterranea', 'Calabarzon');
