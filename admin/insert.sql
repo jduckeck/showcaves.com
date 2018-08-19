fcf5aa9ed2399ffb66fe3dab15e2d21fdf2b0c45
ozymandias <github@jochenduckeck.de>
2018-08-20 01:58:25 +0200

    Belgium

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes',"Grotte d'Eprave", '/english/be/caves/Eprave.html', 'Eprave', 'Europe', 'be', 'Belgium', 'caves', 'Namur');
