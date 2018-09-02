6f782252441de8bb7d1c6708dcf651f38db2f6f4
ozymandias <github@jochenduckeck.de>
2018-09-02 07:56:00 +0200

    fixes

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes',"Grotte d'Eprave", '/english/be/caves/Eprave.html', 'Eprave', 'Europe', 'be', 'Belgium', 'caves', 'Namur');
+VALUES('yes','Grotte d\'Eprave', '/english/be/caves/Eprave.html', 'Eprave', 'Europe', 'be', 'Belgium', 'caves', 'Namur');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Amos Rex', '/english/fi/subterranea/AmosRex.html', 'AmosRex', 'Europe', 'fi', 'Finland', 'subterranea', 'SouthernFinland');
