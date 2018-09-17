7dc9cb7049ce1a37f229e5f1afc600d1fc4e1724
ozymandias <github@jochenduckeck.de>
2018-09-17 22:26:34 +0200

    db update

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,13 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Grotte d\'Eprave', '/english/be/caves/Eprave.html', 'Eprave', 'Europe', 'be', 'Belgium', 'caves', 'Namur');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Amos Rex', '/english/fi/subterranea/AmosRex.html', 'AmosRex', 'Europe', 'fi', 'Finland', 'subterranea', 'SouthernFinland');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Grotte di Val de’ Varri', '/english/it/showcaves/Varri.html', 'Varri', 'Europe', 'it', 'Italy', 'showcaves', 'Lazio');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Grotte Saint-Remacle', '/english/be/subterranea/SaintRemacle.html', 'SaintRemacle', 'Europe', 'be', 'Belgium', 'subterranea', 'Luxembourg');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Montagu Guano Cave', '/english/za/showcaves/Montagu.html', 'Montagu', 'Africa', 'za', 'South Africa', 'showcaves', '$Region$');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
+VALUES('yes','Vatnshellir', '/english/other/showcaves/Vatns.html', 'Vatns', 'Europe', 'is', 'Iceland', 'showcaves', 'is', 'other');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Vashistha Gufa', '/english/in/subterranea/Vashistha.html', 'Vashistha', 'Asia', 'in', 'India', 'subterranea', 'Uttarakhand');
