47d9f1c523dd5d77d32ad75db3dd8a3cc9449216
ozymandias <github@jochenduckeck.de>
2019-04-13 23:01:34 +0200

    Egloffsteiner Felsenkeller

---
+++ admin/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Egloffsteiner Felsenkeller', '/english/de/subterranea/Egloffstein.html', 'Egloffstein', 'Europe', 'de', 'Germany', 'subterranea', 'FraenkischeAlb');
+DELETE FROM sights WHERE sortby='Felsenkell';
