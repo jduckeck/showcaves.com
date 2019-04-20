1cb17400364c452bc99dd49220b767843402764f
ozymandias <github@jochenduckeck.de>
2019-04-20 19:04:30 +0200

    Einödhöhle

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,3 +1,5 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Egloffsteiner Felsenkeller', '/english/de/subterranea/Egloffstein.html', 'Egloffstein', 'Europe', 'de', 'Germany', 'subterranea', 'FraenkischeAlb');
 DELETE FROM sights WHERE sortby='Felsenkell';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Einödhöhle', '/english/at/caves/Einoed.html', 'Einoed', 'Europe', 'at', 'Austria', 'caves', 'Nieder');
