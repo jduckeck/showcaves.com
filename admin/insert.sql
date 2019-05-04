1851ad5bf9a91fc927375faa23dd2a2d4428f872
ozymandias <github@jochenduckeck.de>
2019-05-04 13:43:42 +0200

    Flume Gorge

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Flume Gorge', '/english/usa/gorges/Flume.html', 'Flume', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'nh');
