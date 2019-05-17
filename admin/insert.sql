9c66fd9e219f0983f27313f3a9ac8b5f3efcb2e6
ozymandias <github@jochenduckeck.de>
2019-05-17 17:40:04 +0200

    Ra Paulette

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Flume Gorge', '/english/usa/gorges/Flume.html', 'Flume', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'nh');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ra Paulette's Caves', '/english/usa/subterranea/RaPaulette.html', 'RaPaulette', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'nm');
