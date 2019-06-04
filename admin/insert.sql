d25959c595f10592b27e886c75615d46dc6aa5f4
ozymandias <github@jochenduckeck.de>
2019-06-04 18:54:48 +0200

    David Hasselhoff Museum

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Flume Gorge', '/english/usa/gorges/Flume.html', 'Flume', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'nh');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Ra Paulette's Caves', '/english/usa/subterranea/RaPaulette.html', 'RaPaulette', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'nm');
+VALUES('yes','Ra Paulette\'s Caves', '/english/usa/subterranea/RaPaulette.html', 'RaPaulette', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'nm');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','David Hasselhoff Museum', '/english/de/subterranea/DavidHasselhoff.html', 'DavidHasselhoff', 'Europe', 'de', 'Germany', 'subterranea', 'Berlin');
