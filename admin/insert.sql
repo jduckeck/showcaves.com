77c53bf64284d92018631fd5dcbf6cec65c689ac
ozymandias <github@jochenduckeck.de>
2019-06-28 18:59:26 +0200

    Klamm

---
+++ admin/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Eisenbreche Klamm', '/english/de/gorges/Eisenbreche.html', 'Eisenbreche', 'Europe', 'de', 'Germany', 'gorges', 'Alpen');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Höllentalklamm', '/english/de/gorges/Hoellental.html', 'Hoellental', 'Europe', 'de', 'Germany', 'gorges', 'Alpen');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Dr.-Vogelgesang-Klamm', '/english/at/gorges/Vogelgesang.html', 'Vogelgesang', 'Europe', 'at', 'Austria', 'gorges', 'Ober');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Blejski vintgar', '/english/si/gorges/Vintgar.html', 'Vintgar', 'Europe', 'si', 'Slowenia', 'gorges', 'Alpine');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Bava Pyara Caves', '/english/in/subterranea/BavaPyara.html', 'BavaPyara', 'Asia', 'in', 'India', 'subterranea', 'Gujarat');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Uperkot Caves', '/english/in/subterranea/Uperkot.html', 'Uperkot', 'Asia', 'in', 'India', 'subterranea', 'Gujarat');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Khapra Kodiya Caves', '/english/in/subterranea/KhapraKodiya.html', 'KhapraKodiya', 'Asia', 'in', 'India', 'subterranea', 'Gujarat');
