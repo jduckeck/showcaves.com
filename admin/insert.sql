e4cfa10774193173d294322de76df62442037243
ozymandias <github@jochenduckeck.de>
2019-07-19 19:37:46 +0200

    Easter Island

--- admin/insert.sql
+++ admin/insert.sql
@@ -20,3 +20,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Poço Encantado', '/english/br/springs/Encantado.html', 'Encantado', 'SouthAmerica', 'br', 'Brazil', 'springs', 'Bahia');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Poço Azul', '/english/br/springs/Azul.html', 'Azul', 'SouthAmerica', 'br', 'Brazil', 'springs', 'Bahia');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ana Kai Tangata', '/english/cl/caves/KaiTangata.html', 'KaiTangata', 'SouthAmerica', 'cl', 'Chile', 'caves', 'Easter');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ana Te Pahu', '/english/cl/caves/TePahu.html', 'TePahu', 'SouthAmerica', 'cl', 'Chile', 'caves', 'Easter');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ana Te Pora', '/english/cl/caves/TePora.html', 'TePora', 'SouthAmerica', 'cl', 'Chile', 'caves', 'Easter');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ana Kakenga', '/english/cl/caves/Kakenga.html', 'Kakenga', 'SouthAmerica', 'cl', 'Chile', 'caves', 'Easter');
