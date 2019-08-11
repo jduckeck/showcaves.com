26d845e977dfe6d021f42c33b60b23eeebf19a2b
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2019-08-11 21:46:20 +0200

    Seven Sleepers

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Gottesackerplateau', '/english/de/karst/Gottesackerplateau.html', 'Gottesackerplateau', 'Europe', 'de', 'Germany', 'karst', 'Alpen');
