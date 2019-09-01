4922c2db6208227b3514cd910ffc131d1f8407f4
ozymandias <github@jochenduckeck.de>
2019-09-01 12:33:06 +0200

    Jamaica

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES ('yes', 'Gottesackerplateau', '/english/de/karst/Gottesackerplateau.html', 'Gottesackerplateau', 'Europe', 'de', 'Germany', 'karst', 'Alpen');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
+VALUES('yes','Blue Lagoon', '/english/car/springs/BlueLagoon.html', 'BlueLagoon', 'Caribbean Islands', 'jm', 'Jamaica', 'springs', 'jm', 'car');
