3b6fb2ea37b382bd50713ee7f51fbfd7de151a6f
ozymandias <github@jochenduckeck.de>
2019-09-02 23:51:36 +0200

    Bua Tong th

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Gottesackerplateau', '/english/de/karst/Gottesackerplateau.html', 'Gottesackerplateau', 'Europe', 'de', 'Germany', 'karst', 'Alpen');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
 VALUES('yes','Blue Lagoon', '/english/car/springs/BlueLagoon.html', 'BlueLagoon', 'Caribbean Islands', 'jm', 'Jamaica', 'springs', 'jm', 'car');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Bua Tong', '/english/th/karst/BuaTong.html', 'BuaTong', 'Asia', 'th', 'Thailand', 'karst', 'ChiangMai');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Sa Morakot', '/english/th/springs/SaMorakot.html', 'SaMorakot', 'Asia', 'th', 'Thailand', 'springs', 'Krabi');
