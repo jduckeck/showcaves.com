af0269cf2f379eb1d99c150744eede3e2f9b32e5
ozymandias <github@jochenduckeck.de>
2019-10-28 19:25:54 +0100

    Peștera Duruitoarea

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
+VALUES('yes','Peștera Duruitoarea', '/english/other/caves/Duruitoarea.html', 'Duruitoarea', '$continent$', 'md', 'Republic of Moldova', 'caves', 'md', 'other');
