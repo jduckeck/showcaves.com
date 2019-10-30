bc776f0ece3b0d0b43ec65b2cc52a1e64c1a8c31
ozymandias <github@jochenduckeck.de>
2019-10-30 09:05:00 +0100

    Uracher Tropfsteinhöhle

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
 VALUES('yes','Peștera Duruitoarea', '/english/other/caves/Duruitoarea.html', 'Duruitoarea', '$continent$', 'md', 'Republic of Moldova', 'caves', 'md', 'other');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Uracher Tropfsteinhöhle', '/english/de/showcaves/Uracher.html', 'Uracher', 'Europe', 'de', 'Germany', 'showcaves', 'SchwaebischeAlb');
