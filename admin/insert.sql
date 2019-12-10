061ee7677d8300a5c0afc783670d7f8c5a67f39e
ozymandias <github@jochenduckeck.de>
2019-12-10 19:40:09 +0100

    Peshna Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,2 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
-VALUES('yes','Peștera Duruitoarea', '/english/other/caves/Duruitoarea.html', 'Duruitoarea', '$continent$', 'md', 'Republic of Moldova', 'caves', 'md', 'other');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Uracher Tropfsteinhöhle', '/english/de/showcaves/Uracher.html', 'Uracher', 'Europe', 'de', 'Germany', 'showcaves', 'SchwaebischeAlb');
+VALUES('yes','Peštera Pešna', '/english/other/caves/Peshna.html', 'Peshna', 'Europe', 'mk', 'Makedonija', 'caves', 'mk', 'other');
