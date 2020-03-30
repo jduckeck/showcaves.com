a15ab267d665a7f32b015c0003221a181f04c61d
ozymandias <github@jochenduckeck.de>
2020-03-30 23:40:54 +0200

    Schweiz

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
 VALUES('yes','Peštera Pešna', '/english/other/caves/Peshna.html', 'Peshna', 'Europe', 'mk', 'Makedonija', 'caves', 'mk', 'other');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Domaines Rouvinez', '/english/ch/subterranea/Rouvinez.html', 'Rouvinez', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Wallis');
+DELETE FROM sights WHERE sortby='Geronde';
