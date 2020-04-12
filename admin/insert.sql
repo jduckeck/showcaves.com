7ef178f171520b90ea8c8c061b948f3df30f48b5
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2020-04-12 22:36:48 +0200

    Italy Mines

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Miniera d\'Oro della Guia', '/english/it/mines/Guia.html', 'Guia', 'Europe', 'it', 'Italy', 'mines', 'Piemonte');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Miniera d\'Oro di Sessa', '/english/ch/mines/Sessa.html', 'Sessa', 'Europe', 'ch', 'Switzerland', 'mines', 'Ticino');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Grotta dei Cristalli', '/english/ch/subterranea/Cristalli.html', 'Cristalli', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Ticino');
