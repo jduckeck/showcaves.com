835aa982be3aeb916500475b775c1f7615693b09
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2020-04-22 00:22:43 +0200

    Austria

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Schaubergwerk Sunnpau', '/english/at/mines/Sunnpau.html', 'Sunnpau', 'Europe', 'at', 'Austria', 'mines', 'Salzburg');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Wolfsklamm', '/english/at/gorges/Wolfs.html', 'Wolfs', 'Europe', 'at', 'Austria', 'gorges', 'Tirol');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Notranjski muzej Postojna', '/english/si/subterranea/NotranjskiMuzejPostojna.html', 'NotranjskiMuzejPostojna', 'Europe', 'si', 'Slovenia', 'subterranea', 'Postojna');
