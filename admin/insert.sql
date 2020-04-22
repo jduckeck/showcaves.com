284bc2300ebaae2194bbb86fd650d70999bf0b7e
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2020-04-22 21:39:04 +0200

    Slovenia

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Wolfsklamm', '/english/at/gorges/Wolfs.html', 'Wolfs', 'Europe', 'at', 'Austria', 'gorges', 'Tirol');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES ('yes', 'Notranjski muzej Postojna', '/english/si/subterranea/NotranjskiMuzejPostojna.html', 'NotranjskiMuzejPostojna', 'Europe', 'si', 'Slovenia', 'subterranea', 'Postojna');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Izviri v Retovje', '/english/si/springs/Retovje.html', 'Retovje', 'Europe', 'si', 'Slovenia', 'springs', 'Notranjska');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Izviri v Močilnik', '/english/si/springs/Mocilnik.html', 'Mocilnik', 'Europe', 'si', 'Slovenia', 'springs', 'Notranjska');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Izviri v Bistri', '/english/si/springs/Bistra.html', 'Bistra', 'Europe', 'si', 'Slovenia', 'springs', 'Notranjska');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Lintvern', '/english/si/springs/Lintvern.html', 'Lintvern', 'Europe', 'si', 'Slovenia', 'springs', 'Notranjska');
