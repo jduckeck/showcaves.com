0ed30e8f767a155c9298898c5cd67714dc1badb9
jd <jochen.duckeck@diginuevo.com>
2020-05-10 23:04:04 +0200

    deploy

--- admin/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES ('yes', 'Cueva de Ardales', '/english/es/showcaves/Ardales.html', 'Ardales', 'Europe', 'es', 'Spain', 'showcaves', 'Andalucía');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES ('yes', 'Watkins Glen State Park', '/english/usa/gorges/Watkins.html', 'Watkins', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'ny');
