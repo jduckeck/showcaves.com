1447e580d3d55e380eb31d3b6c90f21ead8e6787
jd <jochen.duckeck@diginuevo.com>
2020-05-18 18:14:54 +0200

    explain

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Xiaozhai Tiankeng', '/english/cn/karst/Xiaozhai.html', 'Xiaozhai', 'Asia', 'cn', 'China', 'karst', 'Chongqing');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Dragon Hole', '/english/cn/karst/DragonHole.html', 'DragonHole', 'Asia', 'cn', 'China', 'karst', 'Hainan');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Dadong', '/english/cn/caves/Dadong.html', 'Dadong', 'Asia', 'cn', 'China', 'caves', 'Guizhou');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Luobi Cave', '/english/cn/caves/Luobi.html', 'Luobi', 'Asia', 'cn', 'China', 'caves', 'Hainan');
