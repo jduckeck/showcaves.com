67ba9550584431db8d2d25f510cafd2d6091cc51
jd <jochen.duckeck@diginuevo.com>
2020-06-15 17:55:55 +0200

    Fiji

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,7 +2,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Padamyar Cave', '/english/mm/showcaves/Padamyar.html', 'Padamyar', 'Asia', 'mm', 'Myanmar', 'showcaves', 'Shan');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES ('yes', 'Akhshtyrskaya Cave', '/english/ru/showcaves/Akhshtyrskaya.html', 'Akhshtyrskaya', 'Asia', 'ru', 'Russia', 'showcaves', 'Sochi');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
-VALUES ('yes', 'Volivoli Cave', '/english/other/caves/Volivoli.html', 'Volivoli', '$continent$', 'fj', 'Fiji', 'caves', 'fj', 'other');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Nothklamm', '/english/at/gorges/Noth.html', 'Noth', 'Europe', 'at', 'Austria', 'gorges', 'Steiermark');
+VALUES ('yes', 'Nothklamm', '/english/at/gorges/Noth.html', 'Noth', 'Europe', 'at', 'Austria', 'gorges', 'Steiermark');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
+VALUES ('yes', 'Volivoli Cave', '/english/other/caves/Volivoli.html', 'Volivoli', 'Oceania', 'fj', 'Fiji', 'caves', 'fj', 'other');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
+VALUES ('yes', 'Nakoroloaloa Cave', '/english/other/showcaves/Nakoroloaloa.html', 'Nakoroloaloa', 'Oceania', 'fj', 'Fidji', 'showcaves', 'fj', 'other');
