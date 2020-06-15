f9b799a0489bf21e620b6d2cf0ccf42cce0c56be
ozymandias <github@jochenduckeck.de>
2020-06-16 01:52:54 +0200

    Gorge

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,10 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Palfauer Wasserloch', '/english/at/springs/PalfauerWasserloch.html', 'PalfauerWasserloch', 'Europe', 'at', 'Austria', 'springs', 'Steiermark');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Kläfferquelle', '/english/at/aprings/Klaeffer.html', 'Klaeffer', 'Europe', 'at', 'Austria', 'aprings', 'Steiermark');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Wassermannsloch', '/english/at/aprings/Wassermannsloch.html', 'Wassermannsloch', 'Europe', 'at', 'Austria', 'aprings', 'Steiermark');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Spitzenbachklamm', '/english/at/gorges/Spitzenbach.html', 'Spitzenbach', 'Europe', 'at', 'Austria', 'gorges', 'Steiermark');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Nothklamm', '/english/at/gorges/Noth.html', 'Noth', 'Europe', 'at', 'Austria', 'gorges', 'Steiermark');
