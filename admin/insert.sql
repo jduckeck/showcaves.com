f861a227b8618049812958a0cccc4ab5c4716af2
jd <jochen.duckeck@diginuevo.com>
2020-06-15 03:58:02 +0200

    prepare prod

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Padamyar Cave', '/english/mm/showcaves/Padamyar.html', 'Padamyar', 'Asia', 'mm', 'Myanmar', 'showcaves', 'Shan');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES ('yes', 'Akhshtyrskaya Cave', '/english/ru/showcaves/Akhshtyrskaya.html', 'Akhshtyrskaya', 'Asia', 'ru', 'Russia', 'showcaves', 'Sochi');
