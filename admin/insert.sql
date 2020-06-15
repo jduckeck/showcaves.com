2b78fa378fe71ad941f133f78833495259dc6da7
ozymandias <github@jochenduckeck.de>
2020-06-15 16:08:14 +0200

    Gorge

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Nothklamm', '/english/at/gorges/Noth.html', 'Noth', 'Europe', 'at', 'Austria', 'gorges', 'Steiermark');
