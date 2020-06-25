fb41ab1292b7e0aee65427e92aa5c91925f293c2
jd <jochen.duckeck@diginuevo.com>
2020-06-26 01:40:37 +0200

    fußzeile

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Oneota Gorge', '/english/usa/gorges/Oneota.html', 'Oneota', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'or');
