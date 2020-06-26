9782ffa762adea4104e576e183d900bae98c38f6
ozymandias <github@jochenduckeck.de>
2020-06-26 23:08:11 +0200

    Witches Gulch

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Oneota Gorge', '/english/usa/gorges/Oneota.html', 'Oneota', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'or');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Witches Gulch', '/english/usa/gorges/Witches.html', 'Witches', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'wi');
