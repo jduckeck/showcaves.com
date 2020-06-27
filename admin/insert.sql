b823b0662137fdebcb0715f15800d6c19b370ca5
ozymandias <github@jochenduckeck.de>
2020-06-27 20:10:03 +0200

    Turkey

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Kaunos Kral Mezarları', '/english/tr/subterranea/Kaunos.html', 'Kaunos', 'Europe', 'tr', 'Turkey', 'subterranea', 'Lycia');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Rock-cut Tombs in Myra', '/english/tr/subterranea/Myra.html', 'Myra', 'Europe', 'tr', 'Turkey', 'subterranea', 'Lycia');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Oneota Gorge', '/english/usa/gorges/Oneonta.html', 'Oneota', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'or');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Madonna della Corona', '/english/it/subterranea/MadonnaDellaCorona.html', 'MadonnaDellaCorona', 'Europe', 'it', 'Italy', 'subterranea', 'Veneto');
