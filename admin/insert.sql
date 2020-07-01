8228c4de42dbb5073b046ba3d65ef6be03112bc1
ozymandias <github@jochenduckeck.de>
2020-07-02 00:17:59 +0200

    public domain

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ayios Sozomenos Cave Church', '/english/cy/subterranea/AyiosSozomenos.html', 'AyiosSozomenos', 'Europe', 'cy', 'Cyprus', 'subterranea', 'Nicosia');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ayia Thekla Cave Church', '/english/cy/subterranea/AyiaThekla.html', 'AyiaThekla', 'Europe', 'cy', 'Cyprus', 'subterranea', 'Famagusta');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ayias Solomonis Cave Church and Catacombs', '/english/cy/subterranea/AyiasSolomonis.html', 'AyiasSolomonis', 'Europe', 'cy', 'Cyprus', 'subterranea', 'Paphos');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Farkaskői Alkotótelep', '/english/hu/subterranea/Noszvaj.html', 'Noszvaj', 'Europe', 'hu', 'Hungary', 'subterranea', 'Lillafured');
