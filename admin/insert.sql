77a9fbfe8069c8917bfdf7aeee57620248e0ea2c
jd <jochen.duckeck@diginuevo.com>
2020-07-08 10:51:35 +0200

    languages

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Panagria Hrysospiliotissa', '/english/gr/subterranea/PanagriaHrysospiliotissa.html', 'PanagriaHrysospiliotissa', 'Europe', 'gr', 'Greece', 'subterranea', 'StereaHellas');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Kipina Monastery', '/english/gr/subterranea/Kipina.html', 'Kipina', 'Europe', 'gr', 'Greece', 'subterranea', 'Epiros');
