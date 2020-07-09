3e0d4fdff250ebcac5c4176c4dcf5358108f3cbe
ozymandias <github@jochenduckeck.de>
2020-07-09 13:50:42 +0200

    poland Kamieńczyka

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,6 +3,8 @@ SET filename = REPLACE(filename, '/showcaves/', '/gorges/'),
     category = 'gorges'
 WHERE sortby = 'BledneSkal';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Kamieńczyk Gorge', '/english/pl/gorges/Kamienczyk.html', 'Kamienczyk', 'Europe', 'pl', 'Poland', 'gorges', 'Dolnoslaskie');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Panagria Hrysospiliotissa', '/english/gr/subterranea/PanagriaHrysospiliotissa.html', 'PanagriaHrysospiliotissa', 'Europe', 'gr', 'Greece', 'subterranea', 'StereaHellas');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Kipina Monastery', '/english/gr/subterranea/Kipina.html', 'Kipina', 'Europe', 'gr', 'Greece', 'subterranea', 'Epiros');
