1cb6b6c79ebda4cf65d09548592502f3d9cedf45
jd <jochen.duckeck@diginuevo.com>
2020-07-17 13:15:23 +0200

    Japan

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Panagria Hrysospiliotissa', '/english/gr/subterranea/PanagriaHrysospiliotissa.html', 'PanagriaHrysospiliotissa', 'Europe', 'gr', 'Greece', 'subterranea', 'StereaHellas');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
 VALUES('yes','Kipina Monastery', '/english/gr/subterranea/Kipina.html', 'Kipina', 'Europe', 'gr', 'Greece', 'subterranea', 'Epiros');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Ashio Copper Mine', '/english/jp/mines/Ashio.html', 'Ashio', 'Asia', 'jp', 'Japan', 'mines', 'Tochigi');
