e9ef1513416cb56a5a2e4deef37d752e6c8bdc15
ozymandias <github@jochenduckeck.de>
2020-08-06 20:55:04 +0200

    Knaben

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
+VALUES('yes','Knaben Gruvemuseum', '/english/no/mines/Knaben.html', 'Knaben', 'Europe', 'no', 'Norway', 'mines', 'VestAgder');
