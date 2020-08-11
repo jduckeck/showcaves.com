98c97c0ba4b5964ecb4d6ebf9a0be254b7feba1f
ozymandias <github@jochenduckeck.de>
2020-08-11 20:59:18 +0200

    location

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,24 +1,26 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Knaben Gruvemuseum', '/english/no/mines/Knaben.html', 'Knaben', 'Europe', 'no', 'Norway', 'mines', 'VestAgder');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Ashab-ı Kehf Mağarası', '/english/tr/caves/AshabIKehf.html', 'AshabIKehf', 'Europe', 'tr', 'Turkey', 'caves', 'SoutheastAnatolia');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Tuyuq Khojam Mazar', '/english/cn/subterranea/TuyuqKhojamMazar.html', 'TuyuqKhojamMazar', 'Asia', 'cn', 'China', 'subterranea', 'Xinjiang');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
-VALUES('yes','Masjid al-Ruqood al-Sebaa', '/english/other/subterranea/Sebaa.html', 'Sebaa', 'Africa', 'tn', 'Tunisia', 'subterranea', 'tn', 'other');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
-VALUES('yes','Ashabi Kahf', '/english/other/caves/AshabiKahf.html', 'AshabiKahf', 'Asia', 'az', 'Azerbaijan', 'caves', 'az', 'other');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Knaben Gruvemuseum', '/english/no/mines/Knaben.html', 'Knaben', 'Europe', 'no', 'Norway', 'mines', 'VestAgder', 58.662677, 7.074775);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ashab-ı Kehf Mağarası', '/english/tr/caves/AshabIKehf.html', 'AshabIKehf', 'Europe', 'tr', 'Turkey', 'caves', 'SoutheastAnatolia', 36.957222, 34.796667);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tuyuq Khojam Mazar', '/english/cn/subterranea/TuyuqKhojamMazar.html', 'TuyuqKhojamMazar', 'Asia', 'cn', 'China', 'subterranea', 'Xinjiang', 42.859379, 89.690795);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Masjid al-Ruqood al-Sebaa', '/english/other/subterranea/Sebaa.html', 'Sebaa', 'Africa', 'tn', 'Tunisia', 'subterranea', 'tn', 'other', 32.911823, 10.263789);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Ashabi Kahf', '/english/other/caves/AshabiKahf.html', 'AshabiKahf', 'Asia', 'az', 'Azerbaijan', 'caves', 'az', 'other', 39.219259, 45.588742);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Eshab-ı Kehf Kulliye', '/english/tr/subterranea/Kulliye.html', 'Kulliye', 'Europe', 'tr', 'Turkey', 'subterranea', 'CentralAnatolia', 38.248544, 36.855168);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Lice Ashâb-ı Kehf Mağarası', '/english/tr/subterranea/Lice.html', 'Lice', 'Europe', 'tr', 'Turkey', 'subterranea', 'SoutheastAnatolia');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region)
-VALUES('yes','Zeche Consolidation', '/english/de/mines/Consol.html', 'Consol', 'Europe', 'de', 'Germany', 'mines', 'Ruhrgebiet');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Lice Ashâb-ı Kehf Mağarası', '/english/tr/subterranea/Lice.html', 'Lice', 'Europe', 'tr', 'Turkey', 'subterranea', 'SoutheastAnatolia', 38.439053, 40.499101);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Zeche Consolidation', '/english/de/mines/Consol.html', 'Consol', 'Europe', 'de', 'Germany', 'mines', 'Ruhrgebiet', 51.528949, 7.109127);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Landgrafenschlucht', '/english/de/gorges/Landgrafenschlucht.html', 'Landgrafenschlucht', 'Europe', 'de', 'Germany', 'gorges', 'ThueringerWald', 50.954139, 10.309430);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Ludwigsklamm', '/english/de/gorges/Ludwigsklamm.html', 'Ludwigsklamm', 'Europe', 'de', 'Germany', 'gorges', 'ThueringerWald', 50.961866, 10.324635);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
-VALUES('yes','\'Ashabi Alkahf', '/english/other/subterranea/AshabeKahf.html', 'AshabeKahf', 'MiddleEast', 'jo', 'Jordan', 'subterranea', 'jo', 'other');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter)
-VALUES('yes','Gruta Colón', '/english/other/showcaves/Colon.html', 'Colon', 'SouthAmerica', 'uy', 'Uruguay', 'showcaves', 'uy', 'other');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','\'Ashabi Alkahf', '/english/other/subterranea/AshabeKahf.html', 'AshabeKahf', 'MiddleEast', 'jo', 'Jordan', 'subterranea', 'jo', 'other', 31.898816, 35.973572);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Gruta Colón', '/english/other/showcaves/Colon.html', 'Colon', 'SouthAmerica', 'uy', 'Uruguay', 'showcaves', 'uy', 'other', -34.285727, -55.269262);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Unterirdisches Zeitz', '/english/de/subterranea/Zeitz.html', 'Zeitz', 'Europe', 'de', 'Germany', 'subterranea', 'Norddeutschland', 51.049676, 12.134723);
