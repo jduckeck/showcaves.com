41f8141531edc3d0b3dbf98b402b9710cae14f3a
ozymandias <github@jochenduckeck.de>
2020-08-22 09:41:03 +0200

    Utah

--- admin/insert.sql
+++ admin/insert.sql
@@ -11,6 +11,8 @@ VALUES('yes','Rokle Malý Kyseľ', '/english/sk/gorges/MalyKysel.html', 'MalyKys
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Rokle Veľký Kyseľ', '/english/sk/gorges/VeľkyKysel.html', 'VeľkyKysel', 'Europe', 'sk', 'Slovakia', 'gorges', 'Kosice', 48.933674,  20.402741);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Golden Cathedral', '/english/usa/caves/Golden.html', 'Golden', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ut', 37.593064, -111.218356);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Rokle Suchá Belá', '/english/sk/gorges/SuchaBela.html', 'SuchaBela', 'Europe', 'sk', 'Slovakia', 'gorges', 'Kosice', 48.962527, 20.384916);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Rokle Piecky', '/english/sk/gorges/Piecky.html', 'Piecky', 'Europe', 'sk', 'Slovakia', 'gorges', 'Kosice', 48.942692, 20.347543);
