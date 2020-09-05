f9aee264adcb892eec9fee478a44cbfedf6ef573
ozymandias <github@jochenduckeck.de>
2020-09-05 12:58:48 +0200

    Slovakia

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Vyvieračka Hlbokô', '/english/sk/springs/Hlboko.html', 'Hlboko', 'Europe', 'sk', 'Slovakia', 'springs', 'Zilina', 48.9977615, 19.675795);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Termálny Prameň Kaďa', '/english/sk/springs/Kada.html', 'Kada', 'Europe', 'sk', 'Slovakia', 'springs', 'Zilina', 49.042985, 19.678079);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Šarkania diera', '/english/sk/caves/SarkaniaDiera.html', 'SarkaniaDiera', 'Europe', 'sk', 'Slovakia', 'caves', 'Zilina', 49.181340, 18.606196);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Jánošíkove diery', '/english/sk/gorges/Janosik.html', 'Janosik', 'Europe', 'sk', 'Slovakia', 'gorges', 'Zilina', 49.256095, 19.067569);
