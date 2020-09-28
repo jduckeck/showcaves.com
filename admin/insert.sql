2c53e9282657b0f32220ca7faab5b9767b005daa
ozymandias <github@jochenduckeck.de>
2020-09-29 01:18:19 +0200

    Czech Republic

--- admin/insert.sql
+++ admin/insert.sql
@@ -9,6 +9,8 @@ VALUES('yes','Kryt 10 - Z', '/english/cz/subterranea/Kryt10-Z.html', 'Kryt10-Z',
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Chrám kamene', '/english/cz/subterranea/ChramKamene.html', 'ChramKamene', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Jihomoravsky', 49.194633, 16.599183);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kryt Denis', '/english/cz/subterranea/KrytDenis.html', 'KrytDenis', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Jihomoravsky', 49.190040, 16.608495);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Důl Hlubina', '/english/cz/mines/Hlubina.html', 'Hlubina', 'Europe', 'cz', 'Czech Republic', 'mines', 'Moravskoslezsky', 49.82130,18.27824);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Bunker B-S 8 Hřbitov', '/english/sk/subterranea/BS8.html', 'BS8', 'Europe', 'sk', 'Slovakia', 'subterranea', 'Bratislava', 48.107950, 17.081344);
