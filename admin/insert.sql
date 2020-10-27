4a5f882b2dab5ad0344d4ce6623812a4cc68028d
Jochen <jochen@showcaves.com>
2020-10-27 19:47:29 +0100

    Durango

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Paseo Túnel Minería Durango', '/english/mx/mines/Durango.html', 'Durango', 'MiddleAmerica', 'mx', 'México', 'mines', 'Durango', 24.023973, -104.670004);
