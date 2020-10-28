66ac0695468272d4b26e6cd952c7c29f381be0a1
Jochen <jochen@showcaves.com>
2020-10-28 09:22:15 +0100

    Nushabad

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Paseo Túnel Minería Durango', '/english/mx/mines/Durango.html', 'Durango', 'MiddleAmerica', 'mx', 'México', 'mines', 'Durango', 24.023973, -104.670004);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Euston Lost Tunnels', '/english/gb/subterranea/Euston.html', 'Euston', 'Europe', 'gb', 'Great Britain', 'subterranea', 'London', 51.525335, -0.135580);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Underground City of Nushabad', '/english/ir/subterranea/Nushabad.html', 'Nushabad', 'MiddleEast', 'ir', 'Iran', 'subterranea', 'Isfahan', 34.080000, 51.437500);
