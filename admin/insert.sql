e79bd957bf4b631ee8a17b44a8a75f48a0e4ff1a
Jochen <jochen@showcaves.com>
2020-10-28 03:00:01 +0100

    Hidden London

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Paseo Túnel Minería Durango', '/english/mx/mines/Durango.html', 'Durango', 'MiddleAmerica', 'mx', 'México', 'mines', 'Durango', 24.023973, -104.670004);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Euston Lost Tunnels', '/english/gb/subterranea/Euston.html', 'Euston', 'Europe', 'gb', 'Great Britain', 'subterranea', 'London', 51.525335, -0.135580);
