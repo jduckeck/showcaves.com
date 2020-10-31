bef79b2615cc835425021f254b5ee7952561d4e6
Jochen <jochen@showcaves.com>
2020-10-31 19:06:20 +0100

    Crimea

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Melek-Chesmensky Kurgan', '/english/ru/subterranea/MelekChesmensky.html', 'MelekChesmensky', 'Asia', 'ru', 'Russia', 'subterranea', 'Crimea');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','El Zanjón de Granados', '/english/ar/subterranea/ElZanjon.html', 'ElZanjon', 'SouthAmerica', 'ar', 'Argentina', 'subterranea', 'BuenosAires', -34.616730, -58.371730);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Gruta de la Virgen de la Medalla Milagrosa', '/english/ar/subterranea/MedallaMilagrosa.html', 'MedallaMilagrosa', 'SouthAmerica', 'ar', 'Argentina', 'subterranea', 'Cordoba', -31.360603, -64.595415);
