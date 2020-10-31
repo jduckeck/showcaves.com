8e50da424d3917844f33b7bf6903bd072aa0d950
Jochen <jochen@showcaves.com>
2020-10-31 08:17:20 +0100

    Argentina

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,12 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','El Zanjón de Granados', '/english/ar/subterranea/ElZanjon.html', 'ElZanjon', 'SouthAmerica', 'ar', 'Argentina', 'subterranea', 'BuenosAires', -34.616730, -58.371730);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta de la Virgen de la Medalla Milagrosa', '/english/ar/subterranea/MedallaMilagrosa.html', 'MedallaMilagrosa', 'SouthAmerica', 'ar', 'Argentina', 'subterranea', 'Cordoba', -31.360603, -64.595415);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Hipasam Iron Mine', '/english/ar/mines/Hipasam.html', 'Hipasam', 'SouthAmerica', 'ar', 'Argentina', 'mines', 'RioNegro', -41.672826, -65.368897);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mina De Oro La Carolina', '/english/ar/mines/LaCarolina.html', 'LaCarolina', 'SouthAmerica', 'ar', 'Argentina', 'mines', 'SanLuis', -32.804967, -66.085812);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Peştera mănăstire de la Sinca Veche', '/english/ro/subterranea/SincaVeche.html', 'SincaVeche', 'Europe', 'ro', 'Romania', 'subterranea', 'TransylvanianPlateau', 45.755281, 25.168071);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Palombaro Lungo', '/english/it/subterranea/PalombaroLungo.html', 'PalombaroLungo', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.666871, 16.606484);
