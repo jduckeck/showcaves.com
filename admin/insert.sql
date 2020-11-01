875b78e96591458c14a4b345823f348c285264a2
Jochen <jochen@showcaves.com>
2020-11-01 09:59:29 +0100

    minor fixes

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Melek-Chesmensky Kurgan', '/english/ru/subterranea/MelekChesmensky.html', 'MelekChesmensky', 'Asia', 'ru', 'Russia', 'subterranea', 'Crimea');
+VALUES('yes','Melek-Chesmensky Kurgan', '/english/ru/subterranea/MelekChesmensky.html', 'MelekChesmensky', 'Asia', 'ru', 'Russia', 'subterranea', 'Crimea', 45.362954, 36.470348);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','El Zanjón de Granados', '/english/ar/subterranea/ElZanjon.html', 'ElZanjon', 'SouthAmerica', 'ar', 'Argentina', 'subterranea', 'BuenosAires', -34.616730, -58.371730);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
@@ -12,3 +12,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Peştera mănăstire de la Sinca Veche', '/english/ro/subterranea/SincaVeche.html', 'SincaVeche', 'Europe', 'ro', 'Romania', 'subterranea', 'TransylvanianPlateau', 45.755281, 25.168071);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Palombaro Lungo', '/english/it/subterranea/PalombaroLungo.html', 'PalombaroLungo', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.666871, 16.606484);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Reservatório da Patriarcal', '/english/pt/subterranea/Patriarcal.html', 'Patriarcal', 'Europe', 'pt', 'Portugal', 'subterranea', 'Lisboa');
