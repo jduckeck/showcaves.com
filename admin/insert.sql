5aa3bcbdc416ea52bad105a07fd7917fbeb76514
Jochen <jochen@showcaves.com>
2020-11-01 19:52:20 +0100

    Eski-Kermen

--- admin/insert.sql
+++ admin/insert.sql
@@ -16,3 +16,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Reservatório da Patriarcal', '/english/pt/subterranea/Patriarcal.html', 'Patriarcal', 'Europe', 'pt', 'Portugal', 'subterranea', 'Lisboa', 38.716173, -9.148729);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Museo Faggiano', '/english/it/subterranea/Faggiano.html', 'Faggiano', 'Europe', 'it', 'Italy', 'subterranea', 'Puglia', 40.350117, 18.172360);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Eski-Kermen', '/english/ru/subterranea/EskiKermen.html', 'EskiKermen', 'Asia', 'ru', 'Russia', 'subterranea', 'Crimea', 44.608668, 33.740215);
