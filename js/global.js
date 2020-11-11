b1a0470342bac4da143933f0080cf368ca189e5b
Jochen <jochen@showcaves.com>
2020-11-11 01:56:06 +0100

    Israel

--- js/global.js
+++ js/global.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
 
 
 function init() {
+
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'')
+                && location.hostname === this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({ scrollTop: target.offset().top -50 }, 500);
+                    return false;
+                }
+            }
+        });
+    });
+
 }
