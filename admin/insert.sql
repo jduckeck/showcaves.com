2e8913652a9977c737c986abdb18ec6cfb9c8b6b
Jochen <jochen@showcaves.com>
2020-11-16 16:54:31 +0100

    Banita

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bănița Gorge', '/english/ro/gorges/Banita.html', 'Banita', 'Europe', 'ro', 'Romania', 'gorges', 'Hunedoara', 45.450172, 23.311174);
