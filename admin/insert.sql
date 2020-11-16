a62d4d2f7b6690719f59d611bb0fc211e08b467f
Jochen <jochen@showcaves.com>
2020-11-16 21:35:02 +0100

    Mesa Arch

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Bănița Gorge', '/english/ro/gorges/Banita.html', 'Banita', 'Europe', 'ro', 'Romania', 'gorges', 'Hunedoara', 45.450172, 23.311174);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mesa Arch', '/english/usa/caves/MesaArch.html', 'MesaArch', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ut', 38.38786, -109.863664);
