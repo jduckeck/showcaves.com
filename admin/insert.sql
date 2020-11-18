1ffcd21113ad4ec9c4925c7d085e4947585ea27a
Jochen <jochen@showcaves.com>
2020-11-18 09:05:16 +0100

    Bruce Peninsula Grotto

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Mesa Arch', '/english/usa/caves/MesaArch.html', 'MesaArch', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ut', 38.38786, -109.863664);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Pestera Valea Cetatii', '/english/ro/showcaves/ValeaCetatii.html', 'ValeaCetatii', 'Europe', 'ro', 'Romania', 'showcaves', 'Brasov', 45.582273, 25.496398);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bruce Peninsula Grotto', '/english/ca/caves/BrucePeninsula.html', 'BrucePeninsula', 'NorthAmerica', 'ca', 'Canada', 'caves', 'Ontario', 45.2452, -81.5243);
