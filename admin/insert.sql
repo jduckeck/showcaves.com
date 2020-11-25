c5532a22e23238f99d69ff56b57a079185454a75
Jochen <jochen@showcaves.com>
2020-11-25 21:50:18 +0100

    Intervales SP

---
+++ admin/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta das Encantadas', '/english/br/caves/Encantadas.html', 'Encantadas', 'SouthAmerica', 'br', 'Brazil', 'caves', 'Parana', -25.574160, -48.311815);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta do Tatu', '/english/br/caves/Tatu.html', 'Tatu', 'SouthAmerica', 'br', 'Brazil', 'caves', 'SaoPaulo', -24.271550, -48.418050);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta Colorida', '/english/br/caves/Colorida.html', 'Colorida', 'SouthAmerica', 'br', 'Brazil', 'caves', 'SaoPaulo', -24.272821911509418, -48.41888192460736);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta do Fogo', '/english/br/caves/Fogo.html', 'Fogo', 'SouthAmerica', 'br', 'Brazil', 'caves', 'SaoPaulo', -24.266833, -48.430667);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta do Fendão', '/english/br/caves/Fendao.html', 'Fendao', 'SouthAmerica', 'br', 'Brazil', 'caves', 'SaoPaulo', -24.2728657, -48.4477544);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta Luminosa', '/english/br/caves/Luminosa.html', 'Luminosa', 'SouthAmerica', 'br', 'Brazil', 'caves', 'SaoPaulo', -24.2877744, -48.4395328);
