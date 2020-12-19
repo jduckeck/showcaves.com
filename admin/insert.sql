0c7afb1b0ad5927b804de2c1d3cd069d6725ddec
Jochen <jochen@showcaves.com>
2020-12-19 13:55:24 +0100

    fixes

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grottes du Pic du Jer', '/english/fr/showcaves/Jer.html', 'Jer', 'Europe', 'fr', 'France', 'showcaves', 'MidiPyrenees', 43.080390, -0.032528);
