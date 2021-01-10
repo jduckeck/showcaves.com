91b0550ce04ea2ebdf03532c7c339effd548bbb0
Jochen <jochen@showcaves.com>
2021-01-10 18:35:22 +0100

    Slovenia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Zámecké sklepení Litomyšl', '/english/cz/subterranea/Litomysl.html', 'Litomysl', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Pardubicky', 49.873502, 16.312940);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ajdovska Jama', '/english/si/showcaves/Ajdovska.html', 'Ajdovska', 'Europe', 'si', 'Slovenia', 'showcaves', 'Dolenjska', 45.949685409387456, 15.425566140186838);
