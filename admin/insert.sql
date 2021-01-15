60d8c61fa9c31f4f0beae8f03bbbfe42c9e98d5d
Jochen <jochen@showcaves.com>
2021-01-15 03:49:57 +0100

    Gunung Mulu

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Ajdovska Jama', '/english/si/showcaves/Ajdovska.html', 'Ajdovska', 'Europe', 'si', 'Slovenia', 'showcaves', 'Dolenjska', 45.949685409387456, 15.425566140186838);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Citadelle de Namur', '/english/be/subterranea/CitadelleNamur.html', 'CitadelleNamur', 'Europe', 'be', 'Belgium', 'subterranea', 'Namur', 50.45948368988112, 4.861980305026423);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Fastlane - Lagang Cave', '/english/my/showcaves/Fastlane.html', 'Fastlane', 'Asia', 'my', 'Malaysia', 'showcaves', 'GunungMulu', 4.050518, 114.823741);
