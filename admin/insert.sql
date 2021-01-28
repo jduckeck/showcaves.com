ab5cc2f3c5bbf761e0e55e21d86b8024fd68f10b
Jochen <jochen@showcaves.com>
2021-01-28 09:52:07 +0100

    deploy

--- admin/insert.sql
+++ admin/insert.sql
@@ -17,3 +17,21 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Worley’s Cave', '/english/usa/caves/Worleys.html', 'Worleys', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'tn', 36.456574420056455, -82.24165153037906);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cova dels Coloms', '/english/es/caves/Coloms2.html', 'Coloms2', 'Europe', 'es', 'Spain', 'caves', 'Mallorca', 39.501031, 3.301896);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Moleta de Binifaldó', '/english/es/karst/Binifaldo.html', 'Binifaldo', 'Europe', 'es', 'Spain', 'karst', 'Mallorca', 39.8170546240977, 2.8933226832574044);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gornerschlucht', '/english/ch/gorges/Gorner.html', 'Gorner', 'Europe', 'ch', 'Switzerland', 'gorges', 'Wallis', 46.0092744541817, 7.7398769068468);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mina Victòria', '/english/es/mines/Victoria.html', 'Victoria', 'Europe', 'es', 'Spain', 'mines', 'Cataluna', 42.77525687758527, 0.7155296690545292);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Museo Termas Romanas de Campo Valdés', '/english/es/subterranea/CampoValdes.html', 'CampoValdes', 'Europe', 'es', 'Spain', 'subterranea', 'Asturias', 43.545449, -5.661337);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta da Fumaça', '/english/br/caves/Fumaca.html', 'Fumaca', 'SouthAmerica', 'br', 'Brazil', 'caves', 'Parana', -12.331672, -41.596600);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cavernas Misteriosas', '/english/br/caves/Misteriosas.html', 'Misteriosas', 'SouthAmerica', 'br', 'Brazil', 'caves', 'Parana', -26.042621, -50.833364);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nucleo Caboclos', '/english/br/caves/Caboclos.html', 'Caboclos', 'SouthAmerica', 'br', 'Brazil', 'caves', 'SaoPaulo', -24.434611, -48.584779);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Langjökull Glacier Grotto', '/english/is/subterranea/Langjokull.html', 'Langjokull', 'Europe', 'is', 'Iceland', 'subterranea', 'Vesturland', 64.69928819120169, -20.86865499449014);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Imi n’Ifri', '/english/other/karst/IminIfri.html', 'IminIfri', 'Africa', 'ma', 'Morocco', 'karst', 'ma', 'other', 31.723945, -6.971613);
