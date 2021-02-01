34624d21c25f2803d360d814ade61302aefffbe5
Jochen <jochen@showcaves.com>
2021-02-01 09:40:04 +0100

    Tianbao

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tianbao Cave', '/english/cn/showcaves/Tianbao.html', 'Tianbao', 'Asia', 'cn', 'China', 'showcaves', 'Sichuan', 28.133512647319666, 106.18864076236902);
