a39725fb9e35cd3db334328198e6c32ea01c61ec
Jochen <jochen@showcaves.com>
2021-02-03 01:49:12 +0100

    Matera

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,32 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tianbao Cave', '/english/cn/showcaves/Tianbao.html', 'Tianbao', 'Asia', 'cn', 'China', 'showcaves', 'Sichuan', 28.133512647319666, 106.18864076236902);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Casa Grotta del Casalnuovo', '/english/it/subterranea/Casalnuovo.html', 'Casalnuovo', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.66243109150818, 16.612071793943233);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','La Raccolta delle Acque', '/english/it/subterranea/RaccoltaDelleAcque.html', 'RaccoltaDelleAcque', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.66353791623552, 16.61131004656782);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Storica Casa Grotta di Vico Solitario', '/english/it/subterranea/CasaGrotta.html', 'CasaGrotta', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.663924872708385, 16.61220246638825);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Casa Cisterna Sotterranea', '/english/it/subterranea/CasaCisternaSotterranea.html', 'CasaCisternaSotterranea', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.663778127421004, 16.610873286450666);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Museo della Scultura Contemporanea Matera', '/english/it/subterranea/MUSMA.html', 'MUSMA', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.66620073570342, 16.61226151124575);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Madonna delle Virtù', '/english/it/subterranea/MadonnaDelleVirtu.html', 'MadonnaDelleVirtu', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.66855782100837, 16.611358526588365);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Casa Grotta Antica Matera', '/english/it/subterranea/AnticaMatera.html', 'AnticaMatera', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.66760135792323, 16.610038540081483);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Materasum Ipogeo', '/english/it/subterranea/MaterasumIpogeo.html', 'MaterasumIpogeo', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.667720236893494, 16.60678092824735);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bunker Soratte', '/english/it/subterranea/Soratte.html', 'Soratte', 'Europe', 'it', 'Italy', 'subterranea', 'Lazio', 42.2366, 12.5082);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Casa Cava', '/english/it/subterranea/CasaCava.html', 'CasaCava', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.671226,16.609605);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Le Cisterne ed il Palombaro dell’Hotel Il Belvedere', '/english/it/subterranea/IlBelvedere.html', 'IlBelvedere', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.66091010476902, 16.613088782409946);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Matera Olive Oil Museum', '/english/it/subterranea/MOOM.html', 'MOOM', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.661754445747206, 16.611734507034726);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Chiese Rupestri di Santa Barbara', '/english/it/subterranea/SantaBarbara.html', 'SantaBarbara', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.65805058846975, 16.61640268438454);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Santa Maria de Idris', '/english/it/subterranea/SantaMariaIdris.html', 'SantaMariaIdris', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.6641759808743, 16.61212807692558);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Antica Casa Grotta di Via Fiorentini', '/english/it/subterranea/CasaGrottaViaFiorentini.html', 'CasaGrottaViaFiorentini', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.66760949577639, 16.610038540081483);
