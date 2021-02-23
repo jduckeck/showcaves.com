c0101be98a2596bf6c362391f24e0230118aaafd
Jochen <jochen@showcaves.com>
2021-02-24 00:39:24 +0100

    Turkey

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Mar Taqla', '/english/other/subterranea/Taqla.html', 'Taqla', 'MiddleEast', 'sy', 'Syria', 'subterranea', 'sy', 'other', 33.847052, 36.546950);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Çankırı Tuz Mağarası', '/english/tr/mines/Cankiri.html', 'Cankiri', 'Europe', 'tr', 'Turkey', 'mines', 'CentralAnatolia', 40.544362, 33.759633);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nevşehir Yeraltı Şehri', '/english/tr/subterranea/Nevsehir.html', 'Nevsehir', 'Europe', 'tr', 'Turkey', 'subterranea', 'CentralAnatolia', 38.620618, 34.714448);
