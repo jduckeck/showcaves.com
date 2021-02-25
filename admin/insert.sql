28d46df0510b799096133d47e3130dfb528a3c4f
Jochen <jochen@showcaves.com>
2021-02-25 22:22:05 +0100

    Turkey

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Nevşehir Yeraltı Şehri', '/english/tr/subterranea/Nevsehir.html', 'Nevsehir', 'Europe', 'tr', 'Turkey', 'subterranea', 'CentralAnatolia', 38.620618, 34.714448);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Sivasa Gökçetoprak Yeraltı Şehri', '/english/tr/subterranea/SivasaGokcetoprak.html', 'SivasaGokcetoprak', 'Europe', 'tr', 'Turkey', 'subterranea', 'CentralAnatolia', 38.657904, 34.299582);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Zindan Mağarası', '/english/tr/showcaves/Zindan.html', 'Zindan', 'Europe', 'tr', 'Turkey', 'showcaves', 'Mediterranean', 37.811765, 31.084988);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yarımburgaz Mağarası', '/english/tr/caves/Yarimburgaz.html', 'Yarimburgaz', 'Europe', 'tr', 'Turkey', 'caves', 'Marmara', 41.075258, 28.741593);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yerküpe Mağarası', '/english/tr/showcaves/Yerkupe.html', 'Yerkupe', 'Europe', 'tr', 'Turkey', 'showcaves', 'Aegean', 37.403627, 28.430115);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','İncirliin Mağarası', '/english/tr/showcaves/Incirliin.html', 'Incirliin', 'Europe', 'tr', 'Turkey', 'showcaves', 'Aegean', 37.189722, 27.761944);
