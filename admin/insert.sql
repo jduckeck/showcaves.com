3436bf7ca188537297d4eb75ab48cbfb52b7a883
Jochen <jochen@showcaves.com>
2021-03-03 19:35:06 +0100

    Japan

--- admin/insert.sql
+++ admin/insert.sql
@@ -36,3 +36,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Covetes del Colomer', '/english/es/subterranea/Colomer.html', 'Colomer', 'Europe', 'es', 'Spain', 'subterranea', 'Valenciana', 38.767657, -0.607065);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cava de Sant Blai', '/english/es/subterranea/SantBlai.html', 'SantBlai', 'Europe', 'es', 'Spain', 'subterranea', 'Valenciana', 38.768900, -0.607940);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Oya History Museum', '/english/jp/mines/Oya.html', 'Oya', 'Asia', 'jp', 'Japan', 'mines', 'Gunma', 36.599989, 139.824745);
