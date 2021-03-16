317a7c50ffd933790da163ac6da42829007c12f1
jd <jochen.duckeck@diginuevo.com>
2021-03-16 11:14:13 +0100

    Valle dei Mulini

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cueva de San Ignacio', '/english/es/subterranea/SanIgnacio.html', 'SanIgnacio', 'Europe', 'es', 'Spain', 'subterranea', 'Barcelona', 41.7215, 1.8312);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Valle dei Mulini', '/english/it/gorges/Mulini.html', 'Mulini', 'Europe', 'it', 'Italy', 'gorges', 'Campania', 40.6247, 14.3756);
