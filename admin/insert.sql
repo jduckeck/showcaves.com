59da8602b7e95a981f52ea26c1e2b2747cb391f7
jd <jochen.duckeck@diginuevo.com>
2021-03-16 10:17:32 +0100

    Cueva de San Ignacio

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cueva de San Ignacio', '/english/es/subterranea/SanIgnacio.html', 'SanIgnacio', 'Europe', 'es', 'Spain', 'subterranea', 'Barcelona', 41.7215, 1.8312);
