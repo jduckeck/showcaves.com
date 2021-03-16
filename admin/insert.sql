14f4431c4dc2f230683743beba2688bbdae5310a
Jochen <jochen@showcaves.com>
2021-03-16 18:42:11 +0100

    Tokyo Caves

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kurasawa Shonyudo', '/english/jp/showcaves/Kurasawa.html', 'Kurasawa', 'Asia', 'jp', 'Japan', 'showcaves', 'Tokyo', 35.854457, 139.072070);
