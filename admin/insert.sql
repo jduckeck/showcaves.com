15a8c41af55726d6804b723265759702deefb685
Jochen <jochen@showcaves.com>
2021-03-18 08:53:17 +0100

    Catalonia

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Valle dei Mulini', '/english/it/gorges/Mulini.html', 'Mulini', 'Europe', 'it', 'Italy', 'gorges', 'Campania', 40.624956, 14.376435);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Mina Eugènia', '/english/es/mines/Eugenia.html', 'Eugenia', 'Europe', 'es', 'Spain', 'mines', 'Cataluna', 41.161314, 0.762697);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Coves de Can Riera', '/english/es/caves/CanRiera.html', 'CanRiera', 'Europe', 'es', 'Spain', 'caves', 'Cataluna', 41.366942, 1.959568);
