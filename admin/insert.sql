75bb3112ef527137e1961a76d773b54960aacc8b
Jochen <jochen@showcaves.com>
2021-04-09 08:39:32 +0200

    Brazil

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Itaimbezinho Canyon', '/english/br/gorges/Itaimbezinho.html', 'Itaimbezinho', 'SouthAmerica', 'br', 'Brazil', 'gorges', 'RioGrandeSul', -28.62551, -49.73511);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Jaskinia Jasna koło Smolenia', '/english/pl/caves/Jasna.html', 'Jasna', 'Europe', 'pl', 'Poland', 'caves', 'Krakow', 50.427778, 19.675722);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tulumtaş Mağarası', '/english/tr/showcaves/Tulumtas.html', 'Tulumtas', 'Europe', 'tr', 'Turkey', 'showcaves', 'CentralAnatolia', 39.774184, 32.683427);
