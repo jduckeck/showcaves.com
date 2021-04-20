621ea9d125c9bd0b71c922b8375712d454a56571
Jochen <jochen@showcaves.com>
2021-04-20 18:57:02 +0200

    Greek Regions

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Vari Cave', '/english/gr/caves/Vari.html', 'Vari', 'Europe', 'gr', 'Greece', 'caves', 'Attica', 37.858146558158346, 23.80184857762393);
