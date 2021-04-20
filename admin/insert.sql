986578b92df939deaab922d9af53bbe10f776c12
Jochen <jochen@showcaves.com>
2021-04-20 21:26:49 +0200

    Dragon Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Vari Cave', '/english/gr/caves/Vari.html', 'Vari', 'Europe', 'gr', 'Greece', 'caves', 'Attica', 37.858146558158346, 23.80184857762393);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cave Of The Dragon Kastoria', '/english/gr/showcaves/DragonKastoria.html', 'DragonKastoria', 'Europe', 'gr', 'Greece', 'showcaves', 'WesternMacedonia', 40.505614,  21.284696);
