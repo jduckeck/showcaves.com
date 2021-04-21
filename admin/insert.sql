c8c35bcc59fb34fd6410e0a9f4fa94a12a588bb2
jd <jochen.duckeck@diginuevo.com>
2021-04-21 21:31:20 +0200

    Spain

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Vari Cave', '/english/gr/caves/Vari.html', 'Vari', 'Europe', 'gr', 'Greece', 'caves', 'Attica', 37.858146558158346, 23.80184857762393);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cave Of The Dragon Kastoria', '/english/gr/showcaves/DragonKastoria.html', 'DragonKastoria', 'Europe', 'gr', 'Greece', 'showcaves', 'WesternMacedonia', 40.505614,  21.284696);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nacimiento del Rio Ebro', '/english/es/springs/Fontibre.html', 'Fontibre', 'Europe', 'es', 'Spain', 'springs', 'Cantabria', 43.016917964121326, -4.190839385801606);
