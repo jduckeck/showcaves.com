75d90fb164a9b14bc5e501d85004f6b7a03c76e6
jd <jochen.duckeck@diginuevo.com>
2021-04-23 08:03:09 +0200

    Spain

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Cave Of The Dragon Kastoria', '/english/gr/showcaves/DragonKastoria.html', 'DragonKastoria', 'Europe', 'gr', 'Greece', 'showcaves', 'WesternMacedonia', 40.505614,  21.284696);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Nacimiento del Rio Ebro', '/english/es/springs/Fontibre.html', 'Fontibre', 'Europe', 'es', 'Spain', 'springs', 'Cantabria', 43.016917964121326, -4.190839385801606);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cueva del Agua in Orbaneja del Castillo', '/english/es/showcaves/AguaOrbaneja.html', 'AguaOrbaneja', 'Europe', 'es', 'Spain', 'showcaves', 'Leon', 42.83393136135049, -3.7943697484255967);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cascada de Orbaneja del Castillo', '/english/es/karst/OrbanejaDelCastillo.html', 'OrbanejaDelCastillo', 'Europe', 'es', 'Spain', 'karst', 'Leon', 42.8345230522435, -3.7925753844575003);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Desfiladero de la Yecla', '/english/es/gorges/Yecla.html', 'Yecla', 'Europe', 'es', 'Spain', 'gorges', 'Leon', 41.94964144182949, -3.4425464384721147);
