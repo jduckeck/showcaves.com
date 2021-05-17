c3ccea3dc42809202404c9e27daa08704c6d5baa
Jochen <jochen@showcaves.com>
2021-05-17 22:56:04 +0200

    Tonto Natural Bridge

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tonto Natural Bridge', '/english/usa/karst/Tonto.html', 'Tonto', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'az', 34.319722, -111.456667);
