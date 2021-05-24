32fc81183c5ce6a27c578148697c131eda54b850
Jochen <jochen@showcaves.com>
2021-05-24 15:27:42 +0200

    Edessa

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Tonto Natural Bridge', '/english/usa/karst/Tonto.html', 'Tonto', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'az', 34.319722, -111.456667);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cave of Saint Barbara', '/english/gr/caves/Varvara.html', 'Varvara', 'Europe', 'gr', 'Greece', 'caves', 'SouthAegean', 36.959133, 26.973695);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Edessa Waterfalls Cave', '/english/gr/showcaves/Edessa.html', 'Edessa', 'Europe', 'gr', 'Greece', 'showcaves', 'CentralMacedonia', 40.803592, 22.055416);
