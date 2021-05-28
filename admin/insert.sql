ac85dddf4a1964c5e917ea45b5d6d17cb294c4a2
Jochen <jochen@showcaves.com>
2021-05-28 22:39:24 +0200

    Polyphemus

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Cave of Saint Barbara', '/english/gr/caves/Varvara.html', 'Varvara', 'Europe', 'gr', 'Greece', 'caves', 'SouthAegean', 36.959133, 26.973695);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Edessa Waterfalls Cave', '/english/gr/showcaves/Edessa.html', 'Edessa', 'Europe', 'gr', 'Greece', 'showcaves', 'CentralMacedonia', 40.803592, 22.055416);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cyclop\'s Cave', '/english/gr/caves/Polyphemus.html', 'Polyphemus', 'Europe', 'gr', 'Greece', 'caves', 'EastMacedonia', 40.84844297935164, 25.7463578638589);
