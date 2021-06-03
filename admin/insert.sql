1b59da7e944290284d65488fce29ea00a446b750
jd <jochen.duckeck@diginuevo.com>
2021-06-03 19:24:14 +0200

    fixes

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kastania Cave', '/english/gr/showcaves/Kastania.html', 'Kastania', 'Europe', 'gr', 'Greece', 'showcaves', 'Peloponnese', 36.545897, 23.124318);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tonto Natural Bridge', '/english/usa/karst/Tonto.html', 'Tonto', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'az', 34.319722, -111.456667);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cave of Saint Barbara', '/english/gr/caves/Varvara.html', 'Varvara', 'Europe', 'gr', 'Greece', 'caves', 'SouthAegean', 36.959133, 26.973695);
