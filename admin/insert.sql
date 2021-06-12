c1eeb170cf1277480563344f7417aad2d56b6484
Jochen <jochen@showcaves.com>
2021-06-12 11:47:24 +0200

    Mauritius

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','François Leguat Giant Tortoise and Cave Reserve', '/english/other/showcaves/FrancoisLeguat.html', 'FrancoisLeguat', 'Africa', 'mu', 'Mauritius', 'showcaves', 'mu', 'other', -19.757834, 63.370690    );
