45336ad684ffcb96ad8152a0a8e52824ac952775
Jochen <jochen@showcaves.com>
2021-06-12 16:13:15 +0200

    Mauritius

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES('yes','François Leguat Giant Tortoise and Cave Reserve', '/english/other/showcaves/FrancoisLeguat.html', 'FrancoisLeguat', 'Africa', 'mu', 'Mauritius', 'showcaves', 'mu', 'other', -19.757834, 63.370690    );
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','La Cave Madame', '/english/other/caves/Madame.html', 'Madame', 'Africa', 'mu', 'Mauritius', 'caves', 'mu', 'other', -20.111305, 57.718467);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Roche Noire Lava Tubes', '/english/other/caves/RocheNoire.html', 'RocheNoire', 'Africa', 'mu', 'Mauritius', 'caves', 'mu', 'other', -20.117219, 57.711408);
