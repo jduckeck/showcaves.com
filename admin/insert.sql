a5c3ebef27e6b56715a7d70291298d30b0cb9f4e
Jochen <jochen@showcaves.com>
2021-06-13 22:12:49 +0200

    Mauritius

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','La Cave Madame', '/english/other/caves/Madame.html', 'Madame', 'Africa', 'mu', 'Mauritius', 'caves', 'mu', 'other', -20.111305, 57.718467);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES('yes','Roche Noire Lava Tubes', '/english/other/caves/RocheNoire.html', 'RocheNoire', 'Africa', 'mu', 'Mauritius', 'caves', 'mu', 'other', -20.117219, 57.711408);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Twilight Caverne', '/english/other/caves/Twilight.html', 'Twilight', 'Africa', 'mu', 'Mauritius', 'caves', 'mu', 'other', -20.3, 57.5833);
