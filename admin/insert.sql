5b29ac598d89a3a46557f0eeb7ea703474b9ae58
Jochen <jochen@showcaves.com>
2021-06-20 15:35:28 +0200

    Czech Elbsandstein

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tiské Stěny', '/english/cz/gorges/TiskeSteny.html', 'TiskeSteny', 'Europe', 'cz', 'Czech Republic', 'gorges', 'Ustecky', 50.787591, 14.028838);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ostrovské Skály', '/english/cz/gorges/Ostrov.html', 'Ostrov', 'Europe', 'cz', 'Czech Republic', 'gorges', 'Ustecky', 50.806170, 14.051630);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Rájecké Skály', '/english/cz/gorges/Rajeck.html', 'Rajeck', 'Europe', 'cz', 'Czech Republic', 'gorges', 'Ustecky', 50.802502, 14.022665);
