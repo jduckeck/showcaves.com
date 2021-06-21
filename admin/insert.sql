aca9690d714bcd375f91f140f224ad812d598cd6
jd <jochen.duckeck@diginuevo.com>
2021-06-21 13:28:44 +0200

    China

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yinhedong', '/english/cn/showcaves/YinheHebei.html', 'YinheHebei', 'Asia', 'cn', 'China', 'showcaves', 'Hebei', 38.265543409351736, 113.7236723715491);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tiské Stěny', '/english/cz/gorges/TiskeSteny.html', 'TiskeSteny', 'Europe', 'cz', 'Czech Republic', 'gorges', 'Ustecky', 50.787591, 14.028838);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Ostrovské Skály', '/english/cz/gorges/Ostrov.html', 'Ostrov', 'Europe', 'cz', 'Czech Republic', 'gorges', 'Ustecky', 50.806170, 14.051630);
