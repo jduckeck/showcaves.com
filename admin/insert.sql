1467f469aef07dd054d1fe7c695cda7bad5b020d
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2021-06-29 13:38:36 +0200

    Lake Sirino

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Lago Sirino', '/english/it/karst/Sirino.html', 'Sirino', 'Europe', 'it', 'Italy', 'karst', 'Basilicata', 40.0924, 15.8088);
