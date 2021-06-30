2ece5f993032870da12322e8b4d328fb90f2a857
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2021-06-30 08:35:43 +0200

    Uzbekistan

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Lago Sirino', '/english/it/karst/Sirino.html', 'Sirino', 'Europe', 'it', 'Italy', 'karst', 'Basilicata', 40.0924, 15.8088);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Hazrat Daud Cave', '/english/other/caves/HazratDaud.html', 'HazratDaud', 'Asia', 'uz', 'Uzbekistan', 'caves', 'uz', 'other', 39.498881418163506, 66.61306099999815);
