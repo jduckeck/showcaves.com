717da290e6e609d4026770ff8758864a082f750d
Jochen <jochen@showcaves.com>
2021-07-02 21:18:23 +0200

    Nottinghamshire

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Hazrat Daud Cave', '/english/other/caves/HazratDaud.html', 'HazratDaud', 'Asia', 'uz', 'Uzbekistan', 'caves', 'uz', 'other', 39.498881418163506, 66.61306099999815);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Devil\'s Punchbowl', '/english/gb/karst/DevilsPunchbowl.html', 'DevilsPunchbowl', 'Europe', 'gb', 'Great Britain', 'karst', 'Norfolk', 52.4683, 0.7633);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bell Inn Caves', '/english/gb/subterranea/BellInn.html', 'BellInn', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Nottinghamshire', 52.9535, -1.1520);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nottingham Castle Cave Tours', '/english/gb/subterranea/NottinghamCastle.html', 'NottinghamCastle', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Nottinghamshire', 52.949834, -1.155059);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Museum of Nottingham Life', '/english/gb/subterranea/NottinghamLife.html', 'NottinghamLife', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Nottinghamshire', 52.949215, -1.152980);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Burlington', '/english/gb/subterranea/Burlington.html', 'Burlington', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Wiltshire', 51.4310, -2.1858);
