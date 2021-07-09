467a3870bdb701807ff97b2c6e1f546d5213649a
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2021-07-09 17:24:42 +0200

    Stark Caverns

---
+++ admin/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Stark Caverns', '/english/usa/showcaves/Stark.html', 'Stark', 'NorthAmerica', 'usa', 'United States of America', 'showcaves', 'mo', 38.2839911567361, -92.59170053066353);
+DELETE FROM sights WHERE sortby='FantasyWor';
