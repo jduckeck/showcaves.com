c4b1a7224a585a0a5a5395b87bbe4ff0b1620c3d
Jochen <jochen@showcaves.com>
2021-07-11 15:13:14 +0200

    various za, mt, kmz

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,3 +1,12 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Stark Caverns', '/english/usa/showcaves/Stark.html', 'Stark', 'NorthAmerica', 'usa', 'United States of America', 'showcaves', 'mo', 38.2839911567361, -92.59170053066353);
 DELETE FROM sights WHERE sortby='FantasyWor';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Motouleng Caves', '/english/za/subterranea/Motouleng.html', 'Motouleng', 'Africa', 'za', 'South Africa', 'subterranea', 'FreeState', -28.6094, 28.3857);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ta’ Bistra Catacombs', '/english/mt/subterranea/TaBistra.html', 'TaBistra', 'Europe', 'mt', 'Malta', 'subterranea', 'Malta', 35.915481, 14.416166);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Holkrans at Golden Gate', '/english/za/caves/HolkransGoldenGate.html', 'Holkrans', 'Africa', 'za', 'South Africa', 'caves', 'FreeState', -28.511582, 28.610145);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Holkrans at Meiringskloof', '/english/za/caves/HolkransMeiringskloof .html', 'HolkransMeiringskloof ', 'Africa', 'za', 'South Africa', 'caves', 'FreeState', -28.599804, 28.228266);
+
