194f769a7c5392282c594c0128bb66de76be4a3d
Jochen <jochen@showcaves.com>
2021-07-18 10:26:23 +0200

    Pla de Petracos

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Santuario del Pla de Petracos', '/english/es/caves/CastellCastells.html', 'CastellCastells', 'Europe', 'es', 'Spain', 'caves', 'Valenciana', 38.76029058556768, -0.18332103644327763);
