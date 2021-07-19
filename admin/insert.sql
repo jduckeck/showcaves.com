52782f65f4a8adf2956b0fac29391ccc0c2fdaf8
Jochen <jochen@showcaves.com>
2021-07-20 00:53:57 +0200

    Gruta do Limoeiro

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Santuario del Pla de Petracos', '/english/es/caves/CastellCastells.html', 'CastellCastells', 'Europe', 'es', 'Spain', 'caves', 'Valenciana', 38.76029058556768, -0.18332103644327763);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Sibaysky Open Pit', '/english/ru/mines/Sibay.html', 'Sibay', 'Asia', 'ru', 'Russia', 'mines', 'Bashkortostan', 52.695607, 58.637973);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta do Limoeiro', '/english/br/showcaves/Limoeiro.html', 'Limoeiro', 'SouthAmerica', 'br', 'Brazil', 'showcaves', 'EspiritoSanto', -20.484423, -41.172105);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta da Boa Sorte', '/english/br/subterranea/BoaSorte.html', 'BoaSorte', 'SouthAmerica', 'br', 'Brazil', 'subterranea', 'EspiritoSanto', -20.580380, -41.096349);
