6982c6f9a51734502bdfec1d92e8264b70279247
Jochen <jochen@showcaves.com>
2021-07-19 19:50:43 +0200

    Sibay quarry

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Santuario del Pla de Petracos', '/english/es/caves/CastellCastells.html', 'CastellCastells', 'Europe', 'es', 'Spain', 'caves', 'Valenciana', 38.76029058556768, -0.18332103644327763);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Sibaysky Open Pit', '/english/ru/mines/Sibay.html', 'Sibay', 'Asia', 'ru', 'Russia', 'mines', 'Bashkortostan', 52.695607, 58.637973);
