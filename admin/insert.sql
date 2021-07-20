d51777c71f963173d37ddd1aadb1efa46faadbc7
Jochen <jochen@showcaves.com>
2021-07-20 20:51:25 +0200

    Brazil

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,4 +5,10 @@ VALUES('yes','Sibaysky Open Pit', '/english/ru/mines/Sibay.html', 'Sibay', 'Asia
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Gruta do Limoeiro', '/english/br/showcaves/Limoeiro.html', 'Limoeiro', 'SouthAmerica', 'br', 'Brazil', 'showcaves', 'EspiritoSanto', -20.484423, -41.172105);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Gruta da Boa Sorte', '/english/br/subterranea/BoaSorte.html', 'BoaSorte', 'SouthAmerica', 'br', 'Brazil', 'subterranea', 'EspiritoSanto', -20.580380, -41.096349);
+VALUES('yes','Gruta da Boa Sorte', '/english/br/subterranea/BoaSorte.html', 'BoaSorte', 'SouthAmerica', 'br', 'Brazil', 'subterranea', 'EspiritoSanto', -20.58049, -41.09629);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta do Sumidouro', '/english/br/caves/Sumidouro.html', 'Sumidouro', 'SouthAmerica', 'br', 'Brazil', 'caves', 'Bahia', -10.074640, -40.754334);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta da Lapa Nova', '/english/br/showcaves/LapaNova.html', 'LapaNova', 'SouthAmerica', 'br', 'Brazil', 'showcaves', 'MinasGerais', -17.984448, -46.890678);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta Lapa Velha', '/english/br/showcaves/LapaVelha.html', 'LapaVelha', 'SouthAmerica', 'br', 'Brazil', 'showcaves', 'MinasGerais', -17.979754, -46.908081);
