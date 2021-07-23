e829b4dba11427b4aa43180f39edc1b2a77f1e69
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2021-07-23 10:01:29 +0200

    Sweden

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Santuário da Penha', '/english/pt/caves/Penha.html', 'Medo', 'Europe', 'pt', 'Portugal', 'caves', 'Braga', 41.43257, -8.26886);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Torkulla Kyrka', '/english/se/caves/TorkullaKyrka.html', 'TorkullaKyrka', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland');
