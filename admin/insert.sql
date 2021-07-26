11624d1b1c4bd87303dac0fe2d225e1de06acab8
Jochen <jochen@showcaves.com>
2021-07-26 08:43:43 +0200

    Lajedo de Soledade

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Santuário da Penha', '/english/pt/caves/Penha.html', 'Medo', 'Europe', 'pt', 'Portugal', 'caves', 'Braga', 41.43257, -8.26886);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Torkulla Kyrka', '/english/se/caves/TorkullaKyrka.html', 'TorkullaKyrka', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Lajedo de Soledade', '/english/br/karst/LajedoSoledade.html', 'LajedoSoledade', 'SouthAmerica', 'br', 'Brazil', 'karst', 'RioGrandeNorte', -5.598118, -37.829916);
