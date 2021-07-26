2874026006ab16f01af5dcc9519d9c6ff6740076
Jochen <jochen@showcaves.com>
2021-07-26 18:36:17 +0200

    Östergötlands

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,6 +3,16 @@ VALUES('yes','Qanat di Palermo', '/english/it/subterranea/QanatPalermo.html', 'Q
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Santuário da Penha', '/english/pt/caves/Penha.html', 'Medo', 'Europe', 'pt', 'Portugal', 'caves', 'Braga', 41.43257, -8.26886);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Torkulla Kyrka', '/english/se/caves/TorkullaKyrka.html', 'TorkullaKyrka', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland');
+VALUES('yes','Torkulla Kyrka', '/english/se/caves/TorkullaKyrka.html', 'TorkullaKyrka', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland', 58.733339, 15.662198);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Lajedo de Soledade', '/english/br/karst/LajedoSoledade.html', 'LajedoSoledade', 'SouthAmerica', 'br', 'Brazil', 'karst', 'RioGrandeNorte', -5.598118, -37.829916);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bjärs Grottor', '/english/se/caves/Bjars.html', 'Bjars', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland', 58.570000, 15.590722);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Björnstugugrottan', '/english/se/caves/Bjornstugu.html', 'Bjornstugu', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland', 58.703650, 15.394480);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Trollegater', '/english/se/caves/Trollegater.html', 'Trollegater', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland', 58.146308,  15.572273);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Vålters Grotta', '/english/se/caves/Valters.html', 'Valters', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland', 58.72912,15.89464);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grottan vid Rövarmon', '/english/se/caves/Rovarmon.html', 'Rovarmon', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland', 58.578250, 15.750180);
