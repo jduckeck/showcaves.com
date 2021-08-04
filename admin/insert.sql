b9b1a34d7fbe92ffaf3c7fedca110cd045c0db3b
Jochen <jochen@showcaves.com>
2021-08-04 18:39:19 +0200

    Cavernas del Río Nus

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Grotta Conza', '/english/it/caves/Conza.html', 'Conza', 'Europe', 'it', 'Italy', 'caves', 'Sicilia', 38.187510396643006, 13.280804657616867);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Grotta di Santa Ninfa', '/english/it/caves/SantaNinfa.html', 'SantaNinfa', 'Europe', 'it', 'Italy', 'caves', 'Sicilia', 37.7797, 12.8976);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Cavernas del Río Nus', '/english/other/caves/RioNus.html', 'RioNus', 'SouthAmerica', 'co', 'Colombia', 'caves', 'co', 'other', 6.305833, -74.691667);
