aa3f7a8e0f30b5d7a6f714db16bec39e9536a867
Jochen <jochen@showcaves.com>
2021-08-11 05:32:59 +0200

    Speleotherapy

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Slănic Mine', '/english/ro/mines/Slanic.html', 'Slanic', 'Europe', 'ro', 'Romania', 'mines', 'Meridionali', 45.235971,  25.941007);
