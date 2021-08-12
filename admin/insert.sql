1585766d565fa37e601e934e77b4b628b86db123
Jochen <jochen@showcaves.com>
2021-08-12 15:00:57 +0200

    Romanian Salt Mines

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Salina Târgu Ocna', '/english/ro/mines/TarguOcna.html', 'TarguOcna', 'Europe', 'ro', 'Romania', 'mines', 'EasternCarpathians', 46.287639, 26.603429);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Changyu Dongtian - Changyu Stone Cave', '/english/cn/subterranea/ChangyuStone.html', 'ChangyuStone', 'Asia', 'cn', 'China', 'subterranea', 'Zhejiang', 28.445098, 121.449105);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Salina Ocnele Mari', '/english/ro/mines/OcneleMari.html', 'OcneleMari', 'Europe', 'ro', 'Romania', 'mines', 'Meridionali', 45.085091, 24.309467);
