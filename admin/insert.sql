a8aee1c1b2ff54f3f8e1da384d4d7b51639fdbae
Jochen <jochen@showcaves.com>
2021-08-12 09:30:47 +0200

    Changyu Dongtian - Changyu Stone Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Slănic Mine', '/english/ro/mines/Slanic.html', 'Slanic', 'Europe', 'ro', 'Romania', 'mines', 'Meridionali', 45.235971,  25.941007);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Salina Târgu Ocna', '/english/ro/mines/TarguOcna.html', 'TarguOcna', 'Europe', 'ro', 'Romania', 'mines', 'EasternCarpathians', 46.287639, 26.603429);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Changyu Dongtian - Changyu Stone Cave', '/english/cn/subterranea/ChangyuStone.html', 'ChangyuStone', 'Asia', 'cn', 'China', 'subterranea', 'Zhejiang', 28.445098, 121.449105);
