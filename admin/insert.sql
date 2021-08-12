faba6ef0b3f46d3b052e9a8fce948dee7468cb71
Jochen <jochen@showcaves.com>
2021-08-13 00:02:15 +0200

    Romania

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Changyu Dongtian - Changyu Stone Cave', '/english/cn/subterranea/ChangyuStone.html', 'ChangyuStone', 'Asia', 'cn', 'China', 'subterranea', 'Zhejiang', 28.445098, 121.449105);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Salina Ocnele Mari', '/english/ro/mines/OcneleMari.html', 'OcneleMari', 'Europe', 'ro', 'Romania', 'mines', 'Meridionali', 45.085091, 24.309467);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Peştera Şugău', '/english/ro/showcaves/Sugau.html', 'Sugau', 'Europe', 'ro', 'Romania', 'showcaves', 'EasternCarpathians', 46.682112, 25.673547);
