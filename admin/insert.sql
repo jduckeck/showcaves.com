b8ad6d615c3adbe44518297eb7913ba0698d3461
Jochen <jochen@showcaves.com>
2021-08-12 09:08:26 +0200

    Romanian Salt Mines

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Slănic Mine', '/english/ro/mines/Slanic.html', 'Slanic', 'Europe', 'ro', 'Romania', 'mines', 'Meridionali', 45.235971,  25.941007);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Salina Târgu Ocna', '/english/ro/mines/TarguOcna.html', 'TarguOcna', 'Europe', 'ro', 'Romania', 'mines', 'EasternCarpathians', 46.287639, 26.603429);
