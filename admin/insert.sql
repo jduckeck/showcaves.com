74d1945ebe79344d8f0da72a08724c1522d7c703
Jochen <jochen@showcaves.com>
2021-08-24 21:50:46 +0200

    China

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Liánhuā dòng', '/english/cn/showcaves/Lianhua.html', 'Lianhua', 'Asia', 'cn', 'China', 'showcaves', 'Jiangxi', 29.621875, 116.003645);
