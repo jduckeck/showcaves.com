722b10f9574062839a76c20ea246b4f6056cadfd
Jochen <jochen@showcaves.com>
2021-08-25 17:56:50 +0200

    China

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Liánhuā dòng', '/english/cn/showcaves/Lianhua.html', 'Lianhua', 'Asia', 'cn', 'China', 'showcaves', 'Jiangxi', 29.621875, 116.003645);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Eremo di San Leonardo', '/english/it/subterranea/SanLeonardo.html', 'SanLeonardo', 'Europe', 'it', 'Italy', 'subterranea', 'Lazio', 42.31213632552154, 12.705482720331995);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Dolina del Revotano', '/english/it/karst/Revotano.html', 'Revotano', 'Europe', 'it', 'Italy', 'karst', 'Lazio', 42.3111, 12.6950);
