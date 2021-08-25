895b900ec3aee4fd5503789980fd2a82d3c2f2f5
Jochen <jochen@showcaves.com>
2021-08-25 23:52:21 +0200

    Romania

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Eremo di San Leonardo', '/english/it/subterranea/SanLeonardo.html', 'SanLeonardo', 'Europe', 'it', 'Italy', 'subterranea', 'Lazio', 42.31213632552154, 12.705482720331995);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Dolina del Revotano', '/english/it/karst/Revotano.html', 'Revotano', 'Europe', 'it', 'Italy', 'karst', 'Lazio', 42.3111, 12.6950);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Peștera Sfântului Antonie de la Iezer', '/english/ro/subterranea/StAnthonyIezer.html', 'StAnthonyIezer', 'Europe', 'ro', 'Romania', 'subterranea', 'Meridionali', 45.205839, 24.176876);
