a29d23cfdc4a367b185a2128468c27f2d8d876c8
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2021-08-26 14:43:10 +0200

    Zig Zag Walk

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Zigzag Path', '/english/gb/subterranea/ZigzagPath.html', 'ZigzagPath', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Kent', 51.0739, 1.1651);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Liánhuā dòng', '/english/cn/showcaves/Lianhua.html', 'Lianhua', 'Asia', 'cn', 'China', 'showcaves', 'Jiangxi', 29.621875, 116.003645);
