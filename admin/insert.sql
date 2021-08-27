4dd70af5d13c80100f4295ef1cc0b3ccba2115ff
Jochen <jochen@showcaves.com>
2021-08-27 19:04:38 +0200

    China

--- admin/insert.sql
+++ admin/insert.sql
@@ -10,3 +10,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Getu River Caves', '/english/cn/showcaves/GetuRiver.html', 'GetuRiver', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 25.685038, 106.271575);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Zhijin Grand Canyon', '/english/cn/gorges/ZhijinGrandCanyon.html', 'ZhijinGrandCanyon', 'Asia', 'cn', 'China', 'gorges', 'Guizhou', 26.7793, 105.8862);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nanjiang Grand Canyon', '/english/cn/gorges/Nanjiang.html', 'Nanjiang', 'Asia', 'cn', 'China', 'gorges', 'Guizhou', 26.944777, 106.966000);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Xuehuadong - Snow Flake Cave', '/english/cn/showcaves/Xuehua.html', 'Xuehua', 'Asia', 'cn', 'China', 'showcaves', 'Henan', 34.626522, 113.184021);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Chongqing Laojun Cave', '/english/cn/showcaves/LaojunChongqing.html', 'LaojunChongqing', 'Asia', 'cn', 'China', 'showcaves', 'Chongqing', 29.545034, 106.597587);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Chongqing Laolong Cave', '/english/cn/showcaves/LaolongChongqing.html', 'LaolongChongqing', 'Asia', 'cn', 'China', 'showcaves', 'Chongqing', 29.476496, 106.581065);
