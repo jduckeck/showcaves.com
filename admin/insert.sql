1c694bdebe80063b58647764d63b7f21f3497eae
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2021-08-27 11:10:00 +0200

    Caves of Filopappou Hill

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,6 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Zigzag Path', '/english/gb/subterranea/ZigzagPath.html', 'ZigzagPath', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Kent', 51.0739, 1.1651);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Caves of Filopappou Hill', '/english/gr/subterranea/Filopappou.html', 'Filopappou', 'Europe', 'gr', 'Greece', 'subterranea', 'Attica', 37.9693, 23.7208);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Liánhuā dòng', '/english/cn/showcaves/Lianhua.html', 'Lianhua', 'Asia', 'cn', 'China', 'showcaves', 'Jiangxi', 29.621875, 116.003645);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Eremo di San Leonardo', '/english/it/subterranea/SanLeonardo.html', 'SanLeonardo', 'Europe', 'it', 'Italy', 'subterranea', 'Lazio', 42.31213632552154, 12.705482720331995);
