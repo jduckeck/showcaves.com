d10ff0ae963100a470ecfb7a62ba4fa60ec71a3d
Jochen <jochen@showcaves.com>
2021-08-30 00:37:35 +0200

    deploy

--- admin/insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Pozzo del Diavolo', '/english/it/caves/Diavolo.html', 'Diavolo', 'Europe', 'it', 'Italy', 'caves', 'Lazio', 42.3436, 12.1798);
