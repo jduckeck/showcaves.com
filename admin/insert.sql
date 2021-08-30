c1aa349bbe06e096661a39651ee97232e933b9fb
Jochen <jochen@showcaves.com>
2021-08-30 22:30:54 +0200

    China

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,6 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Huayangdong', '/english/cn/showcaves/HuayangAnhui.html', 'HuayangAnhui', 'Asia', 'cn', 'China', 'showcaves', 'Anhui', 31.792216, 119.316746);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Menggong Guanyin Dong', '/english/cn/showcaves/MenggongGuanyin.html', 'MenggongGuanyin', 'Asia', 'cn', 'China', 'showcaves', 'Yunnan', 23.961875, 99.231533);
+VALUES('yes','Menggong Guanyin Dong', '/english/cn/showcaves/GuanyinMenggong.html', 'MenggongGuanyin', 'Asia', 'cn', 'China', 'showcaves', 'Yunnan', 23.961875, 99.231533);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Guanyindong', '/english/cn/showcaves/GuanyinQianxi.html', 'GuanyinQianxi', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 26.863325, 105.972786);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Xixiu Guanyin Dong', '/english/cn/showcaves/GuanyinXixiu.html', 'GuanyinXixiu', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 26.115621272596055, 105.87801988055661);
