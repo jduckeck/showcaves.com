82cc35f9fea69cb56027ce91c349391089549b27
Jochen <jochen@showcaves.com>
2021-08-30 20:36:49 +0200

    China

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Huayangdong', '/english/cn/showcaves/HuayangAnhui.html', 'HuayangAnhui', 'Asia', 'cn', 'China', 'showcaves', 'Anhui', 31.792216, 119.316746);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Menggong Guanyin Dong', '/english/cn/showcaves/MenggongGuanyin.html', 'MenggongGuanyin', 'Asia', 'cn', 'China', 'showcaves', 'Yunnan', 23.961875, 99.231533);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Guanyindong', '/english/cn/showcaves/GuanyinQianxi.html', 'GuanyinQianxi', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 26.863325, 105.972786);
