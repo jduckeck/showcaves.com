141855f335d2cd6920e68817f5384a584db94653
Jochen <jochen@showcaves.com>
2021-09-01 23:18:20 +0200

    Lago Sfondato

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Lago Sfondato', '/english/it/karst/Sfondato.html', 'Sfondato', 'Europe', 'it', 'Italy', 'karst', 'Sicilia', 37.581935, 13.947483);
