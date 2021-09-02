eff08de96daa2cb8285f706190c70fe9b3d39090
Jochen <jochen@showcaves.com>
2021-09-02 12:14:17 +0200

    India

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Lago Sfondato', '/english/it/karst/Sfondato.html', 'Sfondato', 'Europe', 'it', 'Italy', 'karst', 'Sicilia', 37.581935, 13.947483);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bhandara Buddhist Caves', '/english/in/subterranea/Bhandara.html', 'Bhandara', 'Asia', 'in', 'India', 'subterranea', 'Maharashtra', 18.747466, 73.733530);
