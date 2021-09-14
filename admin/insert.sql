ecbbb4896ca09a31dd228900345e3d6efba130da
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2021-09-14 21:10:22 +0200

    wv

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Madison Coal Museum', '/english/usa/mines/Madison.html', 'Madison', 'NorthAmerica', 'us', 'United States of America', 'mines', 'wv', 38.0646304, -81.8219515);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Lago Sfondato', '/english/it/karst/Sfondato.html', 'Sfondato', 'Europe', 'it', 'Italy', 'karst', 'Sicilia', 37.581935, 13.947483);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Bhandara Buddhist Caves', '/english/in/subterranea/Bhandara.html', 'Bhandara', 'Asia', 'in', 'India', 'subterranea', 'Maharashtra', 18.747466, 73.733530);
