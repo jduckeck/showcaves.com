cc83e6908c2325045d4f2b93d077cbc2efb93690
Jochen <jochen@showcaves.com>
2021-09-16 17:43:16 +0200

    Laos

--- admin/insert.sql
+++ admin/insert.sql
@@ -16,3 +16,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Ghorawadi Caves', '/english/in/subterranea/Ghorawadi.html', 'Ghorawadi', 'Asia', 'in', 'India', 'subterranea', 'Maharashtra', 18.692307,  73.704069);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Farm8', '/english/kr/subterranea/Farm8.html', 'Farm8', 'Asia', 'kr', 'South Korea', 'subterranea', 'Gyeonggi', 37.5031, 126.9478);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tham Nang Ene', '/english/la/showcaves/NangEne.html', 'NangEne', 'Asia', 'la', 'Laos', 'showcaves', 'Khammouane', 17.444045, 104.948519);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tham Xe Bang Fai', '/english/la/showcaves/XeBangfai.html', 'XeBangfai', 'Asia', 'la', 'Laos', 'showcaves', 'Khammouane', 17.373025, 105.838874);
