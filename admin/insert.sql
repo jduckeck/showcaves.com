9b8e2b254b8532da340e799dd819f5f82705b435
Jochen <jochen@showcaves.com>
2021-09-20 18:50:43 +0200

    Russia

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Karaulnaya Cave', '/english/ru/showcaves/Karaulnaya.html', 'Karaulnaya', 'Asia', 'ru', 'Russia', 'showcaves', 'Krasnoyarsk', 55.973259, 92.588933);
