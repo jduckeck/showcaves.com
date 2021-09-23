99794736b170552b7ad21c6a26ed0e172e697456
Jochen <jochen@showcaves.com>
2021-09-23 20:17:31 +0200

    Andalucía

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Karaulnaya Cave', '/english/ru/showcaves/Karaulnaya.html', 'Karaulnaya', 'Asia', 'ru', 'Russia', 'showcaves', 'Krasnoyarsk', 55.973259, 92.588933);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cueva del Yeso de Baena', '/english/es/showcaves/YesoBaena.html', 'YesoBaena', 'Europe', 'es', 'Spain', 'showcaves', 'Andalucia', 37.675835, -4.356675);
