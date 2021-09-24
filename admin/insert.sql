fc9a29ede5d4852d447795a9c133e4d1befee123
Jochen <jochen@showcaves.com>
2021-09-24 09:40:00 +0200

    Spain

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Karaulnaya Cave', '/english/ru/showcaves/Karaulnaya.html', 'Karaulnaya', 'Asia', 'ru', 'Russia', 'showcaves', 'Krasnoyarsk', 55.973259, 92.588933);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cueva del Yeso de Baena', '/english/es/showcaves/YesoBaena.html', 'YesoBaena', 'Europe', 'es', 'Spain', 'showcaves', 'Andalucia', 37.675835, -4.356675);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cova des Pardals', '/english/es/subterranea/Pardals.html', 'Pardals', 'Europe', 'es', 'Spain', 'subterranea', 'Menorca', 39.922099,  3.858500);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Restaurant Cova Sa Nacra', '/english/es/subterranea/SaNacra.html', 'SaNacra', 'Europe', 'es', 'Spain', 'subterranea', 'Menorca', 39.978981, 3.838136);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Parque Minero de Almadén', '/english/es/mines/Almaden.html', 'Almaden', 'Europe', 'es', 'Spain', 'mines', 'Castilla', 38.775064, -4.842927);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cuevas Romanas', '/english/es/subterranea/Romanas.html', 'Romanas', 'Europe', 'es', 'Spain', 'subterranea', 'Andalucia', 37.922277, -4.753035);
