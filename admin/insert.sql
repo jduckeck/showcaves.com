d7cfaca956c0d5a7ba9800b9045de713fad74339
Jochen <jochen@showcaves.com>
2021-10-06 16:58:29 +0200

    deploy

--- admin/insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Golubovitsa Cave', '/english/bg/caves/Golubovitsa.html', 'Golubovitsa', 'Europe', 'bg', 'Bulgaria', 'caves', 'Smolyan', 41.515294, 24.666666);
