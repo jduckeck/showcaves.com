18b851569e43b16db6f26ef110494c79af62aa49
Jochen <jochen@showcaves.com>
2021-10-07 02:06:25 +0200

    complete

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Alexander Nevsky Cathedral Underground Crypt', '/english/bg/subterranea/AlexanderNevskyCathedral.html', 'AlexanderNevskyCathedral', 'Europe', 'bg', 'Bulgaria', 'subterranea', 'SofiaCity', 42.696070, 23.332536);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ermitage Saint Antoine De Galamus', '/english/fr/subterranea/SaintAntoineDeGalamus.html', 'SaintAntoineDeGalamus', 'Europe', 'fr', 'France', 'subterranea', 'Languedoc', 42.837942, 2.481138);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gorge de Galamus', '/english/fr/gorges/Galamus.html', 'Galamus', 'Europe', 'fr', 'France', 'gorges', 'Languedoc', 42.835803, 2.479961);
