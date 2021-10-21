140615575e2db8982091777222b9239d13a009f5
Jochen <jochen@showcaves.com>
2021-10-22 00:02:45 +0200

    Veryovkina

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude, closed)
+VALUES('yes','Veryovkina', '/english/ge/caves/Veryovkina.html', 'Veryovkina', 'Asia', 'ge', 'Georgia', 'caves', 'Abkhazia', 43.415556, 40.356389, true);
