4f983c64977b53d6d9fc905c89a3143b51b8d935
Jochen <jochen@showcaves.com>
2021-11-07 13:05:57 +0100

    Thailand

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Wuyan Cave', '/english/cn/showcaves/Wuyan.html', 'Wuyan', 'Asia', 'cn', 'China', 'showcaves', 'Hunan');
