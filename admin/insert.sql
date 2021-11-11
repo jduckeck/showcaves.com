680cc3f39c5ee0606ad1fa75e003c97e9943ecfc
Jochen <jochen@showcaves.com>
2021-11-11 12:49:39 +0100

    Thailand

--- admin/insert.sql
+++ admin/insert.sql
@@ -14,3 +14,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Tham Krasaeng', '/english/th/caves/Krasaeng.html', 'Krasaeng', 'Asia', 'th', 'Thailand', 'caves', 'Yala', 6.214055, 101.204095);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tham Hup Pa Tat', '/english/th/karst/HupPaTat.html', 'HupPaTat', 'Asia', 'th', 'Thailand', 'karst', 'UthaiThani', 15.377115, 99.630926);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tham Pu Wai', '/english/th/caves/PuWai.html', 'PuWai', 'Asia', 'th', 'Thailand', 'caves', 'UthaiThani', 15.011020, 99.443297);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Wat Khuha Phimuk', '/english/th/subterranea/KhuhaPhimuk.html', 'KhuhaPhimuk', 'Asia', 'th', 'Thailand', 'subterranea', 'Yala', 6.528671, 101.224724);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tham Mae Nang Montho', '/english/th/caves/MaeNangMontho.html', 'MaeNangMontho', 'Asia', 'th', 'Thailand', 'caves', 'Yala', 6.529861, 101.233417);
