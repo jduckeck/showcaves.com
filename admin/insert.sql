f8185e44e7040d8c1b3a30c1274888ab01bfb3b5
Jochen <jochen@showcaves.com>
2021-11-15 15:50:52 +0100

    Thailand

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tham Phet Pho Tong', '/english/th/showcaves/PhetPhoTong.html', 'PhetPhoTong', 'Asia', 'th', 'Thailand', 'showcaves', 'SaKaeo', 13.413651, 102.325346);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tham Nakha', '/english/th/gorges/Nakha.html', 'Nakha', 'Asia', 'th', 'Thailand', 'gorges', 'BuengKan', 17.952288, 104.104522);
