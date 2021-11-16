ab5feef9aec11a45a60d14ec17acfdc36713305c
Jochen <jochen@showcaves.com>
2021-11-16 13:36:01 +0100

    Georgia

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Tham Sam', '/english/th/caves/Sam.html', 'Sam', 'Asia', 'th', 'Thailand', 'caves', 'PhangNga', 8.464286, 98.538665);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tham Phung Chang', '/english/th/showcaves/PhungChang.html', 'PhungChang', 'Asia', 'th', 'Thailand', 'showcaves', 'PhangNga', 8.442720, 98.515730);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Martvili Canyon', '/english/ge/gorges/Martvili.html', 'Martvili', 'Asia', 'ge', 'Georgia', 'gorges', 'Samegrelo', 42.456717, 42.375708);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Okatse Canyon', '/english/ge/gorges/Okatse.html', 'Okatse', 'Asia', 'ge', 'Georgia', 'gorges', 'Imereti', 42.454564, 42.549371);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kaghu Waterfall', '/english/ge/karst/Kaghu.html', 'Kaghu', 'Asia', 'ge', 'Georgia', 'karst', 'Samegrelo', 42.490996, 42.425886);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Motena Cave', '/english/ge/caves/Motena.html', 'Motena', 'Asia', 'ge', 'Georgia', 'caves', 'Samegrelo', 42.476583, 42.391167);
