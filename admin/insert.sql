80e0d59eb22d824d99dbad19fd931deb0c098867
Jochen <jochen@showcaves.com>
2021-11-22 19:01:06 +0100

    Georgia

---
+++ admin/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Chreli Abano', '/english/ge/subterranea/ChreliAbano.html', 'ChreliAbano', 'Asia', 'ge', 'Georgia', 'subterranea', 'Tbilisi', 41.688123, 44.810923);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Karavanserai Bakery', '/english/ge/subterranea/Karavanserai.html', 'Karavanserai', 'Asia', 'ge', 'Georgia', 'subterranea', 'Tbilisi', 41.690906, 44.807402);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Meidan Bazar', '/english/ge/subterranea/MeidanBazar.html', 'MeidanBazar', 'Asia', 'ge', 'Georgia', 'subterranea', 'Tbilisi', 41.689817, 44.809142);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Khareba Winery', '/english/ge/subterranea/Khareba.html', 'Khareba', 'Asia', 'ge', 'Georgia', 'subterranea', 'Kakheti', 41.936009, 45.833435);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Josef Stalin\'s Underground Printing House Museum', '/english/ge/subterranea/JosefStalin.html', 'JosefStalin', 'Asia', 'ge', 'Georgia', 'subterranea', 'Tbilisi', 41.690532, 44.830110);
