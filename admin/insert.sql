88cc580317ebd99843676914102b9bcef0e08378
Jochen <jochen@showcaves.com>
2021-12-02 00:01:29 +0100

    Rong Dong

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yiyuan Jiutian Cave', '/english/cn/showcaves/JiutianYiyuan.html', 'JiutianYiyuan', 'Asia', 'cn', 'China', 'showcaves', 'Shandong', 36.271655, 118.068413);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yiyuan Lingzhi Cave', '/english/cn/showcaves/Lingzhi.html', 'Lingzhi', 'Asia', 'cn', 'China', 'showcaves', 'Shandong', 36.266889, 118.073283);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yiyuan Zhinu Cave', '/english/cn/showcaves/Zhinu.html', 'Zhinu', 'Asia', 'cn', 'China', 'showcaves', 'Shandong');
