3c3dd366b5a9f54b0ae6537adb6e9b71649fe4c1
Jochen <jochen@showcaves.com>
2021-12-06 04:30:25 +0100

    Guatemala

--- admin/insert.sql
+++ admin/insert.sql
@@ -16,3 +16,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Cuevas El Tecolote', '/english/gt/caves/ElTecolote.html', 'ElTecolote', 'MiddleAmerica', 'gt', 'Guatemala', 'caves', 'ElPeten', 16.894576, -90.008148);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Los Nacimientos', '/english/gt/caves/LosNacimientos.html', 'LosNacimientos', 'MiddleAmerica', 'gt', 'Guatemala', 'caves', 'AltaVerapaz', 15.866920, -90.095510);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cuevas de Se\'tzol', '/english/gt/caves/Setzol.html', 'Setzol', 'MiddleAmerica', 'gt', 'Guatemala', 'caves', 'AltaVerapaz', 15.803258, -89.571041);
