1ed92bd539c8794b7270ba925f52b6412a5eb1f2
Jochen <jochen@showcaves.com>
2021-12-09 23:22:52 +0100

    Guatemala

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grotte di Osimo', '/english/it/subterranea/Osimo.html', 'Osimo', 'Europe', 'it', 'Italy', 'subterranea', 'Marche', 43.484947, 13.483728);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Balneario Las Pozas', '/english/gt/karst/LasPozas.html', 'LasPozas', 'MiddleAmerica', 'gt', 'Guatemala', 'karst', 'ElPeten', 16.393261, -89.497708);
