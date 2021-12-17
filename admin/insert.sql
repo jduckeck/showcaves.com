d7cc1cde73077785057bb445776ebe275fc61958
Jochen <jochen@showcaves.com>
2021-12-17 09:53:48 +0100

    Israel

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Harod Spring', '/english/il/springs/Harod.html', 'Harod', 'MiddleEast', 'il', 'Israel', 'springs', 'Northern', 32.549671, 35.356001);
