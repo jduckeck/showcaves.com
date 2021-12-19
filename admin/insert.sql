58c49ae0bce7a1778900d8433abf3fc3ce7f0896
Jochen <jochen@showcaves.com>
2021-12-19 21:37:31 +0100

    Israel

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Harod Spring', '/english/il/springs/Harod.html', 'Harod', 'MiddleEast', 'il', 'Israel', 'springs', 'Northern', 32.549671, 35.356001);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tomb of the Virgin Mary', '/english/il/subterranea/VirginMary.html', 'VirginMary', 'MiddleEast', 'il', 'Israel', 'subterranea', 'Jerusalem', 31.780139, 35.239407);
