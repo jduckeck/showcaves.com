dae9befd6ace51c716b33b61c7c0107e7451f1e1
Jochen <jochen@showcaves.com>
2021-12-20 20:47:51 +0100

    Indonesia

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Harod Spring', '/english/il/springs/Harod.html', 'Harod', 'MiddleEast', 'il', 'Israel', 'springs', 'Northern', 32.549671, 35.356001);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tomb of the Virgin Mary', '/english/il/subterranea/VirginMary.html', 'VirginMary', 'MiddleEast', 'il', 'Israel', 'subterranea', 'Jerusalem', 31.780139, 35.239407);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Rangko Cave', '/english/id/caves/Rangko.html', 'Rangko', 'Asia', 'id', 'Indonesia', 'caves', 'EastNusaTenggara', -8.432676, 119.963984);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Batu Cermin Cave', '/english/id/showcaves/BatuCermin.html', 'BatuCermin', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastNusaTenggara', -8.479371, 119.899196);
