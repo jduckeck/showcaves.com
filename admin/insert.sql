3f09143ae2067f19436c16c08bfe0a182c9f937b
Jochen <jochen@showcaves.com>
2021-12-21 22:36:14 +0100

    Ireland

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Rangko Cave', '/english/id/caves/Rangko.html', 'Rangko', 'Asia', 'id', 'Indonesia', 'caves', 'EastNusaTenggara', -8.432676, 119.963984);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Batu Cermin Cave', '/english/id/showcaves/BatuCermin.html', 'BatuCermin', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastNusaTenggara', -8.479371, 119.899196);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Oweynagat', '/english/ie/subterranea/Oweynagat.html', 'Oweynagat', 'Europe', 'ie', 'Ireland', 'subterranea', 'Roscommon', 53.797296, -8.310578);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cove Cave', '/english/ie/caves/Cove.html', 'Cove', 'Europe', 'ie', 'Ireland', 'caves', 'Down', 54.165380, -5.954077);
