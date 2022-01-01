e2f821b7b6cb12f2f52156fa1e4dcc56fde2dcbe
Jochen <jochen@showcaves.com>
2022-01-01 19:03:40 +0100

    Lago di Andalo

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grottes de Cravanche', '/english/fr/caves/Cravanche.html', 'Cravanche', 'Europe', 'fr', 'France', 'caves', 'FrancheComte', 47.650000,  6.825278);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Serapeum of Saqqara', '/english/eg/subterranea/Saqqara.html', 'Saqqara', 'Africa', 'eg', 'Egypt', 'subterranea', 'Giza', 29.876059, 31.210234);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Lago di Andalo', '/english/it/karst/Andalo.html', 'Andalo', 'Europe', 'it', 'Italy', 'karst', 'Trentino', 46.175279, 11.006403);
