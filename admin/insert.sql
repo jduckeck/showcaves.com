ae20d86a6f24516b84870104372ff256b6c8ff69
Jochen <jochen@showcaves.com>
2022-01-18 00:29:35 +0100

    Abensberg

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Feuersteinbergwerk von Abensberg-Arnhofen', '/english/de/mines/Abensberg.html', 'Abensberg', 'Europe', 'de', 'Germany', 'mines', 'Bayern', 48.814670, 11.845790);
