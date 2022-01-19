0d9a475df46988199608b94ce26834c1204877da
Jochen <jochen@showcaves.com>
2022-01-19 14:33:55 +0100

    fixes

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Feuersteinbergwerk von Abensberg-Arnhofen', '/english/de/mines/Abensberg.html', 'Abensberg', 'Europe', 'de', 'Germany', 'mines', 'Bayern', 48.814670, 11.845790);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Wolf Rock Cave', '/english/usa/caves/WolfRock.html', 'WolfRock', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'la', 30.972277, -93.192258);
