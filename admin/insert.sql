6b4206e7d1fa2699c4b10124777ac389d61398a4
Jochen <jochen@showcaves.com>
2022-01-21 19:45:20 +0100

    Qiliang Dong

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Feuersteinbergwerk von Abensberg-Arnhofen', '/english/de/mines/Abensberg.html', 'Abensberg', 'Europe', 'de', 'Germany', 'mines', 'Bayern', 48.814670, 11.845790);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Wolf Rock Cave', '/english/usa/caves/WolfRock.html', 'WolfRock', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'la', 30.972277, -93.192258);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Qiliang Dong', '/english/cn/showcaves/Qiliang.html', 'Qiliang', 'Asia', 'cn', 'China', 'showcaves', 'Hunan', 27.9895706177, 109.5914535522);
