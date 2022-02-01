70ed9019a6af35ae52071fec2ed44366e96e6229
Jochen <jochen@showcaves.com>
2022-02-01 03:36:36 +0100

    Canyon Creek Ice Cave

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Canyon Creek Ice Cave', '/english/ca/caves/CanyonCreekIce.html', 'CanyonCreekIce', 'NorthAmerica', 'ca', 'Canada', 'caves', 'Alberta', 50.909449, -114.806248);
