133a27d761156da6ea9ef02de22889e30b7c5def
Jochen <jochen@showcaves.com>
2022-02-01 09:41:30 +0100

    Le Trou du Diable

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Canyon Creek Ice Cave', '/english/ca/caves/CanyonCreekIce.html', 'CanyonCreekIce', 'NorthAmerica', 'ca', 'Canada', 'caves', 'Alberta', 50.909449, -114.806248);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Le Trou du Diable', '/english/ca/caves/TrouDuDiable.html', 'TrouDuDiable', 'NorthAmerica', 'ca', 'Canada', 'caves', 'Quebec', 46.676075, -72.094960);
