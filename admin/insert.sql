c59f98b2a761398f7b2ddfda2f39b129a4573289
Jochen <jochen@showcaves.com>
2022-02-03 01:10:25 +0100

    Eramosa Karst

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Le Trou du Diable', '/english/ca/caves/TrouDuDiable.html', 'TrouDuDiable', 'NorthAmerica', 'ca', 'Canada', 'caves', 'Quebec', 46.676075, -72.094960);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Air Defense Operations Center Kindsbach', '/english/de/subterranea/Kindsbach.html', 'Kindsbach', 'Europe', 'de', 'Germany', 'subterranea', 'PfaelzerBergland', 49.415491, 7.617642);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Eramosa Karst', '/english/ca/karst/Eramosa.html', 'Eramosa', 'NorthAmerica', 'ca', 'Canada', 'karst', 'Ontario', 43.187196, -79.811797);
