50663f0b76485ab91c7cf769f0b31f80bb3b46d1
Jochen <jochen@showcaves.com>
2022-02-04 18:36:44 +0100

    Riversleigh

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Riversleigh World Heritage Area', '/english/au/karst/Riversleigh.html', 'Riversleigh', 'Australia', 'au', 'Australia', 'karst', '$Region$', -19.000867, 138.689876);
