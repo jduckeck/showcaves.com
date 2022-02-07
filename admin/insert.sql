c59dc906b76d326c3ca32aaa16678b9926686f37
Jochen <jochen@showcaves.com>
2022-02-07 17:53:32 +0100

    Hatter's Hideout Cave and Lodge

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Riversleigh World Heritage Area', '/english/au/karst/Riversleigh.html', 'Riversleigh', 'Australia', 'au', 'Australia', 'karst', '$Region$', -19.000867, 138.689876);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Mary Kathleen Mine', '/english/au/mines/MaryKathleen.html', 'MaryKathleen', 'Oceania', 'au', 'Australia', 'mines', 'qld', -20.747000, 140.012300);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Hatter\'s Hideout Cave and Lodge', '/english/au/caves/Hatter.html', 'Hatter', 'Australia', 'au', 'Australia', 'caves', 'nsw', -33.497047, 150.268064);
