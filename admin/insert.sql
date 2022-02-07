800045ca7eaea331fb5aef1035732d38196128a4
Jochen <jochen@showcaves.com>
2022-02-07 08:48:14 +0100

    Mary Kathleen Mine

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Riversleigh World Heritage Area', '/english/au/karst/Riversleigh.html', 'Riversleigh', 'Australia', 'au', 'Australia', 'karst', '$Region$', -19.000867, 138.689876);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mary Kathleen Mine', '/english/au/mines/MaryKathleen.html', 'MaryKathleen', 'Oceania', 'au', 'Australia', 'mines', 'qld', -20.747000, 140.012300);
