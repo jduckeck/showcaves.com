8957b375eaf616b3e01aad5263a27e430abe9156
Jochen <jochen@showcaves.com>
2022-02-13 19:07:41 +0100

    Camotes Islands

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Holy Crystal Cave', '/english/ph/showcaves/HolyCrystal.html', 'HolyCrystal', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.671256, 124.320855);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Paraiso Cave', '/english/ph/showcaves/Paraiso.html', 'Paraiso', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.651240, 124.312556);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Heaven Cave', '/english/ph/showcaves/Heaven.html', 'Heaven', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.669203, 124.312708);
