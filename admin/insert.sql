40588d59320e0a146659db92a1e279b73c6e4ca6
Jochen <jochen@showcaves.com>
2022-02-13 19:02:42 +0100

    Camotes Islands

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Zeniarai Benzaiten Ugafuku Shrine', '/english/jp/subterranea/ZeniaraiBenzaitenUgafuku.html', 'ZeniaraiBenzaitenUgafuku', 'Asia', 'jp', 'Japan', 'subterranea', 'Kanagawa', 35.325699, 139.542206);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Amazing Island Cave', '/english/ph/showcaves/AmazingIsland.html', 'AmazingIsland', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.647597, 124.305174);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Holy Crystal Cave', '/english/ph/showcaves/HolyCrystal.html', 'HolyCrystal', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.671256, 124.320855);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Paraiso Cave', '/english/ph/showcaves/Paraiso.html', 'Paraiso', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.651240, 124.312556);
