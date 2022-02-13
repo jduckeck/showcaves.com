9f37a7ed6693bb98777f2107da6ba4b902e56de5
Jochen <jochen@showcaves.com>
2022-02-13 20:04:21 +0100

    deploy

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Paraiso Cave', '/english/ph/showcaves/Paraiso.html', 'Paraiso', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.651240, 124.312556);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Heaven Cave', '/english/ph/showcaves/Heaven.html', 'Heaven', 'Asia', 'ph', 'Philippines', 'showcaves', 'CentralVisayas', 10.669203, 124.312708);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nakabari Limestone Cave', '/english/jp/showcaves/Nakabari.html', 'Nakabari', 'Asia', 'jp', 'Japan', 'showcaves', 'Okinawa', 24.733042, 125.377842);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','KGB Atominis Bunkeris', '/english/other/subterranea/AtominisBunkeris.html', 'AtominisBunkeris', 'Europe', 'lt', 'Lithuania', 'subterranea', 'lt', 'other', 54.913880, 23.842073);
