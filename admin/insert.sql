e6fe1155ffe3caa88a509a42f50f504b8412d3cf
Jochen <jochen@showcaves.com>
2022-02-13 16:06:09 +0100

    Zeniarai Benzaiten Ugafuku Shrine

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Zeniarai Benzaiten Ugafuku Shrine', '/english/jp/subterranea/ZeniaraiBenzaitenUgafuku.html', 'ZeniaraiBenzaitenUgafuku', 'Asia', 'jp', 'Japan', 'subterranea', 'Kanagawa', 35.325699, 139.542206);
