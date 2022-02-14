85385993361e0f9f85ba0db070069ea93aaed123
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2022-02-14 15:25:46 +0100

    The Eysturoy tunnel

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Eysturoyartunnilin', '/english/other/subterranea/Eysturoy.html', 'Eysturoy', 'Europe', 'fo', 'Faroe Islands', 'subterranea', 'fo', 'other', 62.04431314108319, -6.779482153457231);
