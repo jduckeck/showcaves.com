6393b45df40ff1923de258a2ebeb2668523223c4
Jochen <jochen@showcaves.com>
2022-02-14 23:21:58 +0100

    Apuseni

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Peștera Ghețarul de la Vârtop', '/english/ro/showcaves/Vartop.html', 'Vartop', 'Europe', 'ro', 'Romania', 'showcaves', 'Apuseni', 46.540640, 22.780700);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Peștera Coiba Mică', '/english/ro/caves/CoibaMica.html', 'CoibaMica', 'Europe', 'ro', 'Romania', 'caves', 'Apuseni', 46.538140, 22.776700);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Peștera Coiba Mare', '/english/ro/caves/CoibaMare.html', 'CoibaMare', 'Europe', 'ro', 'Romania', 'caves', 'Apuseni', 46.537314, 22.778031);
