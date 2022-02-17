f553ec4efaf8351b7d4078b80a0634d982827f22
Jochen <jochen@showcaves.com>
2022-02-18 00:29:31 +0100

    Indonesia

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,3 +3,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Goa Gajah', '/english/id/subterranea/Gajah.html', 'Gajah', 'Asia', 'id', 'Indonesia', 'subterranea', 'Bali', -8.523438, 115.287157);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Tukad Cepung Waterfall', '/english/id/gorges/TukadCepung.html', 'TukadCepung', 'Asia', 'id', 'Indonesia', 'gorges', 'Bali', -8.441017, 115.386808);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goa Giri Putri Temple', '/english/id/showcaves/GiriPutri.html', 'GiriPutri', 'Asia', 'id', 'Indonesia', 'showcaves', 'Bali', -8.708951, 115.586116);
