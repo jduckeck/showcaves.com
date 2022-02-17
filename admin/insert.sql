25a64c237361c00c42742915e4cfd6f01ec850b1
Jochen <jochen@showcaves.com>
2022-02-17 06:51:30 +0100

    Indonesia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,14 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goa Lowo Trenggalek', '/english/id/showcaves/LowoTrenggalek.html', 'LowoTrenggalek', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -8.211370, 111.728656);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goa Gong', '/english/id/showcaves/Gong.html', 'Gong', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -8.164919, 110.980120);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gua Gelatik', '/english/id/caves/Gelatik.html', 'Gelatik', 'Asia', 'id', 'Indonesia', 'caves', 'Yogyakarta', -7.926937, 110.648079);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goa Kalisuci', '/english/id/caves/Kalisuci.html', 'Kalisuci', 'Asia', 'id', 'Indonesia', 'caves', 'Yogyakarta', -8.011145, 110.638642);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goa Asri', '/english/id/caves/Asri.html', 'Asri', 'Asia', 'id', 'Indonesia', 'caves', 'Yogyakarta', -7.934300, 110.657710);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goa Lawa Purbalingga', '/english/id/showcaves/LawaPurbalingga.html', 'LawaPurbalingga', 'Asia', 'id', 'Indonesia', 'showcaves', 'CentralJava', -7.231198, 109.318634);
 
 
