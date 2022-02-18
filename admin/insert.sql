e5e4e719c02f80c69174b2f5150348196b872214
Jochen <jochen@showcaves.com>
2022-02-18 16:49:13 +0100

    Western Australia

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,3 +5,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Tukad Cepung Waterfall', '/english/id/gorges/TukadCepung.html', 'TukadCepung', 'Asia', 'id', 'Indonesia', 'gorges', 'Bali', -8.441017, 115.386808);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Goa Giri Putri Temple', '/english/id/showcaves/GiriPutri.html', 'GiriPutri', 'Asia', 'id', 'Indonesia', 'showcaves', 'Bali', -8.708951, 115.586116);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Lennard River Gorge', '/english/au/gorges/LennardRiver.html', 'LennardRiver', 'Oceania', 'au', 'Australia', 'gorges', 'wa', -17.179420, 125.201665);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Jingemia Cave', '/english/au/caves/Jingemia.html', 'Jingemia', 'Australia', 'au', 'Australia', 'caves', 'wa', -30.262931, 116.001563);
