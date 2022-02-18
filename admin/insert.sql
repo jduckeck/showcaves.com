9188f252407395ab6d13f6fb0f22e67155a6d45f
Jochen <jochen@showcaves.com>
2022-02-19 00:08:08 +0100

    Western Australia

--- admin/insert.sql
+++ admin/insert.sql
@@ -9,3 +9,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Lennard River Gorge', '/english/au/gorges/LennardRiver.html', 'LennardRiver', 'Oceania', 'au', 'Australia', 'gorges', 'wa', -17.179420, 125.201665);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Jingemia Cave', '/english/au/caves/Jingemia.html', 'Jingemia', 'Australia', 'au', 'Australia', 'caves', 'wa', -30.262931, 116.001563);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mimbi Caves', '/english/au/caves/Mimbi.html', 'Mimbi', 'Australia', 'au', 'Australia', 'caves', 'wa', -18.722743, 126.068636);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Stockyard Gully Cave', '/english/au/caves/StockyardGully.html', 'StockyardGully', 'Australia', 'au', 'Australia', 'caves', 'wa', -29.939651, 115.098570);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cabaret Cave', '/english/au/showcaves/Cabaret.html', 'Cabaret', 'Oceania', 'au', 'Australia', 'showcaves', 'wa', -31.541638, 115.690058);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yonderup Cave', '/english/au/caves/Yonderup.html', 'Yonderup', 'Australia', 'au', 'Australia', 'caves', 'wa', -31.547349, 115.691400);
