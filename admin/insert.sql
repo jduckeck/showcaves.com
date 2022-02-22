e5ddc92477013385c897af9670e1e0e40afaa239
Jochen <jochen@showcaves.com>
2022-02-22 01:38:07 +0100

    Schweiz

---
+++ admin/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Musée Suisse de la Spéléologie', '/english/ch/subterranea/Chamoson.html', 'Chamoson', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Wallis', 46.207962, 7.215400);
+
