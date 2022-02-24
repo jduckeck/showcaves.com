4577e3ec00d4716fe1d846502eb8b5bbc20dcfab
Jochen <jochen@showcaves.com>
2022-02-24 14:37:30 +0100

    Bixby Ice Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,3 +1,7 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Musée Suisse de la Spéléologie', '/english/ch/subterranea/Chamoson.html', 'Chamoson', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Wallis', 46.207962, 7.215400);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bixby Ice Cave', '/english/usa/mines/Bixby.html', 'Bixby', 'NorthAmerica', 'usa', 'United States of America', 'mines', 'ia', 42.674363, -91.399590);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Decorah Ice Cave', '/english/usa/caves/Decorah.html', 'Decorah', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ia', 43.312625, -91.781271);
 
