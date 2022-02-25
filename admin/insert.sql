9900a41a670bc4b9602eef1fbe93729e7dc14cdb
Jochen <jochen@showcaves.com>
2022-02-25 19:43:53 +0100

    Japan

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,4 +6,6 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Decorah Ice Cave', '/english/usa/caves/Decorah.html', 'Decorah', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ia', 43.312625, -91.781271);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Kameiwa Cave', '/english/jp/karst/Kameiwa.html', 'Kameiwa', 'Asia', 'jp', 'Japan', 'karst', 'Chiba', 35.185400, 140.060130);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Enku Bora', '/english/jp/caves/Enku.html', 'Enku', 'Asia', 'jp', 'Japan', 'caves', 'Gifu', 35.6643090, 136.9254238);
 
