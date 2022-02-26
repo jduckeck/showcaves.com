b1c1570fc2c7638d557529bb64269e4ecf517c87
Jochen <jochen@showcaves.com>
2022-02-26 17:34:53 +0100

    Japan

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,4 +8,10 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Kameiwa Cave', '/english/jp/karst/Kameiwa.html', 'Kameiwa', 'Asia', 'jp', 'Japan', 'karst', 'Chiba', 35.185400, 140.060130);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Enku Bora', '/english/jp/caves/Enku.html', 'Enku', 'Asia', 'jp', 'Japan', 'caves', 'Gifu', 35.6643090, 136.9254238);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Iwaya shōnyūdō', '/english/jp/showcaves/Iwaya.html', 'Iwaya', 'Asia', 'jp', 'Japan', 'showcaves', 'Fukuoka', 33.688817, 130.811268);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tagawa City Coal and History Museum', '/english/jp/mines/TagawaCoalMuseum.html', 'TagawaCoalMuseum', 'Asia', 'jp', 'Japan', 'mines', 'Fukuoka', 33.640830, 130.813890);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mitsui Tagawa Mine Headframe', '/english/jp/mines/IdaShaftHeadframe.html', 'MitsuiTagawaMining', 'Asia', 'jp', 'Japan', 'mines', 'Fukuoka', 33.641430, 130.814940);
 
