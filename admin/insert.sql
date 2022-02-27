0643b31b80d192da8f3efc4669f48e5af85f5dbb
Jochen <jochen@showcaves.com>
2022-02-27 13:57:34 +0100

    Japan

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Horayama', '/english/jp/karst/Horayama.html', 'Horayama', 'Asia', 'jp', 'Japan', 'karst', 'Fukuoka',33.909176, 130.658420);
