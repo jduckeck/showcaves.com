88a7636bcaca22a6e51e0c05b25a4ed911197a6c
Jochen <jochen@showcaves.com>
2022-02-27 21:38:41 +0100

    Japan

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Horayama', '/english/jp/karst/Horayama.html', 'Horayama', 'Asia', 'jp', 'Japan', 'karst', 'Fukuoka',33.909176, 130.658420);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Hiraodai Nature Observation Center', '/english/jp/karst/Hiraodai.html', 'Hiraodai', 'Asia', 'jp', 'Japan', 'karst', 'Fukuoka', 33.758960, 130.889880);
