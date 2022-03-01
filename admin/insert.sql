b4d3061fea6cce20835b2015a3483f65401b1983
Jochen <jochen@showcaves.com>
2022-03-01 14:12:05 +0100

    Japan

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Hiraodai Nature Observation Center', '/english/jp/karst/Hiraodai.html', 'Hiraodai', 'Asia', 'jp', 'Japan', 'karst', 'Fukuoka', 33.758960, 130.889880);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Yooki Cave Ruins', '/english/jp/caves/Yooki.html', 'Yooki', 'Asia', 'jp', 'Japan', 'caves', 'Kagoshima', 27.694540, 128.940790);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Fukui Cave', '/english/jp/caves/Fukui.html', 'Fukui', 'Asia', 'jp', 'Japan', 'caves', 'Nagasaki', 33.292435, 129.696769);
