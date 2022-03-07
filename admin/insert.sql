b50a4c67f620d067e156596699de32975d4d8b6a
Jochen <jochen@showcaves.com>
2022-03-07 19:36:09 +0100

    Saruta Cave

---
+++ admin/insert.sql
@@ -0,0 +1,3 @@
+UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), category='caves', Latitude=33.515735, Longitude=133.357614 WHERE countrycode = 'jp' and sortby='Saruta';
+
+
