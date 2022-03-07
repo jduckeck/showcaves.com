5e92a8dbc298d45b48955722edbda1f4d9ab7fdd
Jochen <jochen@showcaves.com>
2022-03-07 21:08:28 +0100

    Ryukeido

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), catego
 UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), category='caves', Latitude=35.492319, Longitude=133.184247 WHERE countrycode = 'jp' and sortby='Yuki';
 
 
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ryukeido', '/english/jp/showcaves/Ryukei.html', 'Ryukei', 'Asia', 'jp', 'Japan', 'showcaves', 'Shimane', 35.497762, 133.175965);
