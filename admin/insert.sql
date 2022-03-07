19d7202d3b0683a7563c9062eb5531ca9d08e825
Jochen <jochen@showcaves.com>
2022-03-07 20:07:52 +0100

    Yuki-do

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,3 +1,4 @@
 UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), category='caves', Latitude=33.515735, Longitude=133.357614 WHERE countrycode = 'jp' and sortby='Saruta';
+UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), category='caves', Latitude=35.492319, Longitude=133.184247 WHERE countrycode = 'jp' and sortby='Yuki';
 
 
