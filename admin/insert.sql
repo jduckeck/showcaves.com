57861cae36dc919874f8e6ccfffd4a6dc277c069
Jochen <jochen@showcaves.com>
2022-03-10 01:16:09 +0100

    Japan

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,5 +1,6 @@
 UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), category='caves', Latitude=33.515735, Longitude=133.357614 WHERE countrycode = 'jp' and sortby='Saruta';
 UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), category='caves', Latitude=35.492319, Longitude=133.184247 WHERE countrycode = 'jp' and sortby='Yuki';
+UPDATE sights SET filename = REPLACE(filename, '/showcaves/', '/caves/'), category='caves', Latitude=31.779592, Longitude=130.961214 WHERE countrycode = 'jp' and sortby='Mizonoguch';
 
 
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
