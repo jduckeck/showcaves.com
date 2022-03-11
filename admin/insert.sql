c24b6b103c6f02366a427f155f0e2d56af57115d
Jochen <jochen@showcaves.com>
2022-03-11 21:20:50 +0100

    Japan

---
+++ admin/insert.sql
@@ -0,0 +1,3 @@
+UPDATE sights SET filename = REPLACE(filename, '/caves/', '/subterranea/'), category='subterranea', Latitude=38.968226, Longitude=141.058270 WHERE countrycode = 'jp' and sortby='Takkoku';
+
+
