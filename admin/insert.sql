97a65d991ccc50dd5fd8e4b07b222458ae87e6e6
Jochen <jochen@showcaves.com>
2022-03-13 23:27:10 +0100

    Unbuki Underwater Cave Entrance

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,3 +1,7 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Onigashima Cave', '/english/jp/subterranea/Onigashima.html', 'Onigashima', 'Asia', 'jp', 'Japan', 'subterranea', 'Kagawa', 34.396907, 134.050483);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tera Cave', '/english/jp/showcaves/Tera.html', 'Tera', 'Asia', 'jp', 'Japan', 'showcaves', 'Okinawa', 26.593454,  127.22436);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Unbuki Underwater Cave Entrance', '/english/jp/springs/Unbuki.html', 'Unbuki', 'Asia', 'jp', 'Japan', 'springs', 'Kagoshima', 27.827175, 128.883380);
 
