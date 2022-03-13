f99604e84ba9422cd8cb001fdc5e64fe98d18444
Jochen <jochen@showcaves.com>
2022-03-13 04:07:51 +0100

    Otate

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,5 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Mt Omuro Lava Caves', '/english/jp/caves/MtOmuro.html', 'MtOmuro', 'Asia', 'jp', 'Japan', 'caves', 'Yamanashi', 35.4525407, 138.6475598);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Suse Jaana Cave', '/english/jp/caves/SuseJaana.html', 'SuseJaana', 'Asia', 'jp', 'Japan', 'caves', 'Aichi', 34.797260, 137.485300);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Otate Cave', '/english/jp/caves/Otate.html', 'Otate', 'Asia', 'jp', 'Japan', 'caves', 'Yamagata', 38.020895, 140.207978);
 
 
