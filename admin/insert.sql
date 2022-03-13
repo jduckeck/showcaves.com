555e6155fa67aa9356d51f1f22ba39e06e137626
Jochen <jochen@showcaves.com>
2022-03-13 20:33:27 +0100

    Onigashima Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,1 +1,3 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Onigashima Cave', '/english/jp/subterranea/Onigashima.html', 'Onigashima', 'Asia', 'jp', 'Japan', 'subterranea', 'Kagawa', 34.396907, 134.050483);
 
