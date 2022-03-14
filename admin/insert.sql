17c3a602fbc75d4bdfec8e342cd0d6c1ab90278a
Jochen <jochen@showcaves.com>
2022-03-14 01:14:21 +0100

    Nobesawa Ginzan

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,4 +4,6 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Tera Cave', '/english/jp/showcaves/Tera.html', 'Tera', 'Asia', 'jp', 'Japan', 'showcaves', 'Okinawa', 26.593454,  127.22436);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Unbuki Underwater Cave Entrance', '/english/jp/springs/Unbuki.html', 'Unbuki', 'Asia', 'jp', 'Japan', 'springs', 'Kagoshima', 27.827175, 128.883380);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nobesawa Ginkodo', '/english/jp/mines/NobesawaGinkodo.html', 'NobesawaGinkodo', 'Asia', 'jp', 'Japan', 'mines', 'Yamagata', 38.566164, 140.532109);
 
