3dcdfebe51decdaa043ad60e838b40ad24daaf09
Jochen <jochen@showcaves.com>
2022-03-14 01:59:59 +0100

    Ufuyaguchi

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,4 +6,6 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Unbuki Underwater Cave Entrance', '/english/jp/springs/Unbuki.html', 'Unbuki', 'Asia', 'jp', 'Japan', 'springs', 'Kagoshima', 27.827175, 128.883380);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Nobesawa Ginkodo', '/english/jp/mines/NobesawaGinkodo.html', 'NobesawaGinkodo', 'Asia', 'jp', 'Japan', 'mines', 'Yamagata', 38.566164, 140.532109);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ufuyaguchi Shonyudo', '/english/jp/caves/Ufuyaguchi.html', 'Ufuyaguchi', 'Asia', 'jp', 'Japan', 'caves', 'Kagoshima', 28.320403, 129.970357);
 
