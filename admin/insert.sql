e0618b3c022a53e84aba097d8f42d1cab4a5e9a3
Jochen <jochen@showcaves.com>
2022-03-15 23:08:29 +0100

    Reigando Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Kiinen Gongen', '/english/jp/caves/Kiinen.html', 'Kiinen', 'Asia', 'jp', 'Japan', 'caves', 'Kagoshima', 27.694138, 128.983357);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Osarizawa Mine', '/english/jp/mines/Osarizawa.html', 'Osarizawa', 'Asia', 'jp', 'Japan', 'mines', 'Akita', 40.185660, 140.746640);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Reigando Cave', '/english/jp/subterranea/Reigan.html', 'Reigan', 'Asia', 'jp', 'Japan', 'subterranea', 'Kumamoto', 32.820150, 130.621117);
