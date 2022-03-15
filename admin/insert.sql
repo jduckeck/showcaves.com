b7cf41dd4bed52012966c1cd72189240c2071001
Jochen <jochen@showcaves.com>
2022-03-15 17:45:59 +0100

    Osarizawa Mine

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Kiinen Gongen', '/english/jp/caves/Kiinen.html', 'Kiinen', 'Asia', 'jp', 'Japan', 'caves', 'Kagoshima', 27.694138, 128.983357);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Osarizawa Mine', '/english/jp/mines/Osarizawa.html', 'Osarizawa', 'Asia', 'jp', 'Japan', 'mines', 'Akita', 40.185660, 140.746640);
