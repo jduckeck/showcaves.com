7ef39d75930503dd887b2944817346bd36a0d275
Jochen <jochen@showcaves.com>
2022-03-15 15:48:45 +0100

    deploy

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kiinen Gongen', '/english/jp/caves/Kiinen.html', 'Kiinen', 'Asia', 'jp', 'Japan', 'caves', 'Kagoshima', 27.694138, 128.983357);
