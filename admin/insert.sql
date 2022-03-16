035fb7857b10aaf90ec6b7841acce56ed118090e
Jochen <jochen@showcaves.com>
2022-03-16 19:44:36 +0100

    Croatia

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Natural History Museum Rijeka', '/english/hr/subterranea/NaturalHistoryMuseumRijeka.html', 'NaturalHistoryMuseumRijeka', 'Europe', 'hr', 'Croatia', 'subterranea', 'GorskiKotar',45.329585, 14.444206);
