e70036d824e1d793684c462054d4df3859c57bcb
Jochen <jochen@showcaves.com>
2022-03-16 12:19:33 +0100

    deploy

--- admin/insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Kiinen Gongen', '/english/jp/caves/Kiinen.html', 'Kiinen', 'Asia', 'jp', 'Japan', 'caves', 'Kagoshima', 27.694138, 128.983357);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Osarizawa Mine', '/english/jp/mines/Osarizawa.html', 'Osarizawa', 'Asia', 'jp', 'Japan', 'mines', 'Akita', 40.185660, 140.746640);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Reigando Cave', '/english/jp/subterranea/Reigan.html', 'Reigan', 'Asia', 'jp', 'Japan', 'subterranea', 'Kumamoto', 32.820150, 130.621117);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Nippon Mining Museum', '/english/jp/mines/Nippon.html', 'Nippon', 'Asia', 'jp', 'Japan', 'mines', 'Ibaraki', 36.631946, 140.600844);
