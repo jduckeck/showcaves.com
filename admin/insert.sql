3cae7691252a2b49ad3b7f57070379d2b389783c
Jochen <jochen@showcaves.com>
2022-03-16 23:07:32 +0100

    Croatia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Natural History Museum Rijeka', '/english/hr/subterranea/NaturalHistoryMuseumRijeka.html', 'NaturalHistoryMuseumRijeka', 'Europe', 'hr', 'Croatia', 'subterranea', 'GorskiKotar',45.329585, 14.444206);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Slunj Rastoke', '/english/hr/karst/SlunjRastoke.html', 'SlunjRastoke', 'Europe', 'hr', 'Croatia', 'karst', 'Karlovac', 45.120386, 15.586274);
