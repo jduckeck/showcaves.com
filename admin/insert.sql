ac83ba33d6837029047e1b2be3f9e41c34a2d518
Jochen <jochen@showcaves.com>
2022-03-17 09:31:09 +0100

    Croatia

--- admin/insert.sql
+++ admin/insert.sql
@@ -10,3 +10,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Oziđana pećina', '/english/hr/showcaves/Ozidana.html', 'Ozidana', 'Europe', 'hr', 'Croatia', 'showcaves', 'SibenikKnin', 43.907565, 15.980196);
 INSERT INTO sights (visible, closed, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes',1, 'Jama u Predolcu', '/english/hr/showcaves/Predolcu.html', 'Predolcu', 'Europe', 'hr', 'Croatia', 'showcaves', 'DubrovnikNeretva', 43.046304, 17.658494);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Špilja Strašna Peć', '/english/hr/showcaves/StrasnaPec.html', 'StrasnaPec', 'Europe', 'hr', 'Croatia', 'showcaves', 'Dalmatia', 44.004693, 15.038583);
