a7e609543c39c80d4377661a38201acb8e117893
Jochen <jochen@showcaves.com>
2022-03-20 23:31:09 +0100

    fix kmz

---
+++ admin/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grutas de Xoxafi', '/english/mx/caves/Xoxafi.html', 'Xoxafi', 'MiddleAmerica', 'mx', 'México', 'caves', 'Hidalgo', 20.388347, -99.027515);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grutas Tzabnah', '/english/mx/karst/Tzabnah.html', 'Tzabnah', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.730530, -89.474560);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cenote X\'batun', '/english/mx/karst/Xbatun.html', 'Xbatun', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.673700, -89.773150);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cenote Dzonbacal', '/english/mx/karst/Dzonbacal.html', 'Dzonbacal', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.669700, -89.779070);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cenote Yax Há', '/english/mx/karst/YaxHa.html', 'YaxHa', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.672830, -89.741610);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','La Gruta Del Duende', '/english/mx/caves/Duende.html', 'Duende', 'MiddleAmerica', 'mx', 'México', 'caves', 'Puebla', 20.009119, -97.512883);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cueva de la Orquidea', '/english/mx/showcaves/Orquidea.html', 'Orquidea', 'MiddleAmerica', 'mx', 'México', 'showcaves', 'VeracruzLlave', 19.545690, -96.916190);
