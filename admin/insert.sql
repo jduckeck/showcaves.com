977f920019a6d4a5ca26537dc6c96731f1182f3e
Jochen <jochen@showcaves.com>
2022-03-23 18:53:16 +0100

    Republic of Tatarstan

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Goluboye Ozero', '/english/ru/springs/GoluboeOzeroKazan.html', 'GoluboeOzeroKazan', 'Asia', 'ru', 'Russia', 'springs', 'Tatarstan', 55.906603, 49.155171);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Yur\'yevskaya Peshchera', '/english/ru/caves/Yuryevskaya.html', 'Yuryevskaya', 'Asia', 'ru', 'Russia', 'caves', 'Tatarstan', 55.232630, 49.233840);
