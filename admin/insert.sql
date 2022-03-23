263d7e853d4081de0a8c4a3d898bfd2753edf2c5
Jochen <jochen@showcaves.com>
2022-03-23 15:01:21 +0100

    Republic of Tatarstan

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goluboye Ozero', '/english/ru/springs/GoluboeOzeroKazan.html', 'GoluboeOzeroKazan', 'Asia', 'ru', 'Russia', 'springs', 'Tatarstan', 55.906603, 49.155171);
