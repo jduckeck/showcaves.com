45398f6208d5d9b6565606cd226c09cb80aad77b
Jochen <jochen@showcaves.com>
2022-03-24 00:15:49 +0100

    Ottensee

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Goluboye Ozero', '/english/ru/springs/GoluboeOzeroKazan.html', 'GoluboeOzeroKazan', 'Asia', 'ru', 'Russia', 'springs', 'Tatarstan', 55.906603, 49.155171);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Yur\'yevskaya Peshchera', '/english/ru/caves/Yuryevskaya.html', 'Yuryevskaya', 'Asia', 'ru', 'Russia', 'caves', 'Tatarstan', 55.232630, 49.233840);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ottensee', '/english/de/karst/Ottensee.html', 'Ottensee', 'Europe', 'de', 'Germany', 'karst', 'Bauland', 49.354867, 9.818770);
