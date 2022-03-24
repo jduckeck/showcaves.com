78dee4191d643b1a13268b2a966f8b3397580890
Jochen <jochen@showcaves.com>
2022-03-24 20:20:09 +0100

    Grotta del Re Tiberio

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Yur\'yevskaya Peshchera', '/english/ru/caves/Yuryevskaya.html', 'Yuryevskaya', 'Asia', 'ru', 'Russia', 'caves', 'Tatarstan', 55.232630, 49.233840);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Ottensee', '/english/de/karst/Ottensee.html', 'Ottensee', 'Europe', 'de', 'Germany', 'karst', 'Bauland', 49.354867, 9.818770);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grotta del Re Tiberio', '/english/it/showcaves/ReTiberio.html', 'ReTiberio', 'Europe', 'it', 'Italy', 'showcaves', 'Emilia-Romagna', 44.256580, 11.664550);
