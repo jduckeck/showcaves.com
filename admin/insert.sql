a9a09873e7a83952742845552049a6535bef93a2
Jochen <jochen@showcaves.com>
2022-03-28 09:15:35 +0200

    Peștera Hoților

--- admin/insert.sql
+++ admin/insert.sql
@@ -28,3 +28,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','El Templo del Flamenco', '/english/es/subterranea/TemploFlamenco.html', 'TemploFlamenco', 'Europe', 'es', 'Spain', 'subterranea', 'Andalucia', 37.183337, -3.598138);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Venta El Gallo', '/english/es/subterranea/Gallo.html', 'Gallo', 'Europe', 'es', 'Spain', 'subterranea', 'Andalucia', 37.182027, -3.583978);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Peștera Hoților', '/english/ro/caves/Hotilor.html', 'Hotilor', 'Europe', 'ro', 'Romania', 'caves', 'Meridionali', 44.896503, 22.428396);
