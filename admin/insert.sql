7426594a4c85bc108c321a6496ee23e9acd15cb0
Jochen <jochen@showcaves.com>
2022-04-03 18:24:05 +0200

    China

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tianzhong Dong', '/english/cn/showcaves/Tianzhong.html', 'Tianzhong', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 25.240308, 107.678781);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Daqikong', '/english/cn/karst/Daqikong.html', 'Yaofeng', 'Asia', 'cn', 'China', 'karst', 'Guizhou', 25.283645, 107.751905);
