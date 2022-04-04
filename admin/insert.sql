804c3bd9bb492348ac31cbf425fcedefacbfb1de
Jochen <jochen@showcaves.com>
2022-04-04 19:37:02 +0200

    Zhongdong

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Tianzhong Dong', '/english/cn/showcaves/Tianzhong.html', 'Tianzhong', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 25.240308, 107.678781);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Daqikong', '/english/cn/karst/Daqikong.html', 'Yaofeng', 'Asia', 'cn', 'China', 'karst', 'Guizhou', 25.283645, 107.751905);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Zhongdong', '/english/cn/showcaves/Zhongdong.html', 'Zhongdong', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 25.687212, 106.293911);
