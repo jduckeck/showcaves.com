4d76a79418b006c0e1f20488290ee91f389fb9fc
Jochen <jochen@showcaves.com>
2022-04-06 20:16:25 +0200

    Carreau Rodolphe

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Zhongdong', '/english/cn/showcaves/Zhongdong.html', 'Zhongdong', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 25.687212, 106.293911);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Mine de Sel Varangéville', '/english/fr/mines/Varangeville.html', 'Varangeville', 'Europe', 'fr', 'France', 'mines', 'Lorraine', 48.637454, 6.308598);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Carreau Rodolphe', '/english/fr/mines/CarreauRodolphe.html', 'CarreauRodolphe', 'Europe', 'fr', 'France', 'mines', 'Alsace', 47.846700, 7.281590);
