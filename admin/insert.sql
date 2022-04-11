d7e8a9f599aed621025d2b7de99972a99cc3ec58
Jochen <jochen@showcaves.com>
2022-04-11 18:03:36 +0200

    Kessler Kellerführungen

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Derlon Hotel Museum Cellar', '/english/nl/subterranea/Derlon.html', 'Derlon', 'Europe', 'nl', 'Netherlands', 'subterranea', 'Limburg', 50.847778, 5.693339);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kessler Kellerführungen', '/english/de/subterranea/Kessler.html', 'Kessler', 'Europe', 'de', 'Germany', 'subterranea', 'KeuperLias', 48.742116, 9.306990);
