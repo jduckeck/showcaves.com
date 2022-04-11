dd9d53a7fba8a24d7f2225c0d052a8babf088a08
Jochen <jochen@showcaves.com>
2022-04-11 16:35:20 +0200

    Museumkelder Derlon

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Derlon Hotel Museum Cellar', '/english/nl/subterranea/Derlon.html', 'Derlon', 'Europe', 'nl', 'Netherlands', 'subterranea', 'Limburg', 50.847778, 5.693339);
