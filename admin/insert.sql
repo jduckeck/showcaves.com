a80ef8a972c09438a2089b81a4430e530807391c
Jochen <jochen@showcaves.com>
2022-04-11 19:50:38 +0200

    Bunkermuseum Zoutelande

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Derlon Hotel Museum Cellar', '/english/nl/subterranea/Derlon.html', 'Derlon', 'Europe', 'nl', 'Netherlands', 'subterranea', 'Limburg', 50.847778, 5.693339);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Kessler Kellerführungen', '/english/de/subterranea/Kessler.html', 'Kessler', 'Europe', 'de', 'Germany', 'subterranea', 'KeuperLias', 48.742116, 9.306990);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bunkermuseum Zoutelande', '/english/nl/subterranea/Zoutelande.html', 'Zoutelande', 'Europe', 'nl', 'Netherlands', 'subterranea', 'Zeeland', 51.497243, 3.492882);
