76247cd6c644f31d896bfc9634d48e6e7fe87b4e
Jochen <jochen@showcaves.com>
2022-04-11 11:56:29 +0200

    Maastunnel

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Maastunnel', '/english/nl/subterranea/Maastunnel.html', 'Maastunnel', 'Europe', 'nl', 'Netherlands', 'subterranea', 'SouthHolland', 51.897480, 4.468090);
