cf7ad08d518aa24354ae3dbbfee2431d4fe09cb4
Jochen <jochen@showcaves.com>
2022-04-13 20:09:13 +0200

    Hungary

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,12 +5,14 @@ VALUES('yes','Kessler Kellerführungen', '/english/de/subterranea/Kessler.html',
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Bunkermuseum Zoutelande', '/english/nl/subterranea/Zoutelande.html', 'Zoutelande', 'Europe', 'nl', 'Netherlands', 'subterranea', 'Zeeland', 51.497243, 3.492882);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Atlantikwall Museum Noordwijk', '/english/nl/subterranea/Noordwijk.html', 'Noordwijk', 'Europe', 'nl', 'Netherlands', 'subterranea', 'SouthHolland', 52.253724, 4.439560);
+VALUES ('yes', 'Atlantikwall Museum Noordwijk', '/english/nl/subterranea/Noordwijk.html', 'Noordwijk', 'Europe', 'nl', 'Netherlands', 'subterranea', 'SouthHolland', 52.253724, 4.439560);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Itäkeskuksen Uimahalli', '/english/fi/subterranea/Itakeskuksen.html', 'Itakeskuksen', 'Europe', 'fi', 'Finland', 'subterranea', 'SouthernFinland', 60.212709, 25.076424);
+VALUES ('yes', 'Itäkeskuksen Uimahalli', '/english/fi/subterranea/Itakeskuksen.html', 'Itakeskuksen', 'Europe', 'fi', 'Finland', 'subterranea', 'SouthernFinland', 60.212709, 25.076424);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Chemin de la Mâture', '/english/fr/gorges/CheminMature.html', 'CheminMature', 'Europe', 'fr', 'France', 'gorges', 'Aquitaine', 42.888313, -0.552449);
+VALUES ('yes', 'Chemin de la Mâture', '/english/fr/gorges/CheminMature.html', 'CheminMature', 'Europe', 'fr', 'France', 'gorges', 'Aquitaine', 42.888313, -0.552449);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Devil\'s Den', '/english/usa/caves/DevilsDenMA.html', 'DevilsDenMA', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ma', 42.260200, -71.447800);
+VALUES ('yes', 'Devil\'s Den', '/english/usa/caves/DevilsDenMA.html', 'DevilsDenMA', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ma', 42.260200, -71.447800);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Millican Daltons Cave', '/english/gb/mines/MillicanDaltons.html', 'MillicanDaltons', 'Europe', 'gb', 'Great Britain', 'mines', 'Cumbria', 54.533780, -3.158250);
+VALUES ('yes', 'Millican Daltons Cave', '/english/gb/mines/MillicanDaltons.html', 'MillicanDaltons', 'Europe', 'gb', 'Great Britain', 'mines', 'Cumbria', 54.533780, -3.158250);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Magyar Olaj- és Gázipari Múzeum', '/english/hu/mines/MagyarOlajipari.html', 'MagyarOlajipari', 'Europe', 'hu', 'Hungary', 'mines', 'Balaton', 46.850387, 16.827509);
