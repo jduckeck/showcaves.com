5acda1b8d5cf8b766d11bf38f6e6b34354ff09d7
Jochen <jochen@showcaves.com>
2022-04-22 19:56:09 +0200

    Grotte à Farinet

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte d\'Orjobet', '/english/fr/caves/Orjobet.html', 'Orjobet', 'Europe', 'fr', 'France', 'caves', 'Rhone', 46.131630, 6.168857);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte à Farinet', '/english/fr/showcaves/Farinet.html', 'Farinet', 'Europe', 'fr', 'France', 'showcaves', 'Rhone', 46.020363, 6.910180);
