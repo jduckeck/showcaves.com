8c64f48144368db3e98472b5d777665c8471db78
Jochen <jochen@showcaves.com>
2022-04-24 02:26:43 +0200

    fixes

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Grotte d\'Orjobet', '/english/fr/caves/Orjobet.html', 'Orjobet', 'Europe', 'fr', 'France', 'caves', 'Rhone', 46.131630, 6.168857);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Grotte à Farinet', '/english/fr/showcaves/Farinet.html', 'Farinet', 'Europe', 'fr', 'France', 'showcaves', 'Rhone', 46.020363, 6.910180);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Well of Souls', '/english/il/caves/WellOfSouls.html', 'WellOfSouls', 'MiddleEast', 'il', 'Israel', 'caves', 'Jerusalem', 31.778010, 35.235337);
