d21d38448db6e9d43aa4d1b9ef3289761a4c6f7e
Jochen <jochen@showcaves.com>
2022-05-03 16:50:56 +0200

    Valencia painted caves

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Museo de la Valltorta', '/english/es/subterranea/Valltorta.html', 'Valltorta', 'Europe', 'es', 'Spain', 'subterranea', 'Valenciana', 40.406116, 0.069788);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cova Remígia', '/english/es/showcaves/Remigia.html', 'Remigia', 'Europe', 'es', 'Spain', 'showcaves', 'Valenciana', 40.418743, -0.122942);
