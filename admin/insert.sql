e5af32f17ce1d2fa7c7728b4393e8892857dd37d
Jochen <jochen@showcaves.com>
2022-05-03 21:09:30 +0200

    Cuevas de la Lluera

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Museo de la Valltorta', '/english/es/subterranea/Valltorta.html', 'Valltorta', 'Europe', 'es', 'Spain', 'subterranea', 'Valenciana', 40.406116, 0.069788);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cova Remígia', '/english/es/showcaves/Remigia.html', 'Remigia', 'Europe', 'es', 'Spain', 'showcaves', 'Valenciana', 40.418743, -0.122942);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Boyue Cave', '/english/cn/showcaves/Boyue.html', 'Boyue', 'Asia', 'cn', 'China', 'showcaves', 'Hunan', 27.706336, 111.451992);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cuevas de la Lluera', '/english/es/showcaves/Lluera.html', 'Lluera', 'Europe', 'es', 'Spain', 'showcaves', 'Asturias', 43.334700, -5.933950);
