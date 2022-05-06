6b837b14a106a6b4a05ffba189e7dee0442945ec
Jochen <jochen@showcaves.com>
2022-05-06 06:10:21 +0200

    Montenegro

--- admin/insert.sql
+++ admin/insert.sql
@@ -10,3 +10,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cueva de la Olla', '/english/mx/caves/Olla.html', 'Olla', 'MiddleAmerica', 'mx', 'México', 'caves', 'Chihuahua', 30.151756, -108.324941);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Barrancas del Cobre', '/english/mx/gorges/Cobre.html', 'Cobre', 'MiddleAmerica', 'mx', 'México', 'gorges', 'Chihuahua', 27.528056, -107.756111);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Cetinjska Pećina', '/english/other/caves/Cetinjska.html', 'Cetinjska', 'Europe', 'me', 'Montenegro', 'caves', 'me', 'other', 42.387683, 18.920974);
