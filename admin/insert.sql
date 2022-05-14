69db05c605c297ce65a241a42b737768e3cc97dc
Jochen <jochen@showcaves.com>
2022-05-14 20:06:40 +0200

    Kanlıdivane

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Akhayat Sinkhole', '/english/tr/karst/Akhayat.html', 'Akhayat', 'Europe', 'tr', 'Turkey', 'karst', 'Mediterranean', 36.448200, 34.010300);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kanlıdivane', '/english/tr/karst/Kanlidivane.html', 'Kanlidivane', 'Europe', 'tr', 'Turkey', 'karst', 'Mediterranean', 36.525311, 34.179620);
