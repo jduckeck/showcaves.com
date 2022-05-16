da736d50a20e32c8333210f89b5a91429b7ef258
Jochen <jochen@showcaves.com>
2022-05-16 18:26:22 +0200

    Wallis

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Kanlıdivane', '/english/tr/karst/Kanlidivane.html', 'Kanlidivane', 'Europe', 'tr', 'Turkey', 'karst', 'Mediterranean', 36.525311, 34.179620);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bhimkund', '/english/in/karst/Bhimkund.html', 'Bhimkund', 'Asia', 'in', 'India', 'karst', 'MadhyaPradesh', 24.438476, 79.376122);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goldmine Gondo', '/english/ch/mines/Gondo.html', 'Gondo', 'Europe', 'ch', 'Switzerland', 'mines', 'Wallis', 46.195895, 8.140047);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Portail de Fully', '/english/ch/karst/Fully.html', 'Fully', 'Europe', 'ch', 'Switzerland', 'karst', 'Wallis', 46.1579433, 7.0887225);
