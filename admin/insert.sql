47129555d138113dfd0566ac6c58b90d26ab039e
Jochen <jochen@showcaves.com>
2022-05-21 22:27:16 +0200

    meta tags und leer

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES('yes','Museo del Oro Precolombino', '/english/other/subterranea/OroPrecolombino.html', 'OroPrecolombino', 'MiddleAmerica', 'cr', 'Costa Rica', 'subterranea', 'cr', 'other', 9.933524, -84.076800);
+INSERT INTO static (path)
+VALUES('/english/fr/topics/LigneMaginot.html');
