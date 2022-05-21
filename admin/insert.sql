925fb5b611f3aa7b2e37242be64fc456f312fe54
Jochen <jochen@showcaves.com>
2022-05-21 18:02:16 +0200

    Hierarchie

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Museo del Oro Precolombino', '/english/other/subterranea/OroPrecolombino.html', 'OroPrecolombino', 'MiddleAmerica', 'cr', 'Costa Rica', 'subterranea', 'cr', 'other', 9.933524, -84.076800);
