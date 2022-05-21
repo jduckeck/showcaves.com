cba42912e3e97610d38ee7da37cb1d55a7fd1894
Jochen <jochen@showcaves.com>
2022-05-22 01:55:05 +0200

    The Caverns

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO static (path)
 VALUES('/english/explain/Topic/Atlantikwall.html');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Muzeul Aurului din Brad', '/english/ro/mines/Aurului.html', 'Aurului', 'Europe', 'ro', 'Romania', 'mines', 'Hunedoara', 46.128931, 22.790963);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','The Caverns', '/english/usa/showcaves/TheCaverns.html', 'TheCaverns', 'NorthAmerica', 'usa', 'United States of America', 'showcaves', 'tn', 35.331693, -85.824965);
