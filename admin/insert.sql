323ed2888eedc7a69d1a6952244b7d971cb5284c
Jochen <jochen@showcaves.com>
2022-05-21 23:30:36 +0200

    Atlantikwall

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Museo del Oro Precolombino', '/english/other/subterranea/OroPrecolombino.html', 'OroPrecolombino', 'MiddleAmerica', 'cr', 'Costa Rica', 'subterranea', 'cr', 'other', 9.933524, -84.076800);
 INSERT INTO static (path)
 VALUES('/english/fr/topics/LigneMaginot.html');
+INSERT INTO static (path)
+VALUES('/german/explain/Topic/Atlantikwall.html');
+INSERT INTO static (path)
+VALUES('/english/explain/Topic/Atlantikwall.html');
