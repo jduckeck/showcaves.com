0fbdc38f4db9682ce141cdce9cd78b72f8cda30a
Jochen <jochen@showcaves.com>
2022-05-22 22:58:26 +0200

    Colombia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,8 +1,2 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Cueva de los Guácharos del Rio Claro', '/english/co/caves/GuacharoRioClara.html', 'GuacharoRioClara', 'SouthAmerica', 'co', 'Colombia', 'caves', 'Antioquia', 5.887323, -74.849823);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','La Cueva del Esplendor', '/english/co/caves/Esplendor.html', 'Esplendor', 'SouthAmerica', 'co', 'Colombia', 'caves', 'Antioquia', 5.598045, -75.814389);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Ventanas de Tisquizoque', '/english/co/caves/Tisquizoque.html', 'Tisquizoque', 'SouthAmerica', 'co', 'Colombia', 'caves', 'Santander', 5.811162, -73.958661);
 
 
