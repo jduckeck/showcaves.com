35b05e20eff39ac1161c7c429064eb215399a667
Jochen <jochen@showcaves.com>
2022-05-22 21:02:10 +0200

    Cueva de los Guácharos del Rio Claro

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cueva de los Guácharos del Rio Claro', '/english/co/caves/GuacharoRioClara.html', 'GuacharoRioClara', 'SouthAmerica', 'co', 'Colombia', 'caves', 'Antioquia', 5.887323, -74.849823);
 
 
