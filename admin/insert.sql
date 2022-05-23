40cdeff37f8cd54c5e476670e558c9ee9eee6bc7
Jochen <jochen@showcaves.com>
2022-05-24 00:05:48 +0200

    Erdöllehrpfad Prottes

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Erdöl-Erdgas Museum Prottes', '/english/at/mines/Prottes.html', 'Prottes', 'Europe', 'at', 'Austria', 'mines', 'Nieder', 48.387303, 16.738588);
 
 
