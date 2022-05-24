c8a2cac1fb69de25ceb21d01390227ae03b5acaa
Jochen <jochen@showcaves.com>
2022-05-24 14:12:00 +0200

    Philippines

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Erdöl-Erdgas Museum Prottes', '/english/at/mines/Prottes.html', 'Prottes', 'Europe', 'at', 'Austria', 'mines', 'Nieder', 48.387303, 16.738588);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Barracuda Lake', '/english/ph/karst/Barracuda.html', 'Barracuda', 'Asia', 'ph', 'The Philippines', 'karst', 'Mimaropa', 11.956272, 120.216807);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Guinogo-an Cave', '/english/ph/caves/Guinogo.html', 'Guinogo', 'Asia', 'ph', 'Philippines', 'caves', 'EasternVisayas', 12.158300, 124.514064);
 
 
