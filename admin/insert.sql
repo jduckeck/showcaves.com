6cf76b4e8fd5a9c069c07470d20aef53e91c1ea6
Jochen <jochen@showcaves.com>
2022-06-01 15:47:52 +0200

    SWHL

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Skansen Przemysłu Naftowego „Magdalena” w Gorlicach', '/english/pl/mines/Gorlice.html', 'Gorlice', 'Europe', 'pl', 'Poland', 'mines', 'Malopolskie', 49.654351, 21.135185);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Prahistoryczne Kopalnie Krzemienia w Krzemionkach', '/english/pl/mines/Krzemionki.html', 'Krzemionki', 'Europe', 'pl', 'Poland', 'mines', 'Swietokrzyskie', 50.972256, 21.490041);
