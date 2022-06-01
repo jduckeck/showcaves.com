5d399e1a0fc75de66686d96ac16fc067011cc56e
Jochen <jochen@showcaves.com>
2022-06-01 21:18:35 +0200

    Muzeum Żup Krakowskich Wieliczka

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Skansen Przemysłu Naftowego „Magdalena” w Gorlicach', '/english/pl/mines/Gorlice.html', 'Gorlice', 'Europe', 'pl', 'Poland', 'mines', 'Malopolskie', 49.654351, 21.135185);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Prahistoryczne Kopalnie Krzemienia w Krzemionkach', '/english/pl/mines/Krzemionki.html', 'Krzemionki', 'Europe', 'pl', 'Poland', 'mines', 'Swietokrzyskie', 50.972256, 21.490041);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Muzeum Żup Krakowskich Wieliczka', '/english/pl/mines/ZupKrakowskich.html', 'ZupKrakowskich', 'Europe', 'pl', 'Poland', 'mines', 'Malopolskie', 49.983934, 20.060091);
