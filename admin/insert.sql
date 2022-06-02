15211e3956197d2c023b3f10d8697c6f4410cd20
Jochen <jochen@showcaves.com>
2022-06-02 14:47:07 +0200

    Sztolnie Kowary

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Prahistoryczne Kopalnie Krzemienia w Krzemionkach', '/english/pl/mines/Krzemionki.html', 'Krzemionki', 'Europe', 'pl', 'Poland', 'mines', 'Swietokrzyskie', 50.972256, 21.490041);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Muzeum Żup Krakowskich Wieliczka', '/english/pl/mines/ZupKrakowskich.html', 'ZupKrakowskich', 'Europe', 'pl', 'Poland', 'mines', 'Malopolskie', 49.983934, 20.060091);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kopalnia Podgórze', '/english/pl/mines/Podgorze.html', 'Podgorze', 'Europe', 'pl', 'Poland', 'mines', 'Dolnoslaskie', 50.759287, 15.840196);
