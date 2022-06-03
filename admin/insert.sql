7e75171103124738920af7d23f556aab19e96dba
Jochen <jochen@showcaves.com>
2022-06-03 11:05:50 +0200

    Underworld Adventures

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Underworld Adventures', '/english/nz/caves/UnderworldAdventures.html', 'UnderworldAdventures', 'Oceania', 'nz', 'New Zealand', 'caves', 'WestCoast', -41.906030, 171.439230);
