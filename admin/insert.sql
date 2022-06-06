553912048b2bf15cf6d614bc5744e79728207d8a
Jochen <jochen@showcaves.com>
2022-06-07 01:54:24 +0200

    tw

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Underworld Adventures', '/english/nz/caves/UnderworldAdventures.html', 'UnderworldAdventures', 'Oceania', 'nz', 'New Zealand', 'caves', 'WestCoast', -41.906030, 171.439230);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Monkey Mine Museum', '/english/cn/mines/Houtong.html', 'Houtong', 'Asia', 'cn', 'China', 'mines', 'Taiwan', 25.086688, 121.827741);
