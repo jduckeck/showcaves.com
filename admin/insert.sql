41fb5d45f9148ba353f019fbf2e410ae7a444f93
Jochen <jochen@showcaves.com>
2022-06-07 19:05:46 +0200

    tw

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,15 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Underworld Adventures', '/english/nz/caves/UnderworldAdventures.html', 'UnderworldAdventures', 'Oceania', 'nz', 'New Zealand', 'caves', 'WestCoast', -41.906030, 171.439230);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Monkey Mine Museum', '/english/cn/mines/Houtong.html', 'Houtong', 'Asia', 'cn', 'China', 'mines', 'Taiwan', 25.086688, 121.827741);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tunnel 88', '/english/cn/subterranea/Tunnel88.html', 'Tunnel88', 'Asia', 'cn', 'China', 'subterranea', 'Taiwan', 26.160305, 119.953247);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Beihai Tunnel', '/english/cn/subterranea/Beihai.html', 'Beihai', 'Asia', 'cn', 'China', 'subterranea', 'Taiwan', 26.142495, 119.928953);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Dahan Stronghold', '/english/cn/subterranea/Dahan.html', 'Dahan', 'Asia', 'cn', 'China', 'subterranea', 'Taiwan', 26.140630, 119.929070);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Qingtian Hall', '/english/cn/subterranea/Qingtian.html', 'Qingtian', 'Asia', 'cn', 'China', 'subterranea', 'Taiwan', 24.453703, 118.406514);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Zhaishan Tunnel', '/english/cn/subterranea/Zhaishan.html', 'Zhaishan', 'Asia', 'cn', 'China', 'subterranea', 'Taiwan', 24.390301, 118.320617);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Taroko Gorge', '/english/cn/gorges/Taroko.html', 'Taroko', 'Asia', 'cn', 'China', 'gorges', 'Taiwan', 24.171662, 121.529175);
