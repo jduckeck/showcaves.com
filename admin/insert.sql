e936d3247181f384e1fb6f3553c5c30fb768d588
Jochen <jochen@showcaves.com>
2022-06-11 20:52:29 +0200

    Besucherbergwerk Wismutstolln

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Besucherbergwerk Wismutstolln', '/english/de/mines/Wismutstolln.html', 'Wismutstolln', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.932780, 13.013500);
