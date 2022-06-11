4c1b2dc39e604a374d8fc0501320c6c4942677dc
Jochen <jochen@showcaves.com>
2022-06-11 23:59:56 +0200

    Besucherbergwerk Wetzsteinstollen

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Besucherbergwerk Wismutstolln', '/english/de/mines/Wismutstolln.html', 'Wismutstolln', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.932780, 13.013500);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Besucherbergwerk Wetzsteinstollen', '/english/de/mines/Wetzsteinstollen.html', 'Wetzsteinstollen', 'Europe', 'de', 'Germany', 'mines', 'KeuperLias', 49.021385, 9.433293);
