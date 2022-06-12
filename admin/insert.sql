ea8e09fe1a555e76c2a940b758e4aab28582f7dd
Jochen <jochen@showcaves.com>
2022-06-12 19:56:38 +0200

    Muzeum Zimnej Wojny Podborsko 3001

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Besucherbergwerk Wismutstolln', '/english/de/mines/Wismutstolln.html', 'Wismutstolln', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.932780, 13.013500);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Besucherbergwerk Wetzsteinstollen', '/english/de/mines/Wetzsteinstollen.html', 'Wetzsteinstollen', 'Europe', 'de', 'Germany', 'mines', 'KeuperLias', 49.021385, 9.433293);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Muzeum Zimnej Wojny Podborsko 3001', '/english/pl/subterranea/Podborsko.html', 'Podborsko', 'Europe', 'pl', 'Poland', 'subterranea', 'Zachodniopomorskie', 53.956314, 16.121541);
