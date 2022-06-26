a2b741f707701df0efce11f5c5fd9d8ccd6cfc20
Jochen <jochen@showcaves.com>
2022-06-26 19:21:09 +0200

    Zeche Hannover

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Förder- und Aussichtsturm Barbaraturm', '/english/de/mines/Barbaraturm.html', 'Barbaraturm', 'Europe', 'de', 'Germany', 'mines', 'Westerwald', 50.721110, 7.822840);
