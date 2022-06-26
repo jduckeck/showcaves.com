8a2453a56c73b3af8cc702be8c4508e13c5d9735
Jochen <jochen@showcaves.com>
2022-06-26 21:49:24 +0200

    Scotland

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Förder- und Aussichtsturm Barbaraturm', '/english/de/mines/Barbaraturm.html', 'Barbaraturm', 'Europe', 'de', 'Germany', 'mines', 'Westerwald', 50.721110, 7.822840);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Sawney Bean\'s Cave', '/english/gb/caves/SawneyBean.html', 'SawneyBean', 'Europe', 'gb', 'Great Britain', 'caves', 'Ayrshire', 55.146100, -4.984500);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Snib\'s Cave', '/english/gb/caves/Snibs.html', 'Snibs', 'Europe', 'gb', 'Great Britain', 'caves', 'Ayrshire', 55.133117, -4.995154);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Massacre Cave', '/english/gb/caves/Massacre.html', 'Massacre', 'Europe', 'gb', 'Great Britain', 'caves', 'Highland', 56.873690, -6.145606);
