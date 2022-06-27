d51e2bea2fb3ade0f0180c635b267e5b48661b80
Jochen <jochen@showcaves.com>
2022-06-27 15:15:36 +0200

    Grube Bindweide

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Snib\'s Cave', '/english/gb/caves/Snibs.html', 'Snibs', 'Europe', 'gb', 'Great Britain', 'caves', 'Ayrshire', 55.133117, -4.995154);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Massacre Cave', '/english/gb/caves/Massacre.html', 'Massacre', 'Europe', 'gb', 'Great Britain', 'caves', 'Highland', 56.873690, -6.145606);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Besucherstollen Grube Edelstein', '/english/de/mines/Edelstein.html', 'Edelstein', 'Europe', 'de', 'Germany', 'mines', 'Westerwald', 50.702619, 7.831312);
