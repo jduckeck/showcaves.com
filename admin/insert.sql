e0962a400961944e50c79e105a5e2f544e3c33ac
Jochen <jochen@showcaves.com>
2022-06-30 01:09:09 +0200

    Besucherbergwerk Ypsilanta

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Massacre Cave', '/english/gb/caves/Massacre.html', 'Massacre', 'Europe', 'gb', 'Great Britain', 'caves', 'Highland', 56.873690, -6.145606);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Besucherstollen Grube Edelstein', '/english/de/mines/Edelstein.html', 'Edelstein', 'Europe', 'de', 'Germany', 'mines', 'Westerwald', 50.702619, 7.831312);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Besucherbergwerk Ypsilanta', '/english/de/mines/Ypsilanta.html', 'Ypsilanta', 'Europe', 'de', 'Germany', 'mines', 'Westerwald', 50.741520, 8.368320);
