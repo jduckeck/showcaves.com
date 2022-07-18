63eca7e13befed2148877057cdb450068930923f
jd <jochen.duckeck@diginuevo.com>
2022-07-18 21:10:07 +0200

    Grotta di Ciccio Felice

--- admin/insert.sql
+++ admin/insert.sql
@@ -28,3 +28,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Hohllay', '/english/lu/subterranea/Hohllay.html', 'Hohllay', 'Europe', 'lu', 'Luxembourg', 'subterranea', 'Grevenmacher', 49.815089, 6.359489);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Tunnel von Bech', '/english/lu/subterranea/Bech.html', 'Bech', 'Europe', 'lu', 'Luxembourg', 'subterranea', 'Grevenmacher', 49.751467, 6.353549);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grotta di Ciccio Felice', '/english/it/caves/CiccioFelice.html', 'CiccioFelice', 'Europe', 'it', 'Italy', 'caves', 'Abruzzo', 41.99250465439154, 13.428239943353828);
