8905aebe08921e7ff13965c7d6ad4e457be969ed
jd <jochen.duckeck@diginuevo.com>
2022-08-03 13:03:50 +0200

    Cook Islands

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tuatini Cave', '/english/ck/caves/Tuatini.html', 'Tuatini', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Mangaia', -21.951614, -157.928513);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Toru a Puru Cave', '/english/ck/caves/ToruAPuru.html', 'ToruAPuru', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Mangaia', -21.914738, -157.901914);
