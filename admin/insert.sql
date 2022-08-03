cddfda4b785ef85b5943ab9296b8465c4b5ad7c0
jd <jochen.duckeck@diginuevo.com>
2022-08-03 18:41:19 +0200

    Cook Islands

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,5 +1,14 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Tuatini Cave', '/english/ck/caves/Tuatini.html', 'Tuatini', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Mangaia', -21.951614, -157.928513);
+VALUES ('yes', 'Tuatini Cave', '/english/ck/caves/Tuatini.html', 'Tuatini', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Mangaia', -21.951614, -157.928513);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Toru a Puru Cave', '/english/ck/caves/ToruAPuru.html', 'ToruAPuru', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Mangaia', -21.914738, -157.901914);
+VALUES ('yes', 'Toru a Puru Cave', '/english/ck/caves/ToruAPuru.html', 'ToruAPuru', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Mangaia', -21.914738, -157.901914);
 Anatakitaki
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Vai Akaruru', '/english/ck/caves/Akaruru.html', 'Akaruru', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Atiu', -19.990103, -158.088632);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Nurau Hohle', '/english/ck/caves/Nurau.html', 'Nurau', 'Oceania', 'ck', 'Cook Islands', 'caves', 'Atiu', -19.9971630767, -158.091725321);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Vai Nauri', '/english/ck/karst/Nauri.html', 'Nauri', 'Oceania', 'ck', 'Cook Islands', 'karst', 'Mitiaro', -19.8647299, -157.6826989);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Vai Tamaroa', '/english/ck/karst/Tamaroa.html', 'Tamaroa', 'Oceania', 'ck', 'Cook Islands', 'karst', 'Mitiaro', -19.868080, -157.681760);
+Marere
