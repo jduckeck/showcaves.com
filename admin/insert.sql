df1071d6313a371fc297321e26c46893978ef0a0
jd <jochen.duckeck@diginuevo.com>
2022-08-05 11:38:49 +0200

    China

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Shui Lian Dong', '/english/cn/caves/ShuiLianHebei.html', 'ShuiLianHebei', 'Asia', 'cn', 'China', 'caves', 'Hebei', 38.204772, 113.757990);
