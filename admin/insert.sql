36a8f508d68effd126f6c0ebbcd02488aadd2723
jd <jochen.duckeck@diginuevo.com>
2022-08-09 12:56:03 +0200

    deploy

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,5 +1,1 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Shui Lian Dong', '/english/cn/caves/ShuiLianHebei.html', 'ShuiLianHebei', 'Asia', 'cn', 'China', 'caves', 'Hebei', 38.204772, 113.757990);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Hang Phuong Hoang', '/english/vn/showcaves/PhuongHoang.html', 'PhuongHoang', 'Asia', 'vn', 'Vietnam', 'showcaves', 'ThaiNguyen', 21.779442, 106.117839);
-VilaVelha.html
+
