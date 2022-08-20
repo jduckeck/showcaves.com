d2595a99b6446efc271162b79f88af1c55fb1542
jd <jochen.duckeck@diginuevo.com>
2022-08-20 21:36:00 +0200

    Église troglodytique Saint-Christophe

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,1 +1,3 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Église troglodytique Saint-Christophe', '/english/fr/subterranea/SaintChristophe.html', 'SaintChristophe', 'Europe', 'fr', 'France', 'subterranea', 'MidiPyrenees', 44.091561, 2.998862);
 
