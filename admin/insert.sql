02cf09b205b7c2b4f55eac05a35c1f04e5db34a9
jd <jochen.duckeck@diginuevo.com>
2022-08-23 12:31:30 +0200

    Ilbenstadt

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,3 +1,5 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Église troglodytique Saint-Christophe', '/english/fr/subterranea/SaintChristophe.html', 'SaintChristophe', 'Europe', 'fr', 'France', 'subterranea', 'MidiPyrenees', 44.091561, 2.998862);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bunker Ilbenstadt', '/english/de/subterranea/Ilbenstadt.html', 'Ilbenstadt', 'Europe', 'de', 'Germany', 'subterranea', 'Oberrhein', 50.272833, 8.808367);
 
