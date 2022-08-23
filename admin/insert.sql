8a162c5f16c50c2beb99963a6e0618a9df614453
jd <jochen.duckeck@diginuevo.com>
2022-08-23 18:06:40 +0200

    Bunker Eichenthal

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,4 +2,6 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Église troglodytique Saint-Christophe', '/english/fr/subterranea/SaintChristophe.html', 'SaintChristophe', 'Europe', 'fr', 'France', 'subterranea', 'MidiPyrenees', 44.091561, 2.998862);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bunker Ilbenstadt', '/english/de/subterranea/Ilbenstadt.html', 'Ilbenstadt', 'Europe', 'de', 'Germany', 'subterranea', 'Oberrhein', 50.272833, 8.808367);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bunker Eichenthal', '/english/de/subterranea/Eichenthal.html', 'Eichenthal', 'Europe', 'de', 'Germany', 'subterranea', 'Norddeutschland', 54.062736, 12.724632);
 
