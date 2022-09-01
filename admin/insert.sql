333a27b53fcaac142fe779ea534804a166899cd7
jd <jochen.duckeck@diginuevo.com>
2022-09-01 11:55:07 +0200

    Cincinnati Museum Center

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,4 +4,6 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Bunker Ilbenstadt', '/english/de/subterranea/Ilbenstadt.html', 'Ilbenstadt', 'Europe', 'de', 'Germany', 'subterranea', 'Oberrhein', 50.272833, 8.808367);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bunker Eichenthal', '/english/de/subterranea/Eichenthal.html', 'Eichenthal', 'Europe', 'de', 'Germany', 'subterranea', 'Norddeutschland', 54.062736, 12.724632);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cincinnati Museum Center', '/english/usa/subterranea/CincinnatiMuseumCenter.html', 'CincinnatiMuseumCenter', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'oh', 39.109932, -84.537275);
 
