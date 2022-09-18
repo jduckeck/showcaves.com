729763edca1aba1774e3fbe80a753395dda05f22
jd <jochen.duckeck@diginuevo.com>
2022-09-18 13:23:21 +0200

    India

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,4 +8,8 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cincinnati Museum Center', '/english/usa/subterranea/CincinnatiMuseumCenter.html', 'CincinnatiMuseumCenter', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'oh', 39.109932, -84.537275);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Chapel Cave', '/english/usa/caves/Chapel.html', 'Chapel', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'oh', 39.467850, -82.558260);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kachargadh Caves', '/english/in/caves/Kachargadh.html', 'Kachargadh', 'Asia', 'in', 'India', 'caves', 'Maharashtra', 21.287352, 80.602354);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Akka Mahadevi Guhalu', '/english/in/caves/AkkaMahadevi.html', 'AkkaMahadevi', 'Asia', 'in', 'India', 'caves', 'AndhraPradesh', 16.156899, 78.835697);
 
