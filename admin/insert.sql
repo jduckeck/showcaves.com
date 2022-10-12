9f23fca1cc6460879aed2ecc3ee0219e41823d84
jd <jochen.duckeck@diginuevo.com>
2022-10-13 00:00:52 +0200

    Cenote Suytun

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bergbau- und Stadtmuseum Weilburg', '/english/de/mines/Weilburg.html', 'Weilburg', 'Europe', 'de', 'Germany', 'mines', 'Taunus', 50.485861, 8.260726);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenote Suytun', '/english/mx/karst/Suytun.html', 'Suytun', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.698882, -88.124564);
