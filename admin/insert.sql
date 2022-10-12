716cb826ac59a8268be25676bf6db45f117dcdd3
jd <jochen.duckeck@diginuevo.com>
2022-10-12 22:29:48 +0200

    Bergbau- und Stadtmuseum Weilburg

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bergbau- und Stadtmuseum Weilburg', '/english/de/mines/Weilburg.html', 'Weilburg', 'Europe', 'de', 'Germany', 'mines', 'Taunus', 50.485861, 8.260726);
