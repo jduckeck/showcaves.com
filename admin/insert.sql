8e50fd90b10f5740030bbccdceeee09035e7cc9f
jd <jochen.duckeck@diginuevo.com>
2022-10-13 18:57:25 +0200

    Sixtus Lodge Glow Worm Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Bergbau- und Stadtmuseum Weilburg', '/english/de/mines/Weilburg.html', 'Weilburg', 'Europe', 'de', 'Germany', 'mines', 'Taunus', 50.485861, 8.260726);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cenote Suytun', '/english/mx/karst/Suytun.html', 'Suytun', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.698882, -88.124564);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Sixtus Lodge Glow Worm Cave', '/english/nz/caves/SixtusLodge.html', 'SixtusLodge', 'Oceania', 'nz', 'New Zealand', 'caves', 'Manawatu', -39.97426153779256, 176.00724860000307);
