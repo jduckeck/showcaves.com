d3427a237cca80cfd8136501abd896b25b6a0749
jd <jochen.duckeck@diginuevo.com>
2022-10-14 14:34:03 +0200

    Baotu Spring

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cenote Suytun', '/english/mx/karst/Suytun.html', 'Suytun', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.698882, -88.124564);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Sixtus Lodge Glow Worm Cave', '/english/nz/caves/SixtusLodge.html', 'SixtusLodge', 'Oceania', 'nz', 'New Zealand', 'caves', 'Manawatu', -39.97426153779256, 176.00724860000307);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Baotu Spring', '/english/cn/springs/Baotu.html', 'Baotu', 'Asia', 'cn', 'China', 'springs', 'Shandong', 36.660538, 117.009401);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Five Dragon Pool', '/english/cn/springs/FiveDragon.html', 'FiveDragon', 'Asia', 'cn', 'China', 'springs', 'Shandong', 36.665160, 117.008589);
