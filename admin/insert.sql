84c0d176a46ab5cea127fcc7fa337dae814aacd6
jd <jochen.duckeck@diginuevo.com>
2022-10-15 13:15:26 +0200

    L'Orrido di Osteno

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Baotu Spring', '/english/cn/springs/Baotu.html', 'Baotu', 'Asia', 'cn', 'China', 'springs', 'Shandong', 36.660538, 117.009401);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Five Dragon Pool', '/english/cn/springs/FiveDragon.html', 'FiveDragon', 'Asia', 'cn', 'China', 'springs', 'Shandong', 36.665160, 117.008589);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'L\'Orrido di Osteno', '/english/it/gorges/Osteno.html', 'Osteno', 'Europe', 'it', 'Italy', 'gorges', 'Lombardia', 46.005563, 9.084802);
