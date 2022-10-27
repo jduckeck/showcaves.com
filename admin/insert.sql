e72a36cdd357a2d80345c75eaf55663de6bab1a0
jd <jochen.duckeck@diginuevo.com>
2022-10-27 14:44:55 +0200

    Gleirschklamm

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Tre Buchi', '/english/ch/subterranea/TreBuchi.html', 'TreBuchi', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Ticino', 45.879359, 8.989779);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gleirschklamm', '/english/at/gorges/Gleirschklamm.html', 'Gleirschklamm', 'Europe', 'at', 'Austria', 'gorges', 'Tirol', 47.374629, 11.308661);
