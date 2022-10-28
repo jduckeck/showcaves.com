d6d7a2ceab5936290434900bfafcc00e9f10e6fe
jd <jochen.duckeck@diginuevo.com>
2022-10-29 00:16:58 +0200

    Mammoth Spring

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Tre Buchi', '/english/ch/subterranea/TreBuchi.html', 'TreBuchi', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Ticino', 45.879359, 8.989779);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Gleirschklamm', '/english/at/gorges/Gleirschklamm.html', 'Gleirschklamm', 'Europe', 'at', 'Austria', 'gorges', 'Tirol', 47.374629, 11.308661);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gihon Spring', '/english/il/springs/Gihon.html', 'Gihon', 'MiddleEast', 'il', 'Israel', 'springs', 'Jerusalem', 31.773792, 35.236965);
