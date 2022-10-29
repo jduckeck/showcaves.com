ccde4b9b0ff159966212ff5736b6cf58bdc432ee
jd <jochen.duckeck@diginuevo.com>
2022-10-29 23:15:14 +0200

    Intermittent Spring

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Gleirschklamm', '/english/at/gorges/Gleirschklamm.html', 'Gleirschklamm', 'Europe', 'at', 'Austria', 'gorges', 'Tirol', 47.374629, 11.308661);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Gihon Spring', '/english/il/springs/Gihon.html', 'Gihon', 'MiddleEast', 'il', 'Israel', 'springs', 'Jerusalem', 31.773792, 35.236965);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ffynnon Leinw', '/english/gb/springs/Leinw.html', 'Leinw', 'Europe', 'gb', 'Great Britain', 'springs', 'Flintshire', 53.20002821, -3.2186363);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Laywell', '/english/gb/springs/Laywell.html', 'Laywell', 'Europe', 'gb', 'Great Britain', 'springs', 'Devon', 50.383987, -3.5246634);
