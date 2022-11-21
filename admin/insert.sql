a5b7558469b05673c8be5e3c345fc300f3f459d1
jd <jochen.duckeck@diginuevo.com>
2022-11-21 14:41:54 +0100

    Catacumbas de Guadalajara

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO static (path)
 VALUES ('/english/explain/Topic/Bat.html');
 INSERT INTO static (path)
 VALUES ('/german/explain/Topic/Bat.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacumbas de Guadalajara', '/english/mx/subterranea/Guadalajara.html', 'Guadalajara', 'MiddleAmerica', 'mx', 'México', 'subterranea', '$Region$', 20.646043, -103.404640);
