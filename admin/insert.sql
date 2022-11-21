7ed0a909a6999a1d183b5664239293aded84cca0
jd <jochen.duckeck@diginuevo.com>
2022-11-21 14:45:12 +0100

    Funtana Chistagna

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,4 +3,6 @@ VALUES ('/english/explain/Topic/Bat.html');
 INSERT INTO static (path)
 VALUES ('/german/explain/Topic/Bat.html');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Catacumbas de Guadalajara', '/english/mx/subterranea/Guadalajara.html', 'Guadalajara', 'MiddleAmerica', 'mx', 'México', 'subterranea', '$Region$', 20.646043, -103.404640);
+VALUES ('yes', 'Catacumbas de Guadalajara', '/english/mx/subterranea/Guadalajara.html', 'Guadalajara', 'MiddleAmerica', 'mx', 'México', 'subterranea', 'Jalisco', 20.646043, -103.404640);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Funtana Chistagna', '/english/ch/springs/FuntanaChistagna.html', 'FuntanaChistagna', 'Europe', 'ch', 'Switzerland', 'springs', 'Graubuenden', 46.8139289, 10.4254275);
