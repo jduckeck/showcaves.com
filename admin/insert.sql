a1c8dea90a67718318457e5a610b97ff3a8bc56f
jd <jochen.duckeck@diginuevo.com>
2022-11-21 20:23:53 +0100

    Le Puits de la Brême

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Catacumbas de Guadalajara', '/english/mx/subterranea/Guadalajara.html', 'Guadalajara', 'MiddleAmerica', 'mx', 'México', 'subterranea', 'Jalisco', 20.646043, -103.404640);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Funtana Chistagna', '/english/ch/springs/FuntanaChistagna.html', 'FuntanaChistagna', 'Europe', 'ch', 'Switzerland', 'springs', 'Graubuenden', 46.8139289, 10.4254275);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Le Puits de la Brême', '/english/fr/karst/PuitsBreme.html', 'PuitsBreme', 'Europe', 'fr', 'France', 'karst', 'Jura', 47.119300, 6.11992);
