f65b0741de74cd352c6944530a523cf9e38cd709
jd <jochen.duckeck@diginuevo.com>
2022-12-15 18:49:19 +0100

    Funtensee

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Funtensee', '/english/de/karst/Funtensee.html', 'Funtensee', 'Europe', 'de', 'Germany', 'karst', 'Alpen', 47.493396, 12.939911);
