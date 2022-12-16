64270b667d39c6be2796e49e446b5a554e8cb120
jd <jochen.duckeck@diginuevo.com>
2022-12-16 20:21:55 +0100

    Wolfsschlucht Porta Westfalica

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Funtensee', '/english/de/karst/Funtensee.html', 'Funtensee', 'Europe', 'de', 'Germany', 'karst', 'Alpen', 47.493396, 12.939911);
+VALUES ('yes', 'Funtensee', '/english/de/karst/Funtensee.html', 'Funtensee', 'Europe', 'de', 'Germany', 'karst', 'Alpen', 47.493396, 12.939911);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Wolfsschlucht Porta Westfalica', '/english/de/mines/WolfsschluchtPortaWestfalica.html', 'WolfsschluchtPortaWestfalica', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 52.244541, 8.896018);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Silberseestollen', '/english/de/mines/Silberseestollen.html', 'Silberseestollen', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 52.217885, 7.970924);
