3f8dd965fc9fdc4df407fb74d9ff1007d657f579
jd <jochen.duckeck@diginuevo.com>
2023-01-06 19:07:29 +0100

    France Regions

---
+++ admin/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Préhistorama Châtelperron', '/english/fr/subterranea/PrehistoramaChatelperron.html', 'PrehistoramaChatelperron', 'Europe', 'fr', 'France', 'subterranea', 'Auvergne', 46.402530, 3.635175);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tunnel de lave 2004', '/english/fr/caves/2004Flow.html', '2004Flow', 'Europe', 'fr', 'France', 'caves', 'Reunion', -21.220401, 55.806887);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tunnel de lave Bleu', '/english/fr/caves/Bleu.html', 'Bleu', 'Europe', 'fr', 'France', 'caves', 'Reunion', -21.236738, 55.508498);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Caverne Gendarme', '/english/fr/caves/Gendarme.html', 'Gendarme', 'Europe', 'fr', 'France', 'caves', 'Reunion', -21.340202, 55.803468);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tunnel du Bassin Bleu', '/english/fr/caves/BassinBleu.html', 'BassinBleu', 'Europe', 'fr', 'France', 'caves', 'Reunion', -21.047600, 55.257021);
