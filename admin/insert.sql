0ccbe0beb03491f2139bafb76592ca66bf22c31e
jd <jochen.duckeck@diginuevo.com>
2023-01-12 01:24:27 +0100

    Grube Bendisberg

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grube Bendisberg', '/english/de/mines/Bendisberg.html', 'Bendisberg', 'Europe', 'de', 'Germany', 'mines', 'Eifel', 50.360700, 7.102500);
