c40284ca2c368b55667806e8a2c1b01488d84e63
jd <jochen.duckeck@diginuevo.com>
2023-01-13 23:24:23 +0100

    Bat Bar

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Grube Bendisberg', '/english/de/mines/Bendisberg.html', 'Bendisberg', 'Europe', 'de', 'Germany', 'mines', 'Eifel', 50.360700, 7.102500);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bat Bar (Lost Canyon Cave)', '/english/usa/showcaves/BatBar.html', 'BatBar', 'NorthAmerica', 'usa', 'United States of America', 'showcaves', 'mo', 36.530649, -93.253027);
