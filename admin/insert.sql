c09c4fed4ee7bcb3ef40736eb39f35f08cb8cfcc
jd <jochen.duckeck@diginuevo.com>
2023-01-17 03:23:46 +0100

    Bulgaria

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Grube Bendisberg', '/english/de/mines/Bendisberg.html', 'Bendisberg', 'Europe', 'de', 'Germany', 'mines', 'Eifel', 50.360700, 7.102500);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bat Bar (Lost Canyon Cave)', '/english/usa/showcaves/BatBar.html', 'BatBar', 'NorthAmerica', 'usa', 'United States of America', 'showcaves', 'mo', 36.530649, -93.253027);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Water Holes Cave', '/english/bg/caves/WaterHoles.html', 'WaterHoles', 'Europe', 'bg', 'Bulgaria', 'caves', 'Lovech', 42.733653, 24.900148);
