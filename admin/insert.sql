c2fb9049c2ae49481db5b24bb51d8155945e820a
jd <jochen.duckeck@diginuevo.com>
2023-01-18 23:15:35 +0100

    Montenegro

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,7 @@ WHERE countrycode = 'me';
 UPDATE sights
 SET filename = REPLACE(filename, '/other/', '/me/')
 WHERE countrycode = 'me';
+INSERT INTO static (path)
+VALUES ('/english/me/topics/Deep.html');
+INSERT INTO static (path)
+VALUES ('/english/me/topics/Long.html');
