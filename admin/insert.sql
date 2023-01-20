40e63d1287d025daea892f572525aee8b99be635
jd <jochen.duckeck@diginuevo.com>
2023-01-20 05:12:08 +0100

    Bosnia and Herzegovina

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Veliki Samograd', '/english/ba/karst/VelikiSamograd.html', 'VelikiSamograd', 'Europe', 'ba', 'Bosnia and Herzegovina', 'karst', 'Federation', 43.662870, 17.185620);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bukovica', '/english/ba/caves/Bukovica.html', 'Bukovica', 'Europe', 'ba', 'Bosnia and Herzegovina', 'caves', 'Federation', 43.6118759, 17.2625470);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kovači', '/english/ba/springs/Kovaci.html', 'Kovaci', 'Europe', 'ba', 'Bosnia and Herzegovina', 'springs', 'Federation', 43.6760331, 17.1864980);
