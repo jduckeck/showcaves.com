f4890ecf5b6b23963bf6efc5105d4b1d0e90b485
jd <jochen.duckeck@diginuevo.com>
2023-01-21 00:02:26 +0100

    Postojna

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,6 +1,10 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Veliki Samograd', '/english/ba/karst/VelikiSamograd.html', 'VelikiSamograd', 'Europe', 'ba', 'Bosnia and Herzegovina', 'karst', 'Federation', 43.662870, 17.185620);
+VALUES ('yes', 'Veliki Samograd', '/english/ba/karst/VelikiSamograd.html', 'VelikiSamograd', 'Europe', 'ba', 'Bosnia and Herzegovina', 'karst', 'Federation', 43.662870, 17.185620);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Bukovica', '/english/ba/caves/Bukovica.html', 'Bukovica', 'Europe', 'ba', 'Bosnia and Herzegovina', 'caves', 'Federation', 43.6118759, 17.2625470);
+VALUES ('yes', 'Bukovica', '/english/ba/caves/Bukovica.html', 'Bukovica', 'Europe', 'ba', 'Bosnia and Herzegovina', 'caves', 'Federation', 43.6118759, 17.2625470);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Kovači', '/english/ba/springs/Kovaci.html', 'Kovaci', 'Europe', 'ba', 'Bosnia and Herzegovina', 'springs', 'Federation', 43.6760331, 17.1864980);
+VALUES ('yes', 'Kovači', '/english/ba/springs/Kovaci.html', 'Kovaci', 'Europe', 'ba', 'Bosnia and Herzegovina', 'springs', 'Federation', 43.6760331, 17.1864980);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Unška koliševka', '/english/si/karst/Unec.html', 'Unec', 'Europe', 'si', 'Slovenia', 'karst', 'Notranjska', 45.815450, 14.265500);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'The Secret Rooms of Hotel Jama', '/english/si/subterranea/HotelJama.html', 'HotelJama', 'Europe', 'si', 'Slovenia', 'subterranea', 'Notranjska', 45.781236, 14.205748);
