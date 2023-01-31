833cafc47e33349fdf750cfed50f41c0eac24ddf
jd <jochen.duckeck@diginuevo.com>
2023-02-01 00:15:05 +0100

    Sentier Karstique Font d’Urle

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Sentier Karstique Font d’Urle', '/english/fr/karst/FontDUrle.html', 'FontDUrle', 'Europe', 'fr', 'France', 'karst', 'Vercors', 44.897738, 5.320510);
