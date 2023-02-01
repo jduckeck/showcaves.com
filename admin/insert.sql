901ab2d937647e5e247e45015b7223aea3f9019f
jd <jochen.duckeck@diginuevo.com>
2023-02-01 03:04:04 +0100

    Sentier karstique

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Sentier Karstique Font d’Urle', '/english/fr/karst/FontDUrle.html', 'FontDUrle', 'Europe', 'fr', 'France', 'karst', 'Vercors', 44.897738, 5.320510);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Circuit de la Vallée Sèche de Calmoutier', '/english/fr/karst/Calmoutier.html', 'Calmoutier', 'Europe', 'fr', 'France', 'karst', 'FrancheComte', 47.628231, 6.290910);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Sentier karstique ASHVS d\'Arbecey', '/english/fr/karst/Arbecey.html', 'Arbecey', 'Europe', 'fr', 'France', 'karst', 'FrancheComte', 47.734284, 5.919760);
