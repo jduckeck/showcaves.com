2b4bd56321e3338ad098a4d3f471acfa4602144d
jd <jochen.duckeck@diginuevo.com>
2023-02-02 03:21:32 +0100

    Java

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Circuit de la Vallée Sèche de Calmoutier', '/english/fr/karst/Calmoutier.html', 'Calmoutier', 'Europe', 'fr', 'France', 'karst', 'FrancheComte', 47.628231, 6.290910);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Sentier karstique ASHVS d\'Arbecey', '/english/fr/karst/Arbecey.html', 'Arbecey', 'Europe', 'fr', 'France', 'karst', 'FrancheComte', 47.734284, 5.919760);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Maharani Zoo dan Goa', '/english/id/showcaves/Maharani.html', 'Maharani', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -6.868466, 112.361999);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Goa Akbar', '/english/id/showcaves/Akbar.html', 'Akbar', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -6.905420, 112.060555);
