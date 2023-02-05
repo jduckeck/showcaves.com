70f8a33cf475f0400a974bface84537f911ad400
jd <jochen.duckeck@diginuevo.com>
2023-02-05 21:24:59 +0100

    Horreum de Narbonne

--- admin/insert.sql
+++ admin/insert.sql
@@ -7,12 +7,14 @@ VALUES('yes','Sentier karstique ASHVS d\'Arbecey', '/english/fr/karst/Arbecey.ht
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Maharani Zoo dan Goa', '/english/id/showcaves/Maharani.html', 'Maharani', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -6.868466, 112.361999);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Goa Akbar', '/english/id/showcaves/Akbar.html', 'Akbar', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -6.905420, 112.060555);
+VALUES ('yes', 'Goa Akbar', '/english/id/showcaves/Akbar.html', 'Akbar', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -6.905420, 112.060555);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Source de l\'Ain', '/english/fr/springs/Ain.html', 'Ain', 'Europe', 'fr', 'France', 'springs', 'Jura', 46.749634, 6.023257);
+VALUES ('yes', 'Source de l\'Ain', '/english/fr/springs/Ain.html', 'Ain', 'Europe', 'fr', 'France', 'springs', 'Jura', 46.749634, 6.023257);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Pertes de l\'Ain', '/english/fr/gorges/PertesAin.html', 'PertesAin', 'Europe', 'fr', 'France', 'gorges', 'Jura', 46.724638,   5.956160);
+VALUES ('yes', 'Pertes de l\'Ain', '/english/fr/gorges/PertesAin.html', 'PertesAin', 'Europe', 'fr', 'France', 'gorges', 'Jura', 46.724638, 5.956160);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Champagne Alfred Gratien', '/english/fr/subterranea/Gratien.html', 'Gratien', 'Europe', 'fr', 'France', 'subterranea', 'Champagne', 49.038869, 3.961058);
+VALUES ('yes', 'Champagne Alfred Gratien', '/english/fr/subterranea/Gratien.html', 'Gratien', 'Europe', 'fr', 'France', 'subterranea', 'Champagne', 49.038869, 3.961058);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Fort de la Grande Haye', '/english/fr/subterranea/GrandeHaye.html', 'GrandeHaye', 'Europe', 'fr', 'France', 'subterranea', 'Lorraine', 48.206653, 6.415436);
+VALUES ('yes', 'Fort de la Grande Haye', '/english/fr/subterranea/GrandeHaye.html', 'GrandeHaye', 'Europe', 'fr', 'France', 'subterranea', 'Lorraine', 48.206653, 6.415436);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Horreum de Narbonne', '/english/fr/subterranea/Narbonne.html', 'Narbonne', 'Europe', 'fr', 'France', 'subterranea', 'Languedoc', 43.185570, 3.004670);
