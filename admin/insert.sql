a725f91021aa409181524915b7dcf3de494a7f9d
jd <jochen.duckeck@diginuevo.com>
2023-02-07 23:09:25 +0100

    Chaos de Montpellier-le-Vieux

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Chaos de Montpellier-le-Vieux', '/english/fr/karst/MontpellierLeVieux.html', 'MontpellierLeVieux', 'Europe', 'fr', 'France', 'karst', 'GrandsCausses', 44.137829, 3.203223);
