2e8610677111ff33cfe9d817c21c7b2bdb3a79d6
jd <jochen.duckeck@diginuevo.com>
2023-02-08 01:54:47 +0100

    Chaos de Nîmes-le-Vieux

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Chaos de Montpellier-le-Vieux', '/english/fr/karst/MontpellierLeVieux.html', 'MontpellierLeVieux', 'Europe', 'fr', 'France', 'karst', 'GrandsCausses', 44.137829, 3.203223);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Chaos de Nîmes-le-Vieux', '/english/fr/karst/NimesLeVieux.html', 'NimesLeVieux', 'Europe', 'fr', 'France', 'karst', 'Cevennes', 44.225326, 3.511331);
