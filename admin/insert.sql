ac38fce3f6fb10ad37254a8c3c6eaaa4a7b3f822
jd <jochen.duckeck@diginuevo.com>
2023-02-09 12:30:55 +0100

    La Grotte de Glace

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,13 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Chaos de Montpellier-le-Vieux', '/english/fr/karst/MontpellierLeVieux.html', 'MontpellierLeVieux', 'Europe', 'fr', 'France', 'karst', 'GrandsCausses', 44.137829, 3.203223);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Chaos de Nîmes-le-Vieux', '/english/fr/karst/NimesLeVieux.html', 'NimesLeVieux', 'Europe', 'fr', 'France', 'karst', 'Cevennes', 44.225326, 3.511331);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','La Grotte de Glace Valloire', '/english/fr/subterranea/Valloire.html', 'Valloire', 'Europe', 'fr', 'France', 'subterranea', 'Rhone', 45.161978, 6.452876);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','La Grotte de Glace Les Arcs', '/english/fr/subterranea/LesArcs.html', 'LesArcs', 'Europe', 'fr', 'France', 'subterranea', 'Rhone', 45.549822,  6.809687);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','La Grotte de Glace Alpe d’Huez', '/english/fr/subterranea/AlpeDHuez.html', 'AlpeDHuez', 'Europe', 'fr', 'France', 'subterranea', 'Rhone', 45.118773,  6.103756);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','La Grotte de Glace La Grave La Meije', '/english/fr/subterranea/LaGrave.html', 'LaGrave', 'Europe', 'fr', 'France', 'subterranea', 'PACA', 45.007780, 6.264330);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','La Grotte de Glace Les 2 Alpes', '/english/fr/subterranea/LesDeuxAlpes.html', 'LesDeuxAlpes', 'Europe', 'fr', 'France', 'subterranea', 'Rhone', 44.9973390, 6.2059965);
