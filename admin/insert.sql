4006ee19f3dc6a3d368e605857966e221480c2fb
jd <jochen.duckeck@diginuevo.com>
2023-02-14 00:20:27 +0100

    kr

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Tham Nam Khao Siva', '/english/th/caves/NamKhaoSiva.html', 'NamKhaoSiva', 'Asia', 'th', 'Thailand', 'caves', 'SaKaeo', 13.320877, 102.327817);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Tham Phet', '/english/th/caves/Phet.html', 'Phet', 'Asia', 'th', 'Thailand', 'caves', 'Krabi', 8.012537, 98.841405);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', '4th Infiltration Tunnel', '/english/kr/subterranea/Infiltration4.html', 'Infiltration4', 'Asia', 'kr', 'South Korea', 'subterranea', 'Gangwon', 38.319507, 128.110524);
