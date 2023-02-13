26dcde4ad1e41a087b25573b987d6df7f6250769
jd <jochen.duckeck@diginuevo.com>
2023-02-13 14:27:28 +0100

    Submarine Tunnel

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Gouffre de Saint-Sauveur', '/english/fr/springs/SaintSauveur.html', 'SaintSauveur', 'Europe', 'fr', 'France', 'springs', 'Lot', 44.790330, 1.568859);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Paraje de la Morería', '/english/es/caves/Moreria.html', 'Moreria', 'Europe', 'es', 'Spain', 'caves', 'Valenciana', 40.304017, 0.122490);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Submarine Tunnel', '/english/me/subterranea/Submarine.html', 'Submarine', 'Europe', 'me', 'Montenegro', 'subterranea', NULL, 42.427670, 18.590030);
