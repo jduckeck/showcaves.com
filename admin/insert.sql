c98ca255a454f4cbf9403a72c4e515db3692f0df
jd <jochen.duckeck@diginuevo.com>
2023-02-13 12:50:55 +0100

    Paraje de la Morería

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Pertes de Thémines', '/english/fr/karst/Themines.html', 'Themines', 'Europe', 'fr', 'France', 'karst', 'Lot', 44.738654, 1.829566);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Gouffre de Saint-Sauveur', '/english/fr/springs/SaintSauveur.html', 'SaintSauveur', 'Europe', 'fr', 'France', 'springs', 'Lot', 44.790330, 1.568859);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Paraje de la Morería', '/english/es/caves/Moreria.html', 'Moreria', 'Europe', 'es', 'Spain', 'caves', 'Valenciana', 40.304017, 0.122490);
