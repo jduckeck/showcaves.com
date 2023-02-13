8b51305ca6a29f9b55615baac0d277741078dd0c
jd <jochen.duckeck@diginuevo.com>
2023-02-13 01:49:02 +0100

    Gouffre de Saint-Sauveur

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Pertes de Thémines', '/english/fr/karst/Themines.html', 'Themines', 'Europe', 'fr', 'France', 'karst', 'Lot', 44.738654, 1.829566);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gouffre de Saint-Sauveur', '/english/fr/springs/SaintSauveur.html', 'SaintSauveur', 'Europe', 'fr', 'France', 'springs', 'Lot', 44.790330, 1.568859);
