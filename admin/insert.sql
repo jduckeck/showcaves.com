6e44279f8d896ee1058ace70f1470a0981e89b09
jd <jochen.duckeck@diginuevo.com>
2023-02-13 18:33:34 +0100

    Thailand

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kaew Cave', '/english/th/caves/Kaew.html', 'Kaew', 'Asia', 'th', 'Thailand', 'caves', 'Prachuab', 12.203723, 99.992055);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tham Sai', '/english/th/caves/Sai.html', 'Sai', 'Asia', 'th', 'Thailand', 'caves', 'Prachuab', 12.179984, 100.007459);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tham Khao Mai Khew', '/english/th/caves/KhaoMaiKhew.html', 'KhaoMaiKhew', 'Asia', 'th', 'Thailand', 'caves', 'Krabi', 7.536360, 99.071574);
