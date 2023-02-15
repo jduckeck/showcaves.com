c9c1f9eaaf487cda241815a6ad1924b38d8923d5
jd <jochen.duckeck@diginuevo.com>
2023-02-15 17:41:15 +0100

    Kuhfluchtwasserfälle

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kuhfluchtwasserfälle', '/english/de/springs/Kuhflucht.html', 'Kuhflucht', 'Europe', 'de', 'Germany', 'springs', 'Alpen', 47.527070, 11.137330);
