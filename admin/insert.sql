e235074c4ce0c9dbc6bc2c2360d3bdb461f72d4d
jd <jochen.duckeck@diginuevo.com>
2023-02-15 14:54:32 +0100

    misc

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gruta do Lapão', '/english/br/caves/LapaoSantaLuzia.html', 'LapaoSantaLuzia', 'SouthAmerica', 'br', 'Brazil', 'caves', 'Bahia', -15.446105, -39.357190);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte Chapelle Notre-Dame De Remonot', '/english/fr/caves/Remonot.html', 'Remonot', 'Europe', 'fr', 'France', 'caves', 'Jura', 47.035400, 6.512350);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Lileiko Mati', '/english/gr/springs/LileikoMati.html', 'LileikoMati', 'Europe', 'gr', 'Greece', 'springs', 'Peloponnese', 37.455432, 22.751265);
