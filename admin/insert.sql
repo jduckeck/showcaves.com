7ee86e1a3d334662738d4dda0938cc0ce09bda58
jd <jochen.duckeck@diginuevo.com>
2023-02-16 04:27:35 +0100

    Centro di Documentazione Miniera di Ribolla

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Kuhfluchtwasserfälle', '/english/de/springs/Kuhflucht.html', 'Kuhflucht', 'Europe', 'de', 'Germany', 'springs', 'Alpen', 47.527070, 11.137330);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Sinterterrassen von Weißenbrunn', '/english/de/karst/Weissenbrunn.html', 'Weissenbrunn', 'Europe', 'de', 'Germany', 'karst', 'Bayern', 50.3558629, 11.0061841);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Centro di Documentazione Miniera di Ribolla', '/english/it/mines/Ribolla.html', 'Ribolla', 'Europe', 'it', 'Italy', 'mines', 'Toscana', 42.969562, 11.038068);
