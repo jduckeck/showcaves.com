b40c9646b349d75cc342ec4895a6e072ac0da4e1
jd <jochen.duckeck@diginuevo.com>
2023-02-16 01:13:06 +0100

    Sinterterrassen von Weißenbrunn

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Kuhfluchtwasserfälle', '/english/de/springs/Kuhflucht.html', 'Kuhflucht', 'Europe', 'de', 'Germany', 'springs', 'Alpen', 47.527070, 11.137330);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Sinterterrassen von Weißenbrunn', '/english/de/karst/Weissenbrunn.html', 'Weissenbrunn', 'Europe', 'de', 'Germany', 'karst', 'Bayern', 50.3558629, 11.0061841);
