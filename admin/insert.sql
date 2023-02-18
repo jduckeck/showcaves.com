617143cd546c4dc4d91222b60d0f72aeffb19f1e
jd <jochen.duckeck@diginuevo.com>
2023-02-18 15:55:18 +0100

    Grotte di Luppa

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Sinterterrassen von Weißenbrunn', '/english/de/karst/Weissenbrunn.html', 'Weissenbrunn', 'Europe', 'de', 'Germany', 'karst', 'Bayern', 50.3558629, 11.0061841);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Centro di Documentazione Miniera di Ribolla', '/english/it/mines/Ribolla.html', 'Ribolla', 'Europe', 'it', 'Italy', 'mines', 'Toscana', 42.969562, 11.038068);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte di Luppa', '/english/it/caves/Luppa.html', 'Luppa', 'Europe', 'it', 'Italy', 'caves', 'Abruzzo', 42.120812, 13.157237);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Riserva Naturale Grotte di Pietrasecca', '/english/it/karst/Pietrasecca.html', 'Pietrasecca', 'Europe', 'it', 'Italy', 'karst', 'Abruzzo', 42.137232, 13.128676);
