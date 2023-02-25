69a74c45b20a889b1c28a8afa08abccfef0842b0
jd <jochen.duckeck@diginuevo.com>
2023-02-25 19:21:33 +0100

    Vereinsbergwerk Grüner Zweig

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Vereinsbergwerk Grüner Zweig', '/english/de/mines/GruenerZweig.html', 'GruenerZweig', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.557623, 12.931477);
