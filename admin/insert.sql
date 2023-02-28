3abf5528b37306912c0cb7f7410812ac49316443
jd <jochen.duckeck@diginuevo.com>
2023-02-28 13:55:26 +0100

    Nevada State Museum Silver Mine Exhibit

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Vereinsbergwerk Grüner Zweig', '/english/de/mines/GruenerZweig.html', 'GruenerZweig', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.557623, 12.931477);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Replica Underground Coal Mine Experience', '/english/au/mines/Collie.html', 'Collie', 'Oceania', 'au', 'Australia', 'mines', 'wa', -33.358605, 116.149283);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Nevada State Museum Silver Mine Exhibit', '/english/usa/mines/NevadaStateMuseum.html', 'NevadaStateMuseum', 'NorthAmerica', 'us', 'United States of America', 'mines', 'nv', 39.167890, -119.767440);
