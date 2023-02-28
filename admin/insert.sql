71773b5198236732a2c08e7b666b9d083640d00e
jd <jochen.duckeck@diginuevo.com>
2023-02-28 12:03:35 +0100

    Replica Underground Coal Mine Experience

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Vereinsbergwerk Grüner Zweig', '/english/de/mines/GruenerZweig.html', 'GruenerZweig', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.557623, 12.931477);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Replica Underground Coal Mine Experience', '/english/au/mines/Collie.html', 'Collie', 'Oceania', 'au', 'Australia', 'mines', 'wa', -33.358605, 116.149283);
