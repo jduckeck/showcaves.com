3ba5ade5c218736d9a5b4e9515513d02681230a9
jd <jochen.duckeck@diginuevo.com>
2023-03-01 13:17:32 +0100

    Keuper Lias Land

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Weißenstädter Kellerwelt', '/english/de/subterranea/Weissenstadt.html', 'Weissenstadt', 'Europe', 'de', 'Germany', 'subterranea', 'Fichtelgebirge', 50.097244, 11.880918);
