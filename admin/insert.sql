7ad5dbb365bfaa29fcdcd14f0f68240a29988711
jd <jochen.duckeck@diginuevo.com>
2023-03-01 15:11:35 +0100

    deploy

--- admin/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Weißenstädter Kellerwelt', '/english/de/subterranea/Weissenstadt.html', 'Weissenstadt', 'Europe', 'de', 'Germany', 'subterranea', 'Fichtelgebirge', 50.097244, 11.880918);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Bergwerk Werra', '/english/de/mines/Werra.html', 'Werra', 'Europe', 'de', 'Germany', 'mines', 'Fichtelgebirge', 50.089459, 11.883730);
