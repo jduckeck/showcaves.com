2d28fa4e88403d78658caf169b45ca06600e5b11
jd <jochen.duckeck@diginuevo.com>
2023-03-02 19:45:43 +0100

    fixes, updates

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gold Dredge 8', '/english/usa/mines/GoldDredge8.html', 'GoldDredge8', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 64.938380, -147.657844);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'AJ Mine Gastineau Mill Tour', '/english/usa/mines/AJ.html', 'AJ', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 58.265920, -134.333490);
