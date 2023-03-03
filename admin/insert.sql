5119bf40a78f3a9f6c494150142279b2149399f8
jd <jochen.duckeck@diginuevo.com>
2023-03-03 19:32:04 +0100

    Gruta de São Thomé

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Gold Dredge 8', '/english/usa/mines/GoldDredge8.html', 'GoldDredge8', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 64.938380, -147.657844);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'AJ Mine Gastineau Mill Tour', '/english/usa/mines/AJ.html', 'AJ', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 58.265920, -134.333490);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gruta de São Thomé', '/english/br/showcaves/SaoThome.html', 'SaoThome', 'SouthAmerica', 'br', 'Brazil', 'showcaves', 'MinasGerais', -21.722986, -44.985621);
