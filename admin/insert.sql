df9787f3c860177fdf8e8d4451a3bbdf05c94b95
jd <jochen.duckeck@diginuevo.com>
2023-03-04 23:55:21 +0100

    Helvetisches Goldmuseum

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'AJ Mine Gastineau Mill Tour', '/english/usa/mines/AJ.html', 'AJ', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 58.265920, -134.333490);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Gruta de São Thomé', '/english/br/showcaves/SaoThome.html', 'SaoThome', 'SouthAmerica', 'br', 'Brazil', 'showcaves', 'MinasGerais', -21.722986, -44.985621);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Helvetisches Goldmuseum', '/english/ch/mines/HelvetischesGoldmuseum.html', 'HelvetischesGoldmuseum', 'Europe', 'ch', 'Switzerland', 'mines', 'Bern', 47.054915, 7.629253);
