2d958f028ba7cd6547dbd5fc34410736340d63d1
jd <jochen.duckeck@diginuevo.com>
2023-03-08 18:06:50 +0100

    Centro de Interpretación de Risco Caído

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cuevas de Acusa Seca', '/english/es/subterranea/AcusaSeca.html', 'AcusaSeca', 'Europe', 'es', 'Spain', 'subterranea', 'GranCanaria', 28.006722, -15.679729);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Centro de Interpretación de Risco Caído', '/english/es/subterranea/RiscoCaido.html', 'RiscoCaido', 'Europe', 'es', 'Spain', 'subterranea', 'GranCanaria', 28.020447, -15.647619);
