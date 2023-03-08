fc3c3e28e1576cc9ed63f0e83b6fccc6487a83fc
jd <jochen.duckeck@diginuevo.com>
2023-03-08 16:06:18 +0100

    Cuevas de Acusa Seca

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cuevas de Acusa Seca', '/english/es/subterranea/AcusaSeca.html', 'AcusaSeca', 'Europe', 'es', 'Spain', 'subterranea', 'GranCanaria', 28.006722, -15.679729);
