ec934af6f43a677996a1d08b7a2084c348e94a01
jd <jochen.duckeck@diginuevo.com>
2023-03-08 18:09:37 +0100

    Barranco de las Vacas

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cuevas de Acusa Seca', '/english/es/subterranea/AcusaSeca.html', 'AcusaSeca', 'Europe', 'es', 'Spain', 'subterranea', 'GranCanaria', 28.006722, -15.679729);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Centro de Interpretación de Risco Caído', '/english/es/subterranea/RiscoCaido.html', 'RiscoCaido', 'Europe', 'es', 'Spain', 'subterranea', 'GranCanaria', 28.020447, -15.647619);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Barranco de las Vacas', '/english/es/gorges/Vacas.html', 'Vacas', 'Europe', 'es', 'Spain', 'gorges', 'GranCanaria', 27.912783, -15.474675);
