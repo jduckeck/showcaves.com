39513cca96ae7e0f0b403afe316724fb2ea83773
jd <jochen.duckeck@diginuevo.com>
2023-03-10 20:38:12 +0100

    Slovakia

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Centro de Interpretación de Risco Caído', '/english/es/subterranea/RiscoCaido.html', 'RiscoCaido', 'Europe', 'es', 'Spain', 'subterranea', 'GranCanaria', 28.020447, -15.647619);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Barranco de las Vacas', '/english/es/gorges/Vacas.html', 'Vacas', 'Europe', 'es', 'Spain', 'gorges', 'GranCanaria', 27.912783, -15.474675);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte del Lago Grande', '/english/it/subterranea/LagoGrande.html', 'LagoGrande', 'Europe', 'it', 'Italy', 'subterranea', 'Liguria', 44.425837, 8.817248);
