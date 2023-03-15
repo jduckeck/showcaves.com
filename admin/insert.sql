d3c71b58e39ea82054684ee952a02018f5e21860
jd <jochen.duckeck@diginuevo.com>
2023-03-15 12:55:19 +0100

    Indonesia

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goa Mimpi', '/english/id/showcaves/Mimpi.html', 'Mimpi', 'Asia', 'id', 'Indonesia', 'showcaves', 'SulawesiSelatan', -5.016256, 119.681574);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goa Kiskendo Jatimulyo', '/english/id/showcaves/KiskendoJatimulyo.html', 'Kiskendo', 'Asia', 'id', 'Indonesia', 'showcaves', 'Yogyakarta', -7.747110, 110.131020);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goa Kiskendo Trayu', '/english/id/showcaves/KiskendoTrayu.html', 'KiskendoTrayu', 'Asia', 'id', 'Indonesia', 'showcaves', 'CentralJava', -7.059646, 110.241560);
