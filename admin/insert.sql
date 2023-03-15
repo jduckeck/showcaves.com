0e409e84e8f2f61281347e5a55e275ab1d2376ad
jd <jochen.duckeck@diginuevo.com>
2023-03-15 18:46:57 +0100

    Central Java Province

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Goa Kiskendo Jatimulyo', '/english/id/showcaves/KiskendoJatimulyo.html', 'Kiskendo', 'Asia', 'id', 'Indonesia', 'showcaves', 'Yogyakarta', -7.747110, 110.131020);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Goa Kiskendo Trayu', '/english/id/showcaves/KiskendoTrayu.html', 'KiskendoTrayu', 'Asia', 'id', 'Indonesia', 'showcaves', 'CentralJava', -7.059646, 110.241560);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goa Kreo', '/english/id/caves/Kreo.html', 'Kreo', 'Asia', 'id', 'Indonesia', 'caves', 'CentralJava', -7.037211, 110.347616);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goa Jatijajar', '/english/id/showcaves/Jatijajar.html', 'Jatijajar', 'Asia', 'id', 'Indonesia', 'showcaves', 'CentralJava', -7.6698400, 109.4235289);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goa Barat', '/english/id/caves/Barat.html', 'Barat', 'Asia', 'id', 'Indonesia', 'caves', 'CentralJava', -7.665500, 109.435960);
