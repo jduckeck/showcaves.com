a1b52227ae42dc4223ffa6f275a54fdaffcd0b71
jd <jochen.duckeck@diginuevo.com>
2023-03-16 23:17:06 +0100

    misc

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Independence Mine', '/english/usa/mines/Independence.html', 'Independence', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 61.790630, -149.282683);
