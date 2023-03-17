7bb0c8cd92e29113d4f6ed069428a0e98b8d7fa8
jd <jochen.duckeck@diginuevo.com>
2023-03-17 18:52:50 +0100

    GeoParks

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Independence Mine', '/english/usa/mines/Independence.html', 'Independence', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 61.790630, -149.282683);
+INSERT INTO static (path)
+VALUES ('/german/de/geoparks/GrenzWelten.html');
+INSERT INTO static (path)
+VALUES ('/english/de/geoparks/GrenzWelten.html');
