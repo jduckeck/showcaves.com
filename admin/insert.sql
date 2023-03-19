634ab8ada86f904d3cf7cae289997ed93c76cc1d
jd <jochen.duckeck@diginuevo.com>
2023-03-19 08:17:41 +0100

    deploy

--- admin/insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Independence Mine', '/english/usa/mines/Independence.html', 'Independence', 'NorthAmerica', 'us', 'United States of America', 'mines', 'ak', 61.790630, -149.282683);
-INSERT INTO static (path)
-VALUES ('/german/de/geoparks/GrenzWelten.html');
-INSERT INTO static (path)
-VALUES ('/english/de/geoparks/GrenzWelten.html');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Werra-Kalibergbau-Museum Heringen', '/english/de/mines/Heringen.html', 'Heringen', 'Europe', 'de', 'Germany', 'mines', 'Bergland', 50.886852, 10.006635);
