aefffd6ce22da1cfd21b2c93d4cd58e324c764ca
jd <jochen.duckeck@diginuevo.com>
2023-03-20 03:16:54 +0100

    deploy

--- admin/insert.sql
+++
@@ -1,8 +0,0 @@
-UPDATE sights
-SET chapter=NULL,
-    region=NULL,
-    continent='MiddleAmerica'
-WHERE countrycode = 'pr';
-UPDATE sights
-SET filename = REPLACE(filename, '/car/', '/pr/')
-WHERE countrycode = 'pr';
