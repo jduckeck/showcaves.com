2eb2143b0c5ce01d2569b87fc357a8fb9d00f73f
jd <jochen.duckeck@diginuevo.com>
2023-03-21 00:25:48 +0100

    Puerto Rico

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Bergbau-Schaustollen Max', '/english/de/mines/Max.html', 'Max', 'Europe', 'de', 'Germany', 'mines', 'Oberpfalz', 49.494955, 11.757407);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bergbaumuseum Maffeischächte', '/english/de/mines/Maffei.html', 'Maffei', 'Europe', 'de', 'Germany', 'mines', 'Oberpfalz', 49.679499, 11.649373);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva Balcones', '/english/pr/caves/Balcones.html', 'Balcones', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'pr', 18.332923, -66.525246);
