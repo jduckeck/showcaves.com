fcb86d33d0f770b5649511ce0c8cce1289da33d4
jd <jochen.duckeck@diginuevo.com>
2023-03-20 18:16:09 +0100

    Leonie

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bergbau-Schaustollen Max', '/english/de/mines/Max.html', 'Max', 'Europe', 'de', 'Germany', 'mines', 'Oberpfalz', 49.494955, 11.757407);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bergbaumuseum Maffeischächte', '/english/de/mines/Maffei.html', 'Maffei', 'Europe', 'de', 'Germany', 'mines', 'Oberpfalz', 49.679499, 11.649373);
