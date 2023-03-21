4227727fa6e9a1c3b2caa153595647faadc8ef5c
jd <jochen.duckeck@diginuevo.com>
2023-03-21 16:05:11 +0100

    Puerto Rico

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,10 +5,12 @@ VALUES ('yes', 'Bergbaumuseum Maffeischächte', '/english/de/mines/Maffei.html',
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cueva Balcones', '/english/pr/caves/Balcones.html', 'Balcones', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'pr', 18.332923, -66.525246);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Cueva del Viento', '/english/pr/caves/Viento.html', 'Viento', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'Northern', 18.418123, -66.965131);
+VALUES ('yes', 'Cueva del Viento', '/english/pr/caves/Viento.html', 'Viento', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'Northern', 18.411348, -66.976736);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Jacuzzi of Manati', '/english/pr/caves/Manati.html', 'Manati', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'CoastalPlains', 18.476360, -66.494310);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cueva de las Golondrinas', '/english/pr/caves/Golondrinas.html', 'Golondrinas', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'CoastalPlains', 18.478369, -66.509083);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cueva Golondrinas', '/english/pr/caves/GolondrinasIsabela.html', 'GolondrinasIsabela', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'CoastalPlains', 18.496290, -66.987200);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva Arenales', '/english/pr/gorges/Arenales.html', 'Arenales', 'MiddleAmerica', 'pr', 'Puerto Rico', 'gorges', 'Northern', 18.362830, -66.389212);
