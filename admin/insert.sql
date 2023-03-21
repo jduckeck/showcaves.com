c49b666ece55061a6fd08e94a2ca58babdf459c3
jd <jochen.duckeck@diginuevo.com>
2023-03-21 08:37:14 +0100

    Puerto Rico

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Bergbaumuseum Maffeischächte', '/english/de/mines/Maffei.html', 'Maffei', 'Europe', 'de', 'Germany', 'mines', 'Oberpfalz', 49.679499, 11.649373);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cueva Balcones', '/english/pr/caves/Balcones.html', 'Balcones', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'pr', 18.332923, -66.525246);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva del Viento', '/english/pr/caves/Viento.html', 'Viento', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'Northern', 18.418123, -66.965131);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Jacuzzi of Manati', '/english/pr/caves/Manati.html', 'Manati', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'CoastalPlains', 18.476360, -66.494310);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva de las Golondrinas', '/english/pr/caves/Golondrinas.html', 'Golondrinas', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'CoastalPlains', 18.478369, -66.509083);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva Golondrinas', '/english/pr/caves/GolondrinasIsabela.html', 'GolondrinasIsabela', 'MiddleAmerica', 'pr', 'Puerto Rico', 'caves', 'CoastalPlains', 18.496290, -66.987200);
