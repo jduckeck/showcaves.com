68d2bcd62e5bf12722a99264d9275bcfc3e1fec1
jd <jochen.duckeck@diginuevo.com>
2023-03-24 20:55:21 +0100

    links

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES('yes','Fikirini Three Sister Caves', '/english/other/showcaves/Fikirini.html', 'Fikirini', 'Africa', 'ke', 'Kenya', 'showcaves', 'ke', 'other', -4.614126, 39.354253);
+UPDATE sights SET filename = REPLACE(filename, '/caves/', '/showcaves/'), category='showcaves' WHERE countrycode = 'ke' and sortby='PontDArc';
