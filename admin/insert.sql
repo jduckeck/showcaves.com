e30dcf638da5526e3060cc8ce17dfdef5b98ec17
jd <jochen.duckeck@diginuevo.com>
2023-03-24 17:33:51 +0100

    Fikirini Three Sister Caves

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Fikirini Three Sister Caves', '/english/other/showcaves/Fikirini.html', 'Fikirini', 'Africa', 'ke', 'Kenya', 'showcaves', 'ke', 'other', -4.614126, 39.354253);
