13a6ee0411120da106b1a5095eec41e82d4dba81
jd <jochen.duckeck@diginuevo.com>
2023-03-25 12:44:03 +0100

    deploy

--- admin/insert.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
-VALUES('yes','Fikirini Three Sister Caves', '/english/other/showcaves/Fikirini.html', 'Fikirini', 'Africa', 'ke', 'Kenya', 'showcaves', 'ke', 'other', -4.614126, 39.354253);
-UPDATE sights SET filename = REPLACE(filename, '/caves/', '/showcaves/'), category='showcaves' WHERE countrycode = 'ke' and sortby='Shimoni';
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
-VALUES('yes','Shetani Caves', '/english/other/caves/Shetani.html', 'Shetani', 'Africa', 'ke', 'Kenya', 'caves', 'ke', 'other', -2.603120, 37.881290);
-UPDATE sights SET chapter=NULL,region=NULL,continent='Africa' WHERE countrycode = 'ke';
-UPDATE sights SET filename = REPLACE(filename, '/other/', '/ke/') WHERE countrycode = 'ke';
