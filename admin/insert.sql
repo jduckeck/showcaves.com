b566216cde58fcc18f781e5e0077da0420791536
jd <jochen.duckeck@diginuevo.com>
2023-03-25 15:08:18 +0100

    Kenya

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Ol Njorowa Gorge', '/english/ke/gorges/OlNjorowa.html', 'OlNjorowa', 'Africa', 'ke', 'Kenya', 'gorges', 'Nakuru', -0.896984,  36.324525);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Queens Cave', '/english/ke/caves/Queens.html', 'Queens', 'Africa', 'ke', 'Kenya', 'caves', 'Nyeri', -0.487000, 36.709348);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mount Suswa Caves', '/english/ke/caves/MountSuswa.html', 'MountSuswa', 'Africa', 'ke', 'Kenya', 'caves', 'Kajiado', -1.131759,  36.404696);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Mau Mau Cave Nairobi', '/english/ke/caves/MauMauNairobi.html', 'MauMauNairobi', 'Africa', 'ke', 'Kenya', 'caves', 'Nairobi', -1.236271,  36.829156);
