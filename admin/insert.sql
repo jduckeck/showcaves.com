23f19b25eca2ca2db73bb20b08b460c5a7db68bd
jd <jochen.duckeck@diginuevo.com>
2023-03-25 11:17:24 +0100

    Kenya

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,3 +3,5 @@ VALUES('yes','Fikirini Three Sister Caves', '/english/other/showcaves/Fikirini.h
 UPDATE sights SET filename = REPLACE(filename, '/caves/', '/showcaves/'), category='showcaves' WHERE countrycode = 'ke' and sortby='Shimoni';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES('yes','Shetani Caves', '/english/other/caves/Shetani.html', 'Shetani', 'Africa', 'ke', 'Kenya', 'caves', 'ke', 'other', -2.603120, 37.881290);
+UPDATE sights SET chapter=NULL,region=NULL,continent='Africa' WHERE countrycode = 'ke';
+UPDATE sights SET filename = REPLACE(filename, '/other/', '/ke/') WHERE countrycode = 'ke';
