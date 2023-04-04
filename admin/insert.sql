7cdbbba2ff6fb8fc322c776519228c2479846e31
jd <jochen.duckeck@diginuevo.com>
2023-04-04 16:15:10 +0200

    Ghar Boumâaza

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Isteiner Klotz', '/english/de/subterranea/IsteinerKlotz.html', 'IsteinerKlotz', 'Europe', 'de', 'Germany', 'subterranea', 'Oberrhein', 47.661990,   7.531338);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES('yes','Ghar Boumâaza', '/english/other/caves/Boumaaza.html', 'Boumaaza', 'Africa', 'dz', 'Algeria', 'caves', 'dz', 'other', 34.699986, -1.311361);
