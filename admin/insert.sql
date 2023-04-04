ca623e7f08891ddef26d68a8f166ed3e396c8fd5
jd <jochen.duckeck@diginuevo.com>
2023-04-05 01:41:57 +0200

    China

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Ghar Boumâaza', '/english/other/caves/Boumaaza.html', 'Boumaaza', 'Africa', 'dz', 'Algeria', 'caves', 'dz', 'other', 34.699986, -1.311361);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Besucherbergwerk Stock und Scherenberger Erbstollen', '/english/de/mines/StockScherenberger.html', 'StockScherenberger', 'Europe', 'de', 'Germany', 'mines', 'Ruhrgebiet', 51.361327, 7.277009);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Baiyun Dong Hunan', '/english/cn/showcaves/BaiyunHunan.html', 'BaiyunHunan', 'Asia', 'cn', 'China', 'showcaves', 'Hunan', 26.351020, 110.319138);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Baiyun Dong Jiangsu', '/english/cn/showcaves/BaiyunJiangsu.html', 'BaiyunJiangsu', 'Asia', 'cn', 'China', 'showcaves', 'Jiangsu', 34.288777, 117.135662);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tianxing Dong', '/english/cn/showcaves/Tianxing.html', 'Tianxing', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 25.949876, 105.673610);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Meishan Longgong Karst Cave', '/english/cn/showcaves/MeishanLonggong.html', 'MeishanLonggong', 'Asia', 'cn', 'China', 'showcaves', 'Hunan', 27.953192, 111.287588);
