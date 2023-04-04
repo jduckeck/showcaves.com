bcc2e55ca23b9d574e775fa83044565833b6f69b
jd <jochen.duckeck@diginuevo.com>
2023-04-04 18:41:12 +0200

    Besucherbergwerk Stock und Scherenberger Erbstollen

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Isteiner Klotz', '/english/de/subterranea/IsteinerKlotz.html', 'IsteinerKlotz', 'Europe', 'de', 'Germany', 'subterranea', 'Oberrhein', 47.661990,   7.531338);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES('yes','Ghar Boumâaza', '/english/other/caves/Boumaaza.html', 'Boumaaza', 'Africa', 'dz', 'Algeria', 'caves', 'dz', 'other', 34.699986, -1.311361);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Besucherbergwerk Stock und Scherenberger Erbstollen', '/english/de/mines/StockScherenberger.html', 'StockScherenberger', 'Europe', 'de', 'Germany', 'mines', 'Ruhrgebiet', 51.361327, 7.277009);
