07766a6ea4bee0b60746ee3df740a2fa776e5d64
jd <jochen.duckeck@diginuevo.com>
2023-04-04 13:03:31 +0200

    Isteiner Klotz

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Isteiner Klotz', '/english/de/subterranea/IsteinerKlotz.html', 'IsteinerKlotz', 'Europe', 'de', 'Germany', 'subterranea', 'Oberrhein', 47.661990,   7.531338);
