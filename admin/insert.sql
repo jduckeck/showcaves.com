521916a700accb8e64073a7b381213ca25f7b803
jd <jochen.duckeck@diginuevo.com>
2023-04-09 23:53:19 +0200

    Spain

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Lagunas de Cañada del Hoyo', '/english/es/karst/CanadaDelHoyo.html', 'CanadaDelHoyo', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 39.984574, -1.869731);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Nacimiento del Río Cuervo', '/english/es/springs/RioCuervo.html', 'RioCuervo', 'Europe', 'es', 'Spain', 'springs', 'Castilla', 40.427556, -1.895396);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Torcas de los Palancares', '/english/es/karst/Palancares.html', 'Palancares', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 40.023392, -1.959041);
