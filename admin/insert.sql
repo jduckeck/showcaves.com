920aec1aabbf6426e27b7d97680e97870f38bcb7
jd <jochen.duckeck@diginuevo.com>
2023-04-09 22:17:48 +0200

    Lagunas de Cañada del Hoyo

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Lagunas de Cañada del Hoyo', '/english/es/karst/CanadaDelHoyo.html', 'CanadaDelHoyo', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 39.984574, -1.869731);
