6d0c772fbf93efe5de8d6f801365c7feaa3c49a5
jd <jochen.duckeck@diginuevo.com>
2023-04-10 02:29:24 +0200

    Spain

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Nacimiento del Río Cuervo', '/english/es/springs/RioCuervo.html', 'RioCuervo', 'Europe', 'es', 'Spain', 'springs', 'Castilla', 40.427556, -1.895396);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Torcas de los Palancares', '/english/es/karst/Palancares.html', 'Palancares', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 40.023392, -1.959041);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Minas de Ojos Negros', '/english/es/mines/OjosNegros.html', 'OjosNegros', 'Europe', 'es', 'Spain', 'mines', 'Aragon', 40.715818, -1.546358);
