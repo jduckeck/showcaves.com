123bdc6763270aafe9e817bc60b4dd27c77c42d0
jd <jochen.duckeck@diginuevo.com>
2023-04-11 17:34:32 +0200

    Spain

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,12 +3,14 @@ VALUES('yes','Lagunas de Cañada del Hoyo', '/english/es/karst/CanadaDelHoyo.htm
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Nacimiento del Río Cuervo', '/english/es/springs/RioCuervo.html', 'RioCuervo', 'Europe', 'es', 'Spain', 'springs', 'Castilla', 40.427556, -1.895396);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Torcas de los Palancares', '/english/es/karst/Palancares.html', 'Palancares', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 40.023392, -1.959041);
+VALUES ('yes', 'Torcas de los Palancares', '/english/es/karst/Palancares.html', 'Palancares', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 40.023392, -1.959041);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Minas de Ojos Negros', '/english/es/mines/OjosNegros.html', 'OjosNegros', 'Europe', 'es', 'Spain', 'mines', 'Aragon', 40.715818, -1.546358);
+VALUES ('yes', 'Minas de Ojos Negros', '/english/es/mines/OjosNegros.html', 'OjosNegros', 'Europe', 'es', 'Spain', 'mines', 'Aragon', 40.715818, -1.546358);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Ventano del Diablo', '/english/es/karst/VentanoDelDiablo.html', 'VentanoDelDiablo', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 40.237851, -2.071248);
+VALUES ('yes', 'Ventano del Diablo', '/english/es/karst/VentanoDelDiablo.html', 'VentanoDelDiablo', 'Europe', 'es', 'Spain', 'karst', 'Castilla', 40.237851, -2.071248);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Cueva de Montesinos', '/english/es/showcaves/Montesinos.html', 'Montesinos', 'Europe', 'es', 'Spain', 'showcaves', 'Castilla', 38.939047, -2.809922);
+VALUES ('yes', 'Cueva de Montesinos', '/english/es/showcaves/Montesinos.html', 'Montesinos', 'Europe', 'es', 'Spain', 'showcaves', 'Castilla', 38.939047, -2.809922);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Quebrada del Toro', '/english/es/gorges/Toro.html', 'Toro', 'Europe', 'es', 'Spain', 'gorges', 'Castilla', 38.938703, -2.833529);
+VALUES ('yes', 'Quebrada del Toro', '/english/es/gorges/Toro.html', 'Toro', 'Europe', 'es', 'Spain', 'gorges', 'Castilla', 38.938703, -2.833529);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ojos de Valdeminguete', '/english/es/springs/Valdeminguete.html', 'Valdeminguete', 'Europe', 'es', 'Spain', 'springs', 'Castilla', 40.373440, -1.829410);
