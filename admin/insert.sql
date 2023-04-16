98efe081f6ef907fd77faae75edeed69a82c859a
jd <jochen.duckeck@diginuevo.com>
2023-04-16 21:14:12 +0200

    Kryvyi Rih Mine

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kryvyi Rih Mine', '/english/ua/mines/KryvyiRih.html', 'Rodina', 'Europe', 'ua', 'Ukraine', 'mines', 'Dnipropetrovsk', 47.961839, 33.411051);
