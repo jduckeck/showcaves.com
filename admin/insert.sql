7526094dbaf43c17579fcafa39e4600677a9b2c4
jd <jochen.duckeck@diginuevo.com>
2023-04-17 16:30:07 +0200

    Phantomkarst

--- admin/insert.sql
+++ admin/insert.sql
@@ -9,8 +9,8 @@ UPDATE sights
 SET filename = REPLACE(filename, '/other/', '/dk/')
 WHERE countrycode = 'dk';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Christiansborg Slot', '/english/dk/subterranea/Christiansborg.html', 'Christiansborg', 'Europe', 'dk', 'Denmark', 'subterranea', 'dk', 55.676322, 12.580327);
+VALUES ('yes', 'Christiansborg Slot', '/english/dk/subterranea/Christiansborg.html', 'Christiansborg', 'Europe', 'dk', 'Denmark', 'subterranea', 'Hovedstaden', 55.676322, 12.580327);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Koldkrigsmuseet REGAN Vest', '/english/dk/subterranea/REGANVest.html', 'REGANVest', 'Europe', 'dk', 'Denmark', 'subterranea', 'dk', 56.823712, 9.801189);
+VALUES ('yes', 'Koldkrigsmuseet REGAN Vest', '/english/dk/subterranea/REGANVest.html', 'REGANVest', 'Europe', 'dk', 'Denmark', 'subterranea', 'Nordjylland', 56.823712, 9.801189);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Koldkrigsmuseum Stevnsfort', '/english/dk/subterranea/Stevnsfort.html', 'Stevnsfort', 'Europe', 'dk', 'Denmark', 'subterranea', 'dk', 55.264586, 12.409783);
