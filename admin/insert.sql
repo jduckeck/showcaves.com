8c4fd6bc8f74337b19056b8ce98dcd892e46ff5a
jd <jochen.duckeck@diginuevo.com>
2023-04-17 23:52:43 +0200

    Denmark

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'M/S Museet for Søfart', '/english/dk/subterranea/Sofart.html', 'Sofart', 'Europe', 'dk', 'Denmark', 'subterranea', 'Hovedstaden', 56.039022, 12.616078);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kronborg Slot', '/english/dk/subterranea/Kronborg.html', 'Kronborg', 'Europe', 'dk', 'Denmark', 'subterranea', 'Hovedstaden', 56.039078, 12.621699);
