da40425f2dcd32b616cb343b33f1c343fc01e169
jd <jochen.duckeck@diginuevo.com>
2023-04-18 21:23:11 +0200

    deploy

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Store Blåkilde', '/english/dk/springs/StoreBlakilde.html', 'StoreBlakilde', 'Europe', 'dk', 'Denmark', 'springs', 'Nordjylland', 56.782773, 56.782773);
