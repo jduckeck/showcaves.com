7efd5ead489904f317fd630d991e5a0924867340
jd <jochen.duckeck@diginuevo.com>
2023-04-20 21:53:02 +0200

    Magická Jeskyně

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Store Blåkilde', '/english/dk/springs/StoreBlakilde.html', 'StoreBlakilde', 'Europe', 'dk', 'Denmark', 'springs', 'Nordjylland', 56.782773, 56.782773);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Magická Jeskyně', '/english/cz/subterranea/MagicalCavern.html', 'MagicalCavern', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Praha', 50.0814, 14.4002);
