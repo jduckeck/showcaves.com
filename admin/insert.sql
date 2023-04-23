cffc1642872017ca24eb17dc1035cfbd08246972
jd <jochen.duckeck@diginuevo.com>
2023-04-23 21:58:48 +0200

    Goa Margo Tresno

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Goa Margo Tresno', '/english/id/showcaves/MargoTresno.html', 'MargoTresno', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -7.445792532710059, 111.93547681534355);
