bd1cfa4855543593b0c3c3a6272a56525af47f9e
jd <jochen.duckeck@diginuevo.com>
2023-04-24 01:37:32 +0200

    Kalkwerk Lengefeld

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Goa Margo Tresno', '/english/id/showcaves/MargoTresno.html', 'MargoTresno', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -7.445792532710059, 111.93547681534355);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kalkwerk Lengefeld', '/english/de/mines/Lengefeld.html', 'Lengefeld', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.700809, 13.175079);
