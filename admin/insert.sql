d6792f42cd118c20951c025df961c6dffcdbc55a
jd <jochen.duckeck@diginuevo.com>
2023-04-25 01:07:45 +0200

    Paraguay

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,13 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Goa Margo Tresno', '/english/id/showcaves/MargoTresno.html', 'MargoTresno', 'Asia', 'id', 'Indonesia', 'showcaves', 'EastJava', -7.445792532710059, 111.93547681534355);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Kalkwerk Lengefeld', '/english/de/mines/Lengefeld.html', 'Lengefeld', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.700809, 13.175079);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Caverna 54', '/english/other/caves/Caverna54.html', 'Caverna54', 'SouthAmerica', 'py', 'Paraguay', 'caves', 'py', 'other', -22.110370, -57.941200);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Caverna 14 de Julio', '/english/other/caves/14Julio.html', '14Julio', 'SouthAmerica', 'py', 'Paraguay', 'caves', 'py', 'other', -22.2506183, -57.8689760);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Santa Caverna', '/english/other/caves/SantaCaverna.html', 'SantaCaverna', 'SouthAmerica', 'py', 'Paraguay', 'caves', 'py', 'other', -22.2516510, -57.8718513);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Salto Suizo', '/english/other/caves/SaltoSuizo.html', 'SaltoSuizo', 'SouthAmerica', 'py', 'Paraguay', 'caves', 'py', 'other', -25.7732048, -56.2278011);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Caverna Kambá Hópo', '/english/other/caves/KambaHopo.html', 'KambaHopo', 'SouthAmerica', 'py', 'Paraguay', 'caves', 'py', 'other', -22.190028, -57.969470);
