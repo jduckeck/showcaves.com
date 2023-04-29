2d902035899d45e864b2c8f906de9bd084940b30
jd <jochen.duckeck@diginuevo.com>
2023-04-29 17:34:23 +0200

    fixes

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Podpeška Jama', '/english/si/caves/Podpeska.html', 'Podpeska', 'Europe', 'si', 'Slovenia', 'caves', 'Dolenjska', 45.839269,  14.686669);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Peștera Izbândiș', '/english/ro/springs/Izbandis.html', 'Izbandis', 'Europe', 'ro', 'Romania', 'springs', 'Apuseni', 46.933400,  22.520385);
