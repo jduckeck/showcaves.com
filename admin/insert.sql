ee37340a726a96520587d194432e086d0c5ea8aa
jd <jochen.duckeck@diginuevo.com>
2023-04-30 20:29:12 +0200

    Devil's Hole

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,6 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Podpeška Jama', '/english/si/caves/Podpeska.html', 'Podpeska', 'Europe', 'si', 'Slovenia', 'caves', 'Dolenjska', 45.839269,  14.686669);
+VALUES ('yes', 'Podpeška Jama', '/english/si/caves/Podpeska.html', 'Podpeska', 'Europe', 'si', 'Slovenia', 'caves', 'Dolenjska', 45.839269, 14.686669);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Peștera Izbândiș', '/english/ro/springs/Izbandis.html', 'Izbandis', 'Europe', 'ro', 'Romania', 'springs', 'Apuseni', 46.933400,  22.520385);
+VALUES ('yes', 'Peștera Izbândiș', '/english/ro/springs/Izbandis.html', 'Izbandis', 'Europe', 'ro', 'Romania', 'springs', 'Apuseni', 46.933400, 22.520385);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Fort Hommet Gun Casemate', '/english/gb/subterranea/FortHommet.html', 'FortHommet', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Guernsey', 49.473462, -2.609240);
+VALUES ('yes', 'Fort Hommet Gun Casemate', '/english/gb/subterranea/FortHommet.html', 'FortHommet', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Guernsey', 49.473462, -2.609240);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Devil´s Hole', '/english/gb/caves/DevilsHole.html', 'DevilsHole', 'Europe', 'gb', 'Great Britain', 'caves', 'Jersey', 49.251493, -2.172957);
