858f840774b10dfe2bc00725a7f60f82052588ae
jd <jochen.duckeck@diginuevo.com>
2023-04-30 17:13:17 +0200

    Channel Islands

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Podpeška Jama', '/english/si/caves/Podpeska.html', 'Podpeska', 'Europe', 'si', 'Slovenia', 'caves', 'Dolenjska', 45.839269,  14.686669);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Peștera Izbândiș', '/english/ro/springs/Izbandis.html', 'Izbandis', 'Europe', 'ro', 'Romania', 'springs', 'Apuseni', 46.933400,  22.520385);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Fort Hommet Gun Casemate', '/english/gb/subterranea/FortHommet.html', 'FortHommet', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Guernsey', 49.473462, -2.609240);
