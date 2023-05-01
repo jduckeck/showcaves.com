08a9099fd6361b2220f2b909f5e098ecf281ec4c
jd <jochen.duckeck@diginuevo.com>
2023-05-01 21:47:00 +0200

    Croatia

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'The Cave Šibenik', '/english/hr/subterranea/Sibenik.html', 'Sibenik', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.713719, 15.904273);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Morski Tunel', '/english/hr/subterranea/MorskiTunel.html', 'MorskiTunel', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.727290, 15.874219);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Špilja-crkva sv. Ante', '/english/hr/subterranea/SvAnte.html', 'SvAnte', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.726930, 15.873980);
