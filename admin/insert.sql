60e8596697a190917f7e0a6d567b6d4595f1f6f0
jd <jochen.duckeck@diginuevo.com>
2023-05-01 10:30:29 +0200

    Jersey

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Fort Hommet Gun Casemate', '/english/gb/subterranea/FortHommet.html', 'FortHommet', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Guernsey', 49.473462, -2.609240);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Devil´s Hole', '/english/gb/caves/DevilsHole.html', 'DevilsHole', 'Europe', 'gb', 'Great Britain', 'caves', 'Jersey', 49.251493, -2.172957);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Wolf Cave', '/english/gb/caves/Wolf.html', 'Wolf', 'Europe', 'gb', 'Great Britain', 'caves', 'Jersey', 49.253406, -2.128942);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'La Cotte de St Brelade', '/english/gb/gorges/Cotte.html', 'Cotte', 'Europe', 'gb', 'Great Britain', 'gorges', 'Jersey', 49.175563, -2.188237);
