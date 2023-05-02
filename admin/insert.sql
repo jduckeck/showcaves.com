cbe5700754fdfa42ea2bc01c15c3827bc9beb1e0
jd <jochen.duckeck@diginuevo.com>
2023-05-02 20:29:50 +0200

    Croatia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'The Cave Šibenik', '/english/hr/subterranea/Sibenik.html', 'Sibenik', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.713719, 15.904273);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Morski tunel Hitlerove oči', '/english/hr/subterranea/MorskiTunel.html', 'MorskiTunel', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.7271055, 15.8745082);
+VALUES ('yes', 'Morski tunel Hitlerove oči', '/english/hr/subterranea/Hitlerove.html', 'Hitlerove', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.7271055, 15.8745082);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Špilja-crkva sv. Ante', '/english/hr/subterranea/SvAnte.html', 'SvAnte', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.726930, 15.873980);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
@@ -10,3 +10,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Morski Tunel Brijesta', '/english/hr/subterranea/Brijesta.html', 'Brijesta', 'Europe', 'hr', 'Croatia', 'subterranea', 'DubrovnikNeretva', 42.909090, 17.529642);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Kraljičina Špilja', '/english/hr/caves/Kraljicina.html', 'Kraljicina', 'Europe', 'hr', 'Croatia', 'caves', 'Dalmatia', 43.0738211, 16.1010961);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Potkop Parja', '/english/hr/subterranea/Parja.html', 'Parja', 'Europe', 'hr', 'Croatia', 'subterranea', 'Dalmatia', 43.078253, 16.181335);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Potkop Krušica', '/english/hr/subterranea/Krusica.html', 'Krusica', 'Europe', 'hr', 'Croatia', 'subterranea', 'Dalmatia', 43.2813980, 16.5076201);
