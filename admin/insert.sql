f2b35880f454f796e7a967c2942641ab5c53ed8f
jd <jochen.duckeck@diginuevo.com>
2023-05-02 19:13:25 +0200

    Croatia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,6 +1,12 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'The Cave Šibenik', '/english/hr/subterranea/Sibenik.html', 'Sibenik', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.713719, 15.904273);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Morski Tunel', '/english/hr/subterranea/MorskiTunel.html', 'MorskiTunel', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.727290, 15.874219);
+VALUES ('yes', 'Morski tunel Hitlerove oči', '/english/hr/subterranea/MorskiTunel.html', 'MorskiTunel', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.7271055, 15.8745082);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Špilja-crkva sv. Ante', '/english/hr/subterranea/SvAnte.html', 'SvAnte', 'Europe', 'hr', 'Croatia', 'subterranea', 'SibenikKnin', 43.726930, 15.873980);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Titova Špilja', '/english/hr/caves/Titova.html', 'Titova', 'Europe', 'hr', 'Croatia', 'caves', 'Dalmatia', 43.036480, 16.120400);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Morski Tunel Brijesta', '/english/hr/subterranea/Brijesta.html', 'Brijesta', 'Europe', 'hr', 'Croatia', 'subterranea', 'DubrovnikNeretva', 42.909090, 17.529642);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kraljičina Špilja', '/english/hr/caves/Kraljicina.html', 'Kraljicina', 'Europe', 'hr', 'Croatia', 'caves', 'Dalmatia', 43.0738211, 16.1010961);
