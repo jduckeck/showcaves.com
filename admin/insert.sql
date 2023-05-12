410bda3617436e2d97310929c8c6e47991e7bfc2
jd <jochen.duckeck@diginuevo.com>
2023-05-12 21:14:01 +0200

    Tanzania

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,12 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Circ romà', '/english/es/subterranea/CircRoma.html', 'CircRoma', 'Europe', 'es', 'Spain', 'subterranea', 'Cataluna', 41.115822, 1.256902);
+UPDATE sights
+SET chapter=NULL,
+    region=NULL,
+    continent='Africa'
+WHERE countrycode = 'tz';
+UPDATE sights
+SET filename = REPLACE(filename, '/other/', '/tz/')
+WHERE countrycode = 'tz';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kuza Cave', '/english/tz/showcaves/Kuza.html', 'Kuza', 'Africa', 'tz', 'Tanzania', 'showcaves', 'Zanzibar', -6.303449, 39.533965);
