bcd381077e6376f25af48115c55302e9f05ccb7c
jd <jochen.duckeck@diginuevo.com>
2023-05-12 18:42:59 +0200

    Circ romà

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Circ romà', '/english/es/subterranea/CircRoma.html', 'CircRoma', 'Europe', 'es', 'Spain', 'subterranea', 'Cataluna', 41.115822, 1.256902);
