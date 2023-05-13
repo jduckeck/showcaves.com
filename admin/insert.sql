d1a05c4036eef7fb9f56abfdf6af822cece950b8
jd <jochen.duckeck@diginuevo.com>
2023-05-14 01:51:01 +0200

    Lesotho

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Ha Kome Cave Dwellings', '/english/other/caves/HaKome.html', 'HaKome', 'Africa', 'ls', 'Lesotho', 'caves', 'ls', 'other', -29.244378, 27.866821);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Liphofung Rock Shelter', '/english/other/caves/Liphofung.html', 'Liphofung', 'Africa', 'ls', 'Lesotho', 'caves', 'ls', 'other', -28.752905, 28.495677);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Masitise Cave House Museum', '/english/other/subterranea/Masitise.html', 'Masitise', 'Africa', 'ls', 'Lesotho', 'subterranea', 'ls', 'other', -30.404669, 27.643328);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Ha Baroana', '/english/other/caves/HaBaroana.html', 'HaBaroana', 'Africa', 'ls', 'Lesotho', 'caves', 'ls', 'other', -29.362900, 27.787600);
