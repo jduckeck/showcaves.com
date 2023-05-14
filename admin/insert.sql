06c26451ff32b320cf96f43b886ca391d9b9b144
jd <jochen.duckeck@diginuevo.com>
2023-05-14 21:42:38 +0200

    Eswatini

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Masitise Cave House Museum', '/english/other/subterranea/Masitise.html', 'Masitise', 'Africa', 'ls', 'Lesotho', 'subterranea', 'ls', 'other', -30.404669, 27.643328);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES ('yes', 'Ha Baroana', '/english/other/caves/HaBaroana.html', 'HaBaroana', 'Africa', 'ls', 'Lesotho', 'caves', 'ls', 'other', -29.362900, 27.787600);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Ngwenya Mine', '/english/other/mines/Ngwenya.html', 'Ngwenya', '$continent$', 'sz', 'Eswatini', 'mines', 'sz', 'other', -26.195130, 31.031990);
