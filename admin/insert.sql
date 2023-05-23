7f2b91bf10d1cfe3c35d3bbed5fda21af8992c34
jd <jochen.duckeck@diginuevo.com>
2023-05-23 21:10:25 +0200

    Nepal

--- admin/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Mustang Caves', '/english/np/subterranea/Mustang.html', 'Mustang', 'Asia', 'np', 'Nepal', 'subterranea', 'Gandaki', 29.216759, 84.079768);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Asura Cave', '/english/np/subterranea/Asura.html', 'Asura', 'Asia', 'np', 'Nepal', 'subterranea', 'Bagmati', 27.613090, 85.260313);
