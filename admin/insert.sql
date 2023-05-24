63ca06d42b445a62749378f95b114bfcb0fcbffa
jd <jochen.duckeck@diginuevo.com>
2023-05-25 00:37:14 +0200

    Sri Lanka

--- admin/insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Idikundu', '/english/lk/springs/Idikundu.html', 'Idikundu', 'Asia', 'lk', 'Sri Lanka', 'springs', 'Northern', 9.729159, 79.984332);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Keerimalai Sacred Water Spring', '/english/lk/springs/Keerimalai.html', 'Keerimalai', 'Asia', 'lk', 'Sri Lanka', 'springs', 'Northern', 9.814880, 80.011259);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Nilavarai Bottomless Well', '/english/lk/springs/Nilavarai.html', 'Nilavarai', 'Asia', 'lk', 'Sri Lanka', 'springs', 'Northern', 9.754379, 80.088261);
