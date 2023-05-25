498671ee05b81abcd58c9c6cef45856cca6e5b3c
jd <jochen.duckeck@diginuevo.com>
2023-05-25 22:14:33 +0200

    Sri Lanka

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Nildiya Pokuna Cave', '/english/lk/showcaves/NildiyaPokuna.html', 'NildiyaPokuna', 'Asia', 'lk', 'Sri Lanka', 'showcaves', 'Uva', 6.8330528, 81.0658520);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gonawatta Ancient Caves', '/english/lk/subterranea/Gonawatta.html', 'Gonawatta', 'Asia', 'lk', 'Sri Lanka', 'subterranea', 'Central', 7.266448, 80.704681);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Dowa Rajamaha Viharaya', '/english/lk/subterranea/Dowa.html', 'Dowa', 'Asia', 'lk', 'Sri Lanka', 'subterranea', 'Uva', 6.856336, 81.022229);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Lenagala Rajamaha Vihara', '/english/lk/subterranea/LenagalaRajamaha.html', 'LenagalaRajamaha', 'Asia', 'lk', 'Sri Lanka', 'subterranea', 'Sabaragamuwa', 7.116040, 80.264020);
