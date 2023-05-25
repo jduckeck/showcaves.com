fdd945baca74f4f4063b4b989a2ddead1f906f96
jd <jochen.duckeck@diginuevo.com>
2023-05-25 20:10:35 +0200

    Sri Lanka

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Nildiya Pokuna Cave', '/english/lk/showcaves/NildiyaPokuna.html', 'NildiyaPokuna', 'Asia', 'lk', 'Sri Lanka', 'showcaves', 'Uva', 6.8330528, 81.0658520);
