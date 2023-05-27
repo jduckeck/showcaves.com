cf0a57b7d9a56ecb7eaf87487c879474c0e5178b
jd <jochen.duckeck@diginuevo.com>
2023-05-27 10:19:08 +0200

    Armenia

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Satani Kamurj', '/english/am/karst/SataniKamurj.html', 'SataniKamurj', 'Asia', 'am', 'Armenia', 'karst', 'Syunik', 39.395456, 46.257667);
