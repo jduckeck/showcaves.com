9b7ef3c9008bd19a00fe8c3935a5ef75655c36a8
jd <jochen.duckeck@diginuevo.com>
2023-05-27 14:40:50 +0200

    Gornjepoljski Vir

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Satani Kamurj', '/english/am/karst/SataniKamurj.html', 'SataniKamurj', 'Asia', 'am', 'Armenia', 'karst', 'Syunik', 39.395456, 46.257667);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Hrazdan Gorge', '/english/am/gorges/Hrazdan.html', 'Hrazdan', 'Asia', 'am', 'Armenia', 'gorges', 'Yerevan', 40.183158, 44.495069);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gornjepoljski Vir', '/english/me/springs/Gornjepolje.html', 'Gornjepolje', 'Europe', 'me', 'Montenegro', 'springs', NULL, 42.846318, 18.917533);
