bb2043b53dab466ddeb17524d73619ebea9d62cc
jd <jochen.duckeck@diginuevo.com>
2023-05-27 12:46:31 +0200

    Armenia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Satani Kamurj', '/english/am/karst/SataniKamurj.html', 'SataniKamurj', 'Asia', 'am', 'Armenia', 'karst', 'Syunik', 39.395456, 46.257667);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hrazdan Gorge', '/english/am/gorges/Hrazdan.html', 'Hrazdan', 'Asia', 'am', 'Armenia', 'gorges', 'Yerevan', 40.183158, 44.495069);
