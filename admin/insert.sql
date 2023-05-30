4745bd4c2307ad81b556658aaa288b43d4f86e44
jd <jochen.duckeck@diginuevo.com>
2023-05-30 14:01:04 +0200

    Bermuda

--- admin/insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Ecomuseu Minero Abangares', '/english/cr/mines/Abangares.html', 'Abangares', 'MiddleAmerica', 'cr', 'Costa Rica', 'mines', 'Guanacaste', 10.284353, -84.924524);
