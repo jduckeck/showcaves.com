78593c27c4f04ab19e7b4993ce76af59e56595a0
jd <jochen.duckeck@diginuevo.com>
2023-05-31 09:06:18 +0200

    The Cave by Chef Ryan Clift

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'The Cave', '/english/id/subterranea/TheCave.html', 'TheCave', 'Asia', 'id', 'Indonesia', 'subterranea', 'Bali', -8.845794, 115.125564);
