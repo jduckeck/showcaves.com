6ee781ea8580c7a38f3dca80ee912e98a312b6f0
jd <jochen.duckeck@diginuevo.com>
2023-06-08 09:08:43 +0200

    fixes

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Devil’s Millhopper', '/english/usa/karst/DevilsMillhopper.html', 'DevilsMillhopper', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'fl', 29.706888, -82.394212);
