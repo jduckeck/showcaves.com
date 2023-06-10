d1010ab0663dbe14a23d0a802426b7451cb90c47
jd <jochen.duckeck@diginuevo.com>
2023-06-10 15:00:11 +0200

    Cyprus

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Devil’s Millhopper', '/english/usa/karst/DevilsMillhopper.html', 'DevilsMillhopper', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'fl', 29.706888, -82.394212);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Caves of Puropetra', '/english/cy/mines/Puropetra.html', 'Puropetra', 'Europe', 'cy', 'Cyprus', 'mines', 'Nicosia', 35.175173, 33.200106);
