3cdc9c43e448c81e41c01eed21c39767d23701b5
jd <jochen.duckeck@diginuevo.com>
2023-06-12 19:15:21 +0200

    Meghalaya

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Arwah Cave', '/english/in/showcaves/Arwah.html', 'Arwah', 'Asia', 'in', 'India', 'showcaves', 'Meghalaya', 25.298566, 91.726356);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Mawjymbuin Cave', '/english/in/showcaves/Mawjymbuin.html', 'Mawjymbuin', 'Asia', 'in', 'India', 'showcaves', 'Meghalaya', 25.304762116383916, 91.58501715777165);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Garden Of Caves', '/english/in/karst/GardenOfCaves.html', 'GardenOfCaves', 'Asia', 'in', 'India', 'karst', 'Meghalaya', 25.31244321314026, 91.7605034277269);
