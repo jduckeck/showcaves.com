6d26387c72701c6f5ed059031bc85cf1db1702f2
jd <jochen.duckeck@diginuevo.com>
2023-06-13 09:54:07 +0200

    Dover

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Mawjymbuin Cave', '/english/in/showcaves/Mawjymbuin.html', 'Mawjymbuin', 'Asia', 'in', 'India', 'showcaves', 'Meghalaya', 25.304762116383916, 91.58501715777165);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Garden Of Caves', '/english/in/karst/GardenOfCaves.html', 'GardenOfCaves', 'Asia', 'in', 'India', 'karst', 'Meghalaya', 25.31244321314026, 91.7605034277269);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Dover Stone Church', '/english/usa/gorges/Dover.html', 'Dover', 'NorthAmerica', 'usa', 'United States of America', 'gorges', 'ny', 41.738075, -73.588971);
