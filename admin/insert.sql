c7bc2ab801084907d4c2cc7bb0f072ebbc8d60a4
jd <jochen.duckeck@diginuevo.com>
2023-06-17 12:39:39 +0200

    Musée de la Mine Noeux-les-Mines

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Musée de la Mine Noeux-les-Mines', '/english/fr/mines/NoeuxLesMines.html', 'NoeuxLesMines', 'Europe', 'fr', 'France', 'mines', 'Nord', 50.472504, 2.669821);
