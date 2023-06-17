8da01cb548db18727461b691c6f0bfe129137415
jd <jochen.duckeck@diginuevo.com>
2023-06-17 20:29:18 +0200

    Decazeville

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Musée de la Mine Noeux-les-Mines', '/english/fr/mines/NoeuxLesMines.html', 'NoeuxLesMines', 'Europe', 'fr', 'France', 'mines', 'Nord', 50.472504, 2.669821);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Musée du Patrimoine Industriel et Minier', '/english/fr/mines/Decazeville.html', 'Decazeville', 'Europe', 'fr', 'France', 'mines', 'Lot', 44.556863, 2.258103);
