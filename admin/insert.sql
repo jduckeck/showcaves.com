ec150c8302056e2b2a5f629065266f9c409660d1
jd <jochen.duckeck@diginuevo.com>
2023-06-28 13:21:23 +0200

    Le Grand Filon

--- admin/insert.sql
+++ admin/insert.sql
@@ -14,3 +14,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Mines De Pont-Péan', '/english/fr/mines/PontPean.html', 'PontPean', 'Europe', 'fr', 'France', 'mines', 'Bretagne', 48.013114, -1.711155);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Mines de Fer du Salver sur le Canigou à Taurinya', '/english/fr/mines/Taurinya.html', 'Taurinya', 'Europe', 'fr', 'France', 'mines', 'Languedoc', 42.569646, 2.427491);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Le Grand Filon', '/english/fr/mines/GrandFilon.html', 'GrandFilon', 'Europe', 'fr', 'France', 'mines', 'Rhone', 45.510636, 6.280494);
