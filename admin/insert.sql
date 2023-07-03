9647ab99a533188ffe7c8c4e19f806e3e9a18303
jd <jochen.duckeck@diginuevo.com>
2023-07-03 11:13:22 +0200

    Praha

--- admin/insert.sql
+++ admin/insert.sql
@@ -32,3 +32,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Vinárna U Královny Elišky', '/english/cz/subterranea/KralovnyElisky.html', 'KralovnyElisky', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Brno', 49.192500, 16.594269);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Pivnice U Šneka', '/english/cz/subterranea/Sneka.html', 'Sneka', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Jihomoravsky', 48.854742, 16.048985);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'U Krále Brabantského', '/english/cz/subterranea/$FileName$.html', '$FileName$', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Praha', 50.089020, 14.401547);
