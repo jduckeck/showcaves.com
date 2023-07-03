42760cdc154d4f665c3caac39039f182db062112
jd <jochen.duckeck@diginuevo.com>
2023-07-03 13:34:26 +0200

    Zum Turm

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Zum Turm', '/english/it/subterranea/ZumTurm.html', 'ZumTurm', 'Europe', 'it', 'Italy', 'subterranea', 'Trentino', 46.413664, 11.246132);
