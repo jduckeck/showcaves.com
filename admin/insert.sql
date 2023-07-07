8451de953bd04986077deea2ec61b415c7f08c8f
jd <jochen.duckeck@diginuevo.com>
2023-07-07 09:39:33 +0200

    Maison de l'Or en Limousin

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Zum Turm', '/english/it/subterranea/ZumTurm.html', 'ZumTurm', 'Europe', 'it', 'Italy', 'subterranea', 'Trentino', 46.413664, 11.246132);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Mine de la Rodde', '/english/fr/mines/Rodde.html', 'Rodde', 'Europe', 'fr', 'France', 'mines', 'Auvergne', 45.1534894, 3.3289368);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Maison de l\'Or en Limousin', '/english/fr/mines/Limousin.html', 'Limousin', 'Europe', 'fr', 'France', 'mines', 'Limousin', 45.5496209, 1.1304960);
