51d2dcbc6fa3aaf7dcccdeb2c8a7e4f92d8289af
jd <jochen.duckeck@diginuevo.com>
2023-07-31 19:01:31 +0200

    Rainbow Cave

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Rainbow Cave', '/english/il/karst/Keshet.html', 'Keshet', 'MiddleEast', 'il', 'Israel', 'karst', 'Northern', 33.076548, 35.195010);
