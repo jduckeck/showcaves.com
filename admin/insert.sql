7d6034ade976e4cd86bdf1f26a77fdeb3b26edce
jd <jochen.duckeck@diginuevo.com>
2023-08-01 17:32:38 +0200

    Honi HaMe'agel

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Rainbow Cave', '/english/il/karst/Keshet.html', 'Keshet', 'MiddleEast', 'il', 'Israel', 'karst', 'Northern', 33.076548, 35.195010);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Honi HaMe\'agel', '/english/il/subterranea/HoniHaMeagel.html', 'HoniHaMeagel', 'MiddleEast', 'il', 'Israel', 'subterranea', 'Northern', 32.983110, 35.534280);
