a0aeeece482c3fb8b5e8c271580ebce6880fc663
jduckeck <github@jochenduckeck.de>
2023-08-12 14:27:26 +0200

    SauveTerre Musée de Préhistoire

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gisement préhistorique du Martinet', '/english/fr/caves/Martinet.html', 'Martinet', 'Europe', 'fr', 'France', 'caves', 'Vezere', 44.5904232, 1.0137357);
