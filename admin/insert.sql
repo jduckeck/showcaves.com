00dd18deeb5a4243a5d00c8855a3f67932fcf355
jduckeck <github@jochenduckeck.de>
2023-08-12 20:47:35 +0200

    Chiesa Rupestre della Madonna della Croce

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Gisement préhistorique du Martinet', '/english/fr/caves/Martinet.html', 'Martinet', 'Europe', 'fr', 'France', 'caves', 'Vezere', 44.5904232, 1.0137357);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude, closed)
+VALUES ('yes', 'Chiesa Rupestre della Madonna della Croce', '/english/it/subterranea/MadonnaDellaCroce.html', 'MadonnaDellaCroce', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.659110, 16.620540, 1;
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotta di Chitaridd', '/english/it/subterranea/Chitaridd.html', 'Chitaridd', 'Europe', 'it', 'Italy', 'subterranea', 'Basilicata', 40.677680, 16.610200);
