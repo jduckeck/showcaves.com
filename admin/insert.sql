0773cf67a25ce8511691620df7ee822183fb304e
jduckeck <github@jochenduckeck.de>
2023-08-15 19:18:34 +0200

    La Cité de l’Or

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cité de l\'Or à Val-d\'Or', '/english/ca/mines/ValDOr.html', 'ValDOr', 'NorthAmerica', 'ca', 'Canada', 'mines', 'Quebec', 48.094356, -77.766324);
