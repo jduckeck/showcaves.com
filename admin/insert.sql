64a9b61c6f617a3ec65bcfbda2b739adc9a77a0f
jduckeck <github@jochenduckeck.de>
2023-08-17 22:13:28 +0200

    Heritage Blinman Mine

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cité de l\'Or à Val-d\'Or', '/english/ca/mines/ValDOr.html', 'ValDOr', 'NorthAmerica', 'ca', 'Canada', 'mines', 'Quebec', 48.094356, -77.766324);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Heritage Blinman Mine', '/english/au/mines/Blinman.html', 'Blinman', 'Oceania', 'au', 'Australia', 'mines', 'sa', -31.094057, 138.678290);
