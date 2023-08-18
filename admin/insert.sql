89cc36dbb5963fc833e9695288ab5f2fe8eb889c
jduckeck <github@jochenduckeck.de>
2023-08-18 16:22:52 +0200

    South Africa

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cité de l\'Or à Val-d\'Or', '/english/ca/mines/ValDOr.html', 'ValDOr', 'NorthAmerica', 'ca', 'Canada', 'mines', 'Quebec', 48.094356, -77.766324);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Heritage Blinman Mine', '/english/au/mines/Blinman.html', 'Blinman', 'Oceania', 'au', 'Australia', 'mines', 'sa', -31.094057, 138.678290);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ferropolis', '/english/de/mines/Ferropolis.html', 'Ferropolis', 'Europe', 'de', 'Germany', 'mines', 'Norddeutschland', 51.758696, 12.446327);
