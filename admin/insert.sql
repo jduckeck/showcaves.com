a84c099f9cdc3555b020344875b65bc891043e0f
jduckeck <github@jochenduckeck.de>
2023-08-23 19:17:51 +0200

    Capital Prize Gold Mine Tours

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Musée Minier et Métallurgique de Musson-Halanzy', '/english/be/mines/Halanzy.html', 'Halanzy', 'Europe', 'be', 'Belgium', 'mines', 'Luxembourg', 49.553837, 5.710392);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Maison de la Mine à Locmaria-Berrien', '/english/fr/mines/LocmariaBerrien.html', 'LocmariaBerrien', 'Europe', 'fr', 'France', 'mines', 'Bretagne', 48.353695, -3.694910);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Capital Prize Gold Mine Tours', '/english/usa/mines/CapitalPrize.html', 'CapitalPrize', 'NorthAmerica', 'us', 'United States of America', 'mines', 'co', 39.709552, -105.692469);
