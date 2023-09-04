023264023a4b07a39cd1a664a2455bcb0147ff8f
Jochen <jochen@showcaves.com>
2023-09-04 19:04:42 +0200

    Dělostřelecká Tvrz Hůrka

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Dělostřelecká Tvrz Hůrka', '/english/cz/subterranea/Hurka.html', 'Hurka', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Pardubicky', 50.0880856, 16.7691739);
