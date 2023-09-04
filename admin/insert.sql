7857a2b32923b3a2404247fdc910d25ab1fa4cbb
Jochen <jochen@showcaves.com>
2023-09-04 20:25:16 +0200

    Rudické propadání

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Dělostřelecká Tvrz Hůrka', '/english/cz/subterranea/Hurka.html', 'Hurka', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Pardubicky', 50.0880856, 16.7691739);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Rudické propadání', '/english/cz/karst/RudickePropadani.html', 'RudickePropadani', 'Europe', 'cz', 'Czech Republic', 'karst', 'Jihomoravsky', 49.3328282, 16.7333927);
