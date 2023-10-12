6515a7b2a50001cf2329c192a9fc64d195c47e16
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2023-10-12 11:15:16 +0200

    Cava Valsora

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Dělostřelecká Tvrz Hůrka', '/english/cz/subterranea/Hurka.html', 'Hurka', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Pardubicky', 50.0880856, 16.7691739);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Rudické propadání', '/english/cz/karst/RudickePropadani.html', 'RudickePropadani', 'Europe', 'cz', 'Czech Republic', 'karst', 'Jihomoravsky', 49.3328282, 16.7333927);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cava Valsora', '/english/it/mines/Valsora.html', 'Valsora', 'Europe', 'it', 'Italy', 'mines', 'Toscana', 44.066350,  10.222314);
