b0ed4dd1c3f8701e36cdd2e496010e3b8be60e3d
jduckeck <EqV7U2KrtfvCnlMGZKWh>
2023-10-15 19:17:58 +0200

    Grotta di Curtomartino

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Rudické propadání', '/english/cz/karst/RudickePropadani.html', 'RudickePropadani', 'Europe', 'cz', 'Czech Republic', 'karst', 'Jihomoravsky', 49.3328282, 16.7333927);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cava Valsora', '/english/it/mines/Valsora.html', 'Valsora', 'Europe', 'it', 'Italy', 'mines', 'Toscana', 44.066350,  10.222314);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grotta di Curtomartino', '/english/it/showcaves/Curtomartino.html', 'Curtomartino', 'Europe', 'it', 'Italy', 'showcaves', 'Puglia', 40.864473, 16.809641);
