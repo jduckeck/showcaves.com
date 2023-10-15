a1aa7527ab35fb3f5cb83b32d566fe26b603053c
jduckeck <github@jochenduckeck.de>
2023-10-15 19:13:47 +0200

    Maison du Mineur Grand-Combe

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Dělostřelecká Tvrz Hůrka', '/english/cz/subterranea/Hurka.html', 'Hurka', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Pardubicky', 50.0880856, 16.7691739);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Rudické propadání', '/english/cz/karst/RudickePropadani.html', 'RudickePropadani', 'Europe', 'cz', 'Czech Republic', 'karst', 'Jihomoravsky', 49.3328282, 16.7333927);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Maison du Mineur Grand-Combe', '/english/fr/mines/GrandCombe.html', 'GrandCombe', 'Europe', 'fr', 'France', 'mines', 'Occitanie', 44.2208509, 4.0344966);
