af3375f6f5523fa820c4a8fd5fecac936f2e7a7f
jduckeck <github@jochenduckeck.de>
2023-10-16 11:27:18 +0200

    Musée des Mines de la Brutz

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,16 +1,8 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude,
-                    Longitude)
-VALUES ('yes', 'Dělostřelecká Tvrz Hůrka', '/english/cz/subterranea/Hurka.html', 'Hurka', 'Europe', 'cz',
-        'Czech Republic', 'subterranea', 'Pardubicky', 50.0880856, 16.7691739);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude,
-                    Longitude)
-VALUES ('yes', 'Rudické propadání', '/english/cz/karst/RudickePropadani.html', 'RudickePropadani', 'Europe', 'cz',
-        'Czech Republic', 'karst', 'Jihomoravsky', 49.3328282, 16.7333927);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude,
-                    Longitude)
-VALUES ('yes', 'Maison du Mineur Grand-Combe', '/english/fr/mines/GrandCombe.html', 'GrandCombe', 'Europe', 'fr',
-        'France', 'mines', 'Occitanie', 44.2208509, 4.0344966);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude,
-                    Longitude)
-VALUES ('yes', 'Musée des Mines de la Brutz', '/english/fr/mines/Brutz.html', 'Brutz', 'Europe', 'fr', 'France',
-        'mines', 'Bretagne', 47.8096078, -1.4683432);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Dělostřelecká Tvrz Hůrka', '/english/cz/subterranea/Hurka.html', 'Hurka', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Pardubicky', 50.0880856, 16.7691739);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Rudické propadání', '/english/cz/karst/RudickePropadani.html', 'RudickePropadani', 'Europe', 'cz', 'Czech Republic', 'karst', 'Jihomoravsky', 49.3328282, 16.7333927);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Maison du Mineur Grand-Combe', '/english/fr/mines/GrandCombe.html', 'GrandCombe', 'Europe', 'fr', 'France', 'mines', 'Occitanie', 44.2208509, 4.0344966);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Musée des Mines de la Brutz', '/english/fr/mines/Brutz.html', 'Brutz', 'Europe', 'fr', 'France', 'mines', 'Bretagne', 47.8096078, -1.4683432);
