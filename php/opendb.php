df2052fdf91220ef407274d92f0465535507841b
jduckeck <github@jochenduckeck.de>
2023-10-19 19:14:11 +0200

    deploy

--- php/opendb.php
+++ php/opendb.php
@@ -4,23 +4,17 @@ function openDB()
 {
     $SERVER_NAME = $_SERVER['SERVER_NAME'];
 
-// Tragen Sie hier Ihre Werte ein!
-    if ($SERVER_NAME == "localhost") {
-        $server = "localhost";                      // MySQL-Server
-        $user = "db8399";                        // MySQL-Nutzer
-        $pass = "Kreibich";                            // MySQL-Kennwort
-        $dbname = "db8399";                        // Standarddatenbank
-    } else {
-        $server = "mysql5.showcaves.com";        // MySQL-Server
-        $user = "db8399_1";                        // MySQL-Nutzer
-        $pass = "JGdamUUpvJjhcz4oCwAI";            // MySQL-Kennwort
-        $dbname = "db8399_1";                        // Standarddatenbank
-    }
+    // Tragen Sie hier Ihre Werte ein!
+    $server = "mysql";                          // MySQL-Server
+    $user = "db8399_1";                         // MySQL-Nutzer
+    $pass = "JGdamUUpvJjhcz4oCwAI";             // MySQL-Kennwort
+    $dbname = "db8399_1";                       // Standarddatenbank
+
     $pdo = null;
     try {
         $pdo = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8", $user, $pass);
     } catch (PDOException $e) {
-        echo $e->getMessage();
+        echo $e;
     }
 
     return $pdo;
