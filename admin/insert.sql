5196f442eac14a5417224909cf3e319447364ffa
jduckeck <github@jochenduckeck.de>
2023-10-20 23:38:49 +0200

    broken links

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Frantoio Ipogeo Sternatia', '/english/it/subterranea/FrantoioIpogeoSternatia.html', 'FrantoioIpogeoSternatia', 'Europe', 'it', 'Italy', 'subterranea', 'Puglia', 40.219735, 18.228142);
