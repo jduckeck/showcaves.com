d58a94e93cdcb78d2b23e13f635fadbe77aa4140
jduckeck <github@jochenduckeck.de>
2023-10-21 12:18:06 +0200

    Mines de Fer d'Anjou

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Frantoio Ipogeo Sternatia', '/english/it/subterranea/FrantoioIpogeoSternatia.html', 'FrantoioIpogeoSternatia', 'Europe', 'it', 'Italy', 'subterranea', 'Puglia', 40.219735, 18.228142);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Musée des Mines et des Carrières à Flamanville', '/english/fr/mines/Flamanville.html', 'Flamanville', 'Europe', 'fr', 'France', 'mines', 'Normandie', 49.5296913, -1.8656912);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', "Mines de Fer d\'Anjou", '/english/fr/mines/Anjou.html', 'Anjou', 'Europe', 'fr', 'France', 'mines', 'PaysLoire', 47.70364921024803, -0.93154720030725);
