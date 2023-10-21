e68cc97365ad34103323c7091c934ddc7171a441
jduckeck <github@jochenduckeck.de>
2023-10-21 12:16:09 +0200

    Musée des Mines et des Carrières à Flamanville

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Frantoio Ipogeo Sternatia', '/english/it/subterranea/FrantoioIpogeoSternatia.html', 'FrantoioIpogeoSternatia', 'Europe', 'it', 'Italy', 'subterranea', 'Puglia', 40.219735, 18.228142);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Musée des Mines et des Carrières à Flamanville', '/english/fr/mines/Flamanville.html', 'Flamanville', 'Europe', 'fr', 'France', 'mines', 'Normandie', 49.5296913, -1.8656912);
