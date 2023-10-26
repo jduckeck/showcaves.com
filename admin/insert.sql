e32628159a5432bc8aa3b2198bd36918d3654bbb
jduckeck <github@jochenduckeck.de>
2023-10-26 16:21:41 +0200

    Wapsipinicon State Park Caves

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Wapsipinicon State Park Caves', '/english/usa/caves/Wapsipinicon.html', 'Wapsipinicon', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ia', 42.098067, -91.292460);
