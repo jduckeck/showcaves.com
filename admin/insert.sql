b35b23df40a86382786abf8819ae7d4060ea84a1
jduckeck <github@jochenduckeck.de>
2023-10-26 21:46:48 +0200

    Alabama's Natural Bridge

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Wapsipinicon State Park Caves', '/english/usa/caves/Wapsipinicon.html', 'Wapsipinicon', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ia', 42.098067, -91.292460);
+VALUES ('yes', 'Wapsipinicon State Park Caves', '/english/usa/caves/Wapsipinicon.html', 'Wapsipinicon', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ia', 42.098067, -91.292460);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', "Alabama's Natural Bridge", '/english/usa/karst/NaturalBridgeAL.html', 'NaturalBridgeAL', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'al', 34.0936, -87.6153);
