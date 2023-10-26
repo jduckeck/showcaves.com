9f8eea1fb66f88d0e7765d87dd4ebba5cdccffbb
jduckeck <github@jochenduckeck.de>
2023-10-26 23:05:01 +0200

    Cook Museum of Natural Science

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Wapsipinicon State Park Caves', '/english/usa/caves/Wapsipinicon.html', 'Wapsipinicon', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ia', 42.098067, -91.292460);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', "Alabama's Natural Bridge", '/english/usa/karst/NaturalBridgeAL.html', 'NaturalBridgeAL', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'al', 34.0936, -87.6153);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cook Museum', '/english/usa/subterranea/CookMuseum.html', 'CookMuseum', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'al', 34.605047, -86.982913);
