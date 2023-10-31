0c015b596d0fc7feac639fcdd1e2413a19cc4e0b
jduckeck <github@jochenduckeck.de>
2023-10-31 22:34:45 +0100

    URÊKA

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO static (path)
-VALUES('/english/de/topics/Schieferstrasse.html');
+VALUES ('/english/de/topics/Schieferstrasse.html');
 INSERT INTO static (path)
-VALUES('/german/de/topics/Schieferstrasse.html');
+VALUES ('/german/de/topics/Schieferstrasse.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'URÊKA', '/english/fr/mines/UREKA.html', 'UREKA', 'Europe', 'fr', 'France', 'mines', 'Limousin', 46.12341, 1.37204);
