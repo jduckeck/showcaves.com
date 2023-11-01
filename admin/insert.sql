5ab94b64b6a144bb78f5a9d92fcba29c7c97a38e
jduckeck <github@jochenduckeck.de>
2023-11-01 12:19:38 +0100

    Musée de la mine d’Escaro-Aytua

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO static (path)
 VALUES ('/german/de/topics/Schieferstrasse.html');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'URÊKA', '/english/fr/mines/UREKA.html', 'UREKA', 'Europe', 'fr', 'France', 'mines', 'Limousin', 46.12341, 1.37204);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Musée de la mine d’Escaro-Aytua', '/english/fr/mines/EscaroAytua.html', 'EscaroAytua', 'Europe', 'fr', 'France', 'mines', 'Languedoc', 42.536420, 2.315540);
