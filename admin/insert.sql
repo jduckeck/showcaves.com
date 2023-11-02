363af88747a9dde2864eeae25f0d8beeadedc6e0
jduckeck <github@jochenduckeck.de>
2023-11-02 11:00:35 +0100

    Gießenbachklamm

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'URÊKA', '/english/fr/mines/UREKA.html', 'UREKA', 'Europe', 'fr', 'France', 'mines', 'Limousin', 46.12341, 1.37204);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Musée de la mine d’Escaro-Aytua', '/english/fr/mines/EscaroAytua.html', 'EscaroAytua', 'Europe', 'fr', 'France', 'mines', 'Languedoc', 42.536420, 2.315540);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gießenbachklamm', '/english/de/gorges/Giessenbachklamm.html', 'Gießenbachklamm', 'Europe', 'de', 'Germany', 'gorges', 'Alpen', 47.6120181, 12.1463993);
