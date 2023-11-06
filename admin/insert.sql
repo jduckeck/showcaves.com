dfe76b0afb80b318256612490e7858e6ed11b59b
jduckeck <github@jochenduckeck.de>
2023-11-06 20:45:19 +0100

    Westwallmuseum Sinz

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Divnogorye', '/english/ru/subterranea/Divnogorye.html', 'Divnogorye', 'Asia', 'ru', 'Russia', 'subterranea', 'Voronezh', 50.9797419, 39.3185294);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Westwallmuseum Sinz', '/english/de/subterranea/WestwallmuseumSinz.html', 'WestwallmuseumSinz', 'Europe', 'de', 'Germany', 'subterranea', 'Saarland', 49.5355363, 6.4322032);
