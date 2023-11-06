96f1709aa4770d7ef3f5ce2683a29cb871a191df
jduckeck <github@jochenduckeck.de>
2023-11-06 12:26:15 +0100

    Divnogorye

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Divnogorye', '/english/ru/subterranea/Divnogorye.html', 'Divnogorye', 'Asia', 'ru', 'Russia', 'subterranea', 'Voronezh', 50.9797419, 39.3185294);
