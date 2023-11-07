b9534a5789a371aa2c465cfbb7085b79e95721f8
jduckeck <github@jochenduckeck.de>
2023-11-07 11:16:06 +0100

    Hermitage of St. Peter of Koriša

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Westwallmuseum Sinz', '/english/de/subterranea/WestwallmuseumSinz.html', 'WestwallmuseumSinz', 'Europe', 'de', 'Germany', 'subterranea', 'Saarland', 49.5355363, 6.4322032);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Manastir Crna Reka', '/english/rs/subterranea/CrnaReka.html', 'CrnaReka', 'Europe', 'rs', 'Serbia', 'subterranea', 'EasternSerbia', 42.9472694, 20.4681344);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hermitage of St. Peter of Koriša', '/english/rs/subterranea/StPeterKorisa.html', 'StPeterKorisa', 'Europe', 'rs', 'Serbia', 'subterranea', 'Kosovo', 42.2490971, 20.8306257);
