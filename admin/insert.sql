83ff5c94ab590e865616f0fa48d9c26508921a2a
jduckeck <github@jochenduckeck.de>
2023-11-07 13:15:37 +0100

    Westwallmuseum Mettlach

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Manastir Crna Reka', '/english/rs/subterranea/CrnaReka.html', 'CrnaReka', 'Europe', 'rs', 'Serbia', 'subterranea', 'EasternSerbia', 42.9472694, 20.4681344);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Hermitage of St. Peter of Koriša', '/english/rs/subterranea/StPeterKorisa.html', 'StPeterKorisa', 'Europe', 'rs', 'Serbia', 'subterranea', 'Kosovo', 42.2490971, 20.8306257);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Westwallmuseum Mettlach', '/english/de/subterranea/WestwallmuseumMettlach.html', 'WestwallmuseumMettlach', 'Europe', 'de', 'Germany', 'subterranea', 'Saarland', 49.4977594, 6.5971353);
