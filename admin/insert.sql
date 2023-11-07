99abd065060bec32fc2d0bed86d3bb3e1e7caeff
jduckeck <github@jochenduckeck.de>
2023-11-07 01:27:58 +0100

    Manastir Crna Reka

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Divnogorye', '/english/ru/subterranea/Divnogorye.html', 'Divnogorye', 'Asia', 'ru', 'Russia', 'subterranea', 'Voronezh', 50.9797419, 39.3185294);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Westwallmuseum Sinz', '/english/de/subterranea/WestwallmuseumSinz.html', 'WestwallmuseumSinz', 'Europe', 'de', 'Germany', 'subterranea', 'Saarland', 49.5355363, 6.4322032);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Manastir Crna Reka', '/english/rs/subterranea/CrnaReka.html', 'CrnaReka', 'Europe', 'rs', 'Serbia', 'subterranea', 'EasternSerbia', 42.9472694, 20.4681344);
