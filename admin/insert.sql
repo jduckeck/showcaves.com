fa7143512c636c883c0fc71d7047ae229edeaa9a
jduckeck <github@jochenduckeck.de>
2023-11-09 15:56:14 +0100

    Izvir Krupe

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', "Huangdi Dong - Emperor's Cave", '/english/cn/caves/Huangdi.html', 'Huangdi', 'Asia', 'cn', 'China', 'caves', 'Hainan', 18.961762, 109.139493);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Izvir Krupe', '/english/si/springs/Krupe.html', 'Krupe', 'Europe', 'si', 'Slovenia', 'springs', 'Dolenjska', 45.6349253, 15.2166396);
