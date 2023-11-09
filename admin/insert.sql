7a298a0cf9656d7752fcf6f7b94ed0a5dcfd7440
jduckeck <github@jochenduckeck.de>
2023-11-09 13:59:06 +0100

    Hainan

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', "Huangdi Dong - Emperor's Cave", '/english/cn/caves/Huangdi.html', 'Huangdi', 'Asia', 'cn', 'China', 'caves', 'Hainan', 18.961762, 109.139493);
