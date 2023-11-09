c17f3737e3d9fc32ed8c6a695919d5af6f84b803
jduckeck <github@jochenduckeck.de>
2023-11-09 20:36:09 +0100

    Santuario di Santa Rosalia

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', "Huangdi Dong - Emperor's Cave", '/english/cn/caves/Huangdi.html', 'Huangdi', 'Asia', 'cn', 'China', 'caves', 'Hainan', 18.961762, 109.139493);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Izvir Krupe', '/english/si/springs/Krupe.html', 'Krupe', 'Europe', 'si', 'Slovenia', 'springs', 'Dolenjska', 45.6349253, 15.2166396);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Santuario di Santa Rosalia', '/english/it/subterranea/SantaRosalia.html', 'SantaRosalia', 'Europe', 'it', 'Italy', 'subterranea', 'Sicilia', 38.168054, 13.351390);
