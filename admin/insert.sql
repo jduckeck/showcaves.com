be1a0e48e9b1f311f060e4e141eb01a98d152f20
jduckeck <github@jochenduckeck.de>
2023-12-03 19:00:56 +0100

    Medieval Louvre Underground

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Carrières des Capucins', '/english/fr/subterranea/CarrieresCapucins.html', 'CarrieresCapucins', 'Europe', 'fr', 'France', 'subterranea', 'IleDeFrance', 48.83736682233963, 2.3388313490967367);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Crypte archéologique de l’île de la Cité', '/english/fr/subterranea/Crypte.html', 'Crypte', 'Europe', 'fr', 'France', 'subterranea', 'IleDeFrance', 48.85365040000545, 2.347720164418369);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Medieval Louvre Underground', '/english/fr/subterranea/MedievalLouvre.html', 'MedievalLouvre', 'Europe', 'fr', 'France', 'subterranea', 'IleDeFrance', 48.86060404156743, 2.337627906749093);
