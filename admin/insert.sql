c0c2558cf5c3b741b2520002dcaba2c7e604b768
jduckeck <github@jochenduckeck.de>
2023-12-03 15:47:03 +0100

    Catacombe ebraiche di Venosa

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Catacombe ebraiche di Venosa', '/english/it/subterranea/Venosa.html', 'Venosa', 'Europe', 'it', 'Italy', 'subterranea', 'Calabria', 40.978263, 15.839754);
