203bd21bf72243eafc78dee01f43cffd1d019728
jduckeck <github@jochenduckeck.de>
2023-12-03 18:44:15 +0100

    Carrières des Capucins

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Catacombe ebraiche di Venosa', '/english/it/subterranea/Venosa.html', 'Venosa', 'Europe', 'it', 'Italy', 'subterranea', 'Calabria', 40.978263, 15.839754);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Carrières des Capucins', '/english/fr/subterranea/CarrieresCapucins.html', 'CarrieresCapucins', 'Europe', 'fr', 'France', 'subterranea', 'IleDeFrance', 48.83736682233963, 2.3388313490967367);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Crypte archéologique de l’île de la Cité', '/english/fr/subterranea/Crypte.html', 'Crypte', 'Europe', 'fr', 'France', 'subterranea', 'IleDeFrance', 48.85365040000545, 2.347720164418369);
