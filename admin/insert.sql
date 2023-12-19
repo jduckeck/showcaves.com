f414ab0c8d53ec1623e16b9871c348fb3402b114
jduckeck <github@jochenduckeck.de>
2023-12-19 18:16:49 +0100

    Venezuela

---
+++ admin/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cuevas del Pirata', '/english/ve/caves/Pirata.html', 'Pirata', 'SouthAmerica', 've', 'Venezuela', 'caves', 'Merida', 8.712908, -71.440680);
+UPDATE `sights`
+SET `Latitude`='10.1715562',`Longitude`='-63.5536255'
+WHERE `filename`='/english/ve/showcaves/Guacharo.html';
