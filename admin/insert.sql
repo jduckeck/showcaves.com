de95cfce98b0d924b68150ceede799907bab51ff
jduckeck <github@jochenduckeck.de>
2023-12-19 23:28:33 +0100

    Venezuela

--- admin/insert.sql
+++ admin/insert.sql
@@ -3,3 +3,9 @@ VALUES('yes','Cuevas del Pirata', '/english/ve/caves/Pirata.html', 'Pirata', 'So
 UPDATE `sights`
 SET `Latitude`='10.1715562',`Longitude`='-63.5536255'
 WHERE `filename`='/english/ve/showcaves/Guacharo.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Sima Aonda', '/english/ve/karst/Aonda.html', 'Aonda', 'SouthAmerica', 've', 'Venezuela', 'karst', 'Bolivar', 6.014336, -62.620804);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Sima Humboldt', '/english/ve/karst/Humboldt.html', 'Humboldt', 'SouthAmerica', 've', 'Venezuela', 'karst', 'Bolivar', 4.685503, -64.218624);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Sima Martel', '/english/ve/karst/Martel.html', 'Martel', 'SouthAmerica', 've', 'Venezuela', 'karst', 'Bolivar', 4.677700, -64.222039);
