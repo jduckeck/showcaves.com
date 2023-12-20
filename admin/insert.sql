d1cc676c61330c814b35cde5afe06dbb213cd6d0
jduckeck <github@jochenduckeck.de>
2023-12-20 19:39:00 +0100

    Cueva Charles Brewer

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Haitón del Guarataro', '/english/ve/karst/Guarataro.html', 'Guarataro', 'SouthAmerica', 've', 'Venezuela', 'karst', 'Falcon', 11.149748, -69.689774);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cueva Cataratas de Hueque', '/english/ve/caves/CataratasHueque.html', 'CataratasHueque', 'SouthAmerica', 've', 'Venezuela', 'caves', 'Falcon', 11.177169, -69.560762);
+UPDATE `sights`
+SET `Latitude`='5.456329',
+    `Longitude`='-62.456879'
+WHERE `filename` = '/english/ve/caves/Fantasma.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva Charles Brewer', '/english/ve/caves/CharlesBrewer.html', 'CharlesBrewer', 'SouthAmerica', 've', 'Venezuela', 'caves', 'Bolivar', 5.243401, -62.026454);
