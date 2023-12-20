fa1e5b93ded75a62a62b3bb5450f20cfb5e226dd
jduckeck <github@jochenduckeck.de>
2023-12-20 16:01:30 +0100

    Haitón del Guarataro

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Haitón del Guarataro', '/english/ve/karst/Guarataro.html', 'Guarataro', 'SouthAmerica', 've', 'Venezuela', 'karst', 'Falcon', 11.149748, -69.689774);
