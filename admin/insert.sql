461b435363e18d84a84792cc34b01eb99b4bb239
jduckeck <github@jochenduckeck.de>
2023-12-20 23:14:05 +0100

    İnceğiz Mağaraları

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'İnceğiz Mağaraları', '/english/tr/subterranea/Incegiz.html', 'Incegiz', 'Europe', 'tr', 'Turkey', 'subterranea', 'Marmara', 41.182269, 28.403944);
