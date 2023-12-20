b3b949be81d138688842d9d87300ab9672deeeb3
jduckeck <github@jochenduckeck.de>
2023-12-20 17:32:00 +0100

    Cueva Cataratas de Hueque

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Haitón del Guarataro', '/english/ve/karst/Guarataro.html', 'Guarataro', 'SouthAmerica', 've', 'Venezuela', 'karst', 'Falcon', 11.149748, -69.689774);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva Cataratas de Hueque', '/english/ve/caves/CataratasHueque.html', 'CataratasHueque', 'SouthAmerica', 've', 'Venezuela', 'caves', 'Falcon', 11.177169, -69.560762);
