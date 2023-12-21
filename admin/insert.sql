67a58e724e8f3f10002a6cee6ed5b433ac6a8ca3
jduckeck <github@jochenduckeck.de>
2023-12-21 02:05:29 +0100

    Marche

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,10 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'İnceğiz Mağaraları', '/english/tr/subterranea/Incegiz.html', 'Incegiz', 'Europe', 'tr', 'Turkey', 'subterranea', 'Marmara', 41.182269, 28.403944);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Sasso Spaccato', '/english/it/gorges/SassoSpaccato.html', 'SassoSpaccato', 'Europe', 'it', 'Italy', 'gorges', 'Marche', 42.7964021, 13.3806087);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotta del Petrienno', '/english/it/caves/Petrienno.html', 'Petrienno', 'Europe', 'it', 'Italy', 'caves', 'Marche', 42.8117567, 13.3738629);
+UPDATE `sights`
+SET `Latitude`='42.7711770',
+    `Longitude`='13.4097421'
+WHERE `filename` = '/english/it/caves/Acquasanta.html';
