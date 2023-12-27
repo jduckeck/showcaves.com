3ab0c5f607c3ca56d79a5e92ee677404ea39557c
jduckeck <github@jochenduckeck.de>
2023-12-27 20:52:41 +0100

    mv Oman

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,22 +1,4 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'İnceğiz Mağaraları', '/english/tr/subterranea/Incegiz.html', 'Incegiz', 'Europe', 'tr', 'Turkey', 'subterranea', 'Marmara', 41.182269, 28.403944);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Sasso Spaccato', '/english/it/gorges/SassoSpaccato.html', 'SassoSpaccato', 'Europe', 'it', 'Italy', 'gorges', 'Marche', 42.7964021, 13.3806087);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Grotta del Petrienno', '/english/it/caves/Petrienno.html', 'Petrienno', 'Europe', 'it', 'Italy', 'caves', 'Marche', 42.8117567, 13.3738629);
 UPDATE `sights`
-SET `Latitude`='42.7711770',
-    `Longitude`='13.4097421'
-WHERE `filename` = '/english/it/caves/Acquasanta.html';
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Lu Vurghe', '/english/it/springs/Vurghe.html', 'Vurghe', 'Europe', 'it', 'Italy', 'springs', 'Marche', 42.779694, 13.413842);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Gole del Garrafo', '/english/it/gorges/Garrafo.html', 'Garrafo', 'Europe', 'it', 'Italy', 'gorges', 'Marche', 42.7599573, 13.4070033);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Gola del Furlo', '/english/it/gorges/Furlo.html', 'Furlo', 'Europe', 'it', 'Italy', 'gorges', 'Marche', 43.6461857, 12.7228071);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Bruedertöbeli', '/english/ch/caves/Bruedertobeli.html', 'Bruedertobeli', 'Europe', 'ch', 'Switzerland', 'caves', 'StGallen', 47.3616736, 9.0919404);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Diamante Falls', '/english/cr/subterranea/Diamante.html', 'Diamante', 'MiddleAmerica', 'cr', 'Costa Rica', 'subterranea', 'SanJose', 9.257533, -83.774658);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Grotta del Sasso Petruccio', '/english/it/caves/SassoPetruccio.html', 'SassoPetruccio', 'Europe', 'it', 'Italy', 'caves', 'Marche', 42.7381876, 13.3744878);
+SET `Latitude`='23.0820677',
+    `Longitude`='57.3543586'
+WHERE `filename` = '/english/om/showcaves/AlHoti.html';
