6a14a6fc5554da04fc095378d335ce0772783fc9
jduckeck <github@jochenduckeck.de>
2023-12-28 20:01:30 +0100

    Oman

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Khadr Afrar Cave', '/english/om/caves/KhadrAfrar.html', 'KhadrAfrar', 'MiddleEast', 'om', 'Oman', 'caves', 'Dhofar', 17.130110, 54.235850);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ain Sahlounout', '/english/om/karst/AinSahlounout.html', 'AinSahlounout', 'MiddleEast', 'om', 'Oman', 'karst', 'Dhofar', 17.1482122, 54.1783788);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Mitha Cave', '/english/om/caves/Mitha.html', 'Mitha', 'MiddleEast', 'om', 'Oman', 'caves', 'Dhofar', 17.155680, 54.202310);
