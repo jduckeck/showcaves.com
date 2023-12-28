3d876374a2fcad1dffbc67fc7bbd4510d8adcdbd
jduckeck <github@jochenduckeck.de>
2023-12-28 21:36:19 +0100

    Moqal Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Ain Sahlounout', '/english/om/karst/AinSahlounout.html', 'AinSahlounout', 'MiddleEast', 'om', 'Oman', 'karst', 'Dhofar', 17.1482122, 54.1783788);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Mitha Cave', '/english/om/caves/Mitha.html', 'Mitha', 'MiddleEast', 'om', 'Oman', 'caves', 'Dhofar', 17.155680, 54.202310);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Moqal Cave', '/english/om/caves/Moqal.html', 'Moqal', 'MiddleEast', 'om', 'Oman', 'caves', 'AshSharqiyahNorth', 22.6238383, 59.0968204);
