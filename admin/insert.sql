9f6fdef146f151fb83408eb16d370be9d9ecf381
jduckeck <github@jochenduckeck.de>
2023-12-28 01:22:37 +0100

    mv Oman

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ittin Cave', '/english/om/caves/Ittin.html', 'Ittin', 'MiddleEast', 'om', 'Oman', 'caves', 'Dhofar', 17.1075187, 54.0644100);
