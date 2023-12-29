447460f26be8585f2f2aca73c0a98dc242de05a6
jduckeck <github@jochenduckeck.de>
2023-12-29 04:46:14 +0100

    Turkey tombs

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Mitha Cave', '/english/om/caves/Mitha.html', 'Mitha', 'MiddleEast', 'om', 'Oman', 'caves', 'Dhofar', 17.155680, 54.202310);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Moqal Cave', '/english/om/caves/Moqal.html', 'Moqal', 'MiddleEast', 'om', 'Oman', 'caves', 'AshSharqiyahNorth', 22.6238383, 59.0968204);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tombs of the Kings of Pontus', '/english/tr/subterranea/Pontus.html', 'Pontus', 'Europe', 'tr', 'Turkey', 'subterranea', 'BlackSea', 40.653600, 35.830420);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kapilikaya Rock Tomb', '/english/tr/subterranea/Kapilikaya.html', 'Kapilikaya', 'Europe', 'tr', 'Turkey', 'subterranea', 'BlackSea', 40.754317, 34.912595);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Aynalı Mağara', '/english/tr/subterranea/Aynali.html', 'Aynali', 'Europe', 'tr', 'Turkey', 'subterranea', 'BlackSea', 40.672490, 35.849250);
