d2f554f22a035622a818b7eb3072acec921822ca
jduckeck <github@jochenduckeck.de>
2024-01-03 20:32:39 +0100

    Kottukal Cave Temple

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Pakshipathalam Guha', '/english/in/caves/Pakshipathalam.html', 'Pakshipathalam', 'Asia', 'in', 'India', 'caves', 'Kerala', 11.937970916303367, 75.9616449795725);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Peralassery Subrahmanya Temple Pond', '/english/in/subterranea/PeralasserySubrahmanya.html', 'PeralasserySubrahmanya', 'Asia', 'in', 'India', 'subterranea', 'Kerala', 11.832742, 75.485602);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kottukal Cave Temple', '/english/in/subterranea/Kottukal.html', 'Kottukal', 'Asia', 'in', 'India', 'subterranea', 'Kerala', 8.8894728, 76.9052679);
