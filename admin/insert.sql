267cb071129806583935ac719fdc3beb6dd1bf26
jduckeck <github@jochenduckeck.de>
2024-01-03 15:40:17 +0100

    Peralassery Subrahmanya Temple Pond

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ SET `Latitude`='10.736979096880386',
 WHERE `filename` = '/english/in/caves/Punarjani.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Pakshipathalam Guha', '/english/in/caves/Pakshipathalam.html', 'Pakshipathalam', 'Asia', 'in', 'India', 'caves', 'Kerala', 11.937970916303367, 75.9616449795725);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Peralassery Subrahmanya Temple Pond', '/english/in/subterranea/PeralasserySubrahmanya.html', 'PeralasserySubrahmanya', 'Asia', 'in', 'India', 'subterranea', 'Kerala', 11.832742, 75.485602);
