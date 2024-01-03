44753378f3fb9363adb908e6d19aa49614466677
jduckeck <github@jochenduckeck.de>
2024-01-03 14:09:44 +0100

    Pakshipathalam Guha

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+UPDATE `sights`
+SET `Latitude`='10.736979096880386',
+    `Longitude`='76.44807257069641'
+WHERE `filename` = '/english/in/caves/Punarjani.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Pakshipathalam Guha', '/english/in/caves/Pakshipathalam.html', 'Pakshipathalam', 'Asia', 'in', 'India', 'caves', 'Kerala', 11.937970916303367, 75.9616449795725);
