136a98bd7440e8d1b982b1bf97ad30c2ebd2e2bf
jduckeck <github@jochenduckeck.de>
2024-01-13 14:06:20 +0100

    Přečerpávací vodní elektrárna Dlouhé Stráně

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO `static` (`path`)
 VALUES ('/german/explain/Misc/DestroyedCaves.html');
 INSERT INTO `static` (`path`)
 VALUES ('/english/explain/Misc/DestroyedCaves.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Přečerpávací vodní elektrárna Dlouhé Stráně', '/english/cz/subterranea/DlouheStrane.html', 'DlouheStrane', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Olomoucky', 50.0999844, 17.1160165);
