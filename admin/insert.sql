a22bf9552f4de84f38b6a9d50f0f195d8e37a4a0
jduckeck <github@jochenduckeck.de>
2024-01-14 15:54:08 +0100

    Aufberger Loch

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO `static` (`path`)
 VALUES ('/english/explain/Misc/DestroyedCaves.html');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Přečerpávací vodní elektrárna Dlouhé Stráně', '/english/cz/subterranea/DlouheStrane.html', 'DlouheStrane', 'Europe', 'cz', 'Czech Republic', 'subterranea', 'Olomoucky', 50.0999844, 17.1160165);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Aufberger Loch', '/english/de/karst/AufbergerLoch.html', 'AufbergerLoch', 'Europe', 'de', 'Germany', 'karst', 'SchwaebischeAlb', 48.404179, 9.244968);
