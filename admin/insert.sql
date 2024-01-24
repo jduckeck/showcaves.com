5c19c05edf2ff61244fb8ba7641bea3c99593449
jduckeck <github@jochenduckeck.de>
2024-01-24 09:43:46 +0100

    Ali Baba's Cave

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ali Baba\'s Cave', '/english/au/caves/AliBaba.html', 'AliBaba', 'Australia', 'au', 'Australia', 'caves', 'nsw', -33.4208325, 150.2336418);
