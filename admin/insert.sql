1844dbb2e3233cf1742aeb2070f28ae34fc38f03
jduckeck <github@jochenduckeck.de>
2024-01-24 12:13:34 +0100

    deploy

--- admin/insert.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Ali Baba\'s Cave', '/english/au/caves/AliBaba.html', 'AliBaba', 'Australia', 'au', 'Australia', 'caves', 'nsw', -33.4208325, 150.2336418);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Izvor Omble', '/english/hr/springs/Omble.html', 'Omble', 'Europe', 'hr', 'Croatia', 'springs', 'Omble', 42.675871, 18.136774);
