42903230ba926821ea83d211afd04ebd4c0e1064
jduckeck <github@jochenduckeck.de>
2024-01-27 00:44:02 +0100

    Grgosova Špilja

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,18 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Golub Špilja', '/english/hr/caves/Golub.html', 'Golub', 'Europe', 'hr', 'Croatia', 'caves', 'DubrovnikNeretva', 42.622190, 18.125310);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Izvor rijeke Bijele', '/english/hr/springs/Bijela.html', 'Bijela', 'Europe', 'hr', 'Croatia', 'springs', 'Zadar', 44.1348984, 15.6561984);
-UPDATE `sights`
-SET `Latitude`='44.3143024',`Longitude`='15.4750108'
-WHERE `filename`='/english/hr/showcaves/ManitaPec.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Podzemni grad Paklenice', '/english/hr/subterranea/Paklenica.html', 'Paklenica', 'Europe', 'hr', 'Croatia', 'subterranea', 'Zadar', 44.3056353, 15.4725513);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Dva Oka', '/english/hr/springs/DvaOka.html', 'DvaOka', 'Europe', 'hr', 'Croatia', 'springs', 'Split', 43.456720, 17.174996);
-UPDATE `sights`
-SET `Latitude`='43.454564',
-    `Longitude`='17.199074'
-WHERE `filename` = '/english/hr/karst/CrvenoJezero.html';
-UPDATE `sights`
-SET `Latitude`='45.4911651',
-    `Longitude`='14.6894457'
-WHERE `filename` = '/english/hr/springs/Kupa.html';
