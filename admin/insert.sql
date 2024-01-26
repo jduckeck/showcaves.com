e80d73350d7df77c1b0a2f028314a4b327a234a1
jduckeck <github@jochenduckeck.de>
2024-01-26 02:09:53 +0100

    Croatia Locations

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Golub Špilja', '/english/hr/caves/Golub.html', 'Golub', 'Europe', 'hr', 'Croatia', 'caves', 'DubrovnikNeretva', 42.622190, 18.125310);
