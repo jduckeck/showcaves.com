0017fcfd9a1287664df9b50c16ac4594f50ea060
jduckeck <github@jochenduckeck.de>
2024-01-26 14:26:31 +0100

    Izvor rijeke Bijele

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,7 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Golub Špilja', '/english/hr/caves/Golub.html', 'Golub', 'Europe', 'hr', 'Croatia', 'caves', 'DubrovnikNeretva', 42.622190, 18.125310);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Izvor rijeke Bijele', '/english/hr/springs/Bijela.html', 'Bijela', 'Europe', 'hr', 'Croatia', 'springs', 'Zadar', 44.1348984, 15.6561984);
+UPDATE `sights`
+SET `Latitude`='44.3143024',`Longitude`='15.4750108'
+WHERE `filename`='/english/hr/showcaves/ManitaPec.html';
