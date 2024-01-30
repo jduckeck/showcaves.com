2a434bba84df8bc69410fae38c063d220f8fd2ae
jduckeck <github@jochenduckeck.de>
2024-01-30 09:14:09 +0100

    Pećina Megara

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Pećina Megara', '/english/ba/caves/Megara.html', 'Megara', 'Europe', 'ba', 'Bosnia and Herzegovina', 'caves', 'Federation', 43.71777396243176, 18.08420866735654);
+UPDATE `sights`
+SET `Latitude`='43.6342922',
+    `Longitude`='17.9948946'
+WHERE `filename` = '/english/ba/subterranea/Tito.html';
