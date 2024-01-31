836b667a63e741de4ae4fdf8dd04a1f703ce8fb8
jduckeck <github@jochenduckeck.de>
2024-01-31 17:37:30 +0100

    Hrustovačka pećina

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Sarajevski Tunel Spasa', '/english/ba/subterranea/SarajevoRescueTunnel.html', 'SarajevoRescueTunnel', 'Europe', 'ba', 'Bosnia and Herzegovina', 'subterranea', 'Federation', 43.8197567, 18.3373110);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bobijaško oko', '/english/ba/karst/BobijaskoOko.html', 'BobijaskoOko', 'Europe', 'ba', 'Bosnia and Herzegovina', 'karst', 'Federation', 44.7017158, 16.4628874);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hrustovačka pećina', '/english/ba/caves/Hrustovo.html', 'Hrustovo', 'Europe', 'ba', 'Bosnia and Herzegovina', 'caves', 'Federation', 44.6726663, 16.7006463);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Dabarska pećina', '/english/ba/caves/Dabar.html', 'Dabar', 'Europe', 'ba', 'Bosnia and Herzegovina', 'caves', 'Federation', 44.7098185, 16.6386699);
