24802cfaa0231d02caa234a5dc5fe63a5eb6ef2a
jduckeck <github@jochenduckeck.de>
2024-02-10 21:21:04 +0100

    Izvoru Rijeke Krke

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Krka Spring', '/english/hr/springs/Krka.html', 'Krka', 'Europe', 'hr', 'Croatia', 'springs', 'SibenikKnin', 44.0424448, 16.2353325);
