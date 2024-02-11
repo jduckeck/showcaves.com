fb01061a9b79af3e931234afc6662ed41472c73c
jduckeck <github@jochenduckeck.de>
2024-02-11 14:45:23 +0100

    Barton Springs

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Krka Spring', '/english/hr/springs/Krka.html', 'Krka', 'Europe', 'hr', 'Croatia', 'springs', 'SibenikKnin', 44.0424448, 16.2353325);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Barton Springs', '/english/usa/springs/Barton.html', 'Barton', 'NorthAmerica', 'usa', 'United States of America', 'springs', 'tx', 30.2641, -97.7713);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Vrelo Bosne', '/english/ba/springs/Bosne.html', 'Bosne', 'Europe', 'ba', 'Bosnia and Herzegovina', 'springs', 'Federation', 43.8192831, 18.2679465);
