21f4884cd7727a69d5f8f3995c086408c8cbee3a
jduckeck <github@jochenduckeck.de>
2024-02-11 15:17:52 +0100

    Wadi Ghul

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Barton Springs', '/english/usa/springs/Barton.html', 'Barton', 'NorthAmerica', 'usa', 'United States of America', 'springs', 'tx', 30.2641, -97.7713);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Vrelo Bosne', '/english/ba/springs/Bosne.html', 'Bosne', 'Europe', 'ba', 'Bosnia and Herzegovina', 'springs', 'Federation', 43.8192831, 18.2679465);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Mina del Serranillo', '/english/es/mines/Serranillo.html', 'Serranillo', 'Europe', 'es', 'Spain', 'mines', 'Extremadura', 39.349869, -5.463793);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Wadi Ghul', '/english/om/gorges/WadiGhul.html', 'WadiGhul', 'MiddleEast', 'om', 'Oman', 'gorges', 'AdDakhiliyah', 23.190605, 57.211111);
