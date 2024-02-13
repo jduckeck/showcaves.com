0e05962b8e4aa54911cc91d89b8a61743c714327
jduckeck <github@jochenduckeck.de>
2024-02-13 01:48:18 +0100

    Sweden

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Mina del Serranillo', '/english/es/mines/Serranillo.html', 'Serranillo', 'Europe', 'es', 'Spain', 'mines', 'Extremadura', 39.349869, -5.463793);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Wadi Ghul', '/english/om/gorges/WadiGhul.html', 'WadiGhul', 'MiddleEast', 'om', 'Oman', 'gorges', 'AdDakhiliyah', 23.190605, 57.211111);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Höversbygrottan', '/english/se/caves/Hoversby.html', 'Hoversby', 'Europe', 'se', 'Sweden', 'caves', 'Ostergotland', 58.3878344, 16.0302662);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Lasse-Maja Grottan', '/english/se/caves/LasseMaja.html', 'LasseMaja', 'Europe', 'se', 'Sweden', 'caves', 'Kalmar', 57.4365235, 15.6231429);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grottan Johannes Kammare', '/english/se/caves/Johannes.html', 'Johannes', 'Europe', 'se', 'Sweden', 'caves', 'Orebro', 58.780030, 14.699067);
