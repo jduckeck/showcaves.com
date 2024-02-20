37c52be4806a6e163ac560823940afa387e46d8a
jduckeck <github@jochenduckeck.de>
2024-02-20 18:23:16 +0100

    Cisterna de San Roque

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cisterna de San Roque', '/english/es/subterranea/SanRoque.html', 'SanRoque', 'Europe', 'es', 'Spain', 'subterranea', 'Extremadura', 39.4723573, -6.3690184);
