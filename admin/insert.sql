6835ee7234d33606b86679232ce66939f4d32355
jduckeck <github@jochenduckeck.de>
2024-02-22 08:22:26 +0100

    Traben-Trarbacher Unterwelt

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cisterna de San Roque', '/english/es/subterranea/SanRoque.html', 'SanRoque', 'Europe', 'es', 'Spain', 'subterranea', 'Extremadura', 39.4723573, -6.3690184);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Traben-Trarbacher Unterwelt', '/english/de/subterranea/TrabenTrarbach.html', 'TrabenTrarbach', 'Europe', 'de', 'Germany', 'subterranea', 'Eifel', 49.9514039, 7.1187122);
+UPDATE `sights`
+SET `Latitude`='50.5863080',
+    `Longitude`='6.6492221'
+WHERE `filename` = '/english/de/mines/Guennersdorf.html';
