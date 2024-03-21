fd506e5a5c3b497b02f1bd566edd415680685e1e
jduckeck <github@jochenduckeck.de>
2024-03-21 23:11:23 +0100

    Tilas Stoll

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tilas Stoll', '/english/se/mines/TilasStoll.html', 'TilasStoll', 'Europe', 'se', 'Sweden', 'mines', 'Vaermland', 59.7477889, 14.2911373);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Wat Tham Talot', '/english/th/caves/Talot.html', 'Talot', 'Asia', 'th', 'Thailand', 'caves', 'Songkhla', 6.523701145911755, 100.82191170575574);
