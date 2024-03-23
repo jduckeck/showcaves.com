d9f90b6ab1ceec777df949104952c90bcac0cff0
jduckeck <github@jochenduckeck.de>
2024-03-23 18:25:08 +0100

    Pferdegöpel

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Tilas Stoll', '/english/se/mines/TilasStoll.html', 'TilasStoll', 'Europe', 'se', 'Sweden', 'mines', 'Vaermland', 59.7477889, 14.2911373);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Wat Tham Talot', '/english/th/caves/Talot.html', 'Talot', 'Asia', 'th', 'Thailand', 'caves', 'Songkhla', 6.523701145911755, 100.82191170575574);
+INSERT INTO static (path)
+VALUES ('/english/explain/Mining/Huthaus.html');
+INSERT INTO static (path)
+VALUES ('/german/explain/Mining/Huthaus.html');
+INSERT INTO static (path)
+VALUES ('/german/explain/Mining/Pferdegoepel.html');
+INSERT INTO static (path)
+VALUES ('/english/explain/Mining/Pferdegoepel.html');
