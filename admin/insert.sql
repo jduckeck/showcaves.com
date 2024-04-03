3a6d16a1476d635c5acf33dac7fb8031f4c06dd3
jduckeck <github@jochenduckeck.de>
2024-04-03 12:07:30 +0200

    kmz

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grotta di Seiano', '/english/it/subterranea/Seiano.html', 'Seiano', 'Europe', 'it', 'Italy', 'subterranea', 'Napoli', 40.7999824, 14.1762309);
