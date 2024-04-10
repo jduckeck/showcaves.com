38744db8da0edc460736f6015ca88cc0fe62e904
jduckeck <github@jochenduckeck.de>
2024-04-10 17:13:16 +0200

    Burg Marmels

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Burg Marmels', '/english/ch/caves/Marmels.html', 'Marmels', 'Europe', 'ch', 'Switzerland', 'caves', 'Graubuenden', 46.5066168, 9.6273281);
