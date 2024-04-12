4684f6134465930893b1c3da6678241e74cd4c92
jduckeck <github@jochenduckeck.de>
2024-04-12 23:26:58 +0200

    Serbia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Burg Marmels', '/english/ch/caves/Marmels.html', 'Marmels', 'Europe', 'ch', 'Switzerland', 'caves', 'Graubuenden', 46.5066168, 9.6273281);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Šalitrena Pećina', '/english/rs/caves/Salitrena.html', 'Salitrena', 'Europe', 'rs', 'Serbia', 'caves', 'WesternSerbia', 44.1907345, 20.0782511);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ribnička Pećina', '/english/rs/caves/Ribnica.html', 'Ribnica', 'Europe', 'rs', 'Serbia', 'caves', 'WesternSerbia', 44.2055709, 20.0923470);
