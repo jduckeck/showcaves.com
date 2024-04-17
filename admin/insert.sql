29610427d0715c4337b4270b9f567004b7ac603e
jduckeck <github@jochenduckeck.de>
2024-04-17 16:45:28 +0200

    Northern Ireland´s Secret Bunker

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Northern Ireland´s Secret Bunker', '/english/ie/subterranea/NIBunker.html', 'NIBunker', 'Europe', 'ie', 'Ireland', 'subterranea', 'Armagh', 54.45779, -6.51175);
