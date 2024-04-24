efa902251ba3b3280f83de0d187945b7b1192007
jduckeck <github@jochenduckeck.de>
2024-04-24 11:25:26 +0200

    deploy

--- admin/insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Northern Ireland´s Secret Bunker', '/english/ie/subterranea/NIBunker.html', 'NIBunker', 'Europe', 'ie', 'Ireland', 'subterranea', 'Armagh', 54.45779, -6.51175);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Cavan Burren Park', '/english/ie/karst/CavanBurrenPark.html', 'CavanBurrenPark', 'Europe', 'ie', 'Ireland', 'karst', 'Sligo', 54.2649844, -7.8884406);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Cueva de la Muerte', '/english/cr/caves/Muerte.html', 'Muerte', 'MiddleAmerica', 'cr', 'Costa Rica', 'caves', 'Alajuela', 10.32122, -84.24388);
