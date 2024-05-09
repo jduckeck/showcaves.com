ea617767360385e2342f9928614f9f1bb93928f9
jduckeck <github@jochenduckeck.de>
2024-05-09 23:06:43 +0200

    Iraq

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Shanidar Cave', '/english/other/showcaves/Shanidar.html', 'Shanidar', 'MiddleEast', 'iq', 'Iraq', 'showcaves', 'iq', 'other', 36.8348085, 44.2191486);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Hezarmerd Cave', '/english/other/caves/Hezarmerd.html', 'Hezarmerd', 'MiddleEast', 'iq', 'Iraq', 'caves', 'iq', 'other', 35.4939156, 45.3110587);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Jasana Cave', '/english/other/caves/Jasana.html', 'Jasana', 'MiddleEast', 'iq', 'Iraq', 'caves', 'iq', 'other', 35.8839450, 45.0533455);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Rabban Hormizd Monastery', '/english/other/subterranea/RabbanHormizd.html', 'RabbanHormizd', 'MiddleEast', 'iq', 'Iraq', 'subterranea', 'iq', 'other', 36.74912, 43.11555);
