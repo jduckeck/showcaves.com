1399b2467eae9487c80f022eaef541d9d78e819e
jduckeck <github@jochenduckeck.de>
2024-05-14 11:57:37 +0200

    Vịnh Mốc tunnels

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Vịnh Mốc tunnels', '/english/vn/subterranea/VinhMoc.html', 'VinhMoc', 'Asia', 'vn', 'Vietnam', 'subterranea', 'QuangTri', 17.0741960, 107.1098905);
