1353d6caf7d347f47f796f9cc07d20f86882c29a
jduckeck <github@jochenduckeck.de>
2024-05-16 17:07:53 +0200

    cave monasteries

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Vịnh Mốc tunnels', '/english/vn/subterranea/VinhMoc.html', 'VinhMoc', 'Asia', 'vn', 'Vietnam', 'subterranea', 'QuangTri', 17.0741960, 107.1098905);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Manastira Blagoveštenje', '/english/rs/subterranea/Blagovestenje.html', 'Blagovestenje', 'Europe', 'rs', 'Serbia', 'subterranea', 'EasternSerbia', 44.2722376, 21.5371201);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Manastir Gornjak', '/english/rs/subterranea/Gornjak.html', 'Gornjak', 'Europe', 'rs', 'Serbia', 'subterranea', 'EasternSerbia', 44.265743, 21.543618);
