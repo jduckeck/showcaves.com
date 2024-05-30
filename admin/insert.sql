f1311d4d3d36ec8874d01e0ecf1cc890648c3e3c
jduckeck <github@jochenduckeck.de>
2024-05-30 09:11:26 +0200

    Ninh Bình Province

---
+++ admin/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Động Vái Giời', '/english/vn/showcaves/VaiGioi.html', 'VaiGioi', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.2159867, 105.9008740);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Động Tiên Cá', '/english/vn/showcaves/TienCa.html', 'TienCa', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.2174119, 105.8903634);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Thien Ha Cave', '/english/vn/showcaves/ThienHa.html', 'ThienHa', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.2187378, 105.8855244);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hang Bụt', '/english/vn/showcaves/But.html', 'But', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.2168372, 105.8886320);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Động Vân Trình', '/english/vn/showcaves/VanTrinh.html', 'VanTrinh', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.306830427964577, 105.80006111533196);
