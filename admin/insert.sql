3f3b828b4e115759f1da2bebcd21895886fee026
jduckeck <github@jochenduckeck.de>
2024-05-30 10:16:04 +0200

    Buffalo Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Hang Bụt', '/english/vn/showcaves/But.html', 'But', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.2168372, 105.8886320);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Động Vân Trình', '/english/vn/showcaves/VanTrinh.html', 'VanTrinh', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.306830427964577, 105.80006111533196);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Buffalo Cave', '/english/vn/showcaves/Buffalo.html', 'Buffalo', 'Asia', 'vn', 'Vietnam', 'showcaves', 'NinhBinh', 20.2262326, 105.9188825);
