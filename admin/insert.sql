0ccd85080d298ae3de62919f368bb020d0794faf
jduckeck <github@jochenduckeck.de>
2024-06-02 14:46:21 +0200

    Morocco

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,1 +1,3 @@
 
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes',"Cascades D'Ouzoud", '/english/ma/karst/Ouzoud.html', 'Ouzoud', 'Africa', 'ma', 'Morocco', 'karst', 'BeniMellal', 32.0153547, -6.7192297);
