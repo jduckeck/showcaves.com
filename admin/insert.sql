eea2e120cfdbb0681d483d0484435e19b38db965
jduckeck <github@jochenduckeck.de>
2024-06-07 16:27:28 +0200

    Vietnam

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,13 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Hang Thiên Cảnh Sơn', '/english/vn/showcaves/ThienCanhSon.html', 'ThienCanhSon', 'Asia', 'vn', 'Vietnam', 'showcaves', 'QuangNinh', 20.8819256, 107.1695662);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Hang Tiên Ông', '/english/vn/showcaves/TienOng.html', 'TienOng', 'Asia', 'vn', 'Vietnam', 'showcaves', 'QuangNinh', 20.8158341, 107.1223542);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hang Chiều', '/english/vn/caves/Chieu.html', 'Chieu', 'Asia', 'vn', 'Vietnam', 'caves', 'HoaBinh', 20.6532208, 105.0844403);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Động Thác Bờ Ngòi Hoa', '/english/vn/showcaves/ThacBoNgoiHoa.html', 'ThacBoNgoiHoa', 'Asia', 'vn', 'Vietnam', 'showcaves', 'HoaBinh', 20.7571365, 105.1768867);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Động Hoa Tiên', '/english/vn/caves/HoaTien.html', 'HoaTien', 'Asia', 'vn', 'Vietnam', 'caves', 'HoaBinh', 20.741475823756378, 105.1265110737037);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Động Tiên Phi', '/english/vn/showcaves/TienPhi.html', 'TienPhi', 'Asia', 'vn', 'Vietnam', 'showcaves', 'HoaBinh', 20.86251613387124, 105.321888082118);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hang Mỏ Luông', '/english/vn/showcaves/MoLuong.html', 'MoLuong', 'Asia', 'vn', 'Vietnam', 'showcaves', 'HoaBinh', 20.6538170, 105.0789153);
