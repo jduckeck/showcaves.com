b781209046c4a98a336de9a9e9befcabc8f0e7f5
jduckeck <github@jochenduckeck.de>
2024-06-08 09:46:41 +0200

    Vietnam

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Động Mê Cung', '/english/vn/showcaves/MeCung.html', 'MeCung', 'Asia', 'vn', 'Vietnam', 'showcaves', 'QuangNinh', 20.8341057, 107.0845764);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hang Lùng Khúy', '/english/vn/showcaves/LungKhuy.html', 'LungKhuy', 'Asia', 'vn', 'Vietnam', 'showcaves', 'HaGiang', 23.0787737, 105.0199186);
