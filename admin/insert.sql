029ad15c9aecb83a3419a0601a98ef45efd09694
jduckeck <github@jochenduckeck.de>
2024-06-19 20:47:50 +0200

    Schronisko z Żyłą

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Jaskinie na Kadzielni', '/english/pl/showcaves/Kadzielnia.html', 'Kadzielnia', 'Europe', 'pl', 'Poland', 'showcaves', 'Swietokrzyskie', 50.8622220, 20.6183329);
