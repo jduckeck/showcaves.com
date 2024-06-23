704ea1f48d58db84d9ae6ec37193600f6c9e279a
jduckeck <github@jochenduckeck.de>
2024-06-23 06:05:41 +0200

    Cova Hospital de Santa Llúcia

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Jaskinie na Kadzielni', '/english/pl/showcaves/Kadzielnia.html', 'Kadzielnia', 'Europe', 'pl', 'Poland', 'showcaves', 'Swietokrzyskie', 50.8622220, 20.6183329);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cova Hospital de Santa Llúcia', '/english/es/caves/SantaLlucia.html', 'SantaLlucia', 'Europe', 'es', 'Spain', 'caves', 'Cataluna', 41.2869656, 0.7170745);
