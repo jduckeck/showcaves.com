47f7a3609e9f7c9fd71b0dd12b96a1aa667f54ee
jduckeck <github@jochenduckeck.de>
2024-06-27 23:06:16 +0200

    Museo della Miniera Trabia Tallarita

--- admin/insert.sql
+++ admin/insert.sql
@@ -26,3 +26,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Catacomba di Santa Lucia', '/english/it/subterranea/SantaLucia.html', 'SantaLucia', 'Europe', 'it', 'Italy', 'subterranea', 'Sicilia', 37.07305433607064, 15.29139874859614);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Antiche Grotte del Caricatore', '/english/it/subterranea/Caricatore.html', 'Caricatore', 'Europe', 'it', 'Italy', 'subterranea', 'Sicilia', 37.50505, 13.08204);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Museo della Miniera Trabia Tallarita', '/english/it/mines/TrabiaTallarita.html', 'TrabiaTallarita', 'Europe', 'it', 'Italy', 'mines', 'Sicilia', 37.29917, 14.03559);
