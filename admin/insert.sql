3cc66b35a86619690ed60ca42addfe3dbbd62e2b
jduckeck <github@jochenduckeck.de>
2024-07-01 23:24:18 +0200

    Fuente Concejo

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Fuente Concejo', '/english/es/springs/Concejo.html', 'Concejo', 'Europe', 'es', 'Spain', 'springs', 'Extremadura', 39.47260, -6.36803);
