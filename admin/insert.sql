1a2757b1e0b3e2af56b8a740064fccde2b1af70b
jduckeck <github@jochenduckeck.de>
2024-07-02 19:03:37 +0200

    Quintana Roo

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Fuente Concejo', '/english/es/springs/Concejo.html', 'Concejo', 'Europe', 'es', 'Spain', 'springs', 'Extremadura', 39.47260, -6.36803);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenote Jardin del Eden', '/english/mx/karst/JardinDelEden.html', 'JardinDelEden', 'MiddleAmerica', 'mx', 'México', 'karst', 'QuintanaRoo', 20.491576, -87.258332);
