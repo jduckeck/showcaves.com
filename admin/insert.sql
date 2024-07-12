005ce5709dfd137f6556eed196d25c9c00dd0032
jduckeck <github@jochenduckeck.de>
2024-07-12 23:46:21 +0200

    Asturias

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,18 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Maria SS. Addolorata a Grottasanta', '/english/it/subterranea/Grottasanta.html', 'Grottasanta', 'Europe', 'it', 'Italy', 'subterranea', 'Sicilia', 37.0859771, 15.2934441);
+UPDATE `sights`
+SET `Latitude`='43.4344072',
+    `Longitude`='-5.0725057'
+WHERE `filename` = '/english/es/caves/Cuevona.html';
+UPDATE `sights`
+SET `Latitude`='43.3070603',
+    `Longitude`='-5.0540810'
+WHERE `filename` = '/english/es/caves/Covadonga.html';
+UPDATE `sights`
+SET `Latitude`='43.4603631',
+    `Longitude`='-5.0676301'
+WHERE `filename` = '/english/es/showcaves/Ardines.html';
+UPDATE `sights`
+SET `Latitude`='43.4606631',
+    `Longitude`='-5.0676633'
+WHERE `filename` = '/english/es/showcaves/TitoBustillo.html';
