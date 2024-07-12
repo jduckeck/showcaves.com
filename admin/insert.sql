901479cdac122d2709b71491f9cd37c013ec2cf2
jduckeck <github@jochenduckeck.de>
2024-07-12 19:59:39 +0200

    Maria SS. Addolorata a Grottasanta

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Maria SS. Addolorata a Grottasanta', '/english/it/subterranea/Grottasanta.html', 'Grottasanta', 'Europe', 'it', 'Italy', 'subterranea', 'Sicilia', 37.0859771, 15.2934441);
