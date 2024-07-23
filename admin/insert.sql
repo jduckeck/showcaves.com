506c275f60c4e296e7bd988c70679d2a66340ede
jduckeck <github@jochenduckeck.de>
2024-07-23 16:28:41 +0200

    Shpella e Pirogoshit

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Kanioni i Osumit', '/english/al/gorges/Osum.html', 'Osum', 'Europe', 'al', 'Albania', 'gorges', 'Berat', 41.5173228, 19.80031616);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Shpella e Sari Salltikut', '/english/al/subterranea/SariSalltik.html', 'SariSalltik', 'Europe', 'al', 'Albania', 'subterranea', 'Durres', 41.51732441, 19.8003273);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Shpella e Pirogoshit', '/english/al/caves/Pirogoshi.html', 'Pirogoshi', 'Europe', 'al', 'Albania', 'caves', 'Berat', 40.5233506, 20.2579962);
