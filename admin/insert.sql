7888282f28086418b4500da7ab088a5d9626b675
jduckeck <github@jochenduckeck.de>
2024-07-23 15:41:24 +0200

    Albania

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kanioni i Osumit', '/english/al/gorges/Osum.html', 'Osum', 'Europe', 'al', 'Albania', 'gorges', 'Berat', 41.5173228, 19.80031616);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Shpella e Sari Salltikut', '/english/al/subterranea/SariSalltik.html', 'SariSalltik', 'Europe', 'al', 'Albania', 'subterranea', 'Durres', 41.51732441, 19.8003273);
