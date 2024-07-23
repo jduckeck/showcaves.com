f1a9e7a72edbae01218839c472f3b6142f565fde
jduckeck <github@jochenduckeck.de>
2024-07-23 20:28:42 +0200

    Bunk'Art

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Shpella e Sari Salltikut', '/english/al/subterranea/SariSalltik.html', 'SariSalltik', 'Europe', 'al', 'Albania', 'subterranea', 'Durres', 41.51732441, 19.8003273);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Shpella e Pirogoshit', '/english/al/caves/Pirogoshi.html', 'Pirogoshi', 'Europe', 'al', 'Albania', 'caves', 'Berat', 40.5233506, 20.2579962);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bunk´Art 1', '/english/al/subterranea/BunkArt1.html', 'BunkArt1', 'Europe', 'al', 'Albania', 'subterranea', 'Tirana', 41.3531404, 19.8612073);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Bunk´Art 2', '/english/al/subterranea/BunkArt2.html', 'BunkArt2', 'Europe', 'al', 'Albania', 'subterranea', 'Tirana', 41.3270225, 19.8195506);
