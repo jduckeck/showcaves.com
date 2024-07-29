60a84621af74154060cb5054237c87db28033144
jduckeck <github@jochenduckeck.de>
2024-07-29 05:16:57 +0200

    Cornwall

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bude Tunnel', '/english/gb/subterranea/Bude.html', 'Bude', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Cornwall', 50.8319055, -4.5465974);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Botallack Mine', '/english/gb/mines/Botallack.html', 'Botallack', 'Europe', 'gb', 'Great Britain', 'mines', 'Cornwall', 50.1430331, -5.6931398);
