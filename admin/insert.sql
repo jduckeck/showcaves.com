b806b7a3f2b84cf89c9b4186b30df034a2d27e67
jduckeck <github@jochenduckeck.de>
2024-07-29 21:46:57 +0200

    East Pool Mine

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Bude Tunnel', '/english/gb/subterranea/Bude.html', 'Bude', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Cornwall', 50.8319055, -4.5465974);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Botallack Mine', '/english/gb/mines/Botallack.html', 'Botallack', 'Europe', 'gb', 'Great Britain', 'mines', 'Cornwall', 50.1430331, -5.6931398);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'East Pool Mine', '/english/gb/mines/EastPool.html', 'EastPool', 'Europe', 'gb', 'Great Britain', 'mines', 'Cornwall', 50.2310378, -5.2620677);
