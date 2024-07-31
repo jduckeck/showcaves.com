ca4f9b5976d4616a84b99430b1042ca6a4634e5c
jduckeck <github@jochenduckeck.de>
2024-07-31 20:21:52 +0200

    Niasar/Reise

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Reise Cave', '/english/ir/subterranea/Reise.html', 'Reise', 'MiddleEast', 'ir', 'Iran', 'subterranea', 'Isfahan', 33.9717519, 51.1453457);
