5f455d48c9add37f37bdcb8f962fa49899c27f52
jduckeck <github@jochenduckeck.de>
2024-08-01 14:29:59 +0200

    Wonder World Cave

--- admin/insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Reise Cave', '/english/ir/subterranea/Reise.html', 'Reise', 'MiddleEast', 'ir', 'Iran', 'subterranea', 'Isfahan', 33.9717519, 51.1453457);
