8d4f2a3164a05921f7ee28c55d51c5d54e44b741
jduckeck <github@jochenduckeck.de>
2024-08-05 00:30:14 +0200

    Albania

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Drilon Spring', '/english/al/springs/Drilon.html', 'Drilon', 'Europe', 'al', 'Albania', 'springs', 'Korce', 40.891742, 20.712325);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Tushemisht Spring', '/english/al/springs/Tushemisht.html', 'Tushemisht', 'Europe', 'al', 'Albania', 'springs', 'Korce', 40.901835, 20.722102);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Zaveri Cave', '/english/al/karst/Zaveri.html', 'Zaveri', 'Europe', 'al', 'Albania', 'karst', 'Korce', 40.8703324, 20.9299778);
