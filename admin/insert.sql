ce818002174dd9a994c8f71c9f5576266a785e1f
jduckeck <github@jochenduckeck.de>
2024-08-05 22:31:52 +0200

    Chile

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Museo Mina Chiflón del Diablo', '/english/cl/mines/ChiflonDelDiablo.html', 'ChiflonDelDiablo', 'SouthAmerica', 'cl', 'Chile', 'mines', 'Biobio', -37.0761630, -73.1610240);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Mina Tránsito', '/english/cl/mines/Transito.html', 'Transito', 'SouthAmerica', 'cl', 'Chile', 'mines', 'Atacama', -27.52301, -70.34242);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Museo de Sitio Oficinas Salitreras Humberstone y Santa Laura', '/english/cl/mines/Salitreras.html', 'Salitreras', 'SouthAmerica', 'cl', 'Chile', 'mines', 'Tarapacа', -20.2089609, -69.7955091);
