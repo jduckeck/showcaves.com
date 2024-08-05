b8e63d08d696035932a9d41408c63c05ec07d349
jduckeck <github@jochenduckeck.de>
2024-08-05 23:39:26 +0200

    Chile

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Mina Tránsito', '/english/cl/mines/Transito.html', 'Transito', 'SouthAmerica', 'cl', 'Chile', 'mines', 'Atacama', -27.52301, -70.34242);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Museo de Sitio Oficinas Salitreras Humberstone y Santa Laura', '/english/cl/mines/Salitreras.html', 'Salitreras', 'SouthAmerica', 'cl', 'Chile', 'mines', 'Tarapacа', -20.2089609, -69.7955091);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Mina Candelaria', '/english/cl/mines/Candelaria.html', 'Candelaria', 'SouthAmerica', 'cl', 'Chile', 'mines', 'Atacama', -27.52301, -70.34242);
