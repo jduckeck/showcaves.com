fe8ecbb24226aa9ee46709c83a068cb7e86d566c
jduckeck <github@jochenduckeck.de>
2024-08-12 14:47:50 +0200

    Monastery Cave Hotel

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Monastery Cave Hotel', '/english/tr/subterranea/MonasteryCaveHotel.html', 'MonasteryCaveHotel', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.5825292, 34.8968632);
