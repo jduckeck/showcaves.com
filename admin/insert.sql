02866f5822f896c79ca161c62fe5cf394435e17b
jduckeck <github@jochenduckeck.de>
2024-08-12 10:21:00 +0200

    Northern Makedonia

--- admin/insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Grotta di Lourdes del Beato Claudio', '/english/it/subterranea/Lourdes.html', 'Lourdes', 'Europe', 'it', 'Italy', 'subterranea', 'Veneto', 45.5378309, 11.2862985);
