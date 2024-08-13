5afb61237dc419a245c4f5424795107c6c797b01
jduckeck <github@jochenduckeck.de>
2024-08-13 21:02:57 +0200

    Secret Bunker

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,15 +2,3 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Monastery Cave Hotel', '/english/tr/subterranea/MonasteryCaveHotel.html', 'MonasteryCaveHotel', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.5825292, 34.8968632);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Boiling Hole', '/english/bz/springs/Boiling.html', 'Boiling', 'MiddleAmerica', 'bz', 'Belize', 'springs', 'Cayo', 17.1610815, -88.6770151);
-UPDATE `sights`
-SET `Latitude`='17.1660527',
-    `Longitude`='-88.6832679'
-WHERE `filename` = '/english/bz/caves/CavesBranch.html';
-UPDATE `sights`
-SET `Latitude`='16.3218943',
-    `Longitude`='-88.9384330'
-WHERE `filename` = '/english/bz/caves/Tiger.html';
-UPDATE `sights`
-SET `Latitude`='17.1129487',
-    `Longitude`='-88.9293492'
-WHERE `filename` = '/english/bz/caves/BartonCreek.html';
