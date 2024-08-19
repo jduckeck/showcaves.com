329a42ed08381bf8b0277b372bb107b7d78472a0
jduckeck <github@jochenduckeck.de>
2024-08-20 01:56:02 +0200

    Sturmanns

---
+++ admin/insert.sql
@@ -0,0 +1,12 @@
+UPDATE `sights`
+SET `Latitude`='51.4249326',
+    `Longitude`='8.3223884'
+WHERE `filename` = '/english/de/showcaves/Bilstein.html';
+UPDATE `sights`
+SET `Latitude`='48.4311774',
+    `Longitude`='9.6838466'
+WHERE `filename` = '/english/de/showcaves/Sontheim.html';
+UPDATE `sights`
+SET `Latitude`='47.4360115',
+    `Longitude`='10.2352466'
+WHERE `filename` = '/english/de/showcaves/Sturmann.html';
