55e464d02d17eff5d407b860ef017cd223e7e64b
jduckeck <github@jochenduckeck.de>
2024-08-20 11:39:19 +0200

    location

--- admin/insert.sql
+++ admin/insert.sql
@@ -10,3 +10,16 @@ UPDATE `sights`
 SET `Latitude`='47.4360115',
     `Longitude`='10.2352466'
 WHERE `filename` = '/english/de/showcaves/Sturmann.html';
+UPDATE `sights`
+SET `Latitude`='50.924996',
+    `Longitude`='13.550532'
+WHERE `filename` = '/english/de/mines/Aurora.html';
+UPDATE `sights`
+SET `Latitude`='50.763124',
+    `Longitude`='13.768335'
+WHERE `filename` = '/english/de/mines/Altenberg.html';
+UPDATE `sights`
+SET `Latitude`='48.9126843',
+    `Longitude`='11.3955348'
+WHERE `filename` = '/english/de/caves/Arndt.html';
+
