5487c0dca24a473c57736a50648db79a293c3bda
jduckeck <github@jochenduckeck.de>
2024-08-24 01:25:22 +0200

    Queensway Mersey Tunnel

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Queensway Mersey Tunnel', '/english/gb/subterranea/Queensway.html', 'Queensway', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Merseyside', 53.404486, -2.994064);
