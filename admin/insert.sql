ad1a9fecaf2f31aecec4cb5bd82045d990c568a2
jduckeck <github@jochenduckeck.de>
2024-08-29 06:43:52 +0200

    Dumdalen Grotter

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Queensway Mersey Tunnel', '/english/gb/subterranea/Queensway.html', 'Queensway', 'Europe', 'gb', 'Great Britain', 'subterranea', 'Merseyside', 53.404486, -2.994064);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Dumdalen Grotter', '/english/no/caves/Dumdalen.html', 'Dumdalen', 'Europe', 'no', 'Norway', 'caves', 'Innlandet', 61.629974, 8.048961);
