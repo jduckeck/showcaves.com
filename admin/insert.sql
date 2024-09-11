bbd317a126285f7cc5c9ee7e77a7cacdd21c9a60
jduckeck <github@jochenduckeck.de>
2024-09-11 18:08:32 +0200

    Rock Springs

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,3 +8,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Three Sisters Springs', '/english/usa/springs/ThreeSisters.html', 'ThreeSisters', 'NorthAmerica', 'usa', 'United States of America', 'springs', 'fl', 28.888589, -82.589266);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Great Chamber', '/english/usa/caves/GreatChamber.html', 'GreatChamber', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'ut',);
+UPDATE `sights`
+SET `Latitude`='28.7564458',
+    `Longitude`='-81.5017342'
+WHERE `filename` = '/english/usa/springs/Rock.html';
