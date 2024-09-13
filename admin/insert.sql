558a450c0ec8f4fa98d7ba42a181c4280d7e9eb2
jduckeck <github@jochenduckeck.de>
2024-09-13 21:37:03 +0200

    Helsinki

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Formula Center', '/english/fi/subterranea/FormulaCenter.html', 'FormulaCenter', 'Europe', 'fi', 'Finland', 'subterranea', 'Helsinki', 60.2216700, 25.0694457);
