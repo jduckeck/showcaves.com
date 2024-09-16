5e16c58d96c8c785abebdb3a2b46b313afc3a39a
jduckeck <github@jochenduckeck.de>
2024-09-16 21:35:39 +0200

    Blue Grotto

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ UPDATE `sights`
 SET `Latitude`='29.4074933',
     `Longitude`='-82.4764325'
 WHERE `filename` = '/english/usa/springs/DevilsDen.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Blue Grotto Dive Resort', '/english/usa/karst/BlueGrotto.html', 'BlueGrotto', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'fl', 29.38789018148074, -82.48649451797534);
