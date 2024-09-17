d88fb8c1640babf4c4df4fc456d7fcdbbe44647f
jduckeck <github@jochenduckeck.de>
2024-09-17 07:36:45 +0200

    Devil's Den Prehistoric Spring

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,9 @@ SET `Latitude`='29.4074933',
 WHERE `filename` = '/english/usa/springs/DevilsDen.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Blue Grotto Dive Resort', '/english/usa/karst/BlueGrotto.html', 'BlueGrotto', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'fl', 29.38789018148074, -82.48649451797534);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', "Devil's Den", '/english/usa/karst/DevilsDenFL.html', 'DevilsDenFL', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'fl', 29.4074933, -82.4764325);
+UPDATE `sights`
+SET `Latitude`='35.779240',
+    `Longitude`='-94.252832'
+WHERE `filename` = '/english/usa/caves/DevilsDen.html';
