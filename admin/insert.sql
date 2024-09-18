55c356f88e6dbf5b39a46cc52d78196a8ee51255
jduckeck <github@jochenduckeck.de>
2024-09-18 12:32:08 +0200

    deploy

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,14 +1,1 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Formula Center', '/english/fi/subterranea/FormulaCenter.html', 'FormulaCenter', 'Europe', 'fi', 'Finland', 'subterranea', 'Helsinki', 60.2216700, 25.0694457);
-UPDATE `sights`
-SET `Latitude`='29.4074933',
-    `Longitude`='-82.4764325'
-WHERE `filename` = '/english/usa/springs/DevilsDen.html';
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Blue Grotto Dive Resort', '/english/usa/karst/BlueGrotto.html', 'BlueGrotto', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'fl', 29.38789018148074, -82.48649451797534);
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', "Devil's Den", '/english/usa/karst/DevilsDenFL.html', 'DevilsDenFL', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'fl', 29.4074933, -82.4764325);
-UPDATE `sights`
-SET `Latitude`='35.779240',
-    `Longitude`='-94.252832'
-WHERE `filename` = '/english/usa/caves/DevilsDen.html';
+
