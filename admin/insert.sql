93f43e0048dfc82fb3c7003c15a4f58147032486
jduckeck <github@jochenduckeck.de>
2024-09-20 22:35:09 +0200

    Iriyuji-Gama Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,4 +2,6 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Jaana Dragon Cave', '/english/jp/caves/Jaana.html', 'Jaana', 'Asia', 'jp', 'Japan', 'caves', 'Gifu', 35.747608, 137.064276);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Uduigama', '/english/jp/caves/Uduigama.html', 'Uduigama', 'Asia', 'jp', 'Japan', 'caves', 'Okinawa', 26.502077216773014, 127.84419403110996);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Iriyuji-Gama Cave', '/english/jp/caves/IriyujiGama.html', 'IriyujiGama', 'Asia', 'jp', 'Japan', 'caves', 'Okinawa', 26.123211, 127.749158);
 
