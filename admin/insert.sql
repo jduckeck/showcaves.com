396e8691c275f077cd4a41638c24cda77a13af86
jduckeck <github@jochenduckeck.de>
2024-09-20 16:33:47 +0200

    Dragon Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,1 +1,3 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Jaana Dragon Cave', '/english/jp/caves/Jaana.html', 'Jaana', 'Asia', 'jp', 'Japan', 'caves', 'Gifu', 35.747608, 137.064276);
 
