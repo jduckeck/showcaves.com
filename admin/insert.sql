2cea749e859a6b5938f798ae5717cedb4a7c519f
jduckeck <github@jochenduckeck.de>
2024-09-22 15:34:39 +0200

    Bunker Hagen

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,4 +4,6 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Uduigama', '/english/jp/caves/Uduigama.html', 'Uduigama', 'Asia', 'jp', 'Japan', 'caves', 'Okinawa', 26.502077216773014, 127.84419403110996);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Iriyuji-Gama Cave', '/english/jp/caves/IriyujiGama.html', 'IriyujiGama', 'Asia', 'jp', 'Japan', 'caves', 'Okinawa', 26.123211, 127.749158);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bunker Hagen', '/english/de/subterranea/Hagen.html', 'Hagen', 'Europe', 'de', 'Germany', 'subterranea', 'Sauerland', 51.357948, 7.465832);
 
