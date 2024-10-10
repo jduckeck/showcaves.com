c306fe57284588fe66b1ff18960f688d0884f182
jduckeck <github@jochenduckeck.de>
2024-10-10 23:04:56 +0200

    Soteska Peke

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Soteska Pekel', '/english/si/gorges/PekelSoteska.html', 'PekelSoteska', 'Europe', 'si', 'Slovenia', 'gorges', 'Notranjska', 45.890535, 14.372022);
