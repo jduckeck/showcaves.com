d89683d26978a5f45a3a1df445f6255802e96730
jduckeck <github@jochenduckeck.de>
2024-10-11 08:32:07 +0200

    Helgoland

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Soteska Pekel', '/english/si/gorges/PekelSoteska.html', 'PekelSoteska', 'Europe', 'si', 'Slovenia', 'gorges', 'Notranjska', 45.890535, 14.372022);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Zivilschutzbunker Helgoland', '/english/de/subterranea/Helgoland.html', 'Helgoland', 'Europe', 'de', 'Germany', 'subterranea', 'Norddeutschland', 54.1829841, 7.8855315);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bunkerstollen Unterland', '/english/de/subterranea/Unterland.html', 'Unterland', 'Europe', 'de', 'Germany', 'subterranea', 'Norddeutschland', 54.1823565, 7.8877857);
