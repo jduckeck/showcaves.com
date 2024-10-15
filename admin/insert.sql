9193f6e5b4cb6b7f10a5c13bd20a6b68e4385660
jduckeck <github@jochenduckeck.de>
2024-10-15 22:06:33 +0200

    Congo

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Zivilschutzbunker Helgoland', '/english/de/subterranea/Helgoland.html', 'Helgoland', 'Europe', 'de', 'Germany', 'subterranea', 'Norddeutschland', 54.1829841, 7.8855315);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bunkerstollen Unterland', '/english/de/subterranea/Unterland.html', 'Unterland', 'Europe', 'de', 'Germany', 'subterranea', 'Norddeutschland', 54.1823565, 7.8877857);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Nkila Ntari Cave', '/english/other/caves/NkilaNtari.html', 'NkilaNtari', 'Africa', 'cg', 'République du Congo', 'caves', 'cg', 'other', -4.120385, 13.852777);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Grottes de Mbanza-Ngungu', '/english/other/caves/MbanzaNgungu.html', 'MbanzaNgungu', 'Africa', 'cd', 'République Démocratique du Congo', 'caves', 'cd', 'other', -5.286058, 14.8770180);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Grotte Finzolua Ndombolozi', '/english/other/caves/FinzoluaNdombolozi.html', 'FinzoluaNdombolozi', 'Africa', 'cd', 'République Démocratique du Congo', 'caves', 'cd', 'other', -5.3116983, 14.9146031);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Grotte de Dimba', '/english/other/caves/Dimba.html', 'Dimba', 'Africa', 'cd', 'République Démocratique du Congo', 'caves', 'cd', 'other', -5.29039, 14.87052);
