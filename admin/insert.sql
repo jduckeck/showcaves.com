8dbb198b1d6bf30d748aa20258cc189f474f388f
jduckeck <github@jochenduckeck.de>
2024-10-24 11:53:02 +0200

    Bärenhöhle Oberammergau

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bärenhöhle Oberammergau', '/english/de/caves/BaerenOberammergau.html', 'BaerenOberammergau', 'Europe', 'de', 'Germany', 'caves', 'Alpen', 47.5839249, 11.0756475);
