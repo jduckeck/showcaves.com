bd726dfe18212f57a98ed589ca9b4b33ea2a60c8
jduckeck <github@jochenduckeck.de>
2024-10-24 17:45:57 +0200

    Wind Cave at House Rock

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bärenhöhle Oberammergau', '/english/de/caves/BaerenOberammergau.html', 'BaerenOberammergau', 'Europe', 'de', 'Germany', 'caves', 'Alpen', 47.5839249, 11.0756475);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Wind Cave at House Rock', '/english/usa/caves/WindHouseRock.html', 'Wind', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'pa', 39.8746109, -76.3711313);
