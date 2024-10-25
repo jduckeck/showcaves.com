c72e410cb1f7c35bd6cd3f25be415fdd7c1a04cb
jduckeck <github@jochenduckeck.de>
2024-10-25 02:20:43 +0200

    Steinerne Rinne bei Hechlingen

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Bärenhöhle Oberammergau', '/english/de/caves/BaerenOberammergau.html', 'BaerenOberammergau', 'Europe', 'de', 'Germany', 'caves', 'Alpen', 47.5839249, 11.0756475);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Wind Cave at House Rock', '/english/usa/caves/WindHouseRock.html', 'Wind', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'pa', 39.8746109, -76.3711313);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Steinerne Rinne bei Hechlingen', '/english/de/karst/Hechlingen.html', 'Hechlingen', 'Europe', 'de', 'Germany', 'karst', 'FraenkischeAlb', 48.9890432, 10.7351094);
