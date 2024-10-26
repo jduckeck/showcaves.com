aa4635682408cf82784b020583519a5eb73a0a73
jduckeck <github@jochenduckeck.de>
2024-10-26 19:28:56 +0200

    Pirunpesä

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,7 +5,7 @@ VALUES ('yes', 'Wind Cave at House Rock', '/english/usa/caves/WindHouseRock.html
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Steinerne Rinne bei Hechlingen', '/english/de/karst/Hechlingen.html', 'Hechlingen', 'Europe', 'de', 'Germany', 'karst', 'FraenkischeAlb', 48.9890432, 10.7351094);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Pirunpesä', '/english/fi/karst/Pirunpesa.html', 'Pirunpesa', 'Europe', 'fi', 'Finland', 'karst', '$Region$', 62.3990652, 22.8954193);
+VALUES ('yes', 'Pirunpesä', '/english/fi/karst/PirunpesaYylivalli.html', 'Pirunpesa', 'Europe', 'fi', 'Finland', 'karst', '$Region$', 62.3990652, 22.8954193);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Pirunpesä', '/english/fi/caves/PirunpesaSarkola.html', 'PirunpesaSarkola', 'Europe', 'fi', 'Finland', 'caves', 'WesternFinland', 61.4285, 23.2107);
 INSERT INTO static (path)
