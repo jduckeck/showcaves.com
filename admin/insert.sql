f8ffc6505f36842e8024e5007a07eecb082c62c5
jduckeck <github@jochenduckeck.de>
2024-10-26 22:58:55 +0200

    Abri Cro-Magnon

--- admin/insert.sql
+++ admin/insert.sql
@@ -8,5 +8,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Pirunpesä', '/english/fi/karst/PirunpesaYylivalli.html', 'Pirunpesa', 'Europe', 'fi', 'Finland', 'karst', '$Region$', 62.3990652, 22.8954193);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Pirunpesä', '/english/fi/caves/PirunpesaSarkola.html', 'PirunpesaSarkola', 'Europe', 'fi', 'Finland', 'caves', 'WesternFinland', 61.4285, 23.2107);
-INSERT INTO static (path)
-VALUES ('/english/fi/topics/Longest.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Pirunpesä', '/english/fi/gorges/PirunpesaHollola.html', 'PirunpesaHollola', 'Europe', 'fi', 'Finland', 'gorges', 'SouthernFinland', 61.0090748, 25.5360621);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Paistjärven Pirunkirkko', '/english/fi/caves/PaistjarvenPirunkirkko.html', 'PaistjarvenPirunkirkko', 'Europe', 'fi', 'Finland', 'caves', 'SouthernFinland',);
