ee0f103ca0e731be94f0ab1f423da8568a036146
jduckeck <github@jochenduckeck.de>
2024-11-02 15:36:25 +0100

    Spilaio Peristeri

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Polje of Valsamata', '/english/gr/karst/Valsamata.html', 'Valsamata', 'Europe', 'gr', 'Greece', 'karst', 'IonianIslands', 38.17190, 20.58535);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Mavri Spilia', '/english/gr/caves/MavriZakynthos.html', 'MavriZakynthos', 'Europe', 'gr', 'Greece', 'caves', 'IonianIslands', 37.8049767, 20.7600483);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Spilaio Peristeri', '/english/gr/caves/Peristeri.html', 'Peristeri', 'Europe', 'gr', 'Greece', 'caves', 'Attica', 37.5734534, 23.3601740);
