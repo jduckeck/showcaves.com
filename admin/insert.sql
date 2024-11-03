e55b8ac2f9dbaa88cd925067d2f3c12a9af77774
jduckeck <github@jochenduckeck.de>
2024-11-03 15:56:14 +0100

    Cave of the Nymphs

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,4 +5,4 @@ VALUES ('yes', 'Mavri Spilia', '/english/gr/caves/MavriZakynthos.html', 'MavriZa
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Spilaio Peristeri', '/english/gr/caves/Peristeri.html', 'Peristeri', 'Europe', 'gr', 'Greece', 'caves', 'Attica', 37.5734534, 23.3601740);
 INSERT INTO sights (visible, closed, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 1, 'Spílaio Nymfón', '/english/gr/caves/Nymfon.html', 'Nymfon', 'Europe', 'gr', 'Greece', 'caves', 'IonianIslands', 38.3610457, 20.7065059);
+VALUES ('yes', 1, 'Spílaio Nymfón', '/english/gr/showcaves/Nymfon.html', 'Nymfon', 'Europe', 'gr', 'Greece', 'showcaves', 'IonianIslands', 38.3610457, 20.7065059);
