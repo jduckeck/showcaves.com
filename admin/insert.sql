bd2c0423db07bc669d0c1c4b225f0a67116adc64
jduckeck <github@jochenduckeck.de>
2024-11-12 12:16:29 +0100

    United Arab Emirates

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Ski Dubai Ice Cave', '/english/other/subterranea/SkiDubai.html', 'SkiDubai', 'MiddleEast', 'ae', 'United Arab Emirates', 'subterranea', 'ae', 'other', 25.117262, 55.198799);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Atlantic Cave', '/english/other/subterranea/Atlantic.html', 'Atlantic', 'MiddleEast', 'ae', 'United Arab Emirates', 'subterranea', 'ae', 'other', 24.4019770, 54.4958539);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Qasar Masafi Restaurant', '/english/other/subterranea/QasarMasafi.html', 'QasarMasafi', 'MiddleEast', 'ae', 'United Arab Emirates', 'subterranea', 'ae', 'other', 25.326937, 56.167702);
