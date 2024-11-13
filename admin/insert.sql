7b12e80ba9d3da3a652c0c6db6c1ccc145333995
jduckeck <github@jochenduckeck.de>
2024-11-13 11:27:36 +0100

    locations updates

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,19 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Qasar Masafi Restaurant', '/english/other/subterranea/QasarMasafi.html', 'QasarMasafi', 'MiddleEast', 'ae', 'United Arab Emirates', 'subterranea', 'ae', 'other', 25.326937, 56.167702);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES ('yes', 'Al Nahwah Cave', '/english/other/caves/AlNahwah.html', 'AlNahwah', 'MiddleEast', 'ae', 'United Arab Emirates', 'caves', 'ae', 'other', 25.274900, 56.272987);
+UPDATE `sights`
+SET `Latitude`='40.9997796',
+    `Longitude`='22.8703363'
+WHERE `filename` = '/english/gr/showcaves/AgiosGeorgios.html';
+UPDATE `sights`
+SET `Latitude`='37.6957976',
+    `Longitude`='26.9370579'
+WHERE `filename` = '/english/gr/subterranea/PanagiaSpiliani.html';
+UPDATE `sights`
+SET `Latitude`='44.2415929',
+    `Longitude`='7.8399071'
+WHERE `filename` = '/english/it/showcaves/Bossea.html';
+UPDATE `sights`
+SET `Latitude`='44.1197837',
+    `Longitude`='7.8954581'
+WHERE `filename` = '/english/it/subterranea/Messere.html';
