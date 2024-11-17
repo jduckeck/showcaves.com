984a424d5cad67107a0bec4af09adf48790e45b5
jduckeck <github@jochenduckeck.de>
2024-11-17 13:47:14 +0100

    Greek

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+UPDATE `sights`
+SET `Latitude`='35.2082840',`Longitude`='24.8288981'
+WHERE `filename`='/english/gr/caves/IdaioAndro.html';
+UPDATE `sights`
+SET `Latitude`='35.1628621',`Longitude`='25.4450643'
+WHERE `filename`='/english/gr/showcaves/Diktaion.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Trapeza Cave', '/english/gr/caves/Trapeza.html', 'Trapeza', 'Europe', 'gr', 'Greece', 'caves', 'Crete', 35.1977875, 25.4938592);
