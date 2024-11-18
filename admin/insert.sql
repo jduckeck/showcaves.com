893653552fd85241aa6496d82dca7c4dfa209143
jduckeck <github@jochenduckeck.de>
2024-11-18 22:10:50 +0100

    Buca del Corno

--- admin/insert.sql
+++ admin/insert.sql
@@ -14,3 +14,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Taforalt', '/english/ma/caves/Taforalt.html', 'Taforalt', 'Africa', 'ma', 'Morocco', 'caves', 'Oriental', 34.8138284, -2.4037466);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Wat Tham Khao Kuha', '/english/th/caves/KhaoKuha.html', 'KhaoKuha', 'Asia', 'th', 'Thailand', 'caves', 'SuratThani', 9.1545867, 99.4707766);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Buca del Corno', '/english/it/showcaves/Corno.html', 'Corno', 'Europe', 'it', 'Italy', 'showcaves', 'Lombardia', 45.6943539, 9.8801034);
