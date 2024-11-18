3a4e41b76f49c66656a4d99dfb05700d8b907441
jduckeck <github@jochenduckeck.de>
2024-11-18 14:03:35 +0100

    Artificial Intelligence

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,7 @@ SET `Latitude`='35.1628621',`Longitude`='25.4450643'
 WHERE `filename`='/english/gr/showcaves/Diktaion.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Trapeza Cave', '/english/gr/caves/Trapeza.html', 'Trapeza', 'Europe', 'gr', 'Greece', 'caves', 'Crete', 35.1977875, 25.4938592);
+INSERT INTO `static` (`path`)
+VALUES ('/german/explain/Misc/AI.html');
+INSERT INTO `static` (`path`)
+VALUES ('/english/explain/Misc/AI.html');
