0e03e871fa60f36e4429475a49e2ac3a81af282a
jduckeck <github@jochenduckeck.de>
2024-11-19 23:37:42 +0100

    Grotte d'Eprave

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,36 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'La maison des Terrils', '/english/be/mines/Terrils.html', 'Terrils', 'Europe', 'be', 'Belgium', 'mines', 'Liege', 50.6287510, 5.5115341);
-UPDATE `sights`
-SET `Latitude`='35.2082840',`Longitude`='24.8288981'
-WHERE `filename`='/english/gr/caves/IdaioAndro.html';
-UPDATE `sights`
-SET `Latitude`='35.1628621',`Longitude`='25.4450643'
-WHERE `filename`='/english/gr/showcaves/Diktaion.html';
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES('yes','Trapeza Cave', '/english/gr/caves/Trapeza.html', 'Trapeza', 'Europe', 'gr', 'Greece', 'caves', 'Crete', 35.1977875, 25.4938592);
-INSERT INTO `static` (`path`)
-VALUES ('/german/explain/Misc/AI.html');
-INSERT INTO `static` (`path`)
-VALUES ('/english/explain/Misc/AI.html');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Taforalt', '/english/ma/caves/Taforalt.html', 'Taforalt', 'Africa', 'ma', 'Morocco', 'caves', 'Oriental', 34.8138284, -2.4037466);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Wat Tham Khao Kuha', '/english/th/caves/KhaoKuha.html', 'KhaoKuha', 'Asia', 'th', 'Thailand', 'caves', 'SuratThani', 9.1545867, 99.4707766);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Buca del Corno', '/english/it/showcaves/Corno.html', 'Corno', 'Europe', 'it', 'Italy', 'showcaves', 'Lombardia', 45.6943539, 9.8801034);
-UPDATE `sights`
-SET `Latitude`='41.094259',
-    `Longitude`='16.076351'
-WHERE `filename` = '/english/it/caves/SanMicheleMinervinoMurge.html';
-UPDATE `sights`
-SET `Latitude`='42.3794997',
-    `Longitude`='12.8288346'
-WHERE `filename` = '/english/it/caves/SacroSpeco.html';
-UPDATE `sights`
-SET `Latitude`='40.6933242',
-    `Longitude`='15.1481749'
-WHERE `filename` = '/english/it/caves/SanMicheleCampagna.html';
-INSERT INTO static (path)
-VALUES ('/english/explain/Mining/SlagHeap.html');
-INSERT INTO static (path)
-VALUES ('/german/explain/Mining/SlagHeap.html');
