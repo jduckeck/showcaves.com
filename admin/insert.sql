5ed83cbff3b7a9531c4d80591a6e3dd935cc680e
jduckeck <github@jochenduckeck.de>
2024-11-20 03:32:04 +0100

    be

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,11 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Wat Tham Khao Kuha', '/english/th/caves/KhaoKuha.html', 'KhaoKuha', 'Asia', 'th', 'Thailand', 'caves', 'SuratThani', 9.1545867, 99.4707766);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Buca del Corno', '/english/it/showcaves/Corno.html', 'Corno', 'Europe', 'it', 'Italy', 'showcaves', 'Lombardia', 45.6943539, 9.8801034);
+UPDATE `sights`
+SET `Latitude`='50.8470547',
+    `Longitude`='2.8917740'
+WHERE `filename` = '/english/be/subterranea/KazemattenBrewery.html';
+UPDATE `sights`
+SET `Latitude`='50.8505012',
+    `Longitude`='2.8911355'
+WHERE `filename` = '/english/be/subterranea/KasemattenBrasserie.html';
