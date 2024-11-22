f4290b0ae8cc7b771aa19766456e894fb4a658e8
jduckeck <github@jochenduckeck.de>
2024-11-23 00:58:56 +0100

    Kocaín Mağarası

---
+++ admin/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gedelme Mağarası', '/english/tr/showcaves/Gedelme.html', 'Gedelme', 'Europe', 'tr', 'Turkey', 'showcaves', 'Mediterranean', 36.6128264, 30.4416728);
+UPDATE `sights`
+SET `Latitude`='37.2325329',
+    `Longitude`='30.7113959'
+WHERE `filename` = '/english/tr/caves/Kocain.html';
