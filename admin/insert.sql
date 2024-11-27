25aac3dd25b5c66dae1d31b7f1d704f5eaaa98e5
jduckeck <github@jochenduckeck.de>
2024-11-27 12:41:43 +0100

    fixes

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Cave of Skalia', '/english/gr/caves/Skalia.html', 'Skalia', 'Europe', 'gr', 'Greece', 'caves', 'Dodecanese', 37.0298143, 26.9582624);
