7af3ef96637ea8916b1e99ed9fa3d1be49ce410e
jduckeck <github@jochenduckeck.de>
2024-12-02 07:32:09 +0100

    Jindeng Temple

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,7 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Cave of Skalia', '/english/gr/caves/Skalia.html', 'Skalia', 'Europe', 'gr', 'Greece', 'caves', 'Dodecanese', 37.0298143, 26.9582624);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Jindeng Temple', '/english/cn/subterranea/Jindeng.html', 'Jindeng', 'Asia', 'cn', 'China', 'subterranea', 'Shanxi', 36.0201079, 113.6851259);
+UPDATE `sights`
+SET `Latitude`='35.0387189',`Longitude`='106.6716599'
+WHERE `filename`='/english/cn/caves/LongmenBaoji.html';
