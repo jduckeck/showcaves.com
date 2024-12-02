8b5b4e10d1784fa4f27b31f65854bdc84926e5d0
jduckeck <github@jochenduckeck.de>
2024-12-02 23:13:02 +0100

    华阳洞 - Huayangdong

--- admin/insert.sql
+++ admin/insert.sql
@@ -5,3 +5,14 @@ VALUES('yes','Jindeng Temple', '/english/cn/subterranea/Jindeng.html', 'Jindeng'
 UPDATE `sights`
 SET `Latitude`='35.0387189',`Longitude`='106.6716599'
 WHERE `filename`='/english/cn/caves/LongmenBaoji.html';
+UPDATE `sights`
+SET `Latitude`='50.6285971',`Longitude`='123.6040356'
+WHERE `filename`='/english/cn/showcaves/Gaxian.html';
+UPDATE `sights`
+SET `Latitude`='29.7515252',`Longitude`='118.3887295'
+WHERE `filename`='/english/cn/subterranea/Huashan.html';
+UPDATE `sights`
+SET `Latitude`='31.7907363',`Longitude`='118.1274294'
+WHERE `filename`='/english/cn/showcaves/HuayangAnhui.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Huayang Cave', '/english/cn/showcaves/HuayangJiangsu.html', 'HuayangJiangsu', 'Asia', 'cn', 'China', 'showcaves', 'Jiangsu', 31.792216, 119.316746);
