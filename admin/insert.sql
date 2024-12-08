9743d5428c97fc3843e3fdb20b87511432a92fb1
jduckeck <github@jochenduckeck.de>
2024-12-08 01:55:00 +0100

    Sandstein von Eppenbrunn

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Red Sandstone of Eppenbrunn', '/english/de/caves/Eppenbrunn.html', 'Eppenbrunn', 'Europe', 'de', 'Germany', 'caves', 'PfaelzerBergland', 49.1100732, 7.5513464);
