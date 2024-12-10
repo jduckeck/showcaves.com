b6691f4e3b1d05f30c9c6c6709c9ed5b7a88744e
jduckeck <github@jochenduckeck.de>
2024-12-10 14:08:14 +0100

    Sant Gregori de Falset

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Red Sandstone of Eppenbrunn', '/english/de/caves/Eppenbrunn.html', 'Eppenbrunn', 'Europe', 'de', 'Germany', 'caves', 'PfaelzerBergland', 49.1100732, 7.5513464);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Sant Gregori de Falset', '/english/es/caves/SantGregoriFalset.html', 'SantGregoriFalset', 'Europe', 'es', 'Spain', 'caves', 'Cataluna', 41.1396009, 0.8356733);
