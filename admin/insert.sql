9f368386f3db127c78a97ec8eb93947ec6093efc
jduckeck <github@jochenduckeck.de>
2024-12-13 14:08:26 +0100

    Wanfotang Grottoes

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Red Sandstone of Eppenbrunn', '/english/de/caves/Eppenbrunn.html', 'Eppenbrunn', 'Europe', 'de', 'Germany', 'caves', 'PfaelzerBergland', 49.1100732, 7.5513464);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Wanfotang Grottoes', '/english/cn/subterranea/Wanfotang.html', 'Wanfotang', 'Asia', 'cn', 'China', 'subterranea', 'Liaoning', 41.572933, 121.155354);
