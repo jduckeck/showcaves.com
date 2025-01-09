7504e1173995396790ec2ba8664740688f33576a
jduckeck <github@jochenduckeck.de>
2025-01-09 15:03:11 +0100

    Yunnan

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO static (path)
 VALUES ('/english/pl/topics/OldestShowcaves.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Qingxudong Tianshengqiao', '/english/cn/caves/Qingxudong.html', 'Qingxudong', 'Asia', 'cn', 'China', 'caves', 'Guizhou', 27.098760, 105.670826);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Zhenxiong Tiankeng', '/english/cn/karst/Zhenxiong.html', 'Zhenxiong', 'Asia', 'cn', 'China', 'karst', 'Yunnan', 27.596019, 104.771473);
