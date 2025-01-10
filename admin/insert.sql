fdab291995c718f8e02c00ed8b76f74a2b978360
jduckeck <github@jochenduckeck.de>
2025-01-10 17:29:58 +0100

    Huangjin Dong (Tangya River)

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Qingxudong Tianshengqiao', '/english/cn/caves/Qingxudong.html', 'Qingxudong', 'Asia', 'cn', 'China', 'caves', 'Guizhou', 27.098760, 105.670826);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Zhenxiong Tiankeng', '/english/cn/karst/Zhenxiong.html', 'Zhenxiong', 'Asia', 'cn', 'China', 'karst', 'Yunnan', 27.596019, 104.771473);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Huangjin Dong (Tangya River)', '/english/cn/showcaves/HuangjinTangya.html', 'HuangjinTangya', 'Asia', 'cn', 'China', 'showcaves', 'Hubei', 29.9536498, 109.0834324);
