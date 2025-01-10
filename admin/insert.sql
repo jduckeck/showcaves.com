83edba005f6f7d7168f93e1ecc8954b6b84aa408
jduckeck <github@jochenduckeck.de>
2025-01-10 17:31:40 +0100

    Yeren Dong

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Zhenxiong Tiankeng', '/english/cn/karst/Zhenxiong.html', 'Zhenxiong', 'Asia', 'cn', 'China', 'karst', 'Yunnan', 27.596019, 104.771473);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Huangjin Dong (Tangya River)', '/english/cn/showcaves/HuangjinTangya.html', 'HuangjinTangya', 'Asia', 'cn', 'China', 'showcaves', 'Hubei', 29.9536498, 109.0834324);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Yeren Dong', '/english/cn/showcaves/Yeren.html', 'Yeren', 'Asia', 'cn', 'China', 'showcaves', 'Hubei', 31.9188745, 110.7320487);
