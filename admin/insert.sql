5c5e597432804d40797f42a48be3c628c6039fd0
jduckeck <github@jochenduckeck.de>
2025-01-20 00:11:39 +0100

    Seven Springs Tunnel

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Seven Springs Tunnel', '/english/gr/subterranea/SevenSprings.html', 'SevenSprings', 'Europe', 'cy', 'Greece', 'subterranea', 'Dodecanese', 36.2541952, 28.1156602);
