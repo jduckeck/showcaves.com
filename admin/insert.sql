ea7c88205196169929e126e48e61f07afb318604
jduckeck <github@jochenduckeck.de>
2025-01-22 14:28:30 +0100

    Guangxi Zhuang Autonomous Region

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,9 @@ INSERT INTO static (path)
 VALUES ('/english/fi/geoparks/Salpausselka.html');
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Jin Shi Dong', '/english/cn/showcaves/Jinshi.html', 'Jinshi', 'Asia', 'cn', 'China', 'showcaves', 'Hubei', 30.870926, 111.324656);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Chuanlong Dong', '/english/cn/caves/Chuanlong.html', 'Chuanlong', 'Asia', 'cn', 'China', 'caves', 'Guangxi', 24.5527336, 107.0416159);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gongmutang', '/english/cn/springs/Gongmutang.html', 'Gongmutang', 'Asia', 'cn', 'China', 'springs', 'Guangxi', 24.545361, 107.066364);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Yunfengdong', '/english/cn/caves/Yunfeng.html', 'Yunfeng', 'Asia', 'cn', 'China', 'caves', 'Guangxi', 24.538081, 107.053347);
