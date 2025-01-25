1c7c1a56e0c49c17822e79e0a2d077826036db81
jduckeck <github@jochenduckeck.de>
2025-01-26 00:01:34 +0100

    Water Curtain Cave

---
+++ admin/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO static (path)
+VALUES ('/english/cn/topics/WaterCurtain.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Huangguoshu Waterfall Water Curtain Cave', '/english/cn/showcaves/ShuiLianHuangguoshu.html', 'ShuiLianHu', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 25.9922138, 105.6652994);
+UPDATE `sights`
+SET `Latitude`='20.660818',
+    `Longitude`='-88.242542'
+WHERE `filename` = '/english/mx/karst/Dzitnup.html';
