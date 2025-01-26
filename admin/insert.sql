6100a34e717ed39cbe8f59ee7ad33eb58be52669
jduckeck <github@jochenduckeck.de>
2025-01-26 22:51:18 +0100

    Panagia Kakaviotissa

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Panagia Kakaviotissa', '/english/gr/caves/PanagiaKakaviotissa.html', 'PanagiaKakaviotissa', 'Europe', 'gr', 'Greece', 'caves', 'NorthAegean', 39.865177, 25.107035);
