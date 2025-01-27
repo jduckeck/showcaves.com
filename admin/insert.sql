c8f41c6a4b28674d90dde75b8d5a607bfb9f528d
jduckeck <github@jochenduckeck.de>
2025-01-27 21:55:27 +0100

    Qixingyan

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Panagia Kakaviotissa', '/english/gr/caves/PanagiaKakaviotissa.html', 'PanagiaKakaviotissa', 'Europe', 'gr', 'Greece', 'caves', 'NorthAegean', 39.865177, 25.107035);
+UPDATE `sights`
+SET `Latitude`='25.2775518',
+    `Longitude`='110.3075425'
+WHERE `filename` = '/english/cn/showcaves/Qixing.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Qixingyan Zhaoqing - Seven Star Cave', '/english/cn/showcaves/QixingZhaoqing.html', 'QixingZhaoqing', 'Asia', 'cn', 'China', 'showcaves', 'Guangdong', 23.0755153, 112.4713991);
