ebb52666a551788f996ded17ab9a740d47de5483
jduckeck <github@jochenduckeck.de>
2025-01-28 17:49:21 +0100

    Mahabalipuram Caves

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,7 +2,18 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Panagia Kakaviotissa', '/english/gr/caves/PanagiaKakaviotissa.html', 'PanagiaKakaviotissa', 'Europe', 'gr', 'Greece', 'caves', 'NorthAegean', 39.865177, 25.107035);
 UPDATE `sights`
 SET `Latitude`='25.2775518',
-    `Longitude`='110.3075425'
+    `Longitude`='110.3075425',
+    `filename`='/english/cn/showcaves/QixingGuilin.html',
+    `sortby`='QixingGuilin',
+    `name`='Qixingyan Guilin - Seven Star Cave'
 WHERE `filename` = '/english/cn/showcaves/Qixing.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Qixingyan Zhaoqing - Seven Star Cave', '/english/cn/showcaves/QixingZhaoqing.html', 'QixingZhaoqing', 'Asia', 'cn', 'China', 'showcaves', 'Guangdong', 23.0755153, 112.4713991);
+UPDATE `sights`
+SET `Latitude`='12.989483',
+    `Longitude`='79.541681'
+WHERE `filename` = '/english/in/subterranea/Mahendravadi.html';
+UPDATE `sights`
+SET `Latitude`='12.6141797',
+    `Longitude`='80.1912288'
+WHERE `filename` = '/english/in/subterranea/Mahabalipuram.html';
