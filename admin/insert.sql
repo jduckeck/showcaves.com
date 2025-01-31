2f60fe961858bfa89e01590f980d90cbb9531843
jduckeck <github@jochenduckeck.de>
2025-01-31 21:35:26 +0100

    Black Ghost Cave

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Wugui Dong - Black Ghost Cave', '/english/cn/showcaves/Wugui.html', 'Wugui', 'Asia', 'cn', 'China', 'showcaves', 'Taiwan', 22.3296715, 120.3549915);
