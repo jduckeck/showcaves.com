42b63088bc594467ad8a431d704df467c7807966
jduckeck <github@jochenduckeck.de>
2025-02-02 00:03:58 +0100

    Yelang Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,12 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Wugui Dong - Black Ghost Cave', '/english/cn/showcaves/Wugui.html', 'Wugui', 'Asia', 'cn', 'China', 'showcaves', 'Taiwan', 22.3296715, 120.3549915);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Yelang Dong', '/english/cn/showcaves/Yelang.html', 'Yelang', 'Asia', 'cn', 'China', 'showcaves', 'Guizhou', 26.091511, 105.624866);
+UPDATE `sights`
+SET `Latitude`='26.586951',
+    `Longitude`='104.862416'
+WHERE `filename` = '/english/cn/karst/QilinPark.html';
+UPDATE `sights`
+SET `Latitude`='26.5971618',
+    `Longitude`='106.6907408'
+WHERE `filename` = '/english/cn/showcaves/Qilin.html';
