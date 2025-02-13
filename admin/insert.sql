b3d3605d3dd98b4cac0d6c542ac2c4c07521b514
jduckeck <github@jochenduckeck.de>
2025-02-13 12:26:59 +0100

    Cueva de Guitarreros

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ UPDATE `sights`
 SET `Latitude`='30.084394',
     `Longitude`='119.098166'
 WHERE `filename` = '/english/cn/showcaves/Ruijing.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva de Guitarreros', '/english/pe/caves/Guitarrero.html', 'Guitarrero', 'SouthAmerica', 'pe', 'Perú', 'caves', 'Ancash', -9.200554, -77.709821);
