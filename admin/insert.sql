d56a1fdd044cc1119c13abef6b187600a2986b5b
jduckeck <github@jochenduckeck.de>
2025-02-14 19:17:37 +0100

    Peru

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,9 @@ SET `Latitude`='30.084394',
 WHERE `filename` = '/english/cn/showcaves/Ruijing.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cueva de Guitarreros', '/english/pe/caves/Guitarrero.html', 'Guitarrero', 'SouthAmerica', 'pe', 'Perú', 'caves', 'Ancash', -9.200554, -77.709821);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Caverna de Omia', '/english/pe/showcaves/Omia.html', 'Omia', 'SouthAmerica', 'pe', 'Perú', 'showcaves', 'Amazonas', -6.463100, -77.391510);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Caverna de Leo', '/english/pe/caves/Leo.html', 'Leo', 'SouthAmerica', 'pe', 'Perú', 'caves', 'Amazonas', -6.4629513, -77.3929954);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Tunqui Cueva', '/english/pe/showcaves/Tunqui.html', 'Tunqui', 'SouthAmerica', 'pe', 'Perú', 'showcaves', 'Pasco', -10.6061246, -75.4296177);
