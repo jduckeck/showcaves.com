e9ec13c788ff801154b63d5bd7e0e892be664b25
jduckeck <github@jochenduckeck.de>
2025-02-16 21:02:43 +0100

    Aso Windhole

--- admin/insert.sql
+++ admin/insert.sql
@@ -22,3 +22,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cueva de Callacpuma', '/english/pe/caves/Callacpuma.html', 'Callacpuma', 'SouthAmerica', 'pe', 'Perú', 'caves', 'Cajamarca', -7.1845494, -78.4398653);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Hotel Urashima', '/english/jp/caves/Urashima.html', 'Urashima', 'Asia', 'jp', 'Japan', 'caves', 'Wakayama', 33.6248943, 135.9522889);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Aso Windhole', '/english/jp/showcaves/Aso.html', 'Aso', 'Asia', 'jp', 'Japan', 'showcaves', 'Mie', 34.3355359, 136.4438801);
