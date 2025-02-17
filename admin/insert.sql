88cd79bde94ef57e509611517bfac861ba78490e
jduckeck <github@jochenduckeck.de>
2025-02-17 12:29:33 +0100

    Devil’s Kettle

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Devil’s Kettle', '/english/usa/karst/DevilsKettle.html', 'DevilsKettle', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'mn', 47.8292691, -90.0495652);
