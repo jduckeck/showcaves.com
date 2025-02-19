9c673e8c278f982b89c5dcaab50125a9f2386706
jduckeck <github@jochenduckeck.de>
2025-02-19 13:11:35 +0100

    Felsentor Unteremmendorf

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Devil’s Kettle', '/english/usa/karst/DevilsKettle.html', 'DevilsKettle', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'mn', 47.8292691, -90.0495652);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Felsentor Unteremmendorf', '/english/de/karst/Unteremmendorf.html', 'Unteremmendorf', 'Europe', 'de', 'Germany', 'karst', 'FraenkischeAlb', 48.9877848, 11.4347799);
