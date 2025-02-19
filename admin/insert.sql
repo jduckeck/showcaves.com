e199ba477a86cb5b5ee40c8e8235a18020ea4039
jduckeck <github@jochenduckeck.de>
2025-02-19 23:29:04 +0100

    Cistern of the Convent of San Francesco

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES('yes','Devil’s Kettle', '/english/usa/karst/DevilsKettle.html', 'DevilsKettle', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'mn', 47.8292691, -90.0495652);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Felsentor Unteremmendorf', '/english/de/karst/Unteremmendorf.html', 'Unteremmendorf', 'Europe', 'de', 'Germany', 'karst', 'FraenkischeAlb', 48.9877848, 11.4347799);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Cistern of the Convent of San Francesco', '/english/other/subterranea/SanFrancesco.html', 'SanFrancesco', 'Europe', 'sm', 'San Marino', 'subterranea', 'sm', 'other', 43.9354523, 12.4471593);
