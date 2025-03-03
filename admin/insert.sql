e5d0121618c2b3cb961ac83b8d364d8119c191e2
jduckeck <github@jochenduckeck.de>
2025-03-03 21:03:02 +0100

    Ph

--- admin/insert.sql
+++ admin/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Wonderful Cave', '/english/ph/showcaves/Wonderful.html', 'Wonderful', 'Asia', 'ph', 'The Philippines', 'showcaves', 'Ilocos', 16.340900, 119.802340);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bulwang Caves', '/english/ph/showcaves/Bulwang.html', 'Bulwang', 'Asia', 'ph', 'The Philippines', 'showcaves', 'CentralVisayas', 9.6605109, 122.9855004);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Basakan Spring', '/english/ph/springs/Basakan.html', 'Basakan', 'Asia', 'ph', 'The Philippines', 'springs', 'CentralVisayas', 9.806016, 122.970968);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Mabinay Spring', '/english/ph/springs/Mabinay.html', 'Mabinay', 'Asia', 'ph', 'The Philippines', 'springs', 'CentralVisayas', 9.719389, 122.932860);
