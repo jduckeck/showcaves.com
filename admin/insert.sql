d2ec3f250514208a4e1ac4225a1575dc747d16b7
jduckeck <github@jochenduckeck.de>
2025-03-03 19:58:42 +0100

    Swimming

---
+++ admin/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Wonderful Cave', '/english/ph/showcaves/Wonderful.html', 'Wonderful', 'Asia', 'ph', 'The Philippines', 'showcaves', 'Ilocos', 16.340900, 119.802340);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bulwang Caves', '/english/ph/showcaves/Bulwang.html', 'Bulwang', 'Asia', 'ph', 'The Philippines', 'showcaves', 'CentralVisayas', 9.6605109, 122.9855004);
