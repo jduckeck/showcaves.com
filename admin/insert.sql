b9d2f7969a17793a4032ac9d9984e0a37256865f
jduckeck <github@jochenduckeck.de>
2025-03-05 14:45:00 +0100

    Hoyop-Hoyopan Cave

--- admin/insert.sql
+++ admin/insert.sql
@@ -10,3 +10,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Molnár János-barlang', '/english/hu/caves/MolnarJanos.html', 'MolnarJanos', 'Europe', 'hu', 'Hungary', 'caves', 'Budapest', 47.5181319, 19.0359332);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cave Steam Bath "Parenica"', '/english/sk/springs/Parenica.html', 'Parenica', 'Europe', 'sk', 'Slovakia', 'springs', 'BanskaBystrica', 48.5273011, 18.8644994);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Hoyop-Hoyopan Cave', '/english/ph/showcaves/HoyopHoyopan.html', 'HoyopHoyopan', 'Asia', 'ph', 'The Philippines', 'showcaves', 'Bicol', 13.1207143, 123.6563101);
