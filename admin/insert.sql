b6ec2bbdb199a1984718278f4b84234ca460703f
jduckeck <github@jochenduckeck.de>
2025-03-10 15:19:36 +0100

    Kullaberg Grottor

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Kullaberg Grottor', '/english/se/caves/Kullaberg.html', 'Kullaberg', 'Europe', 'se', 'Sweden', 'caves', 'Skane', 56.2994936, 12.4514338);
