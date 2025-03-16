15c8ec3f2ef2cd688e8afbf6deb03dc7281b4604
jduckeck <github@jochenduckeck.de>
2025-03-16 15:18:50 +0100

    Gruta India

---
+++ admin/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Gruta India', '/english/ar/caves/India.html', 'India', 'SouthAmerica', 'ar', 'Argentina', 'caves', 'Misiones',  -26.7586574, -54.9234052);
