f87e784b8f7735cade6d01074d2222c0f9b3223a
jduckeck <github@jochenduckeck.de>
2025-03-26 12:57:30 +0100

    general public

--- admin/insert.sql
+++ admin/insert.sql
@@ -6,3 +6,7 @@ SET `Latitude`='-47.1552731',
 WHERE `filename` = '/english/ar/showcaves/LasManos.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Chapel of Our Lady of the Crag', '/english/gb/subterranea/Crag.html', 'Crag', 'Europe', 'gb', 'Great Britain', 'subterranea', 'NorthYorkshire', 54.0030157, -1.4655233);
+UPDATE `sights`
+SET `Latitude`='44.0015050',
+    `Longitude`='-88.1554197'
+WHERE `filename` = '/english/usa/showcaves/LedgeViewNatureCenter.html';
