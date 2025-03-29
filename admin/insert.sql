de70bb791291057226cbd8a4014b7c0da3f91d64
jduckeck <github@jochenduckeck.de>
2025-03-29 14:21:49 +0100

    Ponors

--- admin/insert.sql
+++ admin/insert.sql
@@ -1,12 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Gruta India', '/english/ar/caves/India.html', 'India', 'SouthAmerica', 'ar', 'Argentina', 'caves', 'Misiones',  -26.7586574, -54.9234052);
-UPDATE `sights`
-SET `Latitude`='-47.1552731',
-    `Longitude`='-70.6559679'
-WHERE `filename` = '/english/ar/showcaves/LasManos.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Chapel of Our Lady of the Crag', '/english/gb/subterranea/Crag.html', 'Crag', 'Europe', 'gb', 'Great Britain', 'subterranea', 'NorthYorkshire', 54.0030157, -1.4655233);
-UPDATE `sights`
-SET `Latitude`='44.0015050',
-    `Longitude`='-88.1554197'
-WHERE `filename` = '/english/usa/showcaves/LedgeViewNatureCenter.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Sumidouro da Ribeira dos Amiais', '/english/pt/karst/Amiais.html', 'Amiais', 'Europe', 'pt', 'Portugal', 'karst', 'Santarem', 39.4461104, -8.7154277);
