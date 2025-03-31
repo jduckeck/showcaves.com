900c02d8ffed865fce48906a8f17d505c835f9c9
jduckeck <github@jochenduckeck.de>
2025-03-31 16:47:37 +0200

    Grand Canyon du Verdon

--- admin/insert.sql
+++ admin/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Chapel of Our Lady of the Crag', '/english/gb/subterranea/Crag.html', 'Crag', 'Europe', 'gb', 'Great Britain', 'subterranea', 'NorthYorkshire', 54.0030157, -1.4655233);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Sumidouro da Ribeira dos Amiais', '/english/pt/karst/Amiais.html', 'Amiais', 'Europe', 'pt', 'Portugal', 'karst', 'Santarem', 39.4461104, -8.7154277);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gorges du Verdon', '/english/fr/gorges/Verdon.html', 'Verdon', 'Europe', 'fr', 'France', 'gorges', 'PACA', 43.8016706, 6.2494422);
