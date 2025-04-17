951e30f7e4a4bf942562e09aab9b2cdd666e3c19
jduckeck <github@jochenduckeck.de>
2025-04-17 13:55:11 +0200

    Muzeum Zlata Nový Knín

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Muzeum zlata Nový Knín', '/english/cz/mines/NovyKnin.html', 'NovyKnin', 'Europe', 'cz', 'Czech Republic', 'mines', 'Stredocesky', 49.7878716, 14.2936472);
