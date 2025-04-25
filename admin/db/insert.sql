aedd252544c0e7b5ac2fab5061dbfff02192087b
jduckeck <github@jochenduckeck.de>
2025-04-25 15:02:32 +0200

    Sirgensteinhöhle

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Muzeum zlata Nový Knín', '/english/cz/mines/NovyKnin.html', 'NovyKnin', 'Europe', 'cz', 'Czech Republic', 'mines', 'Stredocesky', 49.7878716, 14.2936472);
+INSERT INTO static (path)
+VALUES ('/german/de/topics/UnescoAlb.html');
+INSERT INTO static (path)
+VALUES ('/english/de/topics/UnescoAlb.html');
