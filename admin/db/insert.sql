a0aed278053001d9cfd5f8b53089d452ac512ba4
jduckeck <github@jochenduckeck.de>
2025-05-01 16:23:35 +0200

    Bunker Falkenhagen

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,7 @@ UPDATE `sights`
 SET `Latitude`='48.5615400',
     `Longitude`='9.5226488'
 WHERE `filename` = '/english/de/karst/SchopflocherMoor.html';
+UPDATE `sights`
+SET `Latitude`='52.4296181',
+    `Longitude`='14.3557996'
+WHERE `filename` = '/english/de/subterranea/Falkenhagen.html';
