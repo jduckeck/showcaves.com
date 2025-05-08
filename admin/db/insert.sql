8697cc3c7826217f5d2db49fad6dd98c47a87519
jduckeck <github@jochenduckeck.de>
2025-05-08 09:55:34 +0200

    Bunkerpark Wünsdorf

--- admin/db/insert.sql
+++
@@ -1,8 +0,0 @@
-UPDATE `sights`
-SET `Latitude`='48.5615400',
-    `Longitude`='9.5226488'
-WHERE `filename` = '/english/de/karst/SchopflocherMoor.html';
-UPDATE `sights`
-SET `Latitude`='52.4296181',
-    `Longitude`='14.3557996'
-WHERE `filename` = '/english/de/subterranea/Falkenhagen.html';
