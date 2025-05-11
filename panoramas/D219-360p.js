3c42c9697e788cc37410316e6c11edb6c0751aad
jduckeck <github@jochenduckeck.de>
2025-05-11 07:33:02 +0200

    Panorama

---
+++ panoramas/D219-360p.js
@@ -0,0 +1,9 @@
+import {Viewer} from '@photo-sphere-viewer/core';
+
+new Viewer({
+    container: 'viewer',
+    panorama: 'D219-360p.jpg',
+    caption: 'Kubacher Kristallhöhle',
+    canvasBackground: '#77addb',
+    defaultZoomLvl: 0
+});
