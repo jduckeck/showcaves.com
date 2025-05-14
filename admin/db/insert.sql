55e21a4586faed3beb8eec764c70fa54ba6bba2f
jduckeck <github@jochenduckeck.de>
2025-05-14 18:53:50 +0200

    Nigeria

--- admin/db/insert.sql
+++
@@ -1,8 +0,0 @@
-UPDATE `sights`
-SET `Latitude`='18.3063127',
-    `Longitude`='-73.8701264'
-WHERE `filename` = '/english/car/showcaves/Kounoubwa.html';
-UPDATE `sights`
-SET `Latitude`='18.2531679',
-    `Longitude`='-74.0945632'
-WHERE `filename` = '/english/car/showcaves/MarieJeanne.html';
