c4b9cbd624b87e395a7976ca82efebcb11b62a96
jduckeck <github@jochenduckeck.de>
2025-05-15 16:39:49 +0200

    Nigeria

---
+++ admin/db/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Awhum Cave and Waterfall', '/english/ng/gorges/Awhum.html', 'Awhum', 'Africa', 'ng', 'Nigeria', 'gorges', 'Enugu', 6.533098, 7.434031);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ngwo Cave and Waterfall', '/english/ng/gorges/Ngwo.html', 'Ngwo', 'Africa', 'ng', 'Nigeria', 'gorges', 'Enugu', 6.422759, 7.455343);
