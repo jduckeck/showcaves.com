eac0165600d6ad846efe40a3c0553f1acbd0606c
jduckeck <github@jochenduckeck.de>
2025-05-16 11:34:44 +0200

    Amakama Wooden Cave

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Awhum Cave and Waterfall', '/english/ng/gorges/Awhum.html', 'Awhum', 'Africa', 'ng', 'Nigeria', 'gorges', 'Enugu', 6.533098, 7.434031);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Ngwo Cave and Waterfall', '/english/ng/gorges/Ngwo.html', 'Ngwo', 'Africa', 'ng', 'Nigeria', 'gorges', 'Enugu', 6.422759, 7.455343);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Amakama Wooden Cave', '/english/ng/caves/Amakama.html', 'Amakama', 'Africa', 'ng', 'Nigeria', 'caves', 'Abia', 5.469920, 7.484474);
+UPDATE `sights`
+SET `Latitude`='5.544198',
+    `Longitude`='7.485902'
+WHERE `filename` = '/english/ng/subterranea/Ojukwu.html';
