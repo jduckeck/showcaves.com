d7e6b76bd76658dd24c9a6e79b105de30fbd75b5
jduckeck <github@jochenduckeck.de>
2025-05-16 17:18:10 +0200

    Ogba Agu-Owuru

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,7 +4,15 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Ngwo Cave and Waterfall', '/english/ng/gorges/Ngwo.html', 'Ngwo', 'Africa', 'ng', 'Nigeria', 'gorges', 'Enugu', 6.422759, 7.455343);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Amakama Wooden Cave', '/english/ng/caves/Amakama.html', 'Amakama', 'Africa', 'ng', 'Nigeria', 'caves', 'Abia', 5.469920, 7.484474);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Amanchor Cave', '/english/ng/caves/Amanchor.html', 'Amanchor', 'Africa', 'ng', 'Nigeria', 'caves', 'Ebonyi', 5.809069, 7.819980);
 UPDATE `sights`
 SET `Latitude`='5.544198',
     `Longitude`='7.485902'
+WHERE `filename` = '/english/ng/subterranea/VoiceOfBiafra.html';
+UPDATE `sights`
+SET `Latitude`='5.518889',
+    `Longitude`='7.484591'
 WHERE `filename` = '/english/ng/subterranea/Ojukwu.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Agu-Owuru Cave', '/english/ng/caves/AguOwuru.html', 'AguOwuru', 'Africa', 'ng', 'Nigeria', 'caves', 'Enugu', 6.31052, 7.26732);
