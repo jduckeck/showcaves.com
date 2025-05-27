ef86f04fbb1d03deb720f9562032a268f9f24b89
jduckeck <github@jochenduckeck.de>
2025-05-27 13:58:49 +0200

    Utrobata

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Utrobata', '/english/bg/caves/Utrobata.html', 'Utrobata', 'Europe', 'bg', 'Bulgaria', 'caves', 'Kardzhali', 41.7048489, 25.2481141);
