b796fcf5f1ecd46d3b5a80ba87f005fba1494df8
jduckeck <github@jochenduckeck.de>
2025-05-29 10:00:17 +0200

    at caves

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Utrobata', '/english/bg/caves/Utrobata.html', 'Utrobata', 'Europe', 'bg', 'Bulgaria', 'caves', 'Kardzhali', 41.7048489, 25.2481141);
+INSERT INTO static (path)
+VALUES ('/german/at/topics/closed.html');
