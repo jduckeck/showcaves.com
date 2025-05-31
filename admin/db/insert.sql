884330a2fca70006dc4489fd86329abf9171b69b
jduckeck <github@jochenduckeck.de>
2025-05-31 17:19:53 +0200

    Пещера "Тройната утроба"

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -6,3 +6,5 @@ UPDATE `sights`
 SET `Latitude`='47.5729298',
     `Longitude`='13.1940363'
 WHERE `filename` = '/english/at/caves/Brunecker.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Triple Womb Cave', '/english/bg/caves/TroinataUtroba.html', 'TroinataUtroba', 'Europe', 'bg', 'Bulgaria', 'caves', 'Kardzhali', 41.543519, 25.548614);
