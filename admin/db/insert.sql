9a6601bd4f2f825e0e9b60b4fdf155ea9686f140
jduckeck <github@jochenduckeck.de>
2025-06-02 14:38:39 +0200

    Grotta della Ninfe

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotta della Ninfe', '/english/it/caves/Ninfe.html', 'Ninfe', 'Europe', 'it', 'Italy', 'caves', 'Calabria', 39.841176, 16.402609);
