c05349cbbace30079c64a607258310965a54d9c6
jduckeck <github@jochenduckeck.de>
2025-06-02 14:41:30 +0200

    Zlatokopecký skanzen

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Grotta della Ninfe', '/english/it/caves/Ninfe.html', 'Ninfe', 'Europe', 'it', 'Italy', 'caves', 'Calabria', 39.841176, 16.402609);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Zlatokopecký skanzen', '/english/cz/mines/ZlatokopeckySkanzen.html', 'ZlatokopeckySkanzen', 'Europe', 'cz', 'Czech Republic', 'mines', 'Olomoucky', 50.2537709, 17.3663138);
