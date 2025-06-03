594a9b7b682a860f8e5e4e60cd987f59dfaba12c
jduckeck <github@jochenduckeck.de>
2025-06-03 09:22:02 +0200

    Jugendstiltoilette

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Grotta della Ninfe', '/english/it/caves/Ninfe.html', 'Ninfe', 'Europe', 'it', 'Italy', 'caves', 'Calabria', 39.841176, 16.402609);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Zlatokopecký skanzen', '/english/cz/mines/ZlatokopeckySkanzen.html', 'ZlatokopeckySkanzen', 'Europe', 'cz', 'Czech Republic', 'mines', 'Olomoucky', 50.2537709, 17.3663138);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Jugendstiltoilette', '/english/at/subterranea/Jugendstiltoilette.html', 'Jugendstiltoilette', 'Europe', 'at', 'Austria', 'subterranea', 'Wien', 48.2090655, 16.3691848);
