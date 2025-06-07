a8b9ccfc9f628c9eadcf08ea3fe4c6c3e671f8d8
jduckeck <github@jochenduckeck.de>
2025-06-07 18:08:40 +0200

    Guadeloupe

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,4 +1,6 @@
-UPDATE `sights`
-SET `Latitude`='48.0530622',
-    `Longitude`='16.0776554'
-WHERE `filename` = '/english/at/showcaves/Allander.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Gueule Grand Gouffre', '/english/fr/karst/GueuleGrand.html', 'GueuleGrand', 'Europe', 'fr', 'France', 'karst', 'Guadeloupe', 16.0023334, -61.2671517);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Trou à Man Louis', '/english/fr/caves/MadameLouis.html', 'MadameLouis', 'Europe', 'fr', 'France', 'caves', 'Guadeloupe', 16.485838, -61.497291);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Trou Madame Coco', '/english/fr/caves/MadameCoco.html', 'MadameCoco', 'Europe', 'fr', 'France', 'caves', 'Guadeloupe', 16.4901243, -61.4402167);
