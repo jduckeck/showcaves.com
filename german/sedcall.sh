0ebcac7062f7660113a7f018250165f79cb78547
jduckeck <github@jochenduckeck.de>
2025-06-11 12:12:17 +0200

    sed

--- german/sedcall.sh
+++ german/sedcall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo $0
-find . -type f -exec sed -i 's/, offizielle Website/, offizielle Website/g' {} +
-find . -type f -exec sed -i 's/(besucht: /(besucht: /g' {} +
+find . -name '*.html' -type f -exec sed -i 's/, offizielle Website/, offizielle Website/g' {} +
+find . -name '*.html' -type f -exec sed -i 's/(besucht: /(besucht: /g' {} +
