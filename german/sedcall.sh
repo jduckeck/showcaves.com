b137dca86bef00aa1d085fd1bc2ea68edb276320
jduckeck <github@jochenduckeck.de>
2025-06-29 08:56:01 +0200

    sedcall

--- german/sedcall.sh
+++ german/sedcall.sh
@@ -5,7 +5,9 @@ shopt -s globstar dotglob
 for file in **/*.html; do
     if [[ -f $file && ! -L $file ]]; then
       echo $file
-      sed -i 's/, offizielle Website/, offizielle Website/g' "./$file"
-      sed -i 's/(besucht: /(besucht: /g' "./$file"
+      sed -i 's/, official website/, offizielle Website/g' "./$file"
+      sed -i 's/(visited: /(besucht: /g' "./$file"
+      sed -i 's/ \([[:digit:]]+\) EUR/ EUR \1/g' "./$file"
+#      sed -i 's/ <img alt="Deutsch - German" src="../../../graphics/language/de.png" class="language"> / /g' "./$file"
     fi
 done
