d1b003d00aa1fcce12aa5c1c0759bca7df56f77e
jduckeck <github@jochenduckeck.de>
2025-07-12 15:39:43 +0200

    Földvári Aladár-barlang

--- admin/db/insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Földvári Aladár-barlang', '/english/hu/showcaves/Foldvari.html', 'Foldvari', 'Europe', 'hu', 'Hungary', 'showcaves', 'Aggtelek', 48.5162670, 20.7516500);
