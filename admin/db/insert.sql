c4eeab0ac8f3b4e0b0b4ce0213942b9520536855
jduckeck <github@jochenduckeck.de>
2025-07-13 11:24:41 +0200

    Salt de La Caula

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Salt de La Caula', '/english/es/karst/LaCaula.html', 'LaCaula', 'Europe', 'es', 'Spain', 'karst', 'Cataluna', 42.3180205, 2.8853500);
