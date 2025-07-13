e58ef560d6300f3fea6961ff25bc2de5c590d2bd
jduckeck <github@jochenduckeck.de>
2025-07-13 16:19:39 +0200

    Yucatan

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,10 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Salt de La Caula', '/english/es/karst/LaCaula.html', 'LaCaula', 'Europe', 'es', 'Spain', 'karst', 'Cataluna', 42.3180205, 2.8853500);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenote Palomitas', '/english/mx/karst/Palomitas.html', 'Palomitas', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.8192433, -88.0555725);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenotes Agua Dulce Aventuras', '/english/mx/karst/AguaDulce.html', 'AguaDulce', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.8173899, -88.0541607);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenote Xcanahaltun', '/english/mx/karst/Xcanahaltun.html', 'Xcanahaltun', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.8367762, -88.0212094);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenote Secreto Maya', '/english/mx/karst/SecretoMaya.html', 'SecretoMaya', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.8700663, -87.9958003);
