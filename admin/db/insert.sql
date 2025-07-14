b2285b25e184df7a18ccd6663d1999a97b1a74c2
jduckeck <github@jochenduckeck.de>
2025-07-14 13:06:55 +0200

    Yucatan

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -14,3 +14,11 @@ UPDATE `sights`
 SET `Latitude`='38.3003848',
     `Longitude`='57.5189019'
 WHERE `filename` = '/english/other/showcaves/KowAta.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenote Sac Aua', '/english/mx/karst/SacAua.html', 'SacAua', 'MiddleAmerica', 'mx', 'México', 'karst', 'Yucatan', 20.8412199, -88.0510391);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cenote Zazil Tunich', '/english/mx/showcaves/ZazilTunich.html', 'ZazilTunich', 'MiddleAmerica', 'mx', 'México', 'showcaves', 'Yucatan', 20.8410880, -88.0117526);
+UPDATE `sights`
+SET `Latitude`='20.687674',
+    `Longitude`='-88.567648'
+WHERE `filename` = '/english/mx/karst/Sagrado.html';
