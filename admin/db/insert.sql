c5cb91553ca92de26ad6f55e6d6e43e206cc466e
jduckeck <github@jochenduckeck.de>
2025-07-21 19:31:54 +0200

    Rieseneis

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,7 @@ UPDATE `sights`
 SET `Latitude`='46.5097800',
     `Longitude`='14.5483221'
 WHERE `filename` = '/english/at/showcaves/Obir.html';
+UPDATE `sights`
+SET `Latitude`='47.5349012',
+    `Longitude`='13.7187077'
+WHERE `filename` = '/english/at/showcaves/Rieseneis.html';
