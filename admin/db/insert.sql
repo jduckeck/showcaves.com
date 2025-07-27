df86ea9da0179770cd561e1ad7e3aca198c9f289
jduckeck <github@jochenduckeck.de>
2025-07-28 00:26:04 +0200

    Grotte Tufacee Comunali

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte Tufacee Comunali', '/english/it/subterranea/Comunali.html', 'Comunali', 'Europe', 'it', 'Italy', 'subterranea', 'Rimini', 44.0644777, 12.4441573);
