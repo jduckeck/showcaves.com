aa92d3f45f061a1793afbb60e0e781248fd9759a
jduckeck <github@jochenduckeck.de>
2025-07-28 14:49:14 +0200

    Bunker Tour Cervia

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Grotte Tufacee Comunali', '/english/it/subterranea/Comunali.html', 'Comunali', 'Europe', 'it', 'Italy', 'subterranea', 'Rimini', 44.0644777, 12.4441573);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES ('yes', 'Galleria Montale', '/english/other/subterranea/Montale.html', 'Montale', 'Europe', 'sm', 'San Marino', 'subterranea', 'sm', 'other', 43.9301977, 12.4501585);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bunker Tour Cervia', '/english/it/subterranea/Cervia.html', 'Cervia', 'Europe', 'it', 'Italy', 'subterranea', 'Rimini', 44.269247, 12.355890);
