6b8820f9de182c91d40702a759e9e2c70985cddd
jduckeck <github@jochenduckeck.de>
2025-07-28 10:52:03 +0200

    Galleria Montale

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Grotte Tufacee Comunali', '/english/it/subterranea/Comunali.html', 'Comunali', 'Europe', 'it', 'Italy', 'subterranea', 'Rimini', 44.0644777, 12.4441573);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Galleria Montale', '/english/other/subterranea/Montale.html', 'Montale', 'Europe', 'sm', 'San Marino', 'subterranea', 'sm', 'other', 43.9301977, 12.4501585);
