de791bcb2541a586cea53a8d5726f13996b342c9
jduckeck <github@jochenduckeck.de>
2025-08-04 19:39:59 +0200

    Rastenbachklamm

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -6,3 +6,5 @@ UPDATE `sights`
 SET `Latitude`='47.3788808',
     `Longitude`='13.2038693'
 WHERE `filename` = '/english/at/mines/Arthurstollen.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Rastenbachklamm', '/english/it/gorges/Rastenbachklamm.html', 'Rastenbachklamm', 'Europe', 'it', 'Italy', 'gorges', 'Trentino', 46.3808139, 11.2405173);
