89235adf5d670d1590a306bb948451373db0bf32
jduckeck <github@jochenduckeck.de>
2025-08-04 14:40:01 +0200

    Arthurstollen

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,7 @@ UPDATE `sights`
 SET `Latitude`='47.313415',
     `Longitude`='13.189752'
 WHERE `filename` = '/english/at/gorges/Liechtenstein.html';
+UPDATE `sights`
+SET `Latitude`='47.3788808',
+    `Longitude`='13.2038693'
+WHERE `filename` = '/english/at/mines/Arthurstollen.html';
