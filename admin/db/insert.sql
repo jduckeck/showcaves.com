8c8e2f3f8c32db0e262a790811d89b2032498344
jduckeck <github@jochenduckeck.de>
2025-08-06 01:27:46 +0200

    loc, Bleiberg

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -12,3 +12,17 @@ UPDATE `sights`
 SET `Latitude`='47.2507081',
     `Longitude`='15.5194223'
 WHERE `filename` = '/english/at/mines/Arzberg.html';
+UPDATE `sights`
+SET `Latitude`='47.5560719',
+    `Longitude`='13.6452871'
+WHERE `filename` = '/english/at/mines/Hallstatt.html';
+UPDATE `sights`
+SET `Latitude`='47.2022642',
+    `Longitude`='14.6829514'
+WHERE `filename` = '/english/at/mines/Fohnsdorf.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bergbau-Museum Bad Bleiberg', '/english/at/mines/BadBleibergMuseum.html', 'BadBleibergMuseum', 'Europe', 'at', 'Austria', 'mines', 'Kaernten', 46.628234, 13.675036);
+UPDATE `sights`
+SET `Latitude`='46.6295381',
+    `Longitude`='14.2926577'
+WHERE `filename` = '/english/at/subterranea/Klagenfurt.html';
