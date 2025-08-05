c1c209988e21c3b4d922a3f188b93d2f4883a710
jduckeck <github@jochenduckeck.de>
2025-08-05 17:36:46 +0200

    Schau- und Lehrstollen Arzberg

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -8,3 +8,7 @@ SET `Latitude`='47.3788808',
 WHERE `filename` = '/english/at/mines/Arthurstollen.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Rastenbachklamm', '/english/it/gorges/Rastenbachklamm.html', 'Rastenbachklamm', 'Europe', 'it', 'Italy', 'gorges', 'Trentino', 46.3808139, 11.2405173);
+UPDATE `sights`
+SET `Latitude`='47.2507081',
+    `Longitude`='15.5194223'
+WHERE `filename` = '/english/at/mines/Arzberg.html';
