d699a2afdb942b064ee82b23fcd15f038299794a
jduckeck <github@jochenduckeck.de>
2025-08-10 11:51:26 +0200

    Limmern Pumped Storage Plant

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Erdstall Populorum', '/english/at/subterranea/Populorum.html', 'Populorum', 'Europe', 'at', 'Austria', 'subterranea', 'Ober', 48.3509287, 14.6713391);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Limmern Pumped Storage Plant', '/english/ch/subterranea/Limmern.html', 'Limmern', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Glarus', 46.8798621, 8.9811950);
