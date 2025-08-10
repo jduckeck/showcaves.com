8ed0917fe782b678ffdcfe184224a738e324639d
jduckeck <github@jochenduckeck.de>
2025-08-10 07:06:59 +0200

    Erdstall Populorum

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Erdstall Populorum', '/english/at/subterranea/Populorum.html', 'Populorum', 'Europe', 'at', 'Austria', 'subterranea', 'Ober', 48.3509287, 14.6713391);
