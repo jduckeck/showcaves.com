60983084e3ab2ade63d45df045cbe4b8f89ff776
jduckeck <github@jochenduckeck.de>
2025-08-12 09:39:58 +0200

    Klausenhöhlen

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Erdstall Populorum', '/english/at/subterranea/Populorum.html', 'Populorum', 'Europe', 'at', 'Austria', 'subterranea', 'Ober', 48.3509287, 14.6713391);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Limmern Pumped Storage Plant', '/english/ch/subterranea/Limmern.html', 'Limmern', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Glarus', 46.8798621, 8.9811950);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Klausenhöhlen', '/english/de/caves/KlausenEssing.html', 'KlausenEssing', 'Europe', 'de', 'Germany', 'caves', 'FraenkischeAlb', 48.9344962, 11.7842145);
