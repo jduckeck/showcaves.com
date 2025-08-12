c07fb0af5a1a7e7bc825b48077642d24c2a31898
jduckeck <github@jochenduckeck.de>
2025-08-12 19:49:54 +0200

    Robert-Bourassa Generating Station

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Limmern Pumped Storage Plant', '/english/ch/subterranea/Limmern.html', 'Limmern', 'Europe', 'ch', 'Switzerland', 'subterranea', 'Glarus', 46.8798621, 8.9811950);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Klausenhöhlen', '/english/de/caves/KlausenEssing.html', 'KlausenEssing', 'Europe', 'de', 'Germany', 'caves', 'FraenkischeAlb', 48.9344962, 11.7842145);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Robert-Bourassa Generating Station', '/english/ca/subterranea/RobertBourassa.html', 'RobertBourassa', 'NorthAmerica', 'ca', 'Canada', 'subterranea', 'Quebec', 53.7934364, -77.6150210);
