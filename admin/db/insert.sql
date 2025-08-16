4ce728a9c75494b12043cbbaa8dc1ed3d66f2b1b
jduckeck <github@jochenduckeck.de>
2025-08-16 13:16:52 +0200

    Golke

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,7 @@ UPDATE `sights`
 SET `Latitude`='50.7752680',
     `Longitude`='10.8879092'
 WHERE `filename` = '/english/de/springs/Plaue.html';
+UPDATE `sights`
+SET `Latitude`='51.0923419',
+    `Longitude`='10.6196453'
+WHERE `filename` = '/english/de/springs/Golke.html';
