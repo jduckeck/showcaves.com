d62606527429bcd431400bf71727aeaa1e1619ee
jduckeck <github@jochenduckeck.de>
2025-08-16 20:40:12 +0200

    apostroph

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -6,3 +6,55 @@ UPDATE `sights`
 SET `Latitude`='51.0923419',
     `Longitude`='10.6196453'
 WHERE `filename` = '/english/de/springs/Golke.html';
+UPDATE `sights`
+SET `Latitude`='51.3392502',
+    `Longitude`='7.8718536'
+WHERE `filename` = '/english/de/showcaves/Balver.html';
+UPDATE `sights`
+SET `Latitude`='49.8270590',
+    `Longitude`='11.3754143'
+WHERE `filename` = '/english/de/showcaves/Sophien.html';
+UPDATE `sights`
+SET `Latitude`='51.3705565',
+    `Longitude`='7.8630881'
+WHERE `filename` = '/english/de/showcaves/Recken.html';
+UPDATE `sights`
+SET `Latitude`='49.2549562',
+    `Longitude`='11.6900287'
+WHERE `filename` = '/english/de/showcaves/Kingotto.html';
+UPDATE `sights`
+SET `Latitude`='49.4814282',
+    `Longitude`='9.3480103'
+WHERE `filename` = '/english/de/showcaves/Eberstadter.html';
+UPDATE `sights`
+SET `Latitude`='51.1312519',
+    `Longitude`='12.8074340'
+WHERE `filename` = '/english/de/subterranea/Colditz.html';
+UPDATE `sights`
+SET `Latitude`='49.2301866',
+    `Longitude`='6.9922754'
+WHERE `filename` = '/english/de/subterranea/Saarbruecken.html';
+UPDATE `sights`
+SET `Latitude`='36.6912325',
+    `Longitude`='-5.2699924'
+WHERE `filename` = '/english/es/showcaves/Pileta.html';
+UPDATE `sights`
+SET `Latitude`='66.539673',
+    `Longitude`='25.797382'
+WHERE `filename` = '/english/fi/subterranea/SantaPark.html';
+UPDATE `sights`
+SET `Latitude`='41.9162690',
+    `Longitude`='8.7237930'
+WHERE `filename` = '/english/fr/caves/Napoleon.html';
+UPDATE `sights`
+SET `Latitude`='43.8054596',
+    `Longitude`='3.2687406'
+WHERE `filename` = '/english/fr/showcaves/Labeil.html';
+UPDATE `sights`
+SET `Latitude`='44.3695888',
+    `Longitude`='4.5105687'
+WHERE `filename` = '/english/fr/showcaves/Marzal.html';
+UPDATE `sights`
+SET `Latitude`='43.4741675',
+    `Longitude`='2.7141373'
+WHERE `filename` = '/english/fr/showcaves/Deveze.html';
