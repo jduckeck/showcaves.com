6312ab5be6cd64f84d538bd0deefe868fbae27c4
jduckeck <github@jochenduckeck.de>
2025-08-17 19:14:09 +0200

    apostroph

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -6,3 +6,7 @@ UPDATE `sights`
 SET `Latitude`='39.8154960',
     `Longitude`='9.4867997'
 WHERE `filename` = '/english/it/showcaves/SuMarmuri.html';
+UPDATE `sights`
+SET `Latitude`='40.6149249',
+    `Longitude`='14.5675886'
+WHERE `filename` = '/english/it/showcaves/Smeralda.html';
