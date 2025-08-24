888cf09b013a4d4c5afc30e659e05e06c8cba096
jduckeck <github@jochenduckeck.de>
2025-08-24 09:41:21 +0200

    coordinates

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -32,3 +32,39 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Necropoli di Anghelu Ruju', '/english/it/subterranea/AngheluRuju.html', 'AngheluRuju', 'Europe', 'it', 'Italy', 'subterranea', 'Sardinia', 40.633034, 8.326698);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Necropoli di Santu Pedru', '/english/it/subterranea/SantuPedru.html', 'SantuPedru', 'Europe', 'it', 'Italy', 'subterranea', 'Sardinia', 40.6235273, 8.4029480);
+UPDATE `sights`
+SET `Latitude`='-38.473806',
+    `Longitude`='176.143028'
+WHERE `filename` = '/english/nz/showcaves/OrakeiKorako.html';
+UPDATE `sights`
+SET `Latitude`='33.9433976',
+    `Longitude`='35.6417651'
+WHERE `filename` = '/english/lb/showcaves/Jeita.html';
+UPDATE `sights`
+SET `Latitude`='46.3808093',
+    `Longitude`='23.4620463'
+WHERE `filename` = '/english/ro/caves/HudaLuiPapara.html';
+UPDATE `sights`
+SET `Latitude`='46.2931555',
+    `Longitude`='13.7963065'
+WHERE `filename` = '/english/si/springs/Savice.html';
+UPDATE `sights`
+SET `Latitude`='48.5624741',
+    `Longitude`='20.4652489'
+WHERE `filename` = '/english/sk/springs/Cierny.html';
+UPDATE `sights`
+SET `Latitude`='48.656780',
+    `Longitude`='20.531001'
+WHERE `filename` = '/english/sk/mines/Roznava.html';
+UPDATE `sights`
+SET `Latitude`='13.1360917',
+    `Longitude`='99.9330325'
+WHERE `filename` = '/english/th/showcaves/KhaoLuang.html';
+UPDATE `sights`
+SET `Latitude`='8.8302253',
+    `Longitude`='99.3794462'
+WHERE `filename` = '/english/th/showcaves/Khamin.html';
+UPDATE `sights`
+SET `Latitude`='38.639976',
+    `Longitude`='34.845307'
+WHERE `filename` = '/english/tr/subterranea/Goereme.html';
