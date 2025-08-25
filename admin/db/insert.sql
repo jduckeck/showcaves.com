d432a51052c1f806f4c38a1b4acdbd8c10543a15
jduckeck <github@jochenduckeck.de>
2025-08-25 02:25:59 +0200

    coordinates

---
+++ admin/db/insert.sql
@@ -0,0 +1,153 @@
+UPDATE `sights`
+SET `Latitude`='37.0378544',
+    `Longitude`='31.6333540'
+WHERE `filename` = '/english/tr/caves/Altinbesik.html';
+UPDATE `sights`
+SET `Latitude`='37.0470988',
+    `Longitude`='31.1106023'
+WHERE `filename` = '/english/tr/showcaves/Zeytintasi.html';
+UPDATE `sights`
+SET `Latitude`='41.5674768',
+    `Longitude`='-122.2826093'
+WHERE `filename` = '/english/usa/caves/Pluto.html';
+UPDATE `sights`
+SET `Latitude`='22.2203635',
+    `Longitude`='-159.5805949'
+WHERE `filename` = '/english/usa/caves/Waikanaloa.html';
+UPDATE `sights`
+SET `Latitude`='46.9576993',
+    `Longitude`='-121.0946141'
+WHERE `filename` = '/english/usa/caves/BoulderWA.html';
+UPDATE `sights`
+SET `Latitude`='40.5383626',
+    `Longitude`='-112.1447509'
+WHERE `filename` = '/english/usa/mines/Bingham.html';
+UPDATE `sights`
+SET `Latitude`='33.107720',
+    `Longitude`='-86.893106'
+WHERE `filename` = '/english/usa/mines/Aldrich.html';
+UPDATE `sights`
+SET `Latitude`='42.570211',
+    `Longitude`='-90.235739'
+WHERE `filename` = '/english/usa/mines/Badger.html';
+UPDATE `sights`
+SET `Latitude`='47.472527',
+    `Longitude`='-115.924776'
+WHERE `filename` = '/english/usa/mines/Sierra.html';
+UPDATE `sights`
+SET `Latitude`='33.731619',
+    `Longitude`='-87.089723'
+WHERE `filename` = '/english/usa/mines/Alabama.html';
+UPDATE `sights`
+SET `Latitude`='41.113307',
+    `Longitude`='-74.588384'
+WHERE `filename` = '/english/usa/mines/Franklin.html';
+UPDATE `sights`
+SET `Latitude`='47.8194316',
+    `Longitude`='-92.2426445'
+WHERE `filename` = '/english/usa/mines/Soudan.html';
+UPDATE `sights`
+SET `Latitude`='37.2734764',
+    `Longitude`='-94.9382013'
+WHERE `filename` = '/english/usa/mines/BigBrutus.html';
+UPDATE `sights`
+SET `Latitude`='37.9229111',
+    `Longitude`='-90.5513763'
+WHERE `filename` = '/english/usa/mines/BonneTerre.html';
+UPDATE `sights`
+SET `Latitude`='46.0117169',
+    `Longitude`='-112.5178263'
+WHERE `filename` = '/english/usa/mines/BerkeleyPit.html';
+UPDATE `sights`
+SET `Latitude`='59.4150569',
+    `Longitude`='-136.2351915'
+WHERE `filename` = '/english/usa/mines/BigNugget.html';
+UPDATE `sights`
+SET `Latitude`='46.774284',
+    `Longitude`='-89.081942'
+WHERE `filename` = '/english/usa/mines/Adventure.html';
+UPDATE `sights`
+SET `Latitude`='44.352066',
+    `Longitude`='-103.763670'
+WHERE `filename` = '/english/usa/mines/Homestake.html';
+UPDATE `sights`
+SET `Latitude`='34.0317757',
+    `Longitude`='-93.6732374'
+WHERE `filename` = '/english/usa/mines/CraterOfDiamonds.html';
+UPDATE `sights`
+SET `Latitude`='30.235456',
+    `Longitude`='-84.302722'
+WHERE `filename` = '/english/usa/springs/Wakulla.html';
+UPDATE `sights`
+SET `Latitude`='22.0379759',
+    `Longitude`='-159.3584111'
+WHERE `filename` = '/english/usa/showcaves/Fern.html';
+UPDATE `sights`
+SET `Latitude`='29.8863391',
+    `Longitude`='-98.6174334'
+WHERE `filename` = '/english/usa/showcaves/Without.html';
+UPDATE `sights`
+SET `Latitude`='36.697967',
+    `Longitude`='-99.1477'
+WHERE `filename` = '/english/usa/showcaves/Alabaster.html';
+UPDATE `sights`
+SET `Latitude`='35.9638617',
+    `Longitude`='-92.1793284'
+WHERE `filename` = '/english/usa/showcaves/BlanchardSprings.html';
+UPDATE `sights`
+SET `Latitude`='40.745335',
+    `Longitude`='-74.022982'
+WHERE `filename` = '/english/usa/subterranea/Sybil.html';
+UPDATE `sights`
+SET `Latitude`='39.861986',
+    `Longitude`='-84.558285'
+WHERE `filename` = '/english/usa/subterranea/Haunted.html';
+UPDATE `sights`
+SET `Latitude`='41.6466096',
+    `Longitude`='-82.8266522'
+WHERE `filename` = '/english/usa/showcaves/HeinemanWinery.html';
+UPDATE `sights`
+SET `Latitude`='41.6474564',
+    `Longitude`='-82.8292818'
+WHERE `filename` = '/english/usa/showcaves/Perry.html';
+UPDATE `sights`
+SET `Latitude`='34.6124317',
+    `Longitude`='-111.8416591'
+WHERE `filename` = '/english/usa/subterranea/Montezuma.html';
+UPDATE `sights`
+SET `Latitude`='-17.961110',
+    `Longitude`='-67.125000'
+WHERE `filename` = '/english/other/mines/SanJose.html';
+
+
+
+37.0378544, 31.6333540
+37.0470988, 31.1106023
+41.5674768, -122.2826093
+22.2203635, -159.5805949
+46.9576993, -121.0946141
+40.5383626, -112.1447509
+33.107720, -86.893106
+42.570211, -90.235739
+47.472527, -115.924776
+33.731619, -87.089723
+41.113307, -74.588384
+47.8194316, -92.2426445
+37.2734764, -94.9382013
+37.9229111, -90.5513763
+46.0117169, -112.5178263
+59.4150569, -136.2351915
+46.774284, -89.081942
+44.352066, -103.763670
+34.0317757, -93.6732374
+30.235456, -84.302722
+22.0379759, -159.3584111
+29.8863391, -98.6174334
+36.697967, -99.1477
+35.9638617, -92.1793284
+40.745335, -74.022982
+39.861986, -84.558285
+41.6466096, -82.8266522
+41.6474564, -82.8292818
+34.6124317, -111.8416591
+-17.961110, -67.125000
