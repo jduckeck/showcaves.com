c2586f614a0d640316f036076b5fa4473113621d
jduckeck <github@jochenduckeck.de>
2025-10-04 10:03:46 +0200

    cz

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Zion–Mount Carmel Tunnel', '/english/usa/subterranea/ZionMountCarmel.html', 'ZionMountCarmel', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'ut', 37.211305, -112.958228);
 
 
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Důl Jindřich', '/english/cz/mines/Jindrich.html', 'Jindrich', 'Europe', 'cz', 'Czech Republic', 'mines', 'Moravskoslezsky', 49.842113, 18.282991);
