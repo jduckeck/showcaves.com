bae6fb71c4bdc6a231422f73bdc9bd2d9181aef4
jduckeck <github@jochenduckeck.de>
2025-10-18 17:23:22 +0200

    Cave of Wonders'

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Musfur Sinkhole', '/english/other/caves/Musfur.html', 'Musfur', 'MiddleEast', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.1751410, 51.2117201);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES ('yes', 'Dahl Al Mudhlem', '/english/other/caves/Mudhlem.html', 'Mudhlem', 'MiddleEast', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.1227219, 51.2282053);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Cave of Wonders', '/english/other/subterranea/Eajayib.html', 'Eajayib', 'MiddleEast', 'qa', 'Qatar', 'subterranea', 'qa', 'other', 25.288878, 51.548061);
