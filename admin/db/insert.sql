56dbe90c032e6e19696b4a9b271c606870b682f9
jduckeck <github@jochenduckeck.de>
2025-10-18 10:13:29 +0200

    Qatar

---
+++ admin/db/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Dahl Al Hamam', '/english/other/karst/AlHamam.html', 'AlHamam', '$continent$', 'qa', 'Qatar', 'karst', 'qa', 'other', 25.334439, 51.480493);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Musfur Sinkhole', '/english/other/caves/Musfur.html', 'Musfur', '$continent$', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.1751410, 51.2117201);
