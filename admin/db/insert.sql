4f40c925fbcf7e85652fdf61fdc29be046d2af14
jduckeck <github@jochenduckeck.de>
2025-10-18 15:49:49 +0200

    Dahl Al Mudhlem

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
-VALUES ('yes', 'Dahl Al Hamam', '/english/other/karst/AlHamam.html', 'AlHamam', '$continent$', 'qa', 'Qatar', 'karst', 'qa', 'other', 25.334439, 51.480493);
+VALUES ('yes', 'Dahl Al Hamam', '/english/other/karst/Hamam.html', 'AlHamam', 'MiddleEast', 'qa', 'Qatar', 'karst', 'qa', 'other', 25.334439, 51.480493);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
-VALUES ('yes', 'Musfur Sinkhole', '/english/other/caves/Musfur.html', 'Musfur', '$continent$', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.1751410, 51.2117201);
+VALUES ('yes', 'Musfur Sinkhole', '/english/other/caves/Musfur.html', 'Musfur', 'MiddleEast', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.1751410, 51.2117201);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Dahl Al Mudhlem', '/english/other/caves/Mudhlem.html', 'Mudhlem', 'MiddleEast', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.1227219, 51.2282053);
