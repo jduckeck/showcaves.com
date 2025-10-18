f900008ae8f3393a9e31cac629929799c35ec71f
jduckeck <github@jochenduckeck.de>
2025-10-18 16:53:15 +0200

    Dahl Al Hamam

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
-VALUES ('yes', 'Dahl Al Hamam', '/english/other/karst/Hamam.html', 'AlHamam', 'MiddleEast', 'qa', 'Qatar', 'karst', 'qa', 'other', 25.334439, 51.480493);
+VALUES ('yes', 'Dahl Al Hamam', '/english/other/caves/Hamam.html', 'Hamam', 'MiddleEast', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.334439, 51.480493);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
 VALUES ('yes', 'Musfur Sinkhole', '/english/other/caves/Musfur.html', 'Musfur', 'MiddleEast', 'qa', 'Qatar', 'caves', 'qa', 'other', 25.1751410, 51.2117201);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
