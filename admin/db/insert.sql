f0ab5a1cdb52ff4b9b1b73e641ff5aac78561d85
jduckeck <github@jochenduckeck.de>
2025-10-31 01:24:55 +0100

    Zeittunnel Wülfrath

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cave of Wonders', '/english/other/subterranea/Eajayib.html', 'Eajayib', 'MiddleEast', 'qa', 'Qatar', 'subterranea', 'qa', 'other', 25.288878, 51.548061);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Kormokopos Cave', '/english/gr/caves/Kormokopos.html', 'Kormokopos', 'Europe', 'gr', 'Greece', 'caves', 'Crete', 35.2518271, 24.0005032);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Zeittunnel Wülfrath', '/english/de/mines/Wulfrath.html', 'Wulfrath', 'Europe', 'de', 'Germany', 'mines', 'Sauerland', 51.2876405, 7.0501882);
