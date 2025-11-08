016b5579b773fbbd47d9f94e709f430850efc202
jduckeck <github@jochenduckeck.de>
2025-11-08 11:28:48 +0100

    Yeraltı Camii

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -12,3 +12,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Zeittunnel Wülfrath', '/english/de/mines/Wulfrath.html', 'Wulfrath', 'Europe', 'de', 'Germany', 'mines', 'Sauerland', 51.2876405, 7.0501882);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Stasi-Bunker Machern', '/english/de/subterranea/Machern.html', 'Machern', 'Europe', 'de', 'Germany', 'subterranea', 'Sachsen', 51.380060, 12.644719);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Yeraltı Camii', '/english/tr/subterranea/YeraltiCamii.html', 'YeraltiCamii', 'Europe', 'tr', 'Turkey', 'subterranea', 'Istanbul', 41.022351, 28.976792);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Březenský Drak', '/english/cz/mines/BrezenskyDrak.html', 'BrezenskyDrak', 'Europe', 'cz', 'Czech Republic', 'mines', 'Ustecky', 50.3952114, 13.4120911);
