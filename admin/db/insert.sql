272f29ec99590f6be03e7a3947e96c776e3499e3
jduckeck <github@jochenduckeck.de>
2025-11-17 13:55:10 +0100

    Montezuma

--- admin/db/insert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO static (path)
-VALUES ('/english/explain/Topic/Devil.html');
-INSERT INTO static (path)
-VALUES ('/german/explain/Topic/Devil.html');
-INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Cueva del Diablo Mazatlán', '/english/mx/caves/DiabloMazatlan.html', 'DiabloMaza', 'MiddleAmerica', 'mx', 'México', 'caves', 'Sinaloa', 23.2004363, -106.4297553);
