7bf1f20f22aef3c5056d9b436b8ae510faf60952
jduckeck <github@jochenduckeck.de>
2025-11-19 17:55:28 +0100

    Zağ Mağarası

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,3 +4,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cuevas del Pirata Las Salinas', '/english/pe/caves/PirataLasSalinas.html', 'PirataLasSalinas', 'SouthAmerica', 'pe', 'Perú', 'caves', 'Lima', -12.5494243, -76.7344970);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Yerköprü Mağarası', '/english/tr/karst/Yerkopru.html', 'Yerkopru', 'Europe', 'tr', 'Turkey', 'karst', 'CentralAnatolia', 37.0287447, 32.7000704);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Zağ Mağarası', '/english/tr/subterranea/Zag.html', 'Zag', 'Europe', 'tr', 'Turkey', 'subterranea', 'EasternAnatolia', 38.866132, 40.807735);
