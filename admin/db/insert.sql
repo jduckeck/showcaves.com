8b41469af581001b85f4b8194e5d10b8abd1c6d1
jduckeck <github@jochenduckeck.de>
2025-11-19 14:57:47 +0100

    Yerköprü Mağarası

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Cueva del Pirata Quintero', '/english/cl/caves/PirataQuintero.html', 'PirataQuintero', 'SouthAmerica', 'cl', 'Chile', 'caves', 'Valparaiso', -32.7656755, -71.5340802);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Cuevas del Pirata Las Salinas', '/english/pe/caves/PirataLasSalinas.html', 'PirataLasSalinas', 'SouthAmerica', 'pe', 'Perú', 'caves', 'Lima', -12.5494243, -76.7344970);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Yerköprü Mağarası', '/english/tr/karst/Yerkopru.html', 'Yerkopru', 'Europe', 'tr', 'Turkey', 'karst', 'CentralAnatolia', 37.0287447, 32.7000704);
