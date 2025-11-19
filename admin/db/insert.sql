472d75711679b60dda480e57b7902ac47a9371e6
jduckeck <github@jochenduckeck.de>
2025-11-19 01:59:21 +0100

    Cuevas del Pirata Las Salinas

---
+++ admin/db/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cueva del Pirata Quintero', '/english/cl/caves/PirataQuintero.html', 'PirataQuintero', 'SouthAmerica', 'cl', 'Chile', 'caves', 'Valparaiso', -32.7656755, -71.5340802);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cuevas del Pirata Las Salinas', '/english/pe/caves/PirataLasSalinas.html', 'PirataLasSalinas', 'SouthAmerica', 'pe', 'Perú', 'caves', 'Lima', -12.5494243, -76.7344970);
