65704175b4a6d9a54ec7ce718ca3457e6c44a744
jduckeck <github@jochenduckeck.de>
2025-12-08 06:38:34 +0100

    Drachenhöhle Mixnitz

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Grottino del Campidoglio', '/english/it/subterranea/Campidoglio.html', 'Campidoglio', 'Europe', 'it', 'Italy', 'subterranea', 'Roma',);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Apostle Islands Sea Caves', '/english/usa/caves/ApostleIslands.html', 'ApostleIslands', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'wi',);
+UPDATE `sights`
+SET `Latitude`='47.3257064',
+    `Longitude`='15.3794560'
+WHERE `filename` = '/english/at/caves/Drachen.html';
