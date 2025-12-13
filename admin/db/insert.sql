caef68311ec1e6167bc564c3928837ea1ca465bb
jduckeck <github@jochenduckeck.de>
2025-12-13 16:53:10 +0100

    deploy

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,32 +1,8 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Grottino del Campidoglio', '/english/it/subterranea/Campidoglio.html', 'Campidoglio', 'Europe', 'it', 'Italy', 'subterranea', 'Roma',);
+VALUES ('yes', 'Grottino del Campidoglio', '/english/it/subterranea/Campidoglio.html', 'Campidoglio', 'Europe', 'it', 'Italy', 'subterranea', 'Roma', 41.893359, 12.482802);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Apostle Islands Sea Caves', '/english/usa/caves/ApostleIslands.html', 'ApostleIslands', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'wi',);
+VALUES ('yes', 'Apostle Islands Sea Caves', '/english/usa/caves/ApostleIslands.html', 'ApostleIslands', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'wi', 46.965278, -90.664167);
 UPDATE `sights`
-SET `Latitude`='47.3257064',
-    `Longitude`='15.3794560'
-WHERE `filename` = '/english/at/caves/Drachen.html';
-UPDATE `sights`
-SET `Latitude`='46.625195',
-    `Longitude`='13.689378'
-WHERE `filename` = '/english/at/mines/Friedrich.html';
-UPDATE `sights`
-SET `Latitude`='47.5274815',
-    `Longitude`='13.1570583'
-WHERE `filename` = '/english/at/caves/Scheukofen.html';
-UPDATE `sights`
-SET `Latitude`='47.5517382',
-    `Longitude`='12.6687443'
-WHERE `filename` = '/english/at/caves/Prax.html';
-UPDATE `sights`
-SET `Latitude`='47.9347280',
-    `Longitude`='15.4634040'
-WHERE `filename` = '/english/at/caves/Paulinen.html';
-UPDATE `sights`
-SET `Latitude`='47.5240843',
-    `Longitude`='14.6113335'
-WHERE `filename` = '/english/at/caves/Odelstein.html';
-UPDATE `sights`
-SET `Latitude`='47.2693906',
-    `Longitude`='11.2692804'
-WHERE `filename` = '/english/at/caves/KaiserMax.html';
+SET `Latitude`='47.4305617',
+    `Longitude`='11.9723899'
+WHERE `filename` = '/english/at/mines/Wildschoenau.html';
