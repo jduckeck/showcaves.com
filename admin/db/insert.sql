07a4b70479f33a4f0d1d75c21ef56ec68eca0b26
jduckeck <github@jochenduckeck.de>
2025-12-15 13:34:33 +0100

    Südfeldstollen Thomasroith

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Technisches Museum Wien', '/english/at/mines/TechnischesMuseumWien.html', 'TechnischesMuseumWien', 'Europe', 'at', 'Austria', 'mines', 'Wien', 48.1904070, 16.3177667);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bergbaumuseum Goberling', '/english/at/mines/Goberling.html', 'Goberling', 'Europe', 'at', 'Austria', 'mines', 'Burgenland', 47.361366, 16.284058);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Südfeldstollen Thomasroith', '/english/at/mines/Sudfeldstollen.html', 'Sudfeldstollen', 'Europe', 'at', 'Austria', 'mines', 'Ober', 48.077781, 13.618197);
