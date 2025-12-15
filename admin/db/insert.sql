b73071fb627e8b5fe7754a51e0be549724423dbf
jduckeck <github@jochenduckeck.de>
2025-12-15 07:19:24 +0100

    Bergbaumuseum Goberling

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Technisches Museum Wien', '/english/at/mines/TechnischesMuseumWien.html', 'TechnischesMuseumWien', 'Europe', 'at', 'Austria', 'mines', 'Wien', 48.1904070, 16.3177667);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bergbaumuseum Goberling', '/english/at/mines/Goberling.html', 'Goberling', 'Europe', 'at', 'Austria', 'mines', 'Burgenland', 47.361366, 16.284058);
