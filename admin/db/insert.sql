44969d106dc91e0f60ec637ad561d71cd3912795
jduckeck <github@jochenduckeck.de>
2025-12-15 06:16:40 +0100

    Technisches Museum Wien

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Technisches Museum Wien', '/english/at/mines/TechnischesMuseumWien.html', 'TechnischesMuseumWien', 'Europe', 'at', 'Austria', 'mines', 'Wien', 48.1904070, 16.3177667);
