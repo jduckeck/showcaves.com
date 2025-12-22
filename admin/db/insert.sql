9a79484e46ed431aa4e60ad64c0d0709f48b8a7c
jduckeck <github@jochenduckeck.de>
2025-12-22 03:42:09 +0100

    Naida Caves

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Naida Caves', '/english/in/subterranea/Naida.html', 'Naida', 'Asia', 'in', 'India', 'subterranea', 'Dadra', 20.7109607, 70.9807830);
