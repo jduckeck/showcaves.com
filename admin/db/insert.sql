0ac6683a1408647f89714af16e1d154a85a9fb16
jduckeck <github@jochenduckeck.de>
2025-12-22 13:03:37 +0100

    Knappenloch Fallwindes

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Naida Caves', '/english/in/subterranea/Naida.html', 'Naida', 'Asia', 'in', 'India', 'subterranea', 'Dadra', 20.7109607, 70.9807830);
+UPDATE `sights`
+SET `Latitude`='46.977804',
+    `Longitude`='12.641565'
+WHERE `filename` = '/german/at/mines/Fallwindes.html';
