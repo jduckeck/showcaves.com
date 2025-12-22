2baa081ddf50debb15ee87475cad5e11fdc7ef60
jduckeck <github@jochenduckeck.de>
2025-12-22 13:18:59 +0100

    Knappenloch Fallwindes

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,3 +4,5 @@ UPDATE `sights`
 SET `Latitude`='46.977804',
     `Longitude`='12.641565'
 WHERE `filename` = '/german/at/mines/Fallwindes.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Montafoner Bergbaumuseum Silbertal', '/english/at/mines/Silbertal.html', 'Silbertal', 'Europe', 'at', 'Austria', 'mines', 'Vorarlberg', 47.0946993, 9.9784193);
