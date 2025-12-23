f7f436a1bd251af57dc524acd2c169d9f87634d0
jduckeck <github@jochenduckeck.de>
2025-12-23 22:19:11 +0100

    Grotta di San Michele a Gualana

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES('yes','Grotta di San Michele a Gualana', '/english/it/caves/SanMicheleGualana.html', 'SanMicheleGualana', 'Europe', 'it', 'Italy', 'caves', 'Campania', 41.228600, 13.866916);
