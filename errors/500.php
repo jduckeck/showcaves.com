99fe86aa7f88ebd07f33403a3909d130cfe775e5
jduckeck <github@jochenduckeck.de>
2025-12-23 11:03:51 +0100

    htaccess

--- errors/500.php
+++ errors/500.php
@@ -7,14 +7,14 @@
     <meta name="page-topic" content="travel tourism destination">
     <meta name="robots" content="INDEX,FOLLOW">
     <meta name="distribution" content="global">
-    <link rel="shortcut icon" href="../favicon.ico">
-    <link rel="stylesheet" type="text/css" href="../css/global.css">
-    <script type="text/javascript" src="../js/xemhid.js"></script>
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="/css/global.css">
+    <script type="text/javascript" src="/js/xemhid.js"></script>
     <!-- begin responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link href="../css/jquery.mobile-1.4.5.min.css" rel="stylesheet"/>
-    <script src="../js/jquery-1.11.3.min.js"></script>
-    <script src="../js/jquery.mobile-1.4.5.min.js"></script>
+    <link href="/css/jquery.mobile-1.4.5.min.css" rel="stylesheet"/>
+    <script src="/js/jquery-1.11.3.min.js"></script>
+    <script src="/js/jquery.mobile-1.4.5.min.js"></script>
     <!-- end responsive -->
 
     <meta http-equiv="refresh" content="120; URL=https://www.showcaves.com/index.html">
@@ -35,7 +35,7 @@ The page produced a script error.
 This should not happen, as this website uses scripts very carefull.
 Please tell me where the error happened (the page you where coming from).
 Just paste its URL/title into an email and send it to
-    <b>E-mail: <img onClick="xemhid('soylentgreen','showcaves','com')" style="cursor: pointer;" src="https://www.showcaves.com/xemhid.php?p1=soylentgreen&p2=showcaves&p3=com" vspace="0" alt="contact" title="contact" border="0"></b>.
+    <b>E-mail: <img onClick="xemhid('soylentgreen','showcaves','com')" style="cursor: pointer;" src="/css/images/icons-png/mail-black.png" alt="contact" title="contact"></b>.
 </p>
 
 <p>
@@ -50,7 +50,7 @@ Please try again maybe 15 minutes later.
 <div data-role="header" data-position="fixed">
     <div data-role="navbar">
         <ul>
-            <li><a data-ajax="false" href="../german/index.html"><img alt="Deutsch - German" src="../graphics/language/de.png" class="language"></a></li>
+            <li><a data-ajax="false" href="/german/index.html"><img alt="Deutsch - German" src="/graphics/language/de.png" class="language"></a></li>
         </ul>
     </div>
 </div>
@@ -60,18 +60,18 @@ Please try again maybe 15 minutes later.
     <div data-role="footer" data-position="fixed">
         <div data-role="navbar">
             <ul>
-                <li><a data-ajax="false" href="../english/explain/Index/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="../graphics/symbol/Index.png">Index</a></li>
-                <li><a data-ajax="false" href="../english/explain/Topic/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="../graphics/symbol/Topic.png">Topics</a></li>
-                <li><a data-ajax="false" href="../english/explain/Hierarchy/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="../graphics/symbol/Hierarchical.png">Hierarchical</a></li>
-                <li><a data-ajax="false" href="../english/Countries.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="../graphics/symbol/Countries.png">Countries</a></li>
-                <li><a data-ajax="false" href="../english/explain/Maps/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="../graphics/symbol/Map.png">Maps</a></li>
+                <li><a data-ajax="false" href="/english/explain/Index/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="/graphics/symbol/Index.png">Index</a></li>
+                <li><a data-ajax="false" href="/english/explain/Topic/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="/graphics/symbol/Topic.png">Topics</a></li>
+                <li><a data-ajax="false" href="/english/explain/Hierarchy/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="/graphics/symbol/Hierarchical.png">Hierarchical</a></li>
+                <li><a data-ajax="false" href="/english/Countries.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="/graphics/symbol/Countries.png">Countries</a></li>
+                <li><a data-ajax="false" href="/english/explain/Maps/index.html"><img alt="Region" class="ui-li-icon ui-corner-none symbol" src="/graphics/symbol/Map.png">Maps</a></li>
             </ul>
             <ul>
-                <li><a data-ajax="false" href="../english/explain/Index/Search.html"><img alt="Search" class="ui-li-icon ui-corner-none symbol" src="../graphics/symbol/Search.png">Search</a></li>
-                <li><a data-ajax="false" href="../english/explain/index.html">General Information</a></li>
-                <li><a data-ajax="false" href="../english/TermsOfUse.html">Terms of Use</a></li>
-                <li><a data-ajax="false" href="../english/Jochen.html">©Jochen Duckeck</a></li>
-                <li><a data-ajax="false" href="#" onClick="xemhid('soylentgreen','showcaves','com')">Contact <span class="mySiteName">showcaves.com</span>: <img alt="contact" class="xemhid" src="../php/xemhid.php?p1=soylentgreen&p2=showcaves&p3=com"></a></li>
+                <li><a data-ajax="false" href="/english/explain/Index/Search.html"><img alt="Search" class="ui-li-icon ui-corner-none symbol" src="/graphics/symbol/Search.png">Search</a></li>
+                <li><a data-ajax="false" href="/english/explain/index.html">General Information</a></li>
+                <li><a data-ajax="false" href="/english/TermsOfUse.html">Terms of Use</a></li>
+                <li><a data-ajax="false" href="/english/Jochen.html">©Jochen Duckeck</a></li>
+                <li><a data-ajax="false" href="#" onClick="xemhid('soylentgreen','showcaves','com')">Contact <span class="mySiteName">showcaves.com</span>: <img alt="contact" class="xemhid" src="/php/xemhid.php?p1=soylentgreen&p2=showcaves&p3=com"></a></li>
             </ul>
         </div>
     </div>
