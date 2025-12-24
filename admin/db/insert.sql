d323c5fa383eb374c4e34000a12e5ba491908710
jduckeck <github@jochenduckeck.de>
2025-12-24 07:29:04 +0100

    Schaubergwerk Hochfeld

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES('yes','Grotta di San Michele a Gualana', '/english/it/caves/SanMicheleGualana.html', 'SanMicheleGualana', 'Europe', 'it', 'Italy', 'caves', 'Campania', 41.228600, 13.866916);
+UPDATE `sights`
+SET `Latitude`='47.2219588',`Longitude`='12.2739545'
+WHERE `filename`='/english/at/mines/Hochfeld.html';
