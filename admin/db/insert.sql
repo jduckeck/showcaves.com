b53973d0375f17924c66d95d029ad4db3c74e495
jduckeck <github@jochenduckeck.de>
2026-01-02 15:09:09 +0100

    Ötschergräben

--- admin/db/insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO static (path)
-VALUES ('/english/at/geoparks/SteirischeEisenwurzen.html');
-INSERT INTO static (path)
-VALUES ('/german/at/geoparks/SteirischeEisenwurzen.html');
-UPDATE `sights`
-SET `Latitude`='47.6677982',
-    `Longitude`='14.7952523'
-WHERE `filename` = '/english/at/gorges/Noth.html';
