df2df1025686276e5d90a84c592f7cb785276c97
jduckeck <github@jochenduckeck.de>
2026-01-16 19:00:08 +0100

    Surtshellir

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Arnarker', '/english/is/caves/Arnarker.html', 'Arnarker', 'Europe', 'is', 'Iceland', 'caves', 'Sudurland', 63.8929117, -21.4951932);
