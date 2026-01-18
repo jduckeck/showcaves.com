cb8055b6520912b8b9cef4c300507325dfe59f5e
jduckeck <github@jochenduckeck.de>
2026-01-18 18:14:58 +0100

    Göreme

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,5 +1,19 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Arnarker', '/english/is/caves/Arnarker.html', 'Arnarker', 'Europe', 'is', 'Iceland', 'caves', 'Sudurland', 63.8929117, -21.4951932);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kayakapi Mahallesi', '/english/tr/subterranea/Kayakapi.html', 'Kayakapi', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.636194, 34.905742);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Nostalji Cave Sui̇t Hotel', '/english/tr/subterranea/Nostalji.html', 'Nostalji', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.6355521, 34.9027778);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Antique House Cappadocia', '/english/tr/subterranea/Antique.html', 'Antique', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.621770, 34.864520);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Uçhisar Kaya Otel', '/english/tr/subterranea/UchisarKaya.html', 'UchisarKaya', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.6243774, 34.8047154);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Elika Cave Suites', '/english/tr/subterranea/Elika.html', 'Elika', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.6207175, 34.8641513);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Vita Cave Suites', '/english/tr/subterranea/Vita.html', 'Vita', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.62199771848703, 34.861781352204716);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Les Maisons de Cappadoce', '/english/tr/subterranea/MaisonsCappadoce.html', 'MaisonsCappadoce', 'Europe', 'tr', 'Turkey', 'subterranea', 'Cappadocia', 38.628427, 34.806729);
 UPDATE `sights`
 SET `Latitude`='27.5891365',
     `Longitude`='17.6144137'
