fed367c4efa35c0600964ad0444ca48a1406d65a
jduckeck <github@jochenduckeck.de>
2026-01-18 11:42:22 +0100

    fix broken links

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Arnarker', '/english/is/caves/Arnarker.html', 'Arnarker', 'Europe', 'is', 'Iceland', 'caves', 'Sudurland', 63.8929117, -21.4951932);
+UPDATE `sights`
+SET `Latitude`='27.5891365',
+    `Longitude`='17.6144137'
+WHERE `filename` = '/english/other/gorges/FishRiver.html';
