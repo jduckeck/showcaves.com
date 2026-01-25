38e2e9a667833fb5d69c172a2c81b2655e846a22
jduckeck <github@jochenduckeck.de>
2026-01-25 13:38:42 +0100

    fix broken links

---
+++ admin/db/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Mogalarajapuram Caves', '/english/in/subterranea/Mogalarajapuram.html', 'Mogalarajapuram', 'Asia', 'in', 'India', 'subterranea', 'AndhraPradesh', 16.5061635, 80.6470729);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Akkanna Madanna Caves', '/english/in/subterranea/AkkannaMadanna.html', 'AkkannaMadanna', 'Asia', 'in', 'India', 'subterranea', 'AndhraPradesh', 16.5135976, 80.6065017);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bojjannakonda', '/english/in/subterranea/Bojjannakonda.html', 'Bojjannakonda', 'Asia', 'in', 'India', 'subterranea', 'AndhraPradesh', 17.7086013, 83.0157959);
