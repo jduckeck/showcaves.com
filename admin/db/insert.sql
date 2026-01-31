a126e0169ed8571e2144d7c16ef789561ec4e97d
jduckeck <github@jochenduckeck.de>
2026-01-31 19:01:27 +0100

    fix links

---
+++ admin/db/insert.sql
@@ -0,0 +1,8 @@
+UPDATE `sights`
+SET `Latitude`='-37.9389627',
+    `Longitude`='146.4491053'
+WHERE `filename` = '/english/au/mines/LongTunnel.html';
+UPDATE `sights`
+SET `Latitude`='-37.9891787',
+    `Longitude`='140.9900365'
+WHERE `filename` = '/english/au/showcaves/Princess.html';
