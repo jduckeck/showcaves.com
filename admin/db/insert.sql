9e31ee0d559d353640f6d02091d719d10791a048
jduckeck <github@jochenduckeck.de>
2026-02-02 07:19:30 +0100

    fix links

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -8,3 +8,7 @@ SET `Latitude`='-37.9891787',
 WHERE `filename` = '/english/au/showcaves/Princess.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Murrawijinie Cave', '/english/au/caves/Murrawijinie.html', 'Murrawijinie', 'Australia', 'au', 'Australia', 'caves', 'Nullarbor', -31.3641731, 130.8705995);
+UPDATE `sights`
+SET `Latitude`='23.689245',
+    `Longitude`='-74.915896'
+WHERE `filename` = '/english/bs/caves/Hartford.html';
