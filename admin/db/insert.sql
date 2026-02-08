8f3b358a4ad925ef174a0a537456728b4e2bc922
jduckeck <github@jochenduckeck.de>
2026-02-08 14:46:43 +0100

    fix broken links

---
+++ admin/db/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, chapter, Latitude, Longitude)
+VALUES ('yes', 'Um Jarsan Cave', '/english/other/showcaves/UmJarsan.html', 'UmJarsan', 'MiddleEast', 'sa', 'Saudi Arabia', 'showcaves', 'sa', 'other', 25.5889314, 39.7569495);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Le Mystère des Faluns', '/english/fr/subterranea/Faluns.html', 'Faluns', 'Europe', 'fr', 'France', 'subterranea', 'PaysLoire', 47.1906131, -0.2611858);
