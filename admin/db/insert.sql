ef6a60a6f21bd68e530ad32826d5cdc20b628722
jduckeck <github@jochenduckeck.de>
2026-02-09 19:05:06 +0100

    Oracle of Trophonius

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Um Jarsan Cave', '/english/other/showcaves/UmJarsan.html', 'UmJarsan', 'MiddleEast', 'sa', 'Saudi Arabia', 'showcaves', 'sa', 'other', 25.5889314, 39.7569495);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Le Mystère des Faluns', '/english/fr/subterranea/Faluns.html', 'Faluns', 'Europe', 'fr', 'France', 'subterranea', 'PaysLoire', 47.1906131, -0.2611858);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Ex-Sham Shui Po Service Reservoir', '/english/cn/subterranea/ExSham.html', 'ExSham', 'Asia', 'cn', 'China', 'subterranea', 'Hongkong', 22.330005, 114.168355);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Oracle of Trophonius', '/english/gr/subterranea/Trophonius.html', 'Trophonius', 'Europe', 'gr', 'Greece', 'subterranea', '$Region$', 38.4315641, 22.8748924);
