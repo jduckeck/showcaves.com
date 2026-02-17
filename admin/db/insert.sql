bde9bfd88237706426332429839086d10c223d59
jduckeck <github@jochenduckeck.de>
2026-02-17 15:51:39 +0100

    fix links

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,7 @@ UPDATE `sights`
 SET `Latitude`='48.6553396',
     `Longitude`='8.7049576'
 WHERE `filename` = '/german/de/mines/Neubulach.html';
+UPDATE `sights`
+SET `Latitude`='49.0668977',
+    `Longitude`='7.8094567'
+WHERE `filename` = '/english/de/mines/Nothweiler.html';
