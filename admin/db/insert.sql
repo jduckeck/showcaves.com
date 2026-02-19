453ea132665443c427195ec845ccb6de43546a9f
jduckeck <github@jochenduckeck.de>
2026-02-19 17:05:24 +0100

    fix broken links

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Familienburg Scharfenstein', '/english/de/mines/Scharfenstein.html', 'Scharfenstein', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.7049858, 13.0554965);
