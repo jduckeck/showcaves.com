e6e7cbc16d5518015f7cdfa46e91f1a56df86659
jduckeck <github@jochenduckeck.de>
2026-02-24 17:40:08 +0100

    fix links

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Familienburg Scharfenstein', '/english/de/mines/Scharfenstein.html', 'Scharfenstein', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.7049858, 13.0554965);
+UPDATE `sights`
+SET `Latitude`='54.6806710',
+    `Longitude`='13.4316756'
+WHERE `filename` = '/english/de/subterranea/KapArkona.html';
