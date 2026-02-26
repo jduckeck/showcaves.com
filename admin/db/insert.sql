3f6b59c01ea2e093bfb34c7dfb88a5f3cd0dce44
jduckeck <github@jochenduckeck.de>
2026-02-26 16:55:31 +0100

    fix links

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,3 +4,7 @@ UPDATE `sights`
 SET `Latitude`='54.6806710',
     `Longitude`='13.4316756'
 WHERE `filename` = '/english/de/subterranea/KapArkona.html';
+UPDATE `sights`
+SET `Latitude`='29.927250',
+    `Longitude`='119.527526'
+WHERE `filename` = '/english/cn/showcaves/Chuiyun.html';
