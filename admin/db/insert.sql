f037ec665a140d10e84943f94d8dbac14e896b05
jduckeck <github@jochenduckeck.de>
2026-03-01 15:42:58 +0100

    Arizona

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -8,3 +8,17 @@ UPDATE `sights`
 SET `Latitude`='29.927250',
     `Longitude`='119.527526'
 WHERE `filename` = '/english/cn/showcaves/Chuiyun.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Fossil Springs', '/english/usa/karst/FossilSprings.html', 'FossilSprings', 'NorthAmerica', 'usa', 'United States of America', 'karst', 'az', 34.4066265, -111.5679331);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Honanki Cliff Dwellings', '/english/usa/subterranea/Honanki.html', 'Honanki', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'az', 34.9393915, -111.9332653);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Palatki Cliff Dwellings', '/english/usa/subterranea/Palatki.html', 'Palatki', 'NorthAmerica', 'usa', 'United States of America', 'subterranea', 'az', 34.9172613, -111.8993635);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Boynton Subway', '/english/usa/caves/BoyntonSubway.html', 'BoyntonSubway', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'az', 34.9324597, -111.8630917);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Birthing Cave', '/english/usa/caves/Birthing.html', 'Birthing', 'NorthAmerica', 'usa', 'United States of America', 'caves', 'az', 34.9130373, -111.8361720);
+UPDATE `sights`
+SET `Latitude`='40.6860080',
+    `Longitude`='-121.4188540'
+WHERE `filename` = '/english/usa/caves/Subway.html';
