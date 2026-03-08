6d13af708ad7cbff4ca6c1afb04d64912ffdba0e
jduckeck <github@jochenduckeck.de>
2026-03-08 05:16:23 +0100

    Shaanxi

---
+++ admin/db/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', "Tiān'é dòng qún", '/english/cn/showcaves/Tiane.html', 'Tiane', 'Asia', 'cn', 'China', 'showcaves', 'Fujian', 26.3199704, 116.8175929);
+
