07fb2207b1a0ee203517347eb2e84651abdc93a9
jduckeck <github@jochenduckeck.de>
2026-03-15 14:55:28 +0100

    Pirunpesä

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,3 +1,7 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', "Tiān'é dòng qún", '/english/cn/showcaves/Tiane.html', 'Tiane', 'Asia', 'cn', 'China', 'showcaves', 'Fujian', 26.3199704, 116.8175929);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grottstigen', '/english/fi/caves/Grottstigen.html', 'Grottstigen', 'Europe', 'fi', 'Finland', 'caves', 'AlandIslands', 60.3999810, 19.8308187);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Pirunpesä', '/english/fi/caves/PirunpesaKuopio.html', 'PirunpesaKuopio', 'Europe', 'fi', 'Finland', 'caves', 'EasternFinland', 63.2224615, 27.1312963);
 
