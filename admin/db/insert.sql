b0116ab69f1d919cfcb452c0156a479a1501a6c9
jduckeck <github@jochenduckeck.de>
2026-03-26 17:06:46 +0100

    Catacombe di San Callisto

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,4 +4,28 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Grottstigen', '/english/fi/caves/Grottstigen.html', 'Grottstigen', 'Europe', 'fi', 'Finland', 'caves', 'AlandIslands', 60.3999810, 19.8308187);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Pirunpesä', '/english/fi/caves/PirunpesaKuopio.html', 'PirunpesaKuopio', 'Europe', 'fi', 'Finland', 'caves', 'EasternFinland', 63.2224615, 27.1312963);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacomba di S. Pancrazio', '/english/it/subterranea/Pancrazio.html', 'Pancrazio', 'Europe', 'it', 'Italy', 'subterranea', 'Roma', 41.884923, 12.453971);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Fosse Ardeatine', '/english/it/subterranea/FosseArdeatine.html', 'FosseArdeatine', 'Europe', 'it', 'Italy', 'subterranea', 'Roma', 41.8571741, 12.5099516);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacomba di S. Antioco', '/english/it/subterranea/Antioco.html', 'Antioco', 'Europe', 'it', 'Italy', 'subterranea', 'Sardinia', 39.069550, 8.450730);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacomba di Priscilla', '/english/it/subterranea/Priscilla.html', 'Priscilla', 'Europe', 'it', 'Italy', 'subterranea', 'Roma', 41.9297262, 12.5087652);
+INSERT INTO static (path)
+VALUES ('/english/it/topics/Catacombs.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Túnel de Bonaparte', '/english/es/subterranea/Bonaparte.html', 'Bonaparte', 'Europe', 'es', 'Spain', 'subterranea', 'Madrid', 40.4188125, -3.7199367);
+UPDATE `sights`
+SET `Latitude`='39.7099028',
+    `Longitude`='45.2439770'
+WHERE `filename` = '/english/am/caves/Arjeri.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacombe di San Callisto', '/english/it/subterranea/SanCallisto.html', 'SanCallisto', 'Europe', 'it', 'Italy', 'subterranea', 'Roma', 41.8590708, 12.5107044);
+UPDATE `sights`
+SET `Latitude`='32.52444',
+    `Longitude`='-110.70722'
+WHERE `filename` = '/english/usa/caves/Peppersauce.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Galleria Borbonica', '/english/it/subterranea/GalleriaBorbonica.html', 'GalleriaBorbonica', 'Europe', 'it', 'Italy', 'subterranea', 'Napoli', 40.8354768, 14.2462770);
 
