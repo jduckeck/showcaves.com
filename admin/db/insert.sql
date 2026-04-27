c55cba6b42eac75fa14b2cf87707f852a76db2d2
jduckeck <github@jochenduckeck.de>
2026-04-27 09:14:06 +0200

    catacombs

---
+++ admin/db/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacomba di Monte Stallone', '/english/it/subterranea/MonteStallone.html', 'MonteStallone', 'Europe', 'it', 'Italy', 'subterranea', 'Roma', 42.071612, 12.406569);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacomba Ad Decimum', '/english/it/subterranea/AdDecimum.html', 'AdDecimum', 'Europe', 'it', 'Italy', 'subterranea', 'Roma', 41.8029531, 12.6388251);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Catacomba dell’Isola di Pianosa', '/english/it/subterranea/IsolaDiPianosa.html', 'Toscana', 'Europe', 'it', 'Italy', 'subterranea', 'IsolaDiPianosa', 42.5877050, 10.1000720);
