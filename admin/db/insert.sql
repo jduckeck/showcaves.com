bb182d6e5ea748a53c679c2b423aa6ad7d26c46d
jduckeck <github@jochenduckeck.de>
2026-05-14 18:32:30 +0200

    ph

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bergwerkmuseum Rossgang', '/english/de/mines/Rossgang.html', 'Rossgang', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 51.2909274, 9.6434904);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Codugnon Cave', '/english/ph/caves/Codugnon.html', 'Codugnon', 'Asia', 'ph', 'The Philippines', 'caves', 'Mimaropa', 11.0840850, 119.3525766);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Cathedral Cave', '/english/ph/caves/Cathedral.html', 'Cathedral', 'Asia', 'ph', 'The Philippines', 'caves', 'Mimaropa', 11.0759024, 119.3845123);
