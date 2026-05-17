e6a418c3495f311e2d274bcc08fde4f41c25e226
jduckeck <github@jochenduckeck.de>
2026-05-17 12:15:55 +0200

    broken links

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bergwerkmuseum Rossgang', '/english/de/mines/Rossgang.html', 'Rossgang', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 51.2909274, 9.6434904);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Otto-Beyer-Schlucht', '/english/de/gorges/OttoBeyer.html', 'OttoBeyer', 'Europe', 'de', 'Germany', 'gorges', 'Elbsandsteingebirge', 50.9640621, 14.0528098);
