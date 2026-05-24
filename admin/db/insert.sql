19ba2bc83115efecd936e44a8b699faedb67c1f4
jduckeck <github@jochenduckeck.de>
2026-05-24 10:09:27 +0200

    Kızören Gölü

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Kızören Gölü', '/english/tr/karst/KizorenGolu.html', 'KizorenGolu', 'Europe', 'tr', 'Turkey', 'karst', 'CentralAnatolia', 38.174670, 33.185536);
