93c86c26d571e5e2ae8e168b2e8992ea106e1f15
jduckeck <github@jochenduckeck.de>
2026-05-26 00:13:00 +0200

    Bergbaumuseum Nentershausen

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Kızören Gölü', '/english/tr/karst/KizorenGolu.html', 'KizorenGolu', 'Europe', 'tr', 'Turkey', 'karst', 'CentralAnatolia', 38.174670, 33.185536);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bergbaumuseum Nentershausen', '/english/de/mines/Nentershausen.html', 'Nentershausen', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 51.010250, 9.934457);
