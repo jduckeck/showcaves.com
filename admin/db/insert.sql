175dfe44a0a751190e78b536493cefe78fc15837
jduckeck <github@jochenduckeck.de>
2026-06-09 15:55:25 +0200

    Besucherstollen Oberes Schloß

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,3 +4,5 @@ SET `Latitude`='51.5172544',
 WHERE `filename` = '/english/de/mines/Roehrigschacht.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Museum für Bergbau und Stadtgeschichte Obernkirchen', '/english/de/mines/Obernkirchen.html', 'Obernkirchen', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 52.270368, 9.127949);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Besucherstollen Oberes Schloß', '/english/de/mines/Siegen.html', 'Siegen', 'Europe', 'de', 'Germany', 'mines', 'Sauerland', 50.8755067, 8.0302445);
