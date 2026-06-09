1e573a4cb86166b444125a236bdfccd37f39c8e0
jduckeck <github@jochenduckeck.de>
2026-06-09 14:15:54 +0200

    Museum für Bergbau und Stadtgeschichte Obernkirchen

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ UPDATE `sights`
 SET `Latitude`='51.5172544',
     `Longitude`='11.2820637'
 WHERE `filename` = '/english/de/mines/Roehrigschacht.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Museum für Bergbau und Stadtgeschichte Obernkirchen', '/english/de/mines/Obernkirchen.html', 'Obernkirchen', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 52.270368, 9.127949);
