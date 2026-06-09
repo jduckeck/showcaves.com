44ab6bb572225d3f7a9b87a56a50788964aff04f
jduckeck <github@jochenduckeck.de>
2026-06-09 18:41:39 +0200

    Salinenmuseum Unteres Bohrhaus Rottweil

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -6,3 +6,5 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Museum für Bergbau und Stadtgeschichte Obernkirchen', '/english/de/mines/Obernkirchen.html', 'Obernkirchen', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 52.270368, 9.127949);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Besucherstollen Oberes Schloß', '/english/de/mines/Siegen.html', 'Siegen', 'Europe', 'de', 'Germany', 'mines', 'Sauerland', 50.8755067, 8.0302445);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Salinenmuseum Unteres Bohrhaus Rottweil', '/english/de/mines/UnteresBohrhaus.html', 'UnteresBohrhaus', 'Europe', 'de', 'Germany', 'mines', 'Gaue', 48.1481785, 8.6484287);
