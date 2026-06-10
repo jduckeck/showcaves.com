b26d7b1a830cd1e613482abd43af676ee8c3a2b0
jduckeck <github@jochenduckeck.de>
2026-06-11 01:50:46 +0200

    Bergbaumuseum des Kreises Altenkirchen

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -10,3 +10,9 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Salinenmuseum Unteres Bohrhaus Rottweil', '/english/de/mines/UnteresBohrhaus.html', 'UnteresBohrhaus', 'Europe', 'de', 'Germany', 'mines', 'Gaue', 48.1481785, 8.6484287);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Bergbaumuseum Deuben', '/english/de/mines/Deuben.html', 'Deuben', 'Europe', 'de', 'Germany', 'mines', 'Norddeutschland', 51.114370, 12.074870);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Bergbaumuseum des Kreises Altenkirchen', '/english/de/mines/Altenkirchen.html', 'Altenkirchen', 'Europe', 'de', 'Germany', 'mines', 'Westerwald', 50.770390, 7.917393);
+INSERT INTO static (path)
+VALUES ('/german/de/geoparks/Westerwald.html');
+INSERT INTO static (path)
+VALUES ('/english/de/geoparks/Westerwald.html');
