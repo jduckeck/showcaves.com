7122291c306d834cb35fdddeab3aacd715e1cd0d
jduckeck <github@jochenduckeck.de>
2026-06-23 11:37:42 +0200

    Besucherbergwerk Schieferstollen Josefsglück

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -5,7 +5,7 @@ WHERE `filename` = '/english/de/mines/Roehrigschacht.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Museum für Bergbau und Stadtgeschichte Obernkirchen', '/english/de/mines/Obernkirchen.html', 'Obernkirchen', 'Europe', 'de', 'Germany', 'mines', 'Weserbergland', 52.270368, 9.127949);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Besucherstollen Oberes Schloß', '/english/de/mines/Siegen.html', 'Siegen', 'Europe', 'de', 'Germany', 'mines', 'Sauerland', 50.8755067, 8.0302445);
+VALUES ('yes', 'Besucherstollen Oberes Schloß', '/english/de/mines/Siegen.html', 'Siegen', 'Europe', 'de', 'Germany', 'mines', 'Siegerland', 50.8755067, 8.0302445);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Salinenmuseum Unteres Bohrhaus Rottweil', '/english/de/mines/UnteresBohrhaus.html', 'UnteresBohrhaus', 'Europe', 'de', 'Germany', 'mines', 'Gaue', 48.1481785, 8.6484287);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
@@ -16,3 +16,9 @@ INSERT INTO static (path)
 VALUES ('/german/de/geoparks/Westerwald.html');
 INSERT INTO static (path)
 VALUES ('/english/de/geoparks/Westerwald.html');
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Besucherbergwerk Schieferstollen Josefsglück', '/english/de/mines/Josefsglueck.html', 'Josefsglueck', 'Europe', 'de', 'Germany', 'mines', 'Siegerland', 50.808838, 7.947989);
+UPDATE `sights`
+SET `Latitude`='9.6230503',
+    `Longitude`='122.4427814'
+WHERE `filename` = '/english/ph/caves/Ubong.html';
