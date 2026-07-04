fecbd3d25f3029157b00d4316a8403f3dd89443b
jduckeck <github@jochenduckeck.de>
2026-07-04 16:11:02 +0200

    Städtische Sammlungen Freital

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -2,3 +2,5 @@ UPDATE `sights`
 SET `Latitude`='36.9962998',
     `Longitude`='-122.0687753'
 WHERE `filename` = '/english/usa/caves/Empire.html';
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Städtische Sammlungen Freital', '/english/de/mines/Freital.html', 'Freital', 'Europe', 'de', 'Germany', 'mines', 'Vogtland', 50.5537305, 11.7159903);
