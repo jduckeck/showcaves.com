e4987c2cc163d2da101880262a02d84b28997bdd
jduckeck <github@jochenduckeck.de>
2026-07-06 18:22:28 +0200

    Unverhofft Segen Gottes Erbstolln

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -4,3 +4,5 @@ SET `Latitude`='36.9962998',
 WHERE `filename` = '/english/usa/caves/Empire.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Städtische Sammlungen Freital', '/english/de/mines/Freital.html', 'Freital', 'Europe', 'de', 'Germany', 'mines', 'Vogtland', 50.5537305, 11.7159903);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Unverhofft Segen Gottes Erbstolln', '/english/de/mines/UnverhofftSegenGottes.html', 'UnverhofftSegenGottes', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.9052157, 13.2458675);
