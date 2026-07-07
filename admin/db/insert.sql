3590847d22f823b41696e5a0675337929452527f
jduckeck <github@jochenduckeck.de>
2026-07-07 15:39:59 +0200

    Molchner Stolln

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -6,3 +6,7 @@ INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, cou
 VALUES ('yes', 'Städtische Sammlungen Freital', '/english/de/mines/Freital.html', 'Freital', 'Europe', 'de', 'Germany', 'mines', 'Vogtland', 50.5537305, 11.7159903);
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Unverhofft Segen Gottes Erbstolln', '/english/de/mines/UnverhofftSegenGottes.html', 'UnverhofftSegenGottes', 'Europe', 'de', 'Germany', 'mines', 'Erzgebirge', 50.9052157, 13.2458675);
+UPDATE `sights`
+SET `Latitude`='50.6370419',
+    `Longitude`='13.2168811'
+WHERE `filename` = '/english/de/mines/TieferMolchner.html';
