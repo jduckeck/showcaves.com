a248e52fc9b14a442479afd98492e620e1e26f5b
jduckeck <github@jochenduckeck.de>
2026-07-21 17:26:35 +0200

    cavelighting

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte de Chameau', '/english/ma/showcaves/Chameau.html', 'Chameau', 'Africa', 'ma', 'Morocco', 'showcaves', 'Oriental', 34.8382768, -2.3560162);
