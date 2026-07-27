87906227f80fcc9e34a7665ee5da0f5c23bd51ff
jduckeck <github@jochenduckeck.de>
2026-07-27 13:23:47 +0200

    Gua Kelam

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,14 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Grotte de Chameau', '/english/ma/showcaves/Chameau.html', 'Chameau', 'Africa', 'ma', 'Morocco', 'showcaves', 'Oriental', 34.8382768, -2.3560162);
+UPDATE `sights`
+SET `Latitude`='6.6976852',
+    `Longitude`='100.1912465'
+WHERE `filename` = '/english/my/caves/WangBurma.html';
+UPDATE `sights`
+SET `Latitude`='6.6442672',
+    `Longitude`='100.2026603'
+WHERE `filename` = '/english/my/showcaves/Kelam.html';
+UPDATE `sights`
+SET `Latitude`='6.643436',
+    `Longitude`='100.204590'
+WHERE `filename` = '/english/my/showcaves/Kelam2.html';
