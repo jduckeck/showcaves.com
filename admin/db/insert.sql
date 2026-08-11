81877477f8d55a934b9ff3667c5faf0abfd05cc9
jduckeck <github@jochenduckeck.de>
2026-08-11 12:32:14 +0200

    Grotta di Sant'Angelo in Criptis

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -37,5 +37,5 @@ SET `Latitude`='43.166509',
     `Longitude`='-114.340784'
 WHERE `filename` = '/english/usa/showcaves/Shoshone.html';
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
-VALUES ('yes', 'Grotta di Sant'Angelo in Criptis', '/english/it/showcaves/SantAngeloSanteramo.html', 'SantAngeloSanteramo', 'Europe', 'it', 'Italy', 'showcaves', 'Puglia', 40.8248662, 16.7037077);
+VALUES ('yes', 'Grotta di Sant’Angelo in Criptis', '/english/it/showcaves/SantAngeloSanteramo.html', 'SantAngeloSanteramo', 'Europe', 'it', 'Italy', 'showcaves', 'Puglia', 40.8248662, 16.7037077);
 
