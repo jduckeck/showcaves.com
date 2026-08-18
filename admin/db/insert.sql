a4d93c3e7e226280ca48818c28c0dc1155409fb6
jduckeck <github@jochenduckeck.de>
2026-08-18 15:17:13 +0200

    Libtec Crystal Cave

---
+++ admin/db/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Libtec Crystal Cave', '/english/ph/caves/Libtec.html', 'Libtec', 'Asia', 'ph', 'The Philippines', 'caves', 'Cordillera', 17.64496, 120.77459);
