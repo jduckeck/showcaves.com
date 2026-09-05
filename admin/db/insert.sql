b65e0b8a4941ddae5983ef49b13dc30fb778738e
jduckeck <github@jochenduckeck.de>
2026-09-05 09:58:35 +0200

    Grotte Sainte-Reine

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Libtec Crystal Cave', '/english/ph/caves/Libtec.html', 'Libtec', 'Asia', 'ph', 'The Philippines', 'caves', 'Cordillera', 17.64496, 120.77459);
+INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
+VALUES ('yes', 'Grotte Sainte-Reine', '/english/fr/caves/SainteReine.html', 'SainteReine', 'Europe', 'fr', 'France', 'caves', 'Lorraine', 48.6451680, 5.9422403);
