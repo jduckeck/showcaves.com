653eee4bc1cd2055a42e6bee520e7a3e00dd30a7
jduckeck <github@jochenduckeck.de>
2026-09-09 17:54:36 +0200

    fix links

--- admin/db/insert.sql
+++ admin/db/insert.sql
@@ -1,2 +1,6 @@
 INSERT INTO sights (visible, name, filename, sortby, continent, countrycode, country, category, region, Latitude, Longitude)
 VALUES ('yes', 'Libtec Crystal Cave', '/english/ph/caves/Libtec.html', 'Libtec', 'Asia', 'ph', 'The Philippines', 'caves', 'Cordillera', 17.64496, 120.77459);
+UPDATE `sights`
+SET `Latitude`='24,3901040',
+    `Longitude`='118,3192566'
+WHERE `filename` = '/english/cn/subterranea/Zhaishan.html';
