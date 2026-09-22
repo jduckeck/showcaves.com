68304b6ad4fefa99b44a0146594a57ad071da9c7
jduckeck <github@jochenduckeck.de>
2026-09-22 12:10:21 +0200

    Südharz

---
+++ admin/db/insert.sql
@@ -0,0 +1,8 @@
+UPDATE `sights`
+SET `Latitude`='51.4969510',
+    `Longitude`='10.9546502'
+WHERE `filename` = '/english/de/showcaves/Heimkehle.html';
+UPDATE `sights`
+SET `Latitude`='51.5302460',
+    `Longitude`='10.7615761'
+WHERE `filename` = '/english/de/springs/Salza.html';
